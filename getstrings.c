502f2be457de4ee840e4dc9fec50e1d95f2b722c
南山 <junjiemars@gmail.com>
2015-04-12 09:28:42 +0800

    comment getstrings.c

--- getstrings.c
+++ getstrings.c
@@ -2,6 +2,13 @@
 export CFLAGS="-g -Wall -std=gnu11 -O3"  #the usual.
 make getstrings
 */
+
+/*
+ * note:
+ * 1)same the problem as sadstrings.c
+ *
+ */
+
 #define _GNU_SOURCE //cause stdio.h to include asprintf
 #include <stdio.h>
 #include <stdlib.h> //free
@@ -15,4 +22,5 @@ void get_strings(char const *in){
 
 int main(int argc, char **argv){
     get_strings(argv[0]);
+    return (0);
 }
