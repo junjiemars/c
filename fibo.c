893c2a2286a23e7ee5864933c3e9a63870289c59
南山 <junjiemars@gmail.com>
2015-05-06 23:10:55 +0800

    static var in function

--- fibo.c
+++ fibo.c
@@ -3,9 +3,15 @@ make fibo CFLAGS="-g -Wall -std=gnu11 -O3"
 */
 #include <stdio.h>
 
+/*
+ * 1) if function exists then static vars in which exists;
+ * 2) C11 include a _Thread_local memory type for thread safe reson;
+ * 3) static vars initialized before main;
+ */
+
 long long int fibonacci(){
-    static long long int first = 0;
-    static long long int second = 1;
+    static /*_Thread_local*/ long long int first = 0;
+    static /*_Thread_local*/ long long int second = 1;
     long long int out = first+second;
     first=second;
     second=out;
