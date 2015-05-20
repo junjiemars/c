961588130a453b20f73b8890a8c9797a6cd88f76
南山 <junjiemars@gmail.com>
2015-05-20 23:40:48 +0800

    set B to A: copy/alias, struct has copy semantic.

--- copystructs.c
+++ copystructs.c
@@ -2,6 +2,7 @@
 make copystructs CFLAGS="-g -Wall -O3 -std=gnu99"
 */
 #include <assert.h>
+#include <string.h>
 /*
  * 1) set A to B, just struct copy;
  * 2) if the pointer/array in B, so copy the pointer itself;
@@ -16,6 +17,7 @@ int main(){
     demo_s d1 = {.b=1, .c=2, .d=3, .efg=(int[]){4,5,6}};
     demo_s d2 = d1;
 
+    assert(0 == memcmp(&d1, &d2, sizeof(demo_s)));
     d1.b=14;
     d1.c=41;
     d1.efg[0]=7;
@@ -25,4 +27,5 @@ int main(){
     assert(d2.c==2);
     assert(d2.d==3);
     assert(d2.efg[0]==7);
+    assert(0 == memcmp(&d1, &d2, sizeof(demo_s)));
 }
