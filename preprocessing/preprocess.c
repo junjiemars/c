6d169a594167f05af8e31038f9aefbbf2d91c33a
南山 <junjiemars@gmail.com>
2015-08-28 23:32:44 +0800

    how float stored internally

--- preprocessing/preprocess.c
+++ preprocessing/preprocess.c
@@ -6,13 +6,13 @@ make preprocess
 /*
  * 1) ## operator used to concate two identifiers in macro.
  * 2) u can define macro anywhere but avaiable after the defined point.
- * 3) line:28 Length(in) is not important just for readable.
+ * 3) varidic arguments
  */
 
 #include <stdio.h>
 #include <math.h> //NAN
 
-#define Setup_list(name, ...)                               \
+#define setup_list(name, ...)                               \
     double *name ## _list = (double []){__VA_ARGS__, NAN};  \
     int name ## _len = 0;                                   \
     for (name ## _len =0;                                   \
@@ -21,7 +21,7 @@ make preprocess
 
 
 int main(){
-    Setup_list(items, 1, 2, 4, 8);
+    setup_list(items, 1, 2, 4, 8);
     double sum=0;
     for (double *ptr= items_list; !isnan(*ptr); ptr++)
         sum += *ptr;
@@ -29,9 +29,8 @@ int main(){
 
     //#define Length(in) in ## _len
 
-    sum=0;
-    Setup_list(next_set, -1, 2.2, 4.8, 0.1);
-    for (int i=0; i < next_set_len/*Length(next_set)*/; i++)
-        sum += next_set_list[i];
-    printf("total for next set list: %g\n", sum);
+    setup_list(another, -1, 2.2, 4.8, 0.1);
+    for (int i=0, sum=0; i < another_len; i++)
+        sum += another_list[i];
+    printf("total for another list: %g\n", sum);
 }
