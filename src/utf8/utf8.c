d457cec2176bf57ac8c224de75ff41c417eb188c
南山竹 <junjiemars@gmail.com>
2016-03-28 23:45:16 +0800

    16-bits, 24-bits code point to utf-8

--- src/utf8/utf8.c
+++ src/utf8/utf8.c
@@ -3,10 +3,11 @@
 
 #define TO_BASE_N (sizeof(unsigned)*8 + 1)
 #define TO_BASE(n, b) to_base((unsigned char [TO_BASE_N]){""}, (n), (b))
+#define OUT_SIZE(t) {printf("sizeof(%s) \t= %lu bytes\n", #t, sizeof(t));}
 
 const unsigned char *to_base(unsigned char *o, unsigned i, unsigned base) {
     unsigned char *s = &o[TO_BASE_N - 1];
-    *s = '\0';
+    *s = 0;
     do {
         s--;
         *s = "0123456789ABCDEF"[i % base];
@@ -17,24 +18,41 @@ const unsigned char *to_base(unsigned char *o, unsigned i, unsigned base) {
 }
 
 void out(const char *s, unsigned c) {
-    if (c < 0x80) {
-        printf("%s: 0x%08x %s\n", s, c, TO_BASE(c, 2));
-    } else if (c < 0x0080) {
-        printf("%s: 0x%08x %s\n", s, c, TO_BASE(c, 2));
+    if (c < 0x0080) {
+        unsigned char o[1+1] = {0};
+        *o = c;
+        printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(c, 2), o);
     } else if (c < 0x0800) {
-        // need character to utf8 encoding
+        unsigned char o[2+1] = {0};
+        unsigned u2;
+        o[0] = u2 = 0xc0 | ((c >> 6) & 0x1f);
+        o[1] = u2 = (u2 << 8) | (0x80 | (c & 0x3f));
+        printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(u2, 2), o);
+    } else if (c < 0x10000) {
+        unsigned char o[3+1] = {0};
+        unsigned u3;
+        o[0] = u3 = 0xe0 | ((c >> 12) & 0x0f);
+        o[1] = u3 = (u3 << 8) | (0x80 | ((c >> 6) & 0x3f));
+        o[2] = u3 = (u3 << 8) | (0x80 | ((c & 0x3f)));
+        printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(u3, 2), o);
+    } else if (c < 0x200000) {
+        unsigned char o[4+1] = {0};
+        unsigned u4;
+        o[0] = u4 = 0xf0 | ((c >> 18) & 0x07);
+        o[1] = u4 = (u4 << 8) | (0x80 | ((c >> 12) & 0x3f));
+        o[2] = u4 = (u4 << 8) | (0x80 | ((c >> 6) & 0x3f));
+        o[3] = u4 = (u4 << 8) | (0x80 | (c & 0x3f));
+        printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(u4, 2), o);
     }
 }
 
 int main(int argc, const char *argv[]) {
+    OUT_SIZE(unsigned);
+    OUT_SIZE(unsigned char);
     out("$", 0x0024u);
-    out("00a2", 0x00a2u);
-    unsigned char u2[] = {0xc2, 0xa2, 0};
-    printf("%s\n", u2);
-    unsigned char u3[] = {0xe2, 0x82, 0xac, 0};
-    printf("%s\n", u3);
-    unsigned char u4[] = {0xf0, 0x90, 0x8d, 0x88, 0};
-    printf("%s\n", u4);
+    out("¢", 0x00a2u);
+    out("€", 0x20acu);
+    out("𐍈", 0x10348u);
 
     return 0;
 }
