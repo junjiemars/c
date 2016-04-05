083a1e2133755fc51c7bb11424cc501250cc37ce
南山竹 <junjiemars@gmail.com>
2016-04-05 22:50:17 +0800

    refined the find in auto.sh

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 
 MAKE=${MAKE:=make}
 
-for d in $(find src -type d -depth 1); do
+for d in $(find src -maxdepth 1 -mindepth 1 -type d); do
     echo $d
-    test -f "$d/makefile" && ${MAKE} --makefile=Makefile --directory=${d} $@
+    test -f "$d/Makefile" && ${MAKE} --makefile=Makefile --directory=${d} $@
 done
