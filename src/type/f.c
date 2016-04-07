76c9a6e5b14cd1cc590d0649b7f51f5c0f5cca32
南山竹 <junjiemars@gmail.com>
2016-04-07 21:08:21 +0800

    add type

--- src/type/f.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, const char *argv[]) {
-  float f0 = 3.14;
-  float f1 = 2.7182;
-  printf("pi %f stored as %8X\n", f0, *(unsigned int *)&f0);
-  printf("e %f stored as %8X\n", f1, *(unsigned int *)&f1);
-}
