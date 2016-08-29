0ce6ddaf7d72242d6d9ac2771bd9be58f0da76b5
南山竹 <junjiemars@gmail.com>
2016-08-29 06:47:52 +0000

    format --help options

--- src/math/math1.c
+++ src/math/math1.c
@@ -4,32 +4,32 @@
 double ref;
 
 double set_na(){
-    if (!ref) {
-        ref = 0/0.;
-        char *cr = (char *)(&ref);
-        cr[2]='a';
-    }
-    return ref;
+	if (!ref) {
+		ref = 0/0.;
+		char *cr = (char *)(&ref);
+		cr[2]='a';
+	}
+	return ref;
 }
 
 int is_na(double in){
-    if (!ref) return 0;  //set_na was never called==>no NAs yet.
+	if (!ref) return 0;  //set_na was never called==>no NAs yet.
 
-    char *cc = (char *)(&in);
-    char *cr = (char *)(&ref);
-    for (int i=0; i< sizeof(double); i++)
-        if (cc[i] != cr[i]) return 0;
-    return 1;
+	char *cc = (char *)(&in);
+	char *cr = (char *)(&ref);
+	for (int i=0; i< sizeof(double); i++)
+		if (cc[i] != cr[i]) return 0;
+	return 1;
 }
 
 int main(){
-    double x = set_na();
-    printf("8 is not a number = %i\n", is_na(8));
-    printf("0/0. is not a number = %i\n", isnan(0/0.));
-    unsigned long lu = *(unsigned long*)&x;
-    printf("nan spec: 0x%lx\n", lu);
-    printf("0x%lx is not a number = %i, via self:is_na\n", lu, is_na(x));
-    printf("0x%lx is not a number = %i, via stdc:isnan\n", lu, isnan(x));
+	double x = set_na();
+	printf("8 is not a number = %i\n", is_na(8));
+	printf("0/0. is not a number = %i\n", isnan(0/0.));
+	unsigned long lu = *(unsigned long*)&x;
+	printf("nan spec: 0x%lx\n", lu);
+	printf("0x%lx is not a number = %i, via self:is_na\n", lu, is_na(x));
+	printf("0x%lx is not a number = %i, via stdc:isnan\n", lu, isnan(x));
 
-    return 0;
+	return 0;
 }
