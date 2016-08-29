5972ce5d5956c3571a9a2537425ca2be55238a15
南山竹 <junjiemars@gmail.com>
2016-08-29 06:15:12 +0000

    endian up

--- src/env/env.c
+++ src/env/env.c
@@ -1,22 +1,13 @@
-/* Compile with:
-export CFLAGS="-g -Wall -std=gnu11 -O3"  #the usual.
-make getenv
-
-#Sample usage
-reps=10 msg="Ha" ./getenv
-msg="Ha" ./getenv
-reps=20 msg=" " ./getenv
-*/
 #include <stdlib.h> //getenv, atoi
 #include <stdio.h>  //printf
 
 int main(){
-    char *c = getenv("COUNT");
-    int count = c ? atoi(c) : 3;
+	char *c = getenv("COUNT");
+	int count = c ? atoi(c) : 3;
 
-    char *text = getenv("TEXT");
-    if (!text)text = "Hello.";
+	char *text = getenv("TEXT");
+	if (!text)text = "Hello.";
 
-    for (int i=0; i< count; i++)
-        printf("%s\n", text);
+	for (int i=0; i< count; i++)
+		printf("%s\n", text);
 }
