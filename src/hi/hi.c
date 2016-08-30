34f548f37d595d3b94e56a0a704bc7be6b6aad38
南山竹 <junjiemars@gmail.com>
2016-08-30 09:04:09 +0000

    cl_auto_config.h and cl_auto_headers.h for checking configuration and features, --has-hi as a good example

--- src/hi/hi.c
+++ src/hi/hi.c
@@ -1,9 +1,24 @@
+#include <cl_auto_config.h>
+#include <cl_auto_headers.h>
+
+#if (CL_HAVE_STDIO_H)
 #include <stdio.h>
+#endif
+
 
 int main(int argc, const char *argv[]) {
-    if (argc > 1) {
-        printf("Hi, %s\n", argv[1]);
-    }
+	if (argc > 1) {
+		printf("Hi, %s\n", argv[1]);
+	}
 
-    return 0;
+
+#if (CL_HAVE_LOCALTIME_R)
+#include <time.h>
+	struct tm t; 
+	time_t c=0; 
+	localtime_r(&c, &t);
+	printf("%d/%d/%d\n", t.tm_year, t.tm_mon, t.tm_mday);		
+#endif
+
+	return 0;
 }
