aeec850af460c8a246f41d0645377d2a2d75f80b
南山竹 <junjiemars@gmail.com>
2016-11-14 23:42:07 +0800

    prep for preprocessing

---
+++ src/prep/README.md
@@ -0,0 +1,15 @@
+# Preprocessor
+
+## Concate Identifiers
+\#\# operator can be used to concate two identifiers in macro.
+
+## When 
+Macro be avaiable after the point it been defined.
+
+## Varidic Arguments
+```setup_list``` macro show how to define a macro supports varidic arguments.
+
+## Debug
+The output of preprocessing stage proceduced in ```objs/tmp/*.i```
+
+
--- src/prep/prep.c
+++ src/prep/prep.c
@@ -1,36 +1,23 @@
-/* Compile with:
-export CFLAGS="-g -Wall -std=gnu11 -O3"  #the usual.
-make preprocess
-*/
-
-/*
- * 1) ## operator used to concate two identifiers in macro.
- * 2) u can define macro anywhere but avaiable after the defined point.
- * 3) varidic arguments
- */
-
 #include <stdio.h>
 #include <math.h> //NAN
 
-#define setup_list(name, ...)                               \
-    double *name ## _list = (double []){__VA_ARGS__, NAN};  \
-    int name ## _len = 0;                                   \
-    for (name ## _len =0;                                   \
-            !isnan(name ## _list[name ## _len]);            \
-            ) name ## _len ++;
+#define setup_list(name, ...)                             \
+	double *name ## _list = (double []){__VA_ARGS__, NAN};  \
+	int name ## _len = 0;                                   \
+	for (name ## _len =0;                                   \
+	        !isnan(name ## _list[name ## _len]);            \
+	        ) name ## _len ++;
 
 
 int main(){
-    setup_list(items, 1, 2, 4, 8);
-    double sum=0;
-    for (double *ptr= items_list; !isnan(*ptr); ptr++)
-        sum += *ptr;
-    printf("total for items list: %g\n", sum);
-
-    //#define Length(in) in ## _len
-
-    setup_list(another, -1, 2.2, 4.8, 0.1);
-    for (int i=0, sum=0; i < another_len; i++)
-        sum += another_list[i];
-    printf("total for another list: %g\n", sum);
+	setup_list(item, 1, 2, 4, 8);
+	double sum=0;
+	for (double *ptr= item_list; !isnan(*ptr); ptr++)
+	    sum += *ptr;
+	printf("sum=%g, length<|nan=%i of item_list\n", sum, item_len);
+	
+	setup_list(another, -1, 2.2, 4.8, 0.1);
+	for (int i=0, sum=0; i < another_len; i++)
+	    sum += another_list[i];
+	printf("sum=%g, length<|nan=%i of another_list\n", sum, another_len);
 }
