96a46943063bff90ff6ce7f01b8a0aed1ff7dd26
南山竹 <junjiemars@gmail.com>
2016-11-21 01:27:48 +0800

    java loadLibrary: can load IA32 dll on 64bits platform

--- src/jni/square.c
+++ src/jni/square.c
@@ -1,8 +1,20 @@
+#include <nm_auto_headers.h>
 #include <square.h>
 #include <stdio.h>
 
+#if (MSYS_NT)
+
+	#if (CC_MSVC)
+		#define W_C4100(x) { (x); }
+	#endif 
+#endif
+
+
 JNIEXPORT jint JNICALL Java_Java2c_square
   (JNIEnv *env, jobject obj, jint n) {
+			W_C4100(env)
+			W_C4100(obj)
+
       printf("Oh, Gandalf\n");
       int v = n * n;
       return (v);
