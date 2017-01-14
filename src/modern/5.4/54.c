40cd511c4584f77a29c0aa1fe25c99c52bba44d9
南山竹 <junjiemars@gmail.com>
2017-01-14 12:32:10 +0800

    :modern constant with name

--- src/modern/5.4/54.c
+++ src/modern/5.4/54.c
@@ -2,18 +2,31 @@
 #include <stdio.h>
 
 enum corvid { magpie, raven, jay, corvid_n, };
+
 char const*const crows[corvid_n] = {
 	[raven] = "raven",
 	[magpie] = "magpie",
 	[jay] = "jay",
 };
 
+/* (T){INIT} */
+#define CORVID_NAME /**/				\
+(char const*const[corvid_n]) {	\
+	[raven] = "raven",						\
+	[magpie] = "magpie",					\
+	[jay] = "jay",								\
+}
+
 int main(int argc, char* argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
 	for (unsigned i=0; i < corvid_n; ++i) {
-		printf("corvid[%u]=%s\n", i, crows[i]);
+		printf("crows[%u]=%s\n", i, crows[i]);
+	}
+
+	for (unsigned i=0; i < corvid_n; ++i) {
+		printf("corvid[%u]=%s\n", i, CORVID_NAME[i]);
 	}
 
 	return 0;
