7a4a8f5c7df0502ba7a3400462223a98d17683da
南山竹 <junjiemars@gmail.com>
2017-01-20 00:29:48 +0800

    :net dns print twice on darwin

--- src/net/dns/dns.c
+++ src/net/dns/dns.c
@@ -12,12 +12,13 @@ static void
 usage(const char *p) {
 	printf("usage %s [options ...] host\n", p);
 	printf("  -h, --help\t\tthis help text\n");
-	printf("  -4, --4\t\tIPv4, it is default\n");
+	printf("  -4, --4\t\tIPv4\n");
+	printf("  -6, --6\t\tIPv6\n");
 	printf("  -s, --service\t\tservice name: port or name\n");
 }
 
 
-static int opt_type = AF_INET;
+static int opt_type = AF_UNSPEC;
 static char opt_service[INET_ADDRSTRLEN] = {0, };
 
 void 
@@ -27,7 +28,7 @@ get_ipv4_addr(const char* host, const char* service) {
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	
+		
 	int e = getaddrinfo(host, service, &hints, &res);
 	if (e) {
 		fprintf(stderr, "get_ipv4_addr: %s\n", gai_strerror(e));
@@ -43,16 +44,70 @@ get_ipv4_addr(const char* host, const char* service) {
 		printf("+IPv4 Address: %s\n", s);
 	}
 
-	_unused_(hints);
-	_unused_(res);
 
 clean_exit:
 	freeaddrinfo(res);	
 }
 
 void 
-get_ipv6_addr(const char* host) {
-	_unused_(host);
+get_ipv6_addr(const char* host, const char* service) {
+	struct addrinfo hints, *res;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+		
+	int e = getaddrinfo(host, service, &hints, &res);
+	if (e) {
+		fprintf(stderr, "get_ipv6_addr: %s\n", gai_strerror(e));
+		goto clean_exit;
+	}
+	
+	printf("Hostname: %s\n", host);
+	for (struct addrinfo* p=res; p != 0; p=p->ai_next) {
+		struct sockaddr_in6* in = (struct sockaddr_in6 *)p->ai_addr;
+		void* a = &(in->sin6_addr);
+		char s[INET6_ADDRSTRLEN]; 
+		inet_ntop(p->ai_family, a, s, sizeof(s));
+		printf("+IPv6 Address: %s\n", s);
+	}
+
+
+clean_exit:
+	freeaddrinfo(res);	
+}
+
+void 
+get_ip_addr(const char* host, const char* service) {
+	struct addrinfo *res;
+
+	int e = getaddrinfo(host, service, 0, &res);
+	if (e) {
+		fprintf(stderr, "get_ip_addr: %s\n", gai_strerror(e));
+		goto clean_exit;
+	}
+	
+	printf("Hostname: %s\n", host);
+	for (struct addrinfo* p=res; p != 0; p=p->ai_next) {
+		if (AF_INET == p->ai_family) {
+			struct sockaddr_in* in = (struct sockaddr_in *)p->ai_addr;
+			void* a = &(in->sin_addr);
+			char s[INET_ADDRSTRLEN]; 
+			inet_ntop(p->ai_family, a, s, sizeof(s));
+			printf("+IPv4 Address: %s\n", s);
+		} else if (AF_INET6 == p->ai_family) {
+			struct sockaddr_in6* in = (struct sockaddr_in6 *)p->ai_addr;
+			void* a = &(in->sin6_addr);
+			char s[INET6_ADDRSTRLEN]; 
+			inet_ntop(p->ai_family, a, s, sizeof(s));
+			printf("+IPv6 Address: %s\n", s);
+		}
+	}
+
+
+clean_exit:
+	freeaddrinfo(res);	
+
 }
 
 int 
@@ -86,9 +141,11 @@ main(int argc, char* argv[]) {
 
 	for (int i=0; i < argc; ++i) {
 		if (AF_INET6 == opt_type) {
-			get_ipv6_addr(argv[i]);
-		} else {
+			get_ipv6_addr(argv[i], opt_service);
+		} else if (AF_INET == opt_type) {
 			get_ipv4_addr(argv[i], opt_service);
+		} else {
+			get_ip_addr(argv[i], opt_service);
 		}
 	}
 
