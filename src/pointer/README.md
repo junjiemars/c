b1ad3a329b4675633192ec015c75543ed5c30c04
南山竹 <junjiemars@gmail.com>
2017-02-06 16:58:31 +0800

    pointer on Windows

---
+++ src/pointer/README.md
@@ -0,0 +1,4 @@
+# Pointer
+
+## References
+* [Pointers](http://stackoverflow.com/documentation/c/1108/pointers#t=201702060822544818513)
--- src/pointer/p.c
+++ src/pointer/p.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(){
-    char *list[] = {"first", "second", "third", NULL/*must has*/};
-    for (char **p=list; *p != NULL; p++){
-        printf("%s\n", p[0]);
-    }
+	printf("----------\n");
+	char *list[] = {"first", "second", "third", NULL/*must has*/};
+	for (char **p=list; *p != NULL; p++){
+		printf("%s\n", p[0]);
+	}
+	printf("address of the list is:%p\n", list);
+
+	printf("----------\n");
+	printf("sizeof(char*)=%lu bytes\n", sizeof(char*));
+	printf("sizeof(short*)=%lu bytes\n", sizeof(short*));
+	printf("sizeof(int*)=%lu bytes\n", sizeof(int*));
+	printf("sizeof(long*)=%lu bytes\n", sizeof(long*));
+	printf("sizeof(float*)=%lu bytes\n", sizeof(float*));
+	printf("sizeof(double*)=%lu bytes\n", sizeof(double*));
+	printf("sizeof(uint64_t*)=%lu bytes\n", sizeof(uint64_t*));
+
+	return 0;
 }
