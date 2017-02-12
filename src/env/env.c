043a3f33a169793d12c3bbe3f171c392106b95f2
南山竹 <junjiemars@gmail.com>
2017-02-12 13:01:40 +0800

    [command-line args|env vars] > env

--- src/env/env.c
+++ src/env/env.c
@@ -1,17 +1,29 @@
 #include <env.h>
-
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-	printf("your can set $COUNT or $TEXT env vars\n");
+int 
+main(int argc, const char* argv[]) {
+	printf("----------\n");
+	printf("#your can set COUNT, TEXT via command-line args or env vars\n");
+
+	int count = 3;
+	char text[32] = "Hello.";
+
+	for (int i=1; i < argc; ++i) {
+		sscanf(argv[i], "--COUNT=%i", &count);
+		sscanf(argv[i], "--TEXT=%s", text);
+	}
 
 	char *c = getenv("COUNT");
-	int count = c ? atoi(c) : 3;
+	if (c) count = atoi(c);
 
-	char *text = getenv("TEXT");
-	if (!text)text = "Hello.";
+	char *text_env = getenv("TEXT");
+	if (text_env) strncpy(text, text_env, sizeof(text)/sizeof(text[0]));
 
-	for (int i=0; i< count; i++)
+	for (int i=0; i < count; i++) {
 		printf("%s\n", text);
+	}
+	putchar('\n');
 }
