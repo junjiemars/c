e5be80c3d586718d7529eb308b3a82671efc07c0
南山竹 <junjiemars@gmail.com>
2017-05-18 19:58:36 +0800

    stack

---
+++ src/ds/stack/stack.h
@@ -0,0 +1,13 @@
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stddef.h>
+
+typedef struct stack {
+	void *item;
+	size_t size;
+		
+} stack;
+
+#endif
---
+++ src/ds/stack/test.c
@@ -0,0 +1,5 @@
+#include <ds.h>
+
+int
+main() {
+}
