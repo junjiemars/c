783f0a94d731cc52f0f8cb970601d7d8c12c907c
南山竹 <junjiemars@gmail.com>
2017-06-14 01:16:45 +0800

    x86 stack

---
+++ src/x86/stack/stack.c
@@ -0,0 +1,13 @@
+#include <x86.h>
+
+void foo() {
+	int a, b, c;
+	a = 10;
+	b = 5;
+	c = 2;
+}
+
+int 
+main() {
+	foo();
+}
---
+++ src/x86/x86.h
@@ -0,0 +1,11 @@
+#ifndef _X86_H_
+#define _X86_H_
+
+#include <nm_auto_config.h>
+#include <nm_auto_headers.h>
+
+#ifdef CC_MSVC
+#pragma warning(disable:4996) /*_CRT_SECURE_NO_WARNINGS*/
+#endif
+
+#endif
