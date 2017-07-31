a254747c6d79652f15f2a6e8e774af1489d680b4
南山竹 <junjiemars@gmail.com>
2017-07-31 20:26:00 +0800

    printf and %llu %lu on MacOS

--- src/memory/bytes/b.c
+++ src/memory/bytes/b.c
@@ -1,6 +1,7 @@
 #include <_memory_.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 void 
 basic_types() {
@@ -11,16 +12,16 @@ basic_types() {
 	printf("uint8_t-: -%c \t\t= #b"BIT_FMT_8"\n", x1, BIT_8(x1s));
 
 	uint16_t x2 = 'A';
-	printf("uint16_t: %hu \t\t= #b"BIT_FMT_16"\n", x2, BIT_16(x2));
+	printf("uint16_t: %"PRIu16" \t\t= #b"BIT_FMT_16"\n", x2, BIT_16(x2));
 
 	uint16_t x2s = (uint16_t)-65;
-	printf("uint16_t-: %hu \t= #b"BIT_FMT_16"\n", x2s, BIT_16(x2s));
+	printf("uint16_t-: %"PRIu16" \t= #b"BIT_FMT_16"\n", x2s, BIT_16(x2s));
 
 	uint32_t x4 = 'A';
-	printf("uint32_t: %u \t\t= #b"BIT_FMT_32"\n", x4, BIT_32(x4));
+	printf("uint32_t: %"PRIu32" \t\t= #b"BIT_FMT_32"\n", x4, BIT_32(x4));
 
 	uint64_t x8 = 'A';
-	printf("uint64_t: %lu \t\t= #b"BIT_FMT_64"\n", x8, BIT_64(x8));
+	printf("uint64_t: %"PRIu64" \t\t= #b"BIT_FMT_64"\n", x8, BIT_64(x8));
 
 	ufloat32_t x4f = { .f = 65.0f };
 	printf("float: %f \t= #b"BIT_FMT_32"\n", x4f.f, BIT_32(x4f.u));
