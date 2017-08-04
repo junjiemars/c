08aa0f9513bfe5425963ce2de656140e250811dd
南山竹 <junjiemars@gmail.com>
2017-08-05 00:25:23 +0800

    refine print bits and bytes

--- src/memory/bits/b.c
+++ src/memory/bits/b.c
@@ -5,26 +5,26 @@
 
 void unsigned_left_shift() {
   uint16_t x2 = 0x1122;
-  printf("uint16_t: %"PRIu16" \t\t =  "BIT_FMT_16"\n", x2, BIT_16(x2));
-  printf("uint16_t: %"PRIu16" << 2 \t =  "BIT_FMT_16"\n", x2, BIT_16(x2 << 2));
+  printf("uint16_t: %" PRIu16 " \t\t =  " BIT_FMT_16 "\n", x2, BIT_16(x2));
+  printf("uint16_t: %" PRIu16 " << 2 \t =  " BIT_FMT_16 "\n", x2, BIT_16(x2 << 2));
 }
 
 void unsigned_right_shift() {
   uint16_t x2 = 0x1122;
-  printf("uint16_t: %"PRIu16" \t\t =  "BIT_FMT_16"\n", x2, BIT_16(x2));
-  printf("uint16_t: %"PRIu16" >> 2 \t =  "BIT_FMT_16"\n", x2, BIT_16(x2 >> 2));
+  printf("uint16_t: %" PRIu16 " \t\t =  " BIT_FMT_16 "\n", x2, BIT_16(x2));
+  printf("uint16_t: %" PRIu16 " >> 2 \t =  " BIT_FMT_16 "\n", x2, BIT_16(x2 >> 2));
 }
 
 void signed_left_shift() {
   int16_t x2s = -0x0002;
-  printf("int16_t: %"PRIi16" \t\t =  "BIT_FMT_16"\n", x2s, BIT_16((uint16_t)x2s));
-  printf("int16_t: %"PRIi16" << 2 \t =  "BIT_FMT_16"\n", x2s, BIT_16((uint16_t)(x2s << 2)));
+  printf("int16_t: %" PRIi16 " \t\t =  " BIT_FMT_16 "\n", x2s, BIT_16((uint16_t)x2s));
+  printf("int16_t: %" PRIi16 " << 2 \t =  " BIT_FMT_16 "\n", x2s, BIT_16((uint16_t)(x2s << 2)));
 }
 
 void signed_right_shift() {
   int16_t x2s = -0x0002;
-  printf("int16_t: %"PRIi16" \t\t =  "BIT_FMT_16"\n", x2s, BIT_16((uint16_t)x2s));
-  printf("int16_t: %"PRIi16" >> 2 \t =  "BIT_FMT_16"\n", x2s, BIT_16((uint16_t)(x2s >> 2)));
+  printf("int16_t: %" PRIi16 " \t\t =  " BIT_FMT_16 "\n", x2s, BIT_16((uint16_t)x2s));
+  printf("int16_t: %" PRIi16 " >> 2 \t =  " BIT_FMT_16 "\n", x2s, BIT_16((uint16_t)(x2s >> 2)));
 } 
 
 void math() {
--- src/memory/bytes/b.c
+++ src/memory/bytes/b.c
@@ -5,50 +5,50 @@
 void 
 basic_types() {
 	uint8_t  x1 = 'A';
-	printf("uint8_t: %c \t\t=  "BIT_FMT_8"\n", x1, BIT_8(x1));
+	printf("uint8_t: %c \t\t=  " BIT_FMT_8 "\n", x1, BIT_8(x1));
 
 	uint8_t x1s = (uint8_t)-65;
-	printf("uint8_t-: -%c \t\t=  "BIT_FMT_8"\n", x1, BIT_8(x1s));
+	printf("uint8_t-: -%c \t\t=  " BIT_FMT_8 "\n", x1, BIT_8(x1s));
 
 	uint16_t x2 = 'A';
-	printf("uint16_t: %"PRIu16" \t\t=  "BIT_FMT_16"\n", x2, BIT_16(x2));
+	printf("uint16_t: %"PRIu16" \t\t=  " BIT_FMT_16 "\n", x2, BIT_16(x2));
 
 	uint16_t x2s = (uint16_t)-65;
-	printf("uint16_t-: %"PRIu16" \t=  "BIT_FMT_16"\n", x2s, BIT_16(x2s));
+	printf("uint16_t-: %"PRIu16" \t=  " BIT_FMT_16 "\n", x2s, BIT_16(x2s));
 
 	uint32_t x4 = 'A';
-	printf("uint32_t: %"PRIu32" \t\t=  "BIT_FMT_32"\n", x4, BIT_32(x4));
+	printf("uint32_t: %"PRIu32" \t\t=  " BIT_FMT_32 "\n", x4, BIT_32(x4));
 
 	uint64_t x8 = 'A';
-	printf("uint64_t: %"PRIu64" \t\t=  "BIT_FMT_64"\n", x8, BIT_64(x8));
+	printf("uint64_t: %"PRIu64" \t\t=  " BIT_FMT_64 "\n", x8, BIT_64(x8));
 
 	ufloat32_t x0f = { .f = 0.0f };
-	printf("float: %f \t=  "BIT_FMT_32"\n", x0f.f, BIT_32(x0f.u));
+	printf("float: %f \t=  " BIT_FMT_32 "\n", x0f.f, BIT_32(x0f.u));
 
 	ufloat32_t x4f = { .f = 65.0f };
-	printf("float: %f \t=  "BIT_FMT_32"\n", x4f.f, BIT_32(x4f.u));
+	printf("float: %f \t=  " BIT_FMT_32 "\n", x4f.f, BIT_32(x4f.u));
 
 	ufloat32_t x4fs = { .f = -65.0f };
-	printf("float-: %f \t=  "BIT_FMT_32"\n", x4fs.f, BIT_32(x4fs.u));
+	printf("float-: %f \t=  " BIT_FMT_32 "\n", x4fs.f, BIT_32(x4fs.u));
 
 	ufloat64_t x8f = { .f = 65.0l };
-	printf("double: %lf \t=  "BIT_FMT_64"\n", x8f.f, BIT_64(x8f.u));
+	printf("double: %lf \t=  " BIT_FMT_64 "\n", x8f.f, BIT_64(x8f.u));
 
 	ufloat64_t x8fs = { .f = -65.0l };
-	printf("double-: %lf \t=  "BIT_FMT_64"\n", x8fs.f, BIT_64(x8fs.u));
+	printf("double-: %lf \t=  " BIT_FMT_64 "\n", x8fs.f, BIT_64(x8fs.u));
 }
 
 void 
 ieee_float() {
 	ufloat32_t x4f =  { .f = 3.14f };
-	printf("float: %f \t=  "BIT_FMT_32"\n", x4f.f, BIT_32(x4f.u));
+	printf("float: %f \t=  " BIT_FMT_32 "\n", x4f.f, BIT_32(x4f.u));
 
 	ufloat32_t x4fs = { .f = -3.14f };
-	printf("float: %f \t=  "BIT_FMT_32"\n", x4fs.f, BIT_32(x4fs.u));
+	printf("float: %f \t=  " BIT_FMT_32 "\n", x4fs.f, BIT_32(x4fs.u));
 
-	printf("   sign     =  "BIT_FMT_32"\n", BIT_32(x4fs.layout.sign));
-	printf("   exponent =  "BIT_FMT_32"\n", BIT_32(x4fs.layout.exponent));
-	printf("   mantissa =  "BIT_FMT_32"\n", BIT_32(x4fs.layout.mantissa));
+	printf("   sign     =  " BIT_FMT_32 "\n", BIT_32(x4fs.layout.sign));
+	printf("   exponent =  " BIT_FMT_32 "\n", BIT_32(x4fs.layout.exponent));
+	printf("   mantissa =  " BIT_FMT_32 "\n", BIT_32(x4fs.layout.mantissa));
 }
 
 int
