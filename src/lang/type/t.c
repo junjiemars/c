54ecdb307d841f4a080f29f8fa4a49e52f53b81c
南山竹 <junjiemars@gmail.com>
2017-08-25 23:48:33 +0800

    gcc -Wstrict-aliasing in lang/type/t.c

--- src/lang/type/t.c
+++ src/lang/type/t.c
@@ -1,6 +1,13 @@
 #include <_lang_.h>
 #include <stdio.h>
 
+
+#if CC_GCC == 1
+	/* (unsigned init*)&f: */
+	#pragma GCC diagnostic ignored "-Wstrict-aliasing"  
+#endif
+
+
 int 
 main(int argc, const char *argv[]) {
 	_unused_(argc);
@@ -8,6 +15,6 @@ main(int argc, const char *argv[]) {
 
   float f0 = 3.14f;
   double f1 = 2.7182;
-  printf("pi %f stored as %8x\n", f0, *(unsigned int *)&f0);
+  printf("pi %f stored as %8x\n", f0, *(unsigned int*)&f0);
   printf("e %f stored as %8x\n", f1, *(unsigned int*)&f1);
 }
