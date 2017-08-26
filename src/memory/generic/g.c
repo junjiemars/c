1684f4d371363a5c7adf5351bd72223d015d6d33
南山竹 <junjiemars@gmail.com>
2017-08-26 23:53:35 +0800

    swap two diff sized element
    always risky, clang and gcc has different presentation --with-optimize:
    1) clang will failed: abort trap 6 when compile with -O2;
    2) gcc seems has gap between frame variables;

--- src/memory/generic/g.c
+++ src/memory/generic/g.c
@@ -17,8 +17,6 @@ swap_test() {
 	swap(&i1, &i2, sizeof(int));
 	printf("0x%x \t 0x%x\n", i1, i2);
 
-	// dangerous
-
 	int32_t i32 = 0x11223344;
 	int32_t _gap_ = 0; _unused_(_gap_);
 	int16_t i16 = 0x7788; 
@@ -26,9 +24,13 @@ swap_test() {
 	swap(&i32, &i16, sizeof(int16_t));
 	printf("0x%x \t 0x%x\n", i32, i16);
 
-	printf("swap(0x%x, 0x%x, %zu) \t\t => ", i32, i16, sizeof(int32_t));
-	swap(&i32, &i16, sizeof(int32_t));
-	printf("0x%x \t 0x%x | 0x%x\n", i32, i16, _gap_);
+	#ifdef RISKY 
+		// 1) use $(nm_def_opt)RISKY=1 in makefile;
+		// 2) or, use CFLAGS=-DRISKY=1 in make command lien;
+		printf("swap(0x%x, 0x%x, %zu) \t\t => ", i32, i16, sizeof(int32_t));
+		swap(&i32, &i16, sizeof(int32_t));
+		printf("0x%x \t 0x%x | 0x%x\n", i32, i16, _gap_);
+	#endif
 
 
 	char c1[64] = "hello", c2[64] = "world";
