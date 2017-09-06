1f15477d08f9a70d423f81b2ef2dcfa89d6d0fe7
南山竹 <junjiemars@gmail.com>
2017-09-06 14:54:25 +0800

    alloc, and pointer/pointee

--- src/memory/alloc/a.c
+++ src/memory/alloc/a.c
@@ -1,6 +1,13 @@
 #include <_memory_.h>
 #include <stdlib.h>
 
+
+typedef struct node_s {
+	int val;
+	struct node_s* next;
+} node_s;
+
+
 int* 
 cast(const size_t length) {
 	int *x = (int*)malloc(sizeof(int) * length);
@@ -36,4 +43,20 @@ main(int argc, const char *argv[]) {
 	int_ptr = better(10);
 	int_ptr[9] = 0x2211;
 	free(int_ptr);
+
+	node_s *x = malloc(sizeof(node_s));
+	node_s *y = malloc(sizeof(node_s));
+	node_s *z = malloc(sizeof(node_s));
+
+	x->val = 1;
+	y->val = 2;
+	y->val = 3;
+
+	x->next = y;
+	y->next = z;
+	z->next = x;
+
+	free(x);
+	free(y);
+	free(z);
 }
