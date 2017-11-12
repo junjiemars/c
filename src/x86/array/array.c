7b5569dfec97d29cf19f5a0b410ebc6182970519
南山竹 <junjiemars@gmail.com>
2017-11-12 11:02:59 +0800

    array of structure x86/array

--- src/x86/array/array.c
+++ src/x86/array/array.c
@@ -7,10 +7,26 @@ array() {
 	a[19] = 7;
 }
 
+struct binky {
+	int a;
+	char b, c, d, e;
+	short f;
+};
+
+void
+structure() {
+	int i;
+	struct binky a[20];
+	i = 3;
+	a[i].f = i;
+}
+
+
 int
 main(int argc, const char *argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
 	array();
+	structure();
 }
