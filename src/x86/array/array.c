bbefed7f97b0bc6e611538d10d6d5a98b258bb53
南山竹 <junjiemars@gmail.com>
2017-11-19 22:38:24 +0800

    array offset

--- src/x86/array/array.c
+++ src/x86/array/array.c
@@ -1,17 +1,27 @@
 #include <_x86_.h>
+#include <stdio.h>
 
+
+void
+offset() {
+	int i;
+	int a[20];
+	i = 19;
+	a[i] = 0x11223344;
+	i[a]++;
+}
 
 void 
 array() {
 	int a[20];
-	a[19] = 7;
+	a[19] = 0x11223344;
 }
 
 void
 structure() {
 	int i;
 	struct binky a[20];
-	i = 3;
+	i = 19;
 	a[i].f = i;
 }
 
@@ -21,6 +31,9 @@ main(int argc, const char *argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
+	offset();
 	array();
+
+	printf("sizeof(struct binky)=%zu\n", sizeof(struct binky));
 	structure();
 }
