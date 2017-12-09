12bc62c12f1feea5f01f523b0917f07b96a271c7
南山竹 <junjiemars@gmail.com>
2017-12-10 02:31:12 +0800

    x86/call pointer parameter

--- src/x86/call/call.c
+++ src/x86/call/call.c
@@ -1,4 +1,5 @@
 #include <_x86_.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 static int
@@ -15,6 +16,37 @@ caller(void) {
 	n = 0x1f;
 }
 
+static void
+ptr_callee_p(int *p) {
+	*p += 0x1100;
+}
+
+static void
+ptr_caller_p(void) {
+	int i;
+	i = 0x22;
+	ptr_callee_p(&i);	
+	i += 1;
+}
+
+static void
+ptr_callee_pp(int **pp) {
+	*pp = malloc(sizeof(int*));
+	**pp += 0x1100;
+}
+
+static void
+ptr_caller_pp(void) {
+	int i;
+	int *p;
+
+	i = 0x22;
+	p = &i;
+	ptr_callee_pp(&p);		
+	*p += 0x1;
+	free(p);
+}
+
 int
 main(int argc, char* argv[]) {
 	_unused_(argc);
@@ -22,4 +54,7 @@ main(int argc, char* argv[]) {
 
 	adder(0x1100, 0x22);
 	caller();	
+
+	ptr_caller_p();
+	ptr_caller_pp();
 }
