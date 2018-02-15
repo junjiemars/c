04bfef183542e8885504e4684dddfdac4e7470c8
南山竹 <junjiemars@gmail.com>
2018-02-15 20:29:40 +0800

    basic error operations:
    errno
    errno to string

---
+++ src/error/_error_.h
@@ -0,0 +1,11 @@
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#include <nore.h>
+#include <errno.h>
+
+#ifdef MSVC
+#pragma warning(disable:4996) /*_CRT_SECURE_NO_WARNINGS*/
+#endif
+
+#endif
---
+++ src/error/basic/e.c
@@ -0,0 +1,26 @@
+#include <_error_.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("input a filename\n");
+		return 0;
+	}
+
+	FILE* out = 0;
+	int e = 0;
+
+	out = fopen(argv[1], "r");
+	e = errno;
+
+	if (e) {
+		fprintf(stderr, "open %s failed, caused by: %s\n",
+            argv[1], strerror(e));
+	}
+
+  if (out) {
+    fclose(out);
+  }
+}
