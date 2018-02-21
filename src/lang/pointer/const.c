88597391eb11fbb68cd9773384ef28d2f47b7f5d
南山竹 <junjiemars@gmail.com>
2018-02-21 15:10:19 +0800

    split size.c

--- src/lang/pointer/const.c
+++ src/lang/pointer/const.c
@@ -38,22 +38,6 @@ qs_i_cmp(const void* x, const void* y) {
 
 int 
 main(){
-	printf("----------\n");
-	char* ss[] = { "first", "second", "third", NULL/*must has*/ };
-	for (char** p=ss; *p != NULL; p++) {
-		printf("%s ", p[0]);
-	}
-	printf("\naddress of the list is:%p\n", ss);
-
-	printf("----------\n");
-	printf("sizeof(void*)=%zu bytes\n", sizeof(void*));
-	printf("sizeof(char*)=%zu bytes\n", sizeof(char*));
-	printf("sizeof(short*)=%zu bytes\n", sizeof(short*));
-	printf("sizeof(int*)=%zu bytes\n", sizeof(int*));
-	printf("sizeof(long*)=%zu bytes\n", sizeof(long*));
-	printf("sizeof(float*)=%zu bytes\n", sizeof(float*));
-	printf("sizeof(double*)=%zu bytes\n", sizeof(double*));
-	printf("sizeof(uint64_t*)=%zu bytes\n", sizeof(uint64_t*));
 
 	printf("----------\n");
 	int v1 = 0x11223344, v2 = 0x44332211;
---
+++ src/lang/pointer/size.c
@@ -0,0 +1,19 @@
+#include "_lang_.h"
+#include <stdio.h>
+
+
+int
+main(int argc, char **argv) {
+  _unused_(argc);
+  _unused_(argv);
+
+	printf("----------\n");
+	printf("sizeof(void*)=%zu bytes\n", sizeof(void*));
+	printf("sizeof(char*)=%zu bytes\n", sizeof(char*));
+	printf("sizeof(short*)=%zu bytes\n", sizeof(short*));
+	printf("sizeof(int*)=%zu bytes\n", sizeof(int*));
+	printf("sizeof(long*)=%zu bytes\n", sizeof(long*));
+	printf("sizeof(float*)=%zu bytes\n", sizeof(float*));
+	printf("sizeof(double*)=%zu bytes\n", sizeof(double*));
+	printf("sizeof(uint64_t*)=%zu bytes\n", sizeof(uint64_t*));
+}
