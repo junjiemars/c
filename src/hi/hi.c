122b5702ef5788ab9eef126fdf8433c0ad086def
南山竹 <junjiemars@gmail.com>
2018-03-18 19:08:23 +0800

    format

--- src/hi/hi.c
+++ src/hi/hi.c
@@ -1,4 +1,4 @@
-#include "nore.h"
+#include <nore.h>
 #include <stdio.h>
 
 #define HI_VERSION_MAJOR 2
@@ -15,17 +15,17 @@ hi_version(void) {
 int 
 main(int argc, const char *argv[]) {
   if (argc > 1) {
-    printf("Hi, %s, Version:0x%x\n", 
-	   argv[1], 
-	   HI_VERSION/* defined in nore.h */);
+		printf("Hi, %s, Version:0x%x\n",
+					 argv[1], 
+					 HI_VERSION/* defined in nore.h */);
   } else {
     unsigned long ver = hi_version();
     printf("Hello, world!, Version:0x%lx(%lu)<=[major:%lu minor:%lu patch:%lu]\n"
-	   , ver
-	   , ver
-	   , (ver >> 16) & 0xff
-	   , (ver >> 8) & 0xff
-	   , ver & 0xff);
+					 , ver
+					 , ver
+					 , (ver >> 16) & 0xff
+					 , (ver >> 8) & 0xff
+					 , ver & 0xff);
   }
 
   return 0;
