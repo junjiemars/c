493d8859bcdfb2c54e868c5f79bdbacf245de7b9
南山竹 <junjiemars@gmail.com>
2018-03-29 11:09:32 +0800

    counting from stdin

--- src/io/wc/wc.c
+++ src/io/wc/wc.c
@@ -5,20 +5,30 @@
 #include <string.h>
 
 
+size_t
+count_delimeter(FILE *in, int (*test)(char)) {
+	size_t count = 0;
+	char byte;
+	while (1 == fread(&byte, sizeof(char), 1, in)) {
+		if (test(byte)) {
+			count++;
+		}
+	}
+	return count;
+}
 
 int
-count_file(const char * name, size_t *count, int (*test)(char)) {
+count_file(const char * name,
+					 size_t *count,
+					 size_t (*count_delimeter)(FILE*, int (*)(char)),
+					 int (*test)(char)) {
+	
 	FILE* file = fopen(name, "r");
 	if (!file) {
 		return errno;
 	}
 
-	char byte;
-	while (1 == fread(&byte, sizeof(char), 1, file)) {
-		if (test(byte)) {
-			(*count)++;
-		}
-	}
+	*count = count_delimeter(file, test);
 
 	if (file) {
 		fclose(file);
@@ -43,13 +53,32 @@ test_line(char line) {
 	return ('\n' == line);
 }
 
+
+#define OP_MAP(XX)              \
+	XX(0, NONE, "none")						\
+	XX(1, BYTE, "count bytes")    \
+	XX(2, LINE, "count lines")    \
+
+enum op {
+#define XX(num, name, string) OP_##name = num,
+	OP_MAP(XX)
+#undef XX
+};
+
 #define filename_size 16
-#define fn_test(x) (2 == ((x) & 2) ? test_line : test_byte)
-#define str_test(x) (2 == ((x) & 2) ? "line" : "byte")
+
+#define fn_test(x)                                  \
+	(OP_LINE == ((x) & OP_LINE) ? test_line           \
+	 : (OP_BYTE == ((x) & OP_BYTE)) ? test_byte : 0)
+
+#define str_test(x)                                 \
+	(OP_LINE == ((x) & OP_LINE) ? "line"              \
+	 : (OP_BYTE == ((x) & OP_BYTE)) ? "byte" : 0)
+
 
 int 
 main(int argc, char **argv) {
-	int opt_count_test = 0;
+	int opt_count_test = OP_NONE;
 
   char *opt_filename[filename_size] = {0};
 
@@ -60,10 +89,10 @@ main(int argc, char **argv) {
 			usage();
 			return 0;
 		case 'c':
-			opt_count_test = 1;
+			opt_count_test = OP_BYTE;
 			break;
 		case 'l':
-			opt_count_test = 2;
+			opt_count_test = OP_LINE;
 			break;
 		default:
 			if ('-' == optarg[0]) {
@@ -78,11 +107,17 @@ main(int argc, char **argv) {
 		opt_filename[j] = argv[i];
 	}
 
+	if (!*opt_filename) {
+		size_t count = count_delimeter(stdin, fn_test(opt_count_test));
+		printf("# [%s]=%zu\n", str_test(opt_count_test), count);
+		return 0;
+	}
+
 	size_t total = 0;
 	
 	for (char **filename=opt_filename; *filename; filename++) {
 		size_t count = 0;
-		int e = count_file(*filename, &count, fn_test(opt_count_test));
+		int e = count_file(*filename, &count, count_delimeter, fn_test(opt_count_test));
 		if (e) {
 			printf("! %s [%s]=%s\n", *filename, str_test(opt_count_test), strerror(e));
 		} else {
