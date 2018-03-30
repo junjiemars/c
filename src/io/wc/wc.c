b7b9d0fecd6d160a9da5786175af819bf6fec7ba
南山竹 <junjiemars@gmail.com>
2018-03-30 10:42:17 +0800

    io_wc use long option

--- src/io/wc/wc.c
+++ src/io/wc/wc.c
@@ -37,11 +37,6 @@ count_file(const char * name,
 	return 0;
 }
 
-void
-usage() {
-  printf("usage: wc [-cl] [file ...]\n");
-}
-
 int
 test_byte(char c) {
 	_unused_(c);
@@ -65,12 +60,34 @@ enum op {
 	OP_LINE,
 };
 
-
 typedef struct test_fn_s {
 	const char *name;
 	int (*test)(char);
 } test_fn_s;
 
+void
+usage() {
+  printf("Usage: /usr/bin/wc [OPTION]... [FILE]...\n");
+	printf("Print newline, word, and byte counts for each FILE, and a total line if\n\
+more than one FILE is specified.  A word is a non-zero-length sequence of\n\
+characters delimited by white space.\n");
+	printf("\nWith no FILE, or when FILE is -, read standard input.\n");
+	printf("\nThe options below may be used to select which counts are printed, always in\n\
+the following order: newline, word, character, byte, maximum line length.\n");
+	printf("  -h, --help             print this message\n");
+	printf("  -c, --bytes            print the byte counts\n");
+	printf("  -l, --lines            print the newline counts\n");
+	printf("  -w, --words            print the word counts\n");
+}
+
+
+static struct option long_options[] = {
+			{"help",  no_argument, 0, 'h'},
+			{"bytes", no_argument, 0, 'c'},
+			{"lines", no_argument, 0, 'l'},
+			{"words", no_argument, 0, 'w'},
+			{0,       0,           0,  0 },
+};
 
 int 
 main(int argc, char **argv) {
@@ -87,7 +104,7 @@ main(int argc, char **argv) {
 	int opt_count_idx = 0;
 	
   int ch;
-  while (-1 != (ch = getopt(argc, argv, "hcwl"))) {
+  while (-1 != (ch = getopt_long(argc, argv, "hcwl", long_options, 0))) {
     switch (ch) {
 		case 'h':
 			usage();
@@ -122,32 +139,32 @@ main(int argc, char **argv) {
 		return 0;
 	}
 
-	for (char **filename=opt_filename; *filename; filename++) {
-		char out_buf[2048] = {0};
-		int out_idx = 0;
-		int has_error = 0;
+	/* for (char **filename=opt_filename; *filename; filename++) { */
+	/* 	char out_buf[2048] = {0}; */
+	/* 	int out_idx = 0; */
+	/* 	int has_error = 0; */
 
-		out_idx += sprintf(out_buf, "%s", *filename);
+	/* 	out_idx += sprintf(out_buf, "%s", *filename); */
 		
-		for (test_fn_s **p=opt_count_test; *p; p++) {
-			size_t count = 0;
-			int e = count_file(*filename,
-												 &count,
-												 count_delimeter,
-												 (*p)->test);
-			if (e) {
-				has_error += e;
-				out_idx += sprintf(out_buf+out_idx,
-													 " [%s]?%s",
-													 (*p)->name,
-													 strerror(e));
-			} else {
-				out_idx += sprintf(out_buf+out_idx,
-													 " [%s]=%zu",
-													 (*p)->name,
-													 count);
-			}
-		}
-		printf("%s %s\n", (has_error ? "!" : "#"), out_buf);
-	}
+	/* 	for (test_fn_s **p=opt_count_test; *p; p++) { */
+	/* 		size_t count = 0; */
+	/* 		int e = count_file(*filename, */
+	/* 											 &count, */
+	/* 											 count_delimeter, */
+	/* 											 (*p)->test); */
+	/* 		if (e) { */
+	/* 			has_error += e; */
+	/* 			out_idx += sprintf(out_buf+out_idx, */
+	/* 												 " [%s]?%s", */
+	/* 												 (*p)->name, */
+	/* 												 strerror(e)); */
+	/* 		} else { */
+	/* 			out_idx += sprintf(out_buf+out_idx, */
+	/* 												 " [%s]=%zu", */
+	/* 												 (*p)->name, */
+	/* 												 count); */
+	/* 		} */
+	/* 	} */
+	/* 	printf("%s %s\n", (has_error ? "!" : "#"), out_buf); */
+	/* } */
 }
