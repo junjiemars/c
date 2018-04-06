5e91394a8366691e407fe82461a20016fbfd42db
南山竹 <junjiemars@gmail.com>
2018-04-06 18:04:24 +0800

    new io_tmp ansi

--- src/io/_io_.h
+++ src/io/_io_.h
@@ -8,6 +8,12 @@
 
 #if MSVC
 	#pragma warning(disable:4996) /* _CRT_SECURE_NO_WARNINGS  */
+
+#endif
+
+#if CLANG
+	#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
 #endif
 
 #endif /* end of _IO_H_ */
---
+++ src/io/file/tmp.c
@@ -0,0 +1,41 @@
+#include <_io_.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+	printf("P_tmpdir=%s\n", P_tmpdir);
+	printf("L_tmpnam=%i\n", L_tmpnam);
+
+	char *s = tmpnam(0);
+	printf("tmpname(0) = %s\n", s);
+
+	char nam[L_tmpnam];
+	tmpnam(nam);
+	printf("tmpnam(nam) = %s\n", nam);
+
+	strncpy(nam, "xyz.", sizeof(nam));
+	s = tempnam(P_tmpdir, nam);
+	printf("tempnam(%s, \"xyz\") = %s\n", P_tmpdir, s);
+
+	FILE *tf = tmpfile();
+	if (!tf) {
+		fprintf(stderr, "tmpfile() failed, %s\n", strerror(errno));
+		goto error_exit;
+	}
+
+	fprintf(tf, "%s, tmp.c\n", argv[0]);
+
+ error_exit:
+	return 1;
+ clean_exit:
+	if (tf) {
+		fclose(tf);
+	}
+}
