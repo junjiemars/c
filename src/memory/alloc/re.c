a9a63bf3fef437d42adc12d07cce02a0caae21c4
南山竹 <junjiemars@gmail.com>
2018-04-08 22:58:04 +0800

    portable malloc_size()

--- src/memory/alloc/re.c
+++ src/memory/alloc/re.c
@@ -5,6 +5,18 @@
 #include <string.h>
 #include <stdio.h>
 
+
+#if defined(NM_HAVE_MALLOC_SIZE_FN)
+#  if defined(DARWIN)
+#    include <malloc/malloc.h>
+#  elif defined(WINNT)
+#    define malloc_size(x) _msize((x))
+#  elif defined(LINUX)
+#    include <malloc.h>
+#    define malloc_size(x) malloc_usable_size((x))
+#  endif
+#endif
+
 int
 main(int argc, char **argv) {
 	_unused_(argc);
@@ -14,7 +26,7 @@ main(int argc, char **argv) {
 	int *pi = calloc(n, sizeof(*pi));
 	assert((0 == pi) || strerror(errno));
 	assert((0 == pi[n-1]) || "fill with non-zero");
-	printf("calloc() = %zu\n", _msize(pi));
+	printf("calloc() = %zu\n", malloc_size(pi));
 	pi[n-1] = 0x11223344;
 
 	/* enlarge */
@@ -22,13 +34,13 @@ main(int argc, char **argv) {
 	assert((pii == pi) || strerror(errno));
 	assert((0x11223344 == pii[n-1]) || "0x11223344 not be copied");
 	assert((0 != pii[n]) || "filled with zero");
-	printf("realloc() = %zu\n", _msize(pii));
+	printf("realloc() = %zu\n", malloc_size(pii));
 
 	/* shrink */
 	pi = realloc(pii, sizeof(*pi)*n/2);
 	assert((pii == pi) || strerror(errno));
 	assert((0 == pi[0]) || "0x0 not be copied");
-	printf("realloc() = %zu\n", _msize(pi));
+	printf("realloc() = %zu\n", malloc_size(pi));
 
 	/* on Windows, double free: */
 	/* HEAP_CORRUPTION_ACTIONABLE_BlockNotBusy_DOUBLE_FREE_c0000374_memory_realloc.exe!main */
