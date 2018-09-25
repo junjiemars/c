007df55dcf9c4660e8a81626ffa6395ede283fc1
南山竹 <junjiemars@gmail.com>
2018-09-25 14:16:23 +0800

    refine =define= section

--- src/lang/preprocessor/p.c
+++ src/lang/preprocessor/p.c
@@ -1,9 +1,22 @@
 #include <_lang_.h>
+#include <stdio.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int
 main(int argc, char *argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
-	
+	char platform[]
+#if defined(LINUX)
+  = "Linux";
+#elif defined(DARWIN)
+	= "Darwin";
+#else
+	= "Unknown";
+#endif
+
+	printf("platform: %s\n", platform);
+	printf("max: %i in [%i %i]\n", MAX(0,1), 0, 1);
 }
