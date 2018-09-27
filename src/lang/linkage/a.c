1bbef9d8813399c359a3544dc763800fc2f0f367
南山竹 <junjiemars@gmail.com>
2018-09-27 16:54:00 +0800

    lang/linkage code

---
+++ src/lang/linkage/a.c
@@ -0,0 +1,7 @@
+#ifndef _LINKAGE_A_C_
+#define _LINKAGE_A_C_
+
+int g_var_x = 0x11223344;
+
+
+#endif /* end of _LINKAGE_A_C_*/
---
+++ src/lang/linkage/c.c
@@ -0,0 +1,33 @@
+#include <_lang_.h>
+#include <stdio.h>
+
+/* external linkage */
+extern int g_var_x;
+
+/* internal linkage */
+static int f_var_y = 0xaabbccdd;
+
+void
+foo(void) {
+	int b_var_z = 0xa1b2c3d4;
+	printf("b_var_z = 0x%08x\n", b_var_z);
+}
+
+
+int
+main(int argc, char *argv[]) {
+	_unused_(argc);
+	_unused_(argv);
+	
+	printf("\nexternal linkage\n");
+	printf("------------------\n");
+	printf("g_var_x = 0x%08x\n", g_var_x);
+
+	printf("\ninternal linkage\n");
+	printf("------------------\n");
+	printf("f_var_y = 0x%08x\n", f_var_y);
+
+	printf("\nno linkage\n");
+	printf("------------\n");
+	foo();
+}
