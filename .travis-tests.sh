b79a9e89774c8ac34e1c596b8ef3b07d6a5c801d
南山竹 <junjiemars@gmail.com>
2018-10-15 11:13:46 +0800

    just for testing on Windows and it's not part of Travis

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -4,7 +4,13 @@ CC=
 case "$TRAVIS_OS_NAME" in
   osx) CC=clang ;;
   linux) CC=gcc ;;
-	*) : ;;
+	*)
+		# just for testing on Windows and it's not part of Travis.
+		case "`uname -s`" in
+			MSYS_NT-*|MINGW??_NT-*) CC=cl ;;
+			*) CC=gcc ;;
+		esac
+		;;
 esac
 
 CC=$CC ./configure \
