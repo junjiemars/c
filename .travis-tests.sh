1dbb697ed1348adcbdbca3b642153c711885d5de
南山竹 <junjiemars@gmail.com>
2018-10-25 22:36:30 +0800

    refine CC checking

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ -z "$TRAVIS_OS_NAME" ]; then
-	TRAVIS_OS_NAME="`uname -s`"
+if [ -z "$CC" ]; then
+  if [ -z "$TRAVIS_OS_NAME" ]; then
+  	TRAVIS_OS_NAME="`uname -s`"
+  fi
+  
+  case "$TRAVIS_OS_NAME" in
+    osx|Darwin) CC=clang ;;
+    linux|Linux) CC=gcc ;;
+  	MSYS_NT-*|MINGW??_NT-*) CC=cl ;;
+  	*) CC=cc ;;
+  esac
 fi
-
-case "$TRAVIS_OS_NAME" in
-  osx|Darwin) CC=clang ;;
-  linux|Linux) CC=gcc ;;
-	MSYS_NT-*|MINGW??_NT-*) CC=cl ;;
-	*) CC=gcc ;;
-esac
 
 CC=$CC ./configure \
 	--has-ds \
