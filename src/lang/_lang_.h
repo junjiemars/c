aec8ad839356a2b154006e4164f73fd61ea8307d
南山竹 <junjiemars@gmail.com>
2018-10-26 13:00:40 +0800

    gcc -Wstrict-aliasing

--- src/lang/_lang_.h
+++ src/lang/_lang_.h
@@ -11,13 +11,16 @@
    but argument 3 has type ‘long unsigned int’ 
 	 [-Werror=format] */
 #  pragma GCC diagnostic ignored "-Wformat"
+/* error: dereferencing type-punned pointer will break strict-aliasing
+	 rules [-Werror=strict-aliasing] */
+#  pragma GCC diagnostic ignored "-Wstrict-aliasing"
 #elif CLANG
 #  pragma clang diagnostic ignored "-Wformat"
 #elif MSVC
 /* C4477: 'printf' : format string '%llu' requires 
    an argument of type 'unsigned __int64', but variadic 
    argument 2 has type 'unsigned long' */
-#  pragma warning(disable:4477)
+#  pragma warning(disable : 4477)
 #  pragma warning(disable : 4702)
 #  pragma warning(disable : 4996)
 #endif
