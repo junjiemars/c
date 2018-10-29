bc1f1b6e7f2b0bcdbf6d1435f97eda6dae84d2dd
南山竹 <junjiemars@gmail.com>
2018-10-29 14:13:52 +0800

    typedef struct

--- src/lang/typedef/t.c
+++ src/lang/typedef/t.c
@@ -5,6 +5,18 @@
 /* syntax: typedef type declaration */
 typedef int length_t;
 
+struct point_s /* tag: not type */ {
+	int x;
+	int y;
+};
+typedef struct point_s point_s;
+
+typedef struct point1_s {
+	int x;
+	int y;
+} point1_s /* type */;
+
+
 int
 main(int argc, char *argv[]) {
 	_unused_(argc);
