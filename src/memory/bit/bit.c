4ae7f68f16dcd1478f0155a19020d66bb02bb96b
南山竹 <junjiemars@gmail.com>
2018-11-22 14:07:34 +0800

    toggle_a_bit and change_a_bit

--- src/memory/bit/bit.c
+++ src/memory/bit/bit.c
@@ -11,6 +11,16 @@ clear_a_bit(uint8_t x, uint8_t n) {
 	return x &= ~(1u << n);
 }
 
+uint8_t
+toggle_a_bit(uint8_t x, uint8_t n) {
+	return x ^= 1u << n;
+}
+
+uint8_t
+change_a_bit(uint8_t x, uint8_t n, uint8_t v) {
+	return x^= (-!!v ^ x) & (1u << n);
+}
+
 int
 main(void) {
 	uint8_t x = 11;
@@ -25,5 +35,15 @@ main(void) {
 	printf("----------\n");
 	z = clear_a_bit(x, 1u);
 	printf("clear_a_bit(%" PRIu8 ", %u) = " BIT_FMT_8 "\n", x, 1u, BIT_8(z));
+	printf("----------\n");
+	z = toggle_a_bit(x, 2u);
+	printf("toggle_a_bit(%" PRIu8 ", %u) = " BIT_FMT_8 "\n", x, 2u, BIT_8(z));
+	printf("toggle_a_bit(%" PRIu8 ", %u) = " BIT_FMT_8 "\n", z, 2u, BIT_8(toggle_a_bit(z, 2u)));
+
+	printf("----------\n");
+	z = change_a_bit(x, 2u, 3u);
+	printf("change_a_bit(%" PRIu8 ", %u, %u) = " BIT_FMT_8 "\n", x, 2u, 3u, BIT_8(z));
+	z = change_a_bit(x, 2u, 0u);
+	printf("change_a_bit(%" PRIu8 ", %u, %u) = " BIT_FMT_8 "\n", x, 2u, 0u, BIT_8(z));	
 }
 
