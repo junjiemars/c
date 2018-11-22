db7ff51ccf6b39b9f50bd35567251d28ffad5205
南山竹 <junjiemars@gmail.com>
2018-11-22 11:19:26 +0800

    clear_a_bit

--- src/memory/bit/bit.c
+++ src/memory/bit/bit.c
@@ -6,13 +6,24 @@ set_a_bit(uint8_t x, uint8_t n) {
 	return x |= 1u << n;
 }
 
+uint8_t
+clear_a_bit(uint8_t x, uint8_t n) {
+	return x &= ~(1u << n);
+}
+
 int
 main(void) {
 	uint8_t x = 11;
 	uint8_t z;
 	
 	printf("uint8_t: %" PRIu8 " = " BIT_FMT_8 "\n", x, BIT_8(x));
+
+	printf("----------\n");
 	z = set_a_bit(x, 2u);
 	printf("set_a_bit(%" PRIu8 ", %u) = " BIT_FMT_8 "\n", x, 2u, BIT_8(z));
+
+	printf("----------\n");
+	z = clear_a_bit(x, 1u);
+	printf("clear_a_bit(%" PRIu8 ", %u) = " BIT_FMT_8 "\n", x, 1u, BIT_8(z));
 }
 
