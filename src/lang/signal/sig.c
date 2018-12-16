b8e0cb661b8839e405e6d8ba96f9796ccc9377bd
南山竹 <junjiemars@gmail.com>
2018-12-16 21:50:55 +0800

    signal

---
+++ src/lang/signal/sig.c
@@ -0,0 +1,38 @@
+#include <_lang_.h>
+#include <signal.h>
+#include <stdio.h>
+
+#if WINNT
+#  include <windows.h>
+#else
+#  include <unistd.h>
+#endif
+
+
+static volatile int s_flag = 0;
+
+void on_sigint(int sig) {
+	if (SIGINT == sig) {
+		s_flag = SIGINT;
+	}
+}
+
+int main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+	int n = 10;
+	signal(SIGINT, on_sigint);
+
+	do {
+		printf("sleeping...\n");
+		fflush(stdout);
+		if (s_flag) {
+			printf("on_sigint, exit.\n");
+			break;
+		}
+		sleep(1);
+	} while (--n > 0);
+
+	return 0;
+}
