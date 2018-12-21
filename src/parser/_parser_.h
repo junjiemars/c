80f2f1835f1139351f45d2ec1c12437e737d3056
南山竹 <junjiemars@gmail.com>
2018-12-21 15:36:28 +0800

    new src/parser/recursive_descent
    want to make a cdecl tool in elisp

---
+++ src/parser/_parser_.h
@@ -0,0 +1,8 @@
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#include <nore.h>
+
+#define _unused_(x) ((void)(x))
+
+#endif
---
+++ src/parser/recursive_descent/rdp.c
@@ -0,0 +1,156 @@
+#include <_parser_.h>
+#include <error.h>
+
+
+typedef enum {ident, number, lparen, rparen, times, slash, plus,
+							minus, eql, neq, lss, leq, gtr, geq, callsym, beginsym, semicolon,
+							endsym, ifsym, whilesym, becomes, thensym, dosym, constsym, comma,
+							varsym, procsym, period, oddsym} Symbol;
+
+enum parser_error {
+	no_error,
+	except_error,
+	factor_error,
+	condition_error,
+	statement_error
+};
+	
+static Symbol sym;
+void nextsym(void);
+void expression(void);
+
+static char *tape;
+static char *p_tape;
+
+void nextsym(void) {
+	if (0 == p_tape) {
+		return;
+	}
+}
+
+int accept(Symbol s) {
+	if (sym == s) {
+		nextsym();
+		return 1;
+	}
+	return 0;
+}
+
+int expect(Symbol s) {
+	if (accept(s))
+		return 1;
+	error(1, except_error, "expect: unexpected symbol");
+	return 0;
+}
+
+void factor(void) {
+	if (accept(ident)) {
+		;
+	} else if (accept(number)) {
+		;
+	} else if (accept(lparen)) {
+		expression();
+		expect(rparen);
+	} else {
+		error(1, factor_error, "factor: syntax error");
+		nextsym();
+	}
+}
+
+void term(void) {
+	factor();
+	while (sym == times || sym == slash) {
+		nextsym();
+		factor();
+	}
+}
+
+void expression(void) {
+	if (sym == plus || sym == minus)
+		nextsym();
+	term();
+	while (sym == plus || sym == minus) {
+		nextsym();
+		term();
+	}
+}
+
+void condition(void) {
+	if (accept(oddsym)) {
+		expression();
+	} else {
+		expression();
+		if (sym == eql || sym == neq || sym == lss || sym == leq || sym == gtr || sym == geq) {
+			nextsym();
+			expression();
+		} else {
+			error(1, condition_error, "condition: invalid operator");
+			nextsym();
+		}
+	}
+}
+
+void statement(void) {
+	if (accept(ident)) {
+		expect(becomes);
+		expression();
+	} else if (accept(callsym)) {
+		expect(ident);
+	} else if (accept(beginsym)) {
+		do {
+			statement();
+		} while (accept(semicolon));
+		expect(endsym);
+	} else if (accept(ifsym)) {
+		condition();
+		expect(thensym);
+		statement();
+	} else if (accept(whilesym)) {
+		condition();
+		expect(dosym);
+		statement();
+	} else {
+		error(1, statement_error, "statement: syntax error");
+		nextsym();
+	}
+}
+
+void block(void) {
+	if (accept(constsym)) {
+		do {
+			expect(ident);
+			expect(eql);
+			expect(number);
+		} while (accept(comma));
+		expect(semicolon);
+	}
+	if (accept(varsym)) {
+		do {
+			expect(ident);
+		} while (accept(comma));
+		expect(semicolon);
+	}
+	while (accept(procsym)) {
+		expect(ident);
+		expect(semicolon);
+		block();
+		expect(semicolon);
+	}
+	statement();
+}
+
+
+int main(int argc, char **argv) {
+	_unused_(argv);
+
+	if (argc > 1) {
+		tape = argv[1];
+		p_tape = tape;
+		
+		nextsym();
+		block();
+		expect(period);
+	}
+	
+	return 0;
+}
