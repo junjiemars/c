33de640384718aa33b47f8e9a62c5a46e0a8f27e
南山竹 <junjiemars@gmail.com>
2018-12-22 19:19:05 +0800

    get_token

--- src/parser/cdecl/c1.c
+++ src/parser/cdecl/c1.c
@@ -14,9 +14,11 @@ struct token_s {
 
 struct token_s stack[MAX_TOKEN_NUM];
 struct token_s token;
+int token_idx = 0;
 
-void get_token(const char *, int*);
+void get_token(char*, struct token_s*);
 char classify_string(struct token_s *);
+
 
 char classify_string(struct token_s *token) {
 	if (strncmp("char", token->string, MAX_TOKEN_LEN)
@@ -28,28 +30,35 @@ char classify_string(struct token_s *token) {
 	} else {
 		token->type = 'i';
 	}
+	printf("type=%c, string=%s\n", token->type, token->string);
 	return token->type;
 }
 
-void get_token(const char *input, int *idx) {
-	_unused_(idx);
-	if (0 == *input) {
-		return;
-	}
-	
-	const char *p;
+void get_token(char *input, struct token_s *token) {
+	char c;
+	char *s = token->string;
 
-	while (0 != (p = input++)) {
-		if (isalpha(*p)) {
+	while ((c = *input++)) {
+		if (isalpha(c)) {
+			*s++ = c;
+		} else if ('(' == c) {
+			*s++ = c;
+		} else if (')' == c) {
+			*s++ = c;
+			classify_string(token);
+		} else if (' ' == c) {
+			classify_string(token);
 		}
 	}
-	return;
 }
 
 int main(int argc, char **argv) {
 	_unused_(argc);
 	_unused_(argv);
 
-	printf("a=%i, A=%i\n", 'a', 'A');
+	if (argc > 1) {
+		get_token(argv[1], &token);
+	}
+
 	return 0;
 }
