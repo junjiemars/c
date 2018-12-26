ed49d11a1e7f12aa213dea51a8df157f8a7d8bd8
南山竹 <junjiemars@gmail.com>
2018-12-26 13:29:06 +0800

    refine lang/string/basic

--- src/lang/string/basic.c
+++ src/lang/string/basic.c
@@ -10,7 +10,6 @@ self_strlen(const char *s) {
   while (*s++) {
     len++;
   }
-
   return len;
 }
 
@@ -26,8 +25,57 @@ char *
 self_strcpy(char *dst, const char *src) {
   char *r = dst;
   while (0 != (*dst++ = *src++));
-
   return r;
+}
+
+void
+test_strlen(char *s) {
+	size_t len1 = strlen(s);
+  size_t len2 = self_strlen(s);
+  assert((len1 == len2) || "self_strlen() failed");
+#if NDEBUG
+	_unused_(len1);
+	_unused_(len2);
+#endif
+}
+
+void
+test_strcpy(void) {
+	char buf[8];
+	int len = sizeof(buf)/sizeof(buf[0]);
+	memset(buf, 0x11, len);
+	assert(0x11 == buf[len-1] && "strcpy(), 0x11 != tailing");
+
+	/* does not padding 0 */
+	strcpy(buf, "abc");
+	assert(0 == buf[strlen(buf)] && "strcpy(), 0 != string tailing");
+	assert(0x11 == buf[len-1] && "strcpy(), 0x11 != tailing");
+
+	/* no null-terminated */
+#ifndef MSVC
+	/* msvc runtime error:
+		 Security check failure or stack buffer overrun - code c0000409 (!!! second chance !!!)
+		 *** WARNING: Unable to verify checksum for lang_string_basic.exe
+		 d:\agent\_work\3\s\src\vctools\crt\vcstartup\src\gs\gs_report.c(220)+0x5
+		 lang_string_basic!__report_gsfailure+0x1c:
+		 00007ff6`8dc67788 cd29            int     29h
+	 */
+	strcpy(buf, "abcdefgh");
+	assert(0 != buf[len-1] && "strcpy(), 0 == tailing");
+#endif
+}
+
+void
+test_strcmp(char *s) {
+	size_t len = strlen(s);
+	char *d1 = malloc(len+1);
+  char *d2 = strcpy(d1, s);
+	assert(d1 == d2 && "strcpy(), diff between dest and retval");
+  assert(0 == strcmp(s, d1) && "strcpy(), failed");
+	assert(0 == self_strcmp(d1, d2) && "strcpy(), failed");
+#if NDEBUG
+	_unused_(d2);
+#endif
 }
 
 #define STRCMP(a, R, b) (strcmp((a), (b)) R 0)
@@ -36,37 +84,23 @@ self_strcpy(char *dst, const char *src) {
 	 => STRCMP(a, ==, "volatile")
  */
 
-void test_STRCMP(void) {
+void
+test_STRCMP(void) {
 	char *a = "abcd";
 	char *b = "abcd";
 	assert(STRCMP(a, ==, b) && "STRCMP, a != b");
+#if NDEBUG
+	_unused_(a);
+	_unused_(b);
+#endif
 }
 
 int
 main(int argc, char **argv) {
   _unused_(argc);
 
-  size_t len1 = strlen(argv[0]);
-  size_t len2 = self_strlen(argv[0]);
-  assert((len1 == len2) || "self_strlen() failed");
-
-  char *dst1 = malloc(len1+1);
-  char *r1 = strcpy(dst1, argv[0]);
-  assert(((0 == strcmp(r1, argv[0]))
-					&& (r1 == dst1)) || "strcpy() failed");
-#if NDEBUG
-	_unused_(r1);
-#endif
-  free(dst1);
-
-  char *dst2 = malloc(len2+1);
-  char *r2 = self_strcpy(dst2, argv[0]);
-  assert(((0 == self_strcmp(r2, argv[0]))
-					&& (r2 == dst2)) || "self_strcpy() failed");
-#if NDEBUG
-	_unused_(r2);
-#endif
-  free(dst2);
-
+	test_strlen(argv[0]);
+	test_strcpy();
+	test_strcmp(argv[0]);
 	test_STRCMP();
 }
