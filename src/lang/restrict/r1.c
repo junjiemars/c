85d23edde785df6163a5a36e61ff4c3573e21209
南山竹 <junjiemars@gmail.com>
2019-01-20 16:37:04 +0800

    lang/restrict keyword

---
+++ src/lang/restrict/r1.c
@@ -0,0 +1,31 @@
+#include <_lang_.h>
+
+void
+f1(int *p1, int *p2, int *val) {
+	*p1 += *val;
+	*p2 += *val;
+}
+
+#if NM_HAVE_RESTRICT_KEYWORD
+void
+f2(int *restrict p1, int *restrict p2, int *restrict val) {
+	*p1 += *val;
+	*p2 += *val;
+}
+#endif
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+	int x = argc;
+	int y = argc * 10;
+	int z = argc * 100;
+	
+	f1(&x, &y, &z);
+#if NM_HAVE_RESTRICT_KEYWORD
+	f2(&x, &y, &z);
+#endif
+	return 0;
+}
