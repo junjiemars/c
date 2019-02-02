75229f421f66aaacfefc4f4c04a089f8e4b5c48d
南山竹 <junjiemars@gmail.com>
2019-02-02 19:03:01 +0800

    basic io

--- src/io/basic/b.c
+++ src/io/basic/b.c
@@ -7,7 +7,13 @@ main(int argc, char *argv[]) {
 	_unused_(argv);
 
 	int ch;
-	while (EOF != (ch = getchar())) {
-		putchar(ch);
+	while (EOF != (ch = fgetc(stdin))) {
+		fputc(ch, stdout);
 	}
+	if (ferror(stdin)) {
+		perror("read failed from stdin");
+		return 1;
+	}
+
+	return 0;
 }
