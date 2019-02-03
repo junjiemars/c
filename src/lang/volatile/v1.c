4116c0dd274bbd6341be046aca3aa1b238cb47b3
南山竹 <junjiemars@gmail.com>
2019-02-03 17:39:00 +0800

    new lang/volatile

---
+++ src/lang/volatile/v1.c
@@ -0,0 +1,49 @@
+#include <_lang_.h>
+
+static int n;
+static volatile int n1;
+
+void
+bar(void) {
+	/* with clang -O2 optimized will keep function skeleton, just
+		 like:
+		 void bar(void) {}
+		 because n has ever changed
+ */
+	n = 0;
+	int i = 0;
+	while (n != 255) {
+		if (++i > 3) {
+			break;
+		}
+	}
+}
+
+void
+bar1(void) {
+	n1 = 0;
+	int i = 0;
+	while (n1 != 255) {
+		if (++i > 3) {
+			break;
+		}
+	}
+}
+
+int
+incr(void) {
+	n1 += 0xff;
+	return n1;
+}
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+	bar();
+	bar1();
+	incr();
+	
+	return 0;
+}
