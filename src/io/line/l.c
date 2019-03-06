bf87c6aa92ee02f7508875a2d4677e9a50d3e90d
南山竹 <junjiemars@gmail.com>
2019-03-06 10:44:42 +0800

    compiled via msvc

--- src/io/line/l.c
+++ src/io/line/l.c
@@ -1,15 +1,21 @@
 #include <_io_.h>
 #include <stdio.h>
 
+#define LINE_MAXLEN 16
+
+#if MSVC
+#  pragma warning(disable: 4244)
+#endif
+
 void
-read_lines(const char *filename, const int max_line_size) {
+read_lines(const char *filename) {
 	FILE *file = fopen(filename, "r");
 	if (!file) {
 		perror(filename);
 		return;
 	}
 	int c;
-	char line[max_line_size];
+	char line[LINE_MAXLEN];
 	char *p = line;
 	while (EOF != (c = fgetc(file))) {
 		if ('\r' == c || '\n' == c) {
@@ -34,7 +40,7 @@ main(int argc, char **argv) {
 	_unused_(argv);
 
 	if (argc > 1) {
-		read_lines(argv[1], 16);
+		read_lines(argv[1]);
 	}
 	return 0;
 }
