c526a90faa9a2ce49e3b6a5b3dcd969c60f50366
南山竹 <junjiemars@gmail.com>
2019-03-12 15:17:20 +0800

    try to feature test macro

--- src/io/tmp/tmp.c
+++ src/io/tmp/tmp.c
@@ -16,39 +16,66 @@
 #  endif
 #endif
 
+void
+test_consts(void) {
+	printf("P_tmpdir=%s\n", P_tmpdir);
+	printf("L_tmpnam=%i\n", L_tmpnam);
+	printf("TMP_MAX=0x%08x\n", TMP_MAX);
+}
+
+void
+test_tmpnam(void) {
+	char b1[L_tmpnam];
+	strcpy(b1, tmpnam(0)); /* may be overwriten by next call */
+	printf("tmpnam(0) = %s\n", b1);
+	char b2[L_tmpnam];
+	tmpnam(b2);
+	printf("tmpnam(\"non-nil\") = %s\n", b2);
+}
+
+/* void */
+/* test_mkstemp(void) { */
+/* 	char t[L_tmpnam << 4] = "vviviiXXXXXX"; */
+/* 	int fd = mkstemp(t); */
+/* 	_unused_(t); */
+/* 	_unused_(fd); */
+/* } */
+
 int
 main(int argc, char **argv) {
 	_unused_(argc);
+	_unused_(argv);
 
-	printf("P_tmpdir=%s\n", P_tmpdir);
-	printf("L_tmpnam=%i\n", L_tmpnam);
+	test_consts();
+	test_tmpnam();
+	/* test_mkstemp(); */
 
-	char *s = tmpnam(0);
-	printf("tmpname(0) = %s\n", s);
+ /* 	char *s = tmpnam(0); */
+ /* 	printf("tmpname(0) = %s\n", s); */
 
-	char nam[L_tmpnam + sizeof(void*)];
-	tmpnam(nam);
-	printf("tmpnam(nam) = %s\n", nam);
+ /* 	char nam[L_tmpnam + sizeof(void*)]; */
+ /* 	tmpnam(nam); */
+ /* 	printf("tmpnam(nam) = %s\n", nam); */
 
-	strncpy(nam, "xyz.", sizeof(nam));
-	s = tempnam(P_tmpdir, nam);
-	printf("tempnam(%s, \"xyz\") = %s\n", P_tmpdir, s);
+ /* 	strncpy(nam, "xyz.", sizeof(nam)); */
+ /* 	s = tempnam(P_tmpdir, nam); */
+ /* 	printf("tempnam(%s, \"xyz\") = %s\n", P_tmpdir, s); */
 
-	FILE *tf = tmpfile();
-	if (!tf) {
-		perror("call tmpfile() failed");
-		goto error_exit;
-	}
+ /* 	FILE *tf = tmpfile(); */
+ /* 	if (!tf) { */
+ /* 		perror("call tmpfile() failed"); */
+ /* 		goto error_exit; */
+ /* 	} */
 
-	fprintf(tf, "%s, tmp.c\n", argv[0]);
-	goto clean_exit;
+ /* 	fprintf(tf, "%s, tmp.c\n", argv[0]); */
+ /* 	goto clean_exit; */
 
- error_exit:
-	return 1;
+ /* error_exit: */
+ /* 	return 1; */
 
- clean_exit:
-	if (tf) {
-		fclose(tf);
-	}
+ /* clean_exit: */
+ /* 	if (tf) { */
+ /* 		fclose(tf); */
+ /* 	} */
 	return 0;
 }
