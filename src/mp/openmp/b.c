1498f6adc6c7037d43c7d4ce068dce1e9c7a5209
南山竹 <junjiemars@gmail.com>
2019-03-19 15:36:36 +0800

    openmp_for

---
+++ src/mp/openmp/b.c
@@ -0,0 +1,18 @@
+#include <_mp_.h>
+#include <stdio.h>
+
+#if NM_HAVE_OMP_H
+#  include <omp.h>
+#endif
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+	printf("procs[%i]\n----------\n", omp_get_num_procs());
+#pragma omp parallel
+	printf("In omp\n");
+	
+	return 0;
+}
