eff805189034085ea94afabfff0819ec6b6a08ad
南山竹 <junjiemars@gmail.com>
2019-03-19 11:15:52 +0800

    new src/mp

---
+++ src/mp/_mp_.h
@@ -0,0 +1,8 @@
+#ifndef _MP_H_
+#define _MP_H_
+
+#include <nore.h>
+
+#define _unused_(x) ((void)(x))
+
+#endif
---
+++ src/mp/basic/b.c
@@ -0,0 +1,8 @@
+#include <_mp_.h>
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+	return 0;
+}
