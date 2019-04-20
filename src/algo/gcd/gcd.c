f70ce399ebba09f003a589e120d27129d7cdbba7
南山竹 <junjiemars@gmail.com>
2019-04-20 22:23:50 +0800

    refine algo/gcd

--- src/algo/gcd/gcd.c
+++ src/algo/gcd/gcd.c
@@ -3,8 +3,20 @@
 #include <stdlib.h>
 
 
+/* raw while loop */
 int
 gcd(int a, int b) {
+	while (b) {
+		int m = a % b;
+		a = b;
+		b = m;
+	}
+	return a;
+}
+
+/* tail recursion */
+int
+gcd1(int a, int b) {
 	if (0 == b) {
 		return a;
 	} 
@@ -21,6 +33,7 @@ main(int argc, char **argv) {
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 	printf("gcd(%i, %i) = %i\n", a, b, gcd(a, b));
+	printf("gcd1(%i, %i) = %i\n", a, b, gcd1(a, b));
 	
 	return 0;
 }
