a3c0cc1160805120c8aea7ad2d43cb72dac1ca5c
南山竹 <junjiemars@gmail.com>
2019-04-20 23:29:55 +0800

    fix typo

--- src/signal/list/list-sig.c
+++ src/signal/list/list-sig.c
@@ -8,7 +8,7 @@ main(int argc, char **argv) {
 	_unused_(argv);
 
 	for (int i = 0; i < NSIG; i++) {
-		printf("[%02i] [%.8s] %s\n", i, sys_signame[i], sys_siglist[i]);
+		printf("[%02i] [%s] %s\n", i, sys_signame[i], sys_siglist[i]);
 	}
 	
 	return 0;
