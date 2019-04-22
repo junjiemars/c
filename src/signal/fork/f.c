f59232765324144a25b237e8301144426c352c14
南山竹 <junjiemars@gmail.com>
2019-04-22 11:32:31 +0800

    fork(), wait() and waitpid() does not on Windows

--- src/signal/fork/f.c
+++ src/signal/fork/f.c
@@ -1,20 +1,23 @@
 #include <_signal_.h>
 #include <stdio.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
+#ifndef WINNT
+#  include <sys/wait.h>
+#  include <unistd.h>
 static volatile int sig_flag = 0x11;
 
 void on_sigchld(int sig) {
 	_unused_(sig);
 	sig_flag += 0x1;
 }
+#endif
 
 int
 main(int argc, char **argv) {
 	_unused_(argc);
 	_unused_(argv);
 
+#ifndef WINNT	
 	pid_t pid;
 	signal(SIGCHLD, on_sigchld);
 
@@ -24,6 +27,7 @@ main(int argc, char **argv) {
 	}
 	waitpid(pid, 0, 0);
 	printf("sig_flag = 0x%08x\n", sig_flag);
+#endif
 	
 	return 0;
 }
