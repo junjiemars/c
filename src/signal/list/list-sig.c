4a03a553fcf1c4ab3bcde31f6dd45622e68ad30d
南山竹 <junjiemars@gmail.com>
2019-04-23 12:30:43 +0800

    refine list signals

--- src/signal/list/list-sig.c
+++ src/signal/list/list-sig.c
@@ -8,7 +8,12 @@ main(int argc, char **argv) {
 	_unused_(argv);
 
 	for (int i = 0; i < NSIG; i++) {
+#ifdef DARWIN
 		printf("[%02i] [%s] %s\n", i, sys_signame[i], sys_siglist[i]);
+#elif LINUX
+		printf("[%02i] [%s]\n", i, sys_siglist[i]);
+#else
+#endif
 	}
 	
 	return 0;
