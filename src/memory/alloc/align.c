4276e0224acc38b5e2b23459e1f6a49dc940c931
南山竹 <junjiemars@gmail.com>
2019-05-22 22:28:20 +0800

    posix_memalign on Darwin

---
+++ src/memory/alloc/align.c
@@ -0,0 +1,26 @@
+#include <_memory_.h>
+#include <stdlib.h>
+
+#if NM_HAVE_MEMALIGN_FN && DARWIN
+void *
+_malign_(size_t alignment, size_t size) {
+	void *p;
+	int n = posix_memalign(&p, alignment, size);
+	return 0 == n ? p : 0;
+}
+#endif
+
+int
+main(int argc, const char *argv[]) {
+	_unused_(argc);
+	_unused_(argv);
+
+#if NM_HAVE_MEMALIGN_FN || NM_HAVE_MEMALIGN_FN
+	int *p = _malign_(4096, 4096);
+	if (p) {
+		free(p);
+	}
+#endif
+
+	return 0;
+}
