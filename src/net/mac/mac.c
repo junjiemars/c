3974f275cfaaafe8a4c390b13c1b5f8ef7ea22a4
南山竹 <junjiemars@gmail.com>
2019-06-26 23:17:12 +0800

    new net/mac

---
+++ src/net/mac/mac.c
@@ -0,0 +1,26 @@
+#include <_net_.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+
+void
+get_mac_address(const char *interface) {
+	int sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (-1 == sd) {
+		return;
+	}
+	struct ifreq ifr;
+	memset(&ifr, 0, sizeof(ifr));
+	snprintf(ifr.ifr_name, IFNAMSIZ, "%s", interface);
+	if (ioctl(sd, SIOCGIFHWADDR, &ifr) < 0) {
+		return;
+	}
+}
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+	return 0;
+}
