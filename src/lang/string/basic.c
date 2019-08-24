096f750c53195d85ebf370071286f803957d56e4
南山竹 <junjiemars@gmail.com>
2019-08-24 22:04:01 +0800

    refine lang/string/basic

--- src/lang/string/basic.c
+++ src/lang/string/basic.c
@@ -32,7 +32,7 @@ void
 test_strlen(char *s) {
 	size_t len1 = strlen(s);
   size_t len2 = self_strlen(s);
-  assert((len1 == len2) || "self_strlen() failed");
+  assert((len1 == len2) && "self_strlen() failed");
 #if NDEBUG
 	_unused_(len1);
 	_unused_(len2);
@@ -41,27 +41,27 @@ test_strlen(char *s) {
 
 void
 test_strcpy(void) {
-	char buf[8];
-	int len = sizeof(buf)/sizeof(buf[0]);
-	memset(buf, 0x11, len);
-	assert(0x11 == buf[len-1] && "strcpy(), 0x11 != tailing");
+	char buf1[8];
+	int len = sizeof(buf1)/sizeof(*buf1);
+	memset(buf1, 0x11, len);
+	assert(0x11 == buf1[len-1]
+				 && "strcpy(), 0x11 != <tailing>");
 
+	strcpy(buf1, "");
+	assert(0 == strlen(buf1)
+				 && "strcpy(), copy empty string failed");
+	
 	/* does not pad tailing 0 */
-	strcpy(buf, "abc");
-	assert(0 == buf[strlen(buf)] && "strcpy(), 0 != tailing");
-	assert(0x11 == buf[len-1] && "strcpy(), 0x11 != tailing");
+	strcpy(buf1, "abc");
+	assert(0 == buf1[strlen(buf1)]
+				 && "strcpy(), 0 != <tailing>");
+	assert(0x11 == buf1[len-1]
+				 && "strcpy(), 0x11 != <tailing>");
 
-	/* no null-terminated */
-	/* msvc runtime error:
-		 Security check failure or stack buffer overrun - code c0000409 (!!! second chance !!!)
-		 *** WARNING: Unable to verify checksum for lang_string_basic.exe
-		 d:\agent\_work\3\s\src\vctools\crt\vcstartup\src\gs\gs_report.c(220)+0x5
-		 lang_string_basic!__report_gsfailure+0x1c:
-		 00007ff6`8dc67788 cd29            int     29h
-	 */
-	/* lang_string_basic[2834:14486] detected buffer overflow */
-	/* strcpy(buf, "abcdefgh"); */
-	/* assert(0 != buf[len-1] && "strcpy(), 0 == tailing"); */
+	char buf2[8];
+	self_strcpy(buf2, "abc");
+	assert(0 == strncmp(buf1, buf2, len)
+				 && "self_strcpy(), 0 != <tailing>");
 }
 
 void
@@ -70,42 +70,25 @@ test_strcmp(char *s) {
 	char *d1 = malloc(len+1);
   char *d2 = strcpy(d1, s);
 	assert(d1 == d2 && "strcpy(), diff between dest and retval");
-  assert(0 == strcmp(s, d1) && "strcpy(), failed");
-	assert(0 == self_strcmp(d1, d2) && "strcpy(), failed");
-#if NDEBUG
-	_unused_(d2);
-#endif
+  assert(0 == strcmp(s, d1)
+				 && 0 == self_strcmp(s, d1)
+				 && 0 == self_strcmp(d1, s)
+				 && "self_strcpy(), failed");
+
+	char buf[8];
+	d2 = buf;
+	strcpy(d1, "abc");
+	strcpy(d2, "abx");
+	assert(0 != strcmp(d1, d2)
+				 && 0 != self_strcmp(d1, d2)
+				 && "self_strcmp(), 0 !=");
+	assert(0 > strcmp(d1, d2)
+				 && 0 > self_strcmp(d1, d2)
+				 && 0 < self_strcmp(d2, d1)
+				 && "self_strcmp(), 0 ><");
 	free(d1);
 }
 
-void
-test_strcmp1(void) {
-	char buf[8];
-	size_t len = sizeof(buf)/sizeof(buf[0]);
-	const char *s = "abx";
-	memset(buf, 0x11, len);
-  strcpy(buf, "abc");
-	assert(0 > strcmp(buf, s) && "strcmp(), failed");
-	assert(0 > self_strcmp(buf, s) && "self_strcmp(), failed");
-	assert(0 < self_strcmp(s, buf) && "self_strcmp(), failed");
-}
-
-#define STRCMP(a, R, b) (strcmp((a), (b)) R 0)
-/* 0 == strcmp(a, b)
-	 => !strcmp(a, b)
-	 => STRCMP(a, ==, "volatile")
- */
-
-void
-test_STRCMP(void) {
-	char *a = "abcd";
-	char *b = "abcd";
-	assert(STRCMP(a, ==, b) && "STRCMP, a != b");
-#if NDEBUG
-	_unused_(a);
-	_unused_(b);
-#endif
-}
 
 int
 main(int argc, char **argv) {
@@ -113,9 +96,7 @@ main(int argc, char **argv) {
 
 	test_strlen(argv[0]);
 	test_strcpy();
-/* 	test_strcmp(argv[0]); */
-/* 	test_strcmp1(); */
-/* 	test_STRCMP(); */
+	test_strcmp(argv[0]);
 
 	return 0;
 }
