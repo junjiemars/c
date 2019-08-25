97bfca22d8ea842ed07a44a204a9f5f6999dac02
南山竹 <junjiemars@gmail.com>
2019-08-25 19:37:10 +0800

    getdelim

--- src/io/line/l.c
+++ src/io/line/l.c
@@ -1,6 +1,7 @@
 #include <_io_.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #if MSVC
 #  pragma warning(disable: 4244)
@@ -41,7 +42,6 @@ read_lines(const char *filename,
 		goto close_exit;
 	}
  close_exit:
-
 	fclose(file);
 }
 
@@ -51,13 +51,40 @@ print_line(const char *line, ssize_t linelen) {
 	fflush(stdout);
 }
 
+void
+test_read_lines(const char *filename) {
+	read_lines(filename, 16, '\n', print_line);
+}
+
+void
+test_getdelim(const char *filename) {
+	FILE *file = fopen(filename, "r");
+	if (!file) {
+		perror(filename);
+		return;
+	}
+
+	char *line = 0;
+	size_t linecap = 0;
+	ssize_t linelen;
+	while ((linelen = getdelim(&line, &linecap, '\n', file)) > 0) {
+		fwrite(line, linelen, 1, stdout);
+	}
+	fclose(file);
+}
+
 int
 main(int argc, char **argv) {
 	_unused_(argc);
 	_unused_(argv);
 
 	if (argc > 1) {
-		read_lines(argv[1], 16, '\n', print_line);
+		char *f = malloc(strlen(argv[1]) + 1);
+		strcpy(f, argv[1]);
+		test_read_lines(f);
+		fprintf(stdout, "##########\n");
+		test_getdelim(f);
+		free(f);
 	}
 	return 0;
 }
