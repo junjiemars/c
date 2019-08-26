89612dc94606e4b8f43926069d31628cb0c4418d
南山竹 <junjiemars@gmail.com>
2019-08-26 21:41:08 +0800

    strlen_utf8 for count utf8 string

---
+++ src/algo/strlen_utf8/l.c
@@ -0,0 +1,25 @@
+#include <_algo_.h>
+
+size_t
+strlen_utf8(const char *s) {
+	size_t n = 0, i = 0;
+	while (s[i]) {
+		if ((0xc0 & s[i]) != 0x80) {
+			n++;
+		}
+		i++;
+	}
+	return n;
+}
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+	char *s = "a中文bc12def";
+	size_t n = strlen_utf8(s);
+	printf("strlen_utf8(%s) = %zu\n", s, n);
+
+	return 0;
+}
