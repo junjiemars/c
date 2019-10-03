fad2558266c5dfd4f8b5f6a13971c23dfcfd8201
南山竹 <junjiemars@gmail.com>
2019-10-03 11:35:01 +0800

    refine lang/time

--- src/lang/time/t.c
+++ src/lang/time/t.c
@@ -8,7 +8,24 @@ main(int argc, char **argv) {
 	_unused_(argv);
 
 	time_t epoch = time(0);
-	printf("epoch seconds: %llu\n", (unsigned long long)epoch);
+	printf("epoch(7/1/1970 UTC): %zu\n", epoch);
+
+
+	struct tm *local = localtime(&epoch);
+	printf("localtime: %s", asctime(local));
+	time_t t_lt = timelocal(local);
+	printf("time_t from localtime: %zu\n", t_lt);
+
+	struct tm *gmt = gmtime(&epoch);
+	printf("gmtime: %s", asctime(gmt));
+	time_t t_gm = timegm(gmt);
+	printf("time_t from gmtime: %zu\n", t_gm);
+
+	printf("ctime: %s", ctime(&epoch));
+
+	time_t end = epoch + 60*60*24;
+	double d = difftime(end, epoch);
+	printf("difftime(%zu, %zu) = %lf\n", end, epoch, d);
 
 	return 0;
 }
