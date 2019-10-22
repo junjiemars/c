57cb2c869031141a906687fb64567580a46cec70
南山竹 <junjiemars@gmail.com>
2019-10-22 23:37:34 +0800

    posix_memalign and aligned_alloc (iso)

--- src/memory/alloc/align.c
+++ src/memory/alloc/align.c
@@ -1,29 +1,30 @@
 #include <_memory_.h>
 
-#if NM_HAVE_MEMALIGN_FN
-#  if (GCC)
-#    include <malloc.h>
-#    define _malign_ memalign
-#  endif
-#elif NM_HAVE_POSIX_MEMALIGN_FN
-#  if (GCC)
-#    include <stdlib.h>
-#    define _malign_ posix_memalign
-#  endif
-#endif
 
+#if (DARWIN || LINUX)
+#  include <stdlib.h>
+#elif (WINNT)
+#  include <malloc.h>
+#endif
 
 int
 main(int argc, const char *argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
-#if NM_HAVE_MEMALIGN_FN || NM_HAVE_MEMALIGN_FN
-	int *p = _malign_(4096, 4096);
+	int *p = 0;
+
+#if NM_HAVE_ALIGNED_ALLOC_FN
+	p = aligned_alloc(1024, 4096);
+
+#elif NM_HAVE_POSIX_MEMALIGN_FN
+	posix_memalign((void **)&p, 1024, 4096);
+
+#endif
+
 	if (p) {
 		free(p);
 	}
-#endif
 
 	return 0;
 }
