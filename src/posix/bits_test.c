7b080e301814518233d4b19e87aaebd503e3f597
南山竹 <junjiemars@gmail.com>
2019-12-22 20:27:03 +0800

    bit8_pre

--- src/posix/bits_test.c
+++ src/posix/bits_test.c
@@ -9,13 +9,14 @@ main(int argc, char **argv) {
 	_unused_(argc);
 	_unused_(argv);
 
-#ifdef BIT_PRE
-#  undef  BIT_PRE
-#  define BIT_PRE "2#"
+#ifdef BIT8_PRE
+#  undef  BIT8_PRE
+#  define BIT8_PRE "2#"
 #endif
 
 	printf("0x%x: " BIT8_FMT "\n", 0x12, BIT8(0x12));
 	printf("0x%x: " BIT16_FMT "\n", 0x1234, BIT16(0x1234));
+	printf("0x%x: " BIT32_FMT "\n", 0x12345678, BIT32(0x12345678));
 	
 	return 0;
 }
