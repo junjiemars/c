ffe6888b8a8be3d73ec41aef19818ca0170ae1a8
南山竹 <junjiemars@gmail.com>
2020-03-22 20:10:23 +0800

    format

--- src/cpu/id/id.c
+++ src/cpu/id/id.c
@@ -13,64 +13,64 @@ check_cpuid(uint32_t fn, uint32_t *buf) {
 
 #if ( MSVC )
 
-	__cpuid((int32_t*) buf, fn);
-	return 1;
+  __cpuid((int32_t*) buf, fn);
+  return 1;
 
 #elif ( __amd64__ ) || ( __x86_64__ )
 
-	uint32_t eax, ebx, ecx, edx;
-	__asm__ (
-		  "cpuid"
-			: "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
-			: "a" (fn) );
+  uint32_t eax, ebx, ecx, edx;
+  __asm__ (
+           "cpuid"
+           : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+           : "a" (fn) );
 
-	buf[0] = eax;
-	buf[1] = ebx;
-	buf[2] = ecx;
-	buf[3] = edx;
-	return 1;
+  buf[0] = eax;
+  buf[1] = ebx;
+  buf[2] = ecx;
+  buf[3] = edx;
+  return 1;
 
 #else
 	
-	_unused_(fn);
-	_unused_(buf);
-	return 0;
+  _unused_(fn);
+  _unused_(buf);
+  return 0;
 	
 #endif
 }
 
 static char *
 cpu_vendor(const uint32_t *cpuid, char *vendor) {
-	const size_t w = sizeof(uint32_t);
-	char *p = (char *) vendor;
+  const size_t w = sizeof(uint32_t);
+  char *p = (char *) vendor;
 
-	/* ebx, edx, ecx */
-	strncpy(p, (const char *) &cpuid[1], w);
-	strncpy(p += w, (const char *) &cpuid[3], w);
-	strncpy(p += w, (const char *) &cpuid[2], w);
+  /* ebx, edx, ecx */
+  strncpy(p, (const char *) &cpuid[1], w);
+  strncpy(p += w, (const char *) &cpuid[3], w);
+  strncpy(p += w, (const char *) &cpuid[2], w);
 
-	return vendor;
+  return vendor;
 }
 
 static void
 test_cpu_vendor(void) {
-	uint32_t buf[5];
-	memset(buf, 0, sizeof(buf));
+  uint32_t buf[5];
+  memset(buf, 0, sizeof(buf));
 	
-	if (check_cpuid(0, buf)) {
-		char vendor[sizeof(uint32_t)*3 + 1];
-		printf("vendor: %s\n", cpu_vendor(buf, vendor));
-	}
+  if (check_cpuid(0, buf)) {
+    char vendor[sizeof(uint32_t)*3 + 1];
+    printf("vendor: %s\n", cpu_vendor(buf, vendor));
+  }
 }
 
 
 int
 main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
+  _unused_(argc);
+  _unused_(argv);
 
-	/* vendor */
-	test_cpu_vendor();
+  /* vendor */
+  test_cpu_vendor();
 
-	return 0;
+  return 0;
 }
