e2cd48cabbe79f20ea1df038a411aec96b1c0e1a
南山竹 <junjiemars@gmail.com>
2020-04-29 10:05:07 +0800

    jna

---
+++ src/ffi/jna/Hello.java
@@ -0,0 +1,27 @@
+// package com.sun.jna.examples;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+/** Simple example of JNA interface mapping and usage. */
+public class Hello {
+
+    // This is the standard, stable way of mapping, which supports extensive
+    // customization and mapping of Java to native types.
+
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary)
+            Native.load((Platform.isWindows() ? "msvcrt" : "c"),
+                                CLibrary.class);
+
+        void printf(String format, Object... args);
+    }
+
+    public static void main(String[] args) {
+        CLibrary.INSTANCE.printf("Hello, World\n");
+        for (int i=0;i < args.length;i++) {
+            CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
+        }
+    }
+}
