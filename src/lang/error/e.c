043a3f9531f225aa03a558438ed255154e23d9d0
南山竹 <junjiemars@gmail.com>
2020-05-01 10:49:42 +0800

    strerror_r

--- src/lang/error/e.c
+++ src/lang/error/e.c
@@ -5,20 +5,27 @@
 
 int
 main(int argc, char* argv[]) {
-	if (argc < 2) {
-		printf("input a filename\n");
-		return 0;
-	}
+  if (argc < 2) {
+    printf("input a filename\n");
+    return 0;
+  }
 
-	FILE* out = 0;
-
-	out = fopen(argv[1], "r");
+  FILE* out = 0;
+  out = fopen(argv[1], "r");
   if (0 == out) {
     int e = errno;
   	if (e) {
-			perror(argv[1]);
-  		fprintf(stderr, "open %s failed, caused by: %s\n",
+      perror(argv[1]);
+      fprintf(stderr, "#open %s failed, caused by: %s\n",
               argv[1], strerror(e));
+      
+      char buf[512];
+      int r = strerror_r(e, buf, sizeof(buf));
+      if (0 == r) {
+        fprintf(stderr, "#r\"%s\"\n", buf);
+      } else {
+        fprintf(stderr, "#strerror_r failed: %s\n", strerror(r));
+      }
   	}
   }
 
@@ -26,5 +33,5 @@ main(int argc, char* argv[]) {
     fclose(out);
   }
 
-	return 0;
+  return 0;
 }
