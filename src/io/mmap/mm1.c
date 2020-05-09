db5119b4e5219a9d1c19ec17d0e83a7cf64e2ff7
南山竹 <junjiemars@gmail.com>
2020-05-09 11:30:51 +0800

    refine test_read fn on Linux

--- src/io/mmap/mm1.c
+++ src/io/mmap/mm1.c
@@ -1,5 +1,4 @@
 #include <_io_.h>
-#include <stdio.h>
 
 #if DARWIN
 #  include <fcntl.h>
@@ -16,41 +15,55 @@
 #  include <sys/mman.h>
 #endif
 
+#include <stdio.h>
+#include <string.h>
+
+void
+hex_out(const char *ss) {
+  unsigned char c;
+  while (0 != (c = *ss++)) {
+    fprintf(stdout, "0x%02x ", c);
+  }
+  fprintf(stdout, "\n");
+}
 
 #if NM_HAVE_OPEN_FN && NM_HAVE_MMAP_FN
 void
-read_mmapped(const char *file, size_t len) {
+test_read(const char *file, size_t len) {
   int fd = open(file, O_RDONLY);
   if (-1 == fd) {
     perror("!panic");
     return;
   }
 
-  char *m = mmap(0, len, PROT_READ, MAP_FILE, fd, 0);
-  if (!m) {
-    perror("!panic");
+  char *m = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (MAP_FAILED == m) {
+    perror("!panic, mmap failed, caused by");
     goto clean_exit;
   }
 
   char buf[16];
-  if (-1 == read(fd, buf, sizeof(buf)-1)) {
-    perror("!panic");
-    goto clean_exit;
-  }
+  size_t offset = 0;
+  memcpy(buf, m+offset, sizeof(buf)-1);
 
-  fprintf(stdout, "read from %s\n====\n", file);
-  fprintf(stdout, "%s\n", buf);
+  fprintf(stdout, "read from %s@%zu\n====\n", file, offset);
+  hex_out(buf);
 
+  offset = 16;
+  memcpy(buf, m+offset, sizeof(buf)-1);
+  fprintf(stdout, "read from %s@%zu\n====\n", file, offset);
+  hex_out(buf);
+  
  clean_exit:
-  munmap(m, len);
+  if (munmap(m, len)) {
+    perror("!panic, munmap failed, caused by");
+  }
   close(fd);
 }
 #endif
 
 int
 main(int argc, char **argv) {
-  _unused_(argv);
-  
   if (argc < 3) {
     fprintf(stdout, "where the input file location and size\n");
     return 0;
@@ -64,7 +77,7 @@ main(int argc, char **argv) {
   }
   
 #if NM_HAVE_OPEN_FN
-  read_mmapped(file, len);
+  test_read(file, len);
 #endif
   
   return 0;
