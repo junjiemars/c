4c79593cb51e1114a2a1241cfb32b5c276d431f9
南山竹 <junjiemars@gmail.com>
2020-05-16 22:52:03 +0800

    CURLOPT_WRITEDATA

--- src/curl/w.c
+++ src/curl/w.c
@@ -3,60 +3,74 @@
 #include <stdlib.h>
 
 void
-test_stdout() {
+test_write(const char *outfile) {
   CURL *curl = 0;
   CURLcode code = 0;
   char *errbuf = 0;
-
-  curl_version_info_data *ver = curl_version_info(CURLVERSION_NOW);
-  if (ver) {
-    fprintf(stdout, "curl current version: %s\n----------\n", ver->version);
-  }
+  FILE *out = 0;
   
   curl = curl_easy_init();
   if (!curl) {
     fprintf(stderr, "!panic, init failed\n");
+    return;
+  }
+
+  errbuf = malloc(CURL_ERROR_SIZE);
+  if (0 == errbuf) {
+    perror("!panic, alloc memory for errbuf failed");
     goto clean_exit;
   }
+
+  code = curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+  if (code) {
+    fprintf(stderr, "setopt CURLOPT_ERRORBUFFER failed\n");
+    goto clean_exit;
+  }
+  
   code = curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se");
   if (code) {
     fprintf(stderr, "!panic, setopt CURLOPT_URL failed\n");
     goto clean_exit;
   }
   
-  errbuf = malloc(CURL_ERROR_SIZE);
-  if (0 == errbuf) {
-    perror("!panic, alloc memory for errbuf failed");
+  out = fopen(outfile, "wb");
+  if (!out) {
+    perror("!panic, fopen failed");
     goto clean_exit;
   }
-  code = curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+
+  code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
   if (code) {
-    fprintf(stderr, "!panic, setopt CURLOPT_ERRORBUFFER failed\n");
+    fprintf(stderr, "!panic, setopt CURLOPT_WRITEFUNCTION failed\n");
     goto clean_exit;
   }
-  
+
   code = curl_easy_perform(curl);
   if (code) {
-    fprintf(stderr, "!panic, perform failed caused by: %s\n", errbuf);
+    fprintf(stderr, "%s\n", errbuf);
   }
 
  clean_exit:
   curl_easy_cleanup(curl);
-  free(errbuf);
+  fclose(out);
 }
 
 int
 main(int argc, char **argv) {
-  _unused_(argc);
-  _unused_(argv);
+  if (argc < 2) {
+    fprintf(stderr, "where the output file located\n");
+    return 0;
+  }
   
   CURLcode code = curl_global_init(CURL_GLOBAL_DEFAULT);
   if (CURLE_OK != code) {
     perror("!panic");
     return code;
   }
+
+  const char *outfile = argv[1];
   
-  test_stdout();
+  test_write(outfile);
   
   return 0;
 }
