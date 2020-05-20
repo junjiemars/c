1a81c3109c547a561d68d26445c3682280e4c1a9
南山竹 <junjiemars@gmail.com>
2020-05-20 22:48:16 +0800

    64bits

--- src/posix/bits/bits_test.c
+++ src/posix/bits/bits_test.c
@@ -1,8 +1,10 @@
 #include <nore.h>
-#include <bits.h>
+#include "bits.h"
 #include <stdio.h>
 
-#define _unused_(x) ((void)(x))
+#if !defined(_unused_)
+#  define _unused_(x) ((void)(x))
+#endif
 
 int
 main(int argc, char **argv) {
@@ -17,6 +19,11 @@ main(int argc, char **argv) {
 	printf("0x%x: " BIT8_FMT "\n", 0x12, BIT8(0x12));
 	printf("0x%x: " BIT16_FMT "\n", 0x1234, BIT16(0x1234));
 	printf("0x%x: " BIT32_FMT "\n", 0x12345678, BIT32(0x12345678));
+  
+#if defined(NM_CPU_CACHE_LINE) && (NM_CPU_CACHE_LINE == 64)
+  printf("0x%lx: " BIT64_FMT "\n",
+         0x1234567890abcdef, BIT64(0x1234567890abcdef));
+#endif
 	
 	return 0;
 }
