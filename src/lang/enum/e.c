8bbba1b70078a430750bf63fe11308ad0baafad0
南山竹 <junjiemars@gmail.com>
2020-05-31 23:22:11 +0800

    refine lang/enum

--- src/lang/enum/e.c
+++ src/lang/enum/e.c
@@ -1,21 +1,6 @@
 #include <_lang_.h>
 #include <stdio.h>
 
-
-#if GCC
-/* error: format ‘%zi’ expects argument of type ‘signed size_t’, but
-	 argument 2 has type ‘int’ [-Werror=format=] */
-#  pragma GCC diagnostic ignored "-Wformat"
-#elif CLANG
-/* error: format specifies type 'ssize_t' (aka 'long') but the
-	 argument has type 'int' [-Werror,-Wformat] */
-#  pragma clang diagnostic ignored "-Wformat"
-#elif MSVC
-/* C4477: 'printf' : format string '%zi' requires an argument of type
-	 'unsigned __int64', but variadic argument 1 has type 'int' */
-#  pragma warning(disable : 4477)
-#endif
-
 enum { RED, GREEN, BLUE, };
 
 enum order { first, second, third, };
@@ -23,11 +8,13 @@ enum order { first, second, third, };
 typedef enum { one, two, three } number_t;
 
 	
-#define CORVID_MAP(XX)                                                                         \
-	XX(101, BEGIN,    "BEGIN")                                                                   \
-	XX(101, MAGPIE,   "Including the black and white Eurasian magpie")                           \
-	XX(102, RAVEN,    "A raven is one of several larger-bodied species of the genus Corvus")     \
-	XX(103, JAY,      "Jays are several species of medium-sized, usually colorful and noisy")    \
+#define CORVID_MAP(XX)                                                \
+	XX(101, BEGIN,    "BEGIN")                                          \
+	XX(101, MAGPIE,   "Including the black and white Eurasian magpie")  \
+	XX(102, RAVEN,    "A raven is one of several larger-bodied "        \
+                    "species of the genus Corvus")                    \
+	XX(103, JAY,      "Jays are several species of medium-sized, "      \
+                    "usually colorful and noisy")                     \
 	XX(104, END,      "END")
 
 enum corvid { 
@@ -37,19 +24,22 @@ enum corvid {
 };
 
 int
-main(int argc, const char* argv[]) {
+main(int argc, const char **argv) {
 	_unused_(argc);
 	_unused_(argv);
 
-	printf("RED = %zi, sizeof(RED) = %zu\n", RED, sizeof(RED));
+	printf("RED = %i, sizeof(RED) = %zu\n", RED, sizeof(RED));
+	printf("GREEN = %i, sizeof(GREEN) = %zu\n", GREEN, sizeof(GREEN));
+	printf("BLUE = %i, sizeof(BLUE) = %zu\n", BLUE, sizeof(BLUE));
 	
 	printf("sizeof(enum order) = %zu\n", sizeof(enum order));
 
 	number_t n = three;
-	printf("three = %zi, sizeof(number_t) = %zu\n", n, sizeof(n));
+	printf("three = %i, sizeof(number_t) = %zu\n", n, sizeof(n));
 	
 	for (int i = CORVID_BEGIN; i < CORVID_END; i++) {
 		printf("corvid#: %i\n", i);
 	}
 
+  return 0;
 }
