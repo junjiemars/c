bad71b164a0cd77ea3cd56b5c651dc5f65892ab2
南山竹 <junjiemars@gmail.com>
2020-06-01 23:13:27 +0800

    refine src/memory

--- src/memory/alloc/a.c
+++ src/memory/alloc/a.c
@@ -4,19 +4,23 @@
 #include <assert.h>
 #include <string.h>
 
-#if MSVC
-#  include <malloc.h>
-#else
-#  include <alloca.h>
+#if (NM_HAVE_ALLOCA_FN)
+#  if MSVC
+#    include <malloc.h>
+#  else
+#    include <alloca.h>
+#  endif
 #endif
 
-void foo() {
+#if (NM_HAVE_ALLOCA_FN)
+void foo(void) {
 	int *pi = alloca(sizeof(int) * 10);
 	assert((0 != pi) && strerror(errno));
 	pi[0] = 0x11223344;
 	/* depend on implementation */
 	/* assert((0 == pi[1]) && "filled with non-zero"); */
 }
+#endif
 
 int
 main(int argc, char **argv) {
