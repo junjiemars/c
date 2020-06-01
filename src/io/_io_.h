915b5488ac3c36980ce9cf78d879e46a6f3c1880
南山竹 <junjiemars@gmail.com>
2020-06-01 21:54:11 +0800

    refine src/io

--- src/io/_io_.h
+++ src/io/_io_.h
@@ -4,21 +4,6 @@
 #include <nore.h>
 #include "norstd.h"
 
-#define _unused_(x) (void)(x)
-
-/* #if MSVC */
-/* #  pragma warning(disable:4996) /\* _CRT_SECURE_NO_WARNINGS  *\/ */
-/* #endif */
-
-/* #if CLANG */
-/* #  pragma clang diagnostic ignored "-Wdeprecated-declarations" */
-/* #endif */
-
-/* #if GCC */
-/* #  pragma GCC diagnostic ignored "-Wdeprecated-declarations" */
-/* /\* #  include <features.h> *\/ */
-/* #endif */
-
 
 void hex_out(const char *ss);
 
