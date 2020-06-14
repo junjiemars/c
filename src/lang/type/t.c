f37ed1c9b8537421003093007685ad7332699165
南山竹 <junjiemars@gmail.com>
2020-06-14 17:06:43 +0800

    lang: printf types

--- src/lang/type/t.c
+++ src/lang/type/t.c
@@ -1,67 +1,81 @@
-#include <_lang_.h>
+#include "_lang_.h"
 #include <stdio.h>
 #include <stddef.h>
-/* #include <limits.h> */
-/* #include <inttypes.h> /\* C99 *\/ */
 
-#if GCC
-/* error: dereferencing type-punned pointer will break strict-aliasing
-	 rules [-Werror=strict-aliasing] */
-#  pragma GCC diagnostic ignored "-Wstrict-aliasing"
-/* format ‘%zi’ expects argument of type ‘signed size_t’, but argument
-	 3 has type ‘int’ [-Werror=format=] */
-#  pragma GCC diagnostic ignored "-Wformat"
-#  include <sys/types.h>
-#elif CLANG
-#  pragma clang diagnostic ignored "-Wformat"
-#elif MSVC
-/* warning C4477: 'printf' : format string '%zi' requires an argument
-	 of type 'unsigned __int64', but variadic argument 2 has type
-	 'int' */
-#  pragma warning(disable : 4477)
-#  include <BaseTsd.h>
+
+#if MSVC
+#include <BaseTsd.h>
 typedef SSIZE_T ssize_t;
 #endif
 
 
 void 
-integer_type() {
+test_integer_type(void) {
 	char c = 'a';
 	short s = 0x1122;
   int i = 0x11223344;
-	long l = 0x11223344;
-	long long ll = 0x11223344aabbccdd;
+	long l = 0x11223344aabbccddL;
+	long long ll = 0x11223344aabbccddLL;
 
-	printf("char c/%zu = %zi, 0x%zx\n", 
-				 sizeof(c), c, *(unsigned char*)&c);
+	printf("%20s=%02zu, %020i, 0x%016x\n", 
+				 "sizeof(char)", sizeof(c), c, *(unsigned char*)&c);
 	
-	printf("short s/%zu = %zi, 0x%zx\n", 
-				 sizeof(s), s, *(unsigned short*)&s);
+	printf("%20s=%02zu, %020i, 0x%016x\n",
+				 "sizeof(short)", sizeof(s), s, *(unsigned short*)&s);
 	
-	printf("int i/%zu = %zi, 0x%zx\n", 
-				 sizeof(i), i, *(unsigned int*)&i);
+	printf("%20s=%02zu, %020i, 0x%016x\n",
+				 "sizeof(int)", sizeof(i), i, *(unsigned int*)&i);
 	
-	printf("long l/%zu = %zi, 0x%zx\n", 
-				 sizeof(l), l, *(unsigned long*)&l);
+	printf("%20s=%02zu, %020li, 0x%016lx\n",
+				 "sizeof(long)", sizeof(l), l, *(unsigned long*)&l);
 	
-	printf("long long ll/%zu = %zi, 0x%zx\n",
-				 sizeof(ll), ll, *(unsigned long long*)&ll);
+	printf("%20s=%02zu, %020lli, 0x%016llx\n",
+				 "sizeof(long long)", sizeof(ll), ll, *(unsigned long long*)&ll);
 }
 
-void 
-float_type() {
+void
+test_float_type(void) {
   float f = 3.14f;
-  double d = 2.7182L;
-	long double ld = 2.1782;
+  double d = 2.7182; /* non suffix means double */
+	long double ld = 2.718281828459L;
 
-	printf("float f/%zu = %f, 0x%zx\n", 
-				 sizeof(f), f, *(unsigned int*)&f);
+	printf("%20s=%02zu, %020f, 0x%016x\n",
+				 "sizeof(float)", sizeof(f), f, *(unsigned int*)&f);
 	
-	printf("double d/%zu = %lf, 0x%zx\n", 
-				 sizeof(d), d, *(unsigned long*)&d);
+	printf("%20s=%02zu, %020lf, 0x%16lx\n",
+				 "sizeof(double)", sizeof(d), d, *(unsigned long*)&d);
 	
-	printf("long double ld/%zu = %lf, 0x%zx\n",
-				 sizeof(ld), ld, *(unsigned long long*)&ld);
+	printf("%20s=%02zu, %020Lf, 0x%16llx\n",
+				 "sizeof(long double)", sizeof(ld), ld, *(unsigned long long*)&ld);
+}
+
+void
+test_size_type(void) {
+  size_t st = 0x11223344;
+  ssize_t sst = 0x11223344;
+  
+  printf("%20s=%02zu, %020zu, 0x%016lx\n",
+         "sizeof(size_t)", sizeof(st), st, *(unsigned long*)&st);
+  printf("%20s=%02zu, %020zi, 0x%016lx\n",
+         "sizeof(ssize_t)", sizeof(sst), sst, *(unsigned long*)&sst);
+}
+
+void
+test_pointer_type(void) {
+	char s[] = "lang/type";
+	ptrdiff_t p20 = &s[2]-&s[0];
+	ptrdiff_t p02 = &s[0]-&s[2];
+
+	printf("%20s=%02zu, %20s, %p\n",
+         "sizeof(char*0)", sizeof(&s[0]), s, (unsigned long*)&s[0]);
+	printf("%20s=%02zu, %20s, %p\n",
+         "sizeof(char*1)", sizeof(&s[1]), s, (unsigned long*)&s[1]);
+	printf("%20s=%02zu, %20s, %p\n",
+         "sizeof(char*2)", sizeof(&s[2]), s, (unsigned long*)&s[2]);
+	printf("%20s=%02zu, %020td\n",
+         "sizeof(ptrdiff_t)", sizeof(p20), p20);
+	printf("%20s=%02zu, %020td\n",
+         "sizeof(ptrdiff_t)", sizeof(p02), p02);
 }
 
 
@@ -70,29 +84,19 @@ main(int argc, const char *argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
-	printf("\ninteger basic type\n");
+	printf("\ninteger type\n");
 	printf("--------------------\n");
-  integer_type();
+  test_integer_type();
 
 	printf("\nfloat type\n");
 	printf("------------\n");
-  float_type();
+  test_float_type();
 
-	printf("\nsize_t\n");
-	printf("------------\n");	
-	printf("sizeof(size_t) = %zu\n", sizeof(size_t));
-	printf("sizeof(char) = %zu\n", sizeof(char));
-
-	printf("\nssize_t\n");
-	printf("------------\n");	
-	printf("sizeof(ssize_t) = %zu\n", sizeof(ssize_t));
-
-	printf("\nptrdiff_t\n");
+	printf("\nsize type\n");
 	printf("------------\n");
-	char s[] = "lang/type";
-	ptrdiff_t p20 = &s[2]-&s[0];
-	ptrdiff_t p02 = &s[0]-&s[2];
-	printf("char s[%zu] = \"%s\"\n", sizeof(s)/sizeof(char), s);
-	printf("ptrdiff_t of (&s[2]-&s[0]) is %td\n", p20);
-	printf("ptrdiff_t of (&s[0]-&s[1]) is %td\n", p02);
+  test_size_type();
+
+	printf("\npointer type\n");
+	printf("------------\n");
+  test_pointer_type();
 }
