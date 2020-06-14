58034d82b0576cf6426b548928f0bac00e538f84
南山竹 <junjiemars@gmail.com>
2020-06-14 23:01:05 +0800

    lang: typeof

--- src/lang/typeof/to.c
+++ src/lang/typeof/to.c
@@ -7,26 +7,43 @@
 		 a = b;                                     \
      b = _swap_1_;                              \
 	 } while (0)
+#else
+#  include <string.h>
+#  define swap(a, b, w) do {                    \
+     unsigned char _swap_2_[w];                 \
+		 memcpy(_swap_2_, a, w);                    \
+     memmov(a, b, w);                           \
+     memcpy(b, _swap_2_, w);                    \
+	 } while (0)
 #endif
+
+void
+test_swap(void) {
+	int i1 = 0x1122, i2 = 0x3344;
+	printf("before swap: i1 = 0x%x, i2 = 0x%x\n", i1, i2);
+#if NM_HAVE_TYPEOF  
+	swap(i1, i2);
+#else
+  swap(&i1, &i2, sizeof(int*));
+#endif
+	printf("after swap:  i1 = 0x%x, i2 = 0x%x\n", i1, i2);
+
+	char *s1 = "abc", *s2 = "123";
+	printf("before swap: s1 = %s, s2 = %s\n", s1, s2);
+#if NM_HAVE_TYPEOF
+	swap(s1, s2);
+#else
+  swap(s1, s2, sizeof(char*));
+#endif
+	printf("after swap:  s1 = %s, s2 = %s\n", s1, s2);
+  
+}
 
 int
 main(int argc, char **argv) {
 	_unused_(argc);
 	_unused_(argv);
 
-#if NM_HAVE_TYPEOF
-	int i1 = 0x1122, i2 = 0x3344;
-	printf("before swap: i1 = 0x%x, i2 = 0x%x\n", i1, i2);
-	swap(i1, i2);
-	printf("after swap:  i1 = 0x%x, i2 = 0x%x\n", i1, i2);
-
-	char *s1 = "abc", *s2 = "123";
-	printf("before swap: s1 = %s, s2 = %s\n", s1, s2);
-	swap(s1, s2);
-	printf("after swap:  s1 = %s, s2 = %s\n", s1, s2);
-#else
-  printf("does not support 'typeof' operator\n");
-#endif
-
+  test_swap();
 	return 0;
 }
