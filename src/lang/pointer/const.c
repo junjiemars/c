b9eea1e6f539e4283f53bd16aa9a85cb81316dda
南山竹 <junjiemars@gmail.com>
2020-06-25 21:49:36 +0800

    const keyword

--- src/lang/pointer/const.c
+++ src/lang/pointer/const.c
@@ -1,46 +1,127 @@
 #include <_lang_.h>
 #include <stdio.h>
 
+void
+test_ptr_to_const_obj(void) {
+  int v1 = 0x11223344;
+	const int *p1 = &v1;
+
+  /* read-only variable is not assignable */
+  /* *p1 += 1; */
+
+	**(int**)&p1 += 1;
+
+	printf("%32s: p1@%p = 0x%x\n", "const int *p1", p1, *p1);
+}
+
+void
+test_const_ptr_to_obj(void) {
+  int v1 = 0x11223344;
+  int *const p1 = &v1;
+
+  /* object is not const */
+  *p1 += 1;
+
+  /* cannot assign to variable 'p1' with const-qualified type 'int *const' */
+  /* int v2 = 0x44332211; */
+  /* p1 = &v2; */
+  
+  **(int**)&p1 += 1;
+
+  printf("%32s: p1@%p = 0x%x\n", "int *const p1", p1, *p1);
+}
+
+void
+test_const_ptr_to_const_obj(void) {
+  int v1 = 0x11223344;
+  const int *const p1 = &v1;
+
+  **(int**)&p1 += 1;
+  
+  printf("%32s: p1@%p = 0x%x\n", "const int *const p1", p1, *p1);
+}
+
+void
+test_ptr_to_ptr_to_const_obj(void) {
+  int v1 = 0x1122;
+  int *p1 = &v1;
+  const int **p2 = (const int **)&p1;
+
+  int v2 = 0x3344;
+  int *p3 = &v2;
+  p2 = (const int **)&p3;
+
+  /* read-only variable is not assignable */
+  /* **p2 += 1; */
+
+  printf("%32s: p2@%p = 0x%x\n", "const int **p2", p2, **p2);
+}
+
+void
+test_ptr_to_const_ptr_to_const_obj(void) {
+  int v1 = 0x1122;
+  int *p1 = &v1;
+  const int *const *p2 = (const int *const *)&p1;
+
+  int v2 = 0x3344;
+  int *p3 = &v2;
+  p2 = (const int *const *)&p3;
+
+  /* read-only variable is not assignable */
+  /* *p2 = p3; */
+  /* **p2 += 1; */
+
+  printf("%32s: p2@%p = 0x%x\n", "const int *const *p2", p2, **p2);
+}
+
+void
+test_const_ptr_to_ptr_to_const_obj(void) {
+  int v1 = 0x1122;
+  int *p1 = &v1;
+  const int **const p2 = (const int **const)&p1;
+
+  int v2 = 0x3344;
+  int *p3 = &v2;
+  *p2 = p3;
+
+  /* cannot assign to variable 'p2' with const-qualified type */
+  /*     'const int **const' */
+  /* p2 = (const int **const)&p3; */
+
+  /* read-only variable is not assignable */
+  /* **p2 += 1; */
+
+  printf("%32s: p2@%p = 0x%x\n", "const int **const p2", p2, **p2);
+}
+
+void
+test_const_ptr_to_const_ptr_to_obj(void) {
+  int v1 = 0x1122;
+  int *p1 = &v1;
+  int *const *const p2 = (int *const *const)&p1;
+
+  **p2 += 1;
+
+  /* cannot assign to variable 'p2' with const-qualified */
+  /*     type 'int *const *const' */
+  /* int v2 = 0x3344; */
+  /* int *p3 = &v2; */
+  /* p2 = (int *const *const)&p3; */
+
+  printf("%32s: p2@%p = 0x%x\n", "int *const *const p2", p2, **p2);
+}
 
 int 
-main(){
-
+main(void){
 	printf("----------\n");
-	int v1 = 0x11223344, v2 = 0x44332211;
-	const int *p1 = &v1;
-	**(int**)&p1 = v2+0x1;
-	printf("const int* p1@%p = 0x%x\n", p1, *p1);
 
-	int *const p2 = &v1;
-	*(int**)&p2 = &v2;
-	printf("int* const p2@%p = 0x%x\n", p2, *p2);
+  test_ptr_to_const_obj();
+  test_const_ptr_to_obj();
+  test_const_ptr_to_const_obj();
+  test_ptr_to_ptr_to_const_obj();
+  test_ptr_to_const_ptr_to_const_obj();
+  test_const_ptr_to_ptr_to_const_obj();
+  test_const_ptr_to_const_ptr_to_obj();
 
-	const int *const p3 = &v1;
-	*(int**)&p3 = &v2;
-	printf("const int* const p3@%p = 0x%x\n", p3, *p3);
-	**(int**)&p3 = *p3+0x1;
-	printf("const int* const p3@%p = 0x%x\n", p3, *p3);
-
-	int *p41 = &v1;
-	int* *p4 = &p41;
-	printf("int** p4@%p = 0x%x\n", *p4, **p4);
-	
-	const int** p5 = &p1;
-	printf("const int** p5@%p = 0x%x\n", *p5, **p5);	
-
-	int *const *p6 = &p2;
-	printf("int* const*: p6@%p = 0x0x%x\n", *p6, **p6);
-
-	int **const p7 = &p41;
-	printf("int** const p7@%p = 0x0x%x\n", *p7, **p7);
-
-	const int *const *p8 = &p1;
-	printf("const int* const* p8@%p = 0x%x\n", *p8, **p8);
-
-	const int **const p9 = &p1;
-	printf("const int** const p9@%p = 0x%x\n", *p9, **p9);
-
-	int *const *const p0 = &p41;
-	printf("int* const* const p0@%p = 0x%x\n", *p0, **p0);
-
+  return 0;
 }
