518684e80f1813f3a4e6af70af92782dac46f424
南山竹 <junjiemars@gmail.com>
2020-06-25 22:27:49 +0800

    linear search

--- src/algo/search/linear.c
+++ src/algo/search/linear.c
@@ -3,26 +3,17 @@
 long
 linear_search(void *what, void *base, size_t nel, size_t width,
 							int (*comp)(const void *, const void *)) {
-	assert(0 != what && "argument:what should not be null");
-	assert(0 != base && "argument:base should not be null");
-	assert(0 != comp && "argument:comp should not be null");
-	if (0 == what || 0 == base || 0 == comp) {
-		return -1;
-	}
 	for (size_t i = 0; i < nel; i++) {
 		if (0 == comp((char*)what, (char*)base + i * width)) {
 			return (long)i;
 			break;
 		}
 	}
-	return -2;
+	return -1;
 }
 
-int
-main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
-
+void
+test_linear_search_int(void) {
 	int found;
 	int a1[] = { 0x3, 0x5, 0x4, 0x1, 0x2 };
 	int what = 0x1;
@@ -31,12 +22,37 @@ main(int argc, char **argv) {
   found = linear_search(&what, a1, sizeof(a1)/sizeof(*a1), sizeof(*a1), comp_int);
 	printf("found: [%i]\n", found);
 
+  what = 0x6;
+	printf("linear search for %i\n----------\n", what);
+	list_array(a1, sizeof(a1)/sizeof(*a1), sizeof(*a1), print_int);
+  found = linear_search(&what, a1, sizeof(a1)/sizeof(*a1), sizeof(*a1), comp_int);
+	printf("found: [%i]\n", found);
+}
+
+void
+test_linear_search_str(void) {
+  int found;
 	char *s1[] = { "a", "ccc", "bb", "dddd" };
 	char *ss1 = "dddd";
 	printf("linear search for %s\n----------\n", ss1);
 	list_array(s1, sizeof(s1)/sizeof(*s1), sizeof(*s1), print_str);
   found = linear_search(&ss1, s1, sizeof(s1)/sizeof(*s1), sizeof(*s1), comp_str);
 	printf("found: [%i]\n", found);
+
+  ss1 = "eeeee";
+	printf("linear search for %s\n----------\n", ss1);
+	list_array(s1, sizeof(s1)/sizeof(*s1), sizeof(*s1), print_str);
+  found = linear_search(&ss1, s1, sizeof(s1)/sizeof(*s1), sizeof(*s1), comp_str);
+	printf("found: [%i]\n", found);
+}
+
+int
+main(int argc, char **argv) {
+	_unused_(argc);
+	_unused_(argv);
+
+  test_linear_search_int();
+  test_linear_search_str();
 
 	return 0;
 }
