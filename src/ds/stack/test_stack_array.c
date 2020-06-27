f97fa4015ae786dab184f6e71f091d36b43fa880
南山竹 <junjiemars@gmail.com>
2020-06-27 11:29:06 +0800

    ds/stack: array, string

--- src/ds/stack/test_stack_array.c
+++ src/ds/stack/test_stack_array.c
@@ -3,6 +3,7 @@
 #include "stack.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 node_s*
 int_node_new(node_s *node, size_t n) {
@@ -16,6 +17,7 @@ int_node_new(node_s *node, size_t n) {
 
 void
 int_node_free(node_s *node) {
+  free(node->data);
   free(node);
 }
 
@@ -36,6 +38,42 @@ void*
 int_pop_val(stack_s *const stack, void *val) {
   int *top = (int*)stack->top - sizeof(int);
   *(int*)val = *(int*)stack->top;
+  return top;
+}
+
+node_s*
+str_node_new(node_s *node, size_t n) {
+  if (0 == node) {
+    node = malloc(sizeof(node_s));
+    node->data = 0;
+  }
+  node->data = realloc(node->data, sizeof(char**)*n);
+  return node;
+}
+
+void
+str_node_free(node_s *node) {
+  free(node->data);
+  free(node);
+}
+
+int
+str_stack_full(stack_s *const stack) {
+  size_t len = ((char**)stack->top - (char**)stack->node->data);
+  return sizeof(char**) * (stack->n - 1) == len;
+}
+
+void*
+str_push_val(stack_s *const stack, void *val) {
+  char **top = (char**)((char*)stack->top + sizeof(char*));
+  *top = *(char**)val;
+  return top;
+}
+
+void*
+str_pop_val(stack_s *const stack, void *val) {
+  *(char**)val = *(char**)stack->top;
+  char **top = (char**)((char*)stack->top - sizeof(char*));
   return top;
 }
 
@@ -72,10 +110,46 @@ test_stack_push_pop_int(void) {
   stack_free(s, int_node_free);
 }
 
+void
+test_stack_new_free_str() {
+  stack_s *s = stack_new(0, str_node_new, 4);
+  stack_free(s, str_node_free);
+}
+
+void
+test_stack_push_pop_str(void) {
+  stack_s *s = stack_new(0, str_node_new, 4);
+  char *ss[] = {"a", "bb", "ccc", "dddd"};
+
+  printf("stack str push/pop [%zu]\n", s->n);
+  printf("---------------------\n");
+  
+  for (size_t i = 0; i < sizeof(ss)/sizeof(ss[0]); i++) {
+    stack_push(s, &ss[i], str_stack_full, str_node_new, str_push_val);
+    printf("%8s %16s\n", "push", ss[i]);
+  }
+
+  printf("---------------------\n");
+  for (size_t i = 0; i < sizeof(ss)/sizeof(ss[0]); i++) {
+    char *buf = 0;
+    int empty = stack_pop(s, &buf, str_pop_val);
+    if (0 == empty) {
+      break;
+    }
+    printf("%8s %16s\n", "pop", buf);
+  }
+
+  stack_free(s, int_node_free);
+}
+
+
 int
 main(void) {
-  test_stack_new_free_int();
-  test_stack_push_pop_int();
+  /* test_stack_new_free_int(); */
+  /* test_stack_push_pop_int(); */
+  
+  /* test_stack_new_free_str(); */
+  test_stack_push_pop_str();
   
   return 0;
 }
