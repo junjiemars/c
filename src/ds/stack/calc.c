90e8155cafca38e0066bec581c64478b776da5d4
南山竹 <junjiemars@gmail.com>
2020-07-01 00:15:01 +0800

    ds/stack: need queue

--- src/ds/stack/calc.c
+++ src/ds/stack/calc.c
@@ -12,7 +12,7 @@
 #define _stdin_ 0
 static char _str_in_[MAX_EXPR];
 static int _str_in_i_ = 0;
-static char
+static int
 _getc_() {
   if (MAX_EXPR == _str_in_i_) {
     return EOF;
@@ -32,15 +32,27 @@ _ungetc_(int c, FILE* stream) {
 static char expr_buf[1024];
 
 int
-precedence(char c) {
+precedence(int c) {
   switch (c) {
-  case '(': return 0;
-  case '+':
-  case '-': return 3;
+  case '^': return 3;
   case '*':
-  case '/': return 5;
+  case '/': return 2;
+  case '+':
+  case '-': return 1;
   default: return 0;
   }
+}
+
+int
+isop(int c) {
+  switch (c) {
+  case '^':
+  case '*':
+  case '/':
+  case '+':
+  case '-': return 1;
+  default: return 0;
+  }    
 }
 
 char
@@ -58,38 +70,44 @@ token(void) {
   return EOF;
 }
 
-int
-postfix(char *const expr) {
-  int i = 0;
-  stack_s *s = stack_new(0, sizeof(char));
-  char t, c;
+void
+postfix(stack_s *const expr, char *const buf) {
+  stack_s *s = stack_new(0, 8, sizeof(int));
+  int i, t, c, v;
 
   while (EOF != (t = token())) {
     if (isdigit(t)) {
-      expr[i++] = t;
+      i = 0;
+      buf[i++] = t;
       while (isdigit(c = _getc_())) {
-        expr[i++] = c;
+        buf[i++] = c;
       }
+      buf[i] = 0;
       _ungetc_(c, _stdin_);
+      sscanf(&buf[0], "%i", &v);
+      stack_push(expr, &v);
     } else if ('(' == t) {
       stack_push(s, &t);
     } else if (')' == t) {
-      while ('(' != *(char*)(stack_pop(s, &c))) {
-        expr[i++] = c;
+      while ('(' != *(int*)(stack_pop(s, &c))) {
+        stack_push(expr, &c);
       }
     } else {
       while (!stack_empty(s)
              && precedence(*(char*)(stack_peek(s, &c))) >= precedence(t)) {
-        expr[i++] = *(char*)stack_pop(s, &c);
+        stack_pop(s, &c);
+        stack_push(expr, &c);
       }
       stack_push(s, &t);
     }
   }
 
   while (!stack_empty(s)) {
-    expr[i++] = *(char*)stack_pop(s, &c);
+    stack_pop(s, &c);
+    stack_push(expr, &c);
   }
-  return i;
+
+  stack_free(s);
 }
 
 int
@@ -109,12 +127,20 @@ main(int argc, char **argv) {
 
   printf("postfix:\n");
 
-  int n = postfix(expr_buf);
+  stack_s *expr = stack_new(0, 8, sizeof(int));
+  postfix(expr, expr_buf);
 
-  for (int i = 0; i < n; i++) {
-    printf("%c ", expr_buf[i]);
+  int v;
+  while (!stack_empty(expr)) {
+    stack_pop(expr, &v);
+    if (isop(v)) {
+      printf("%c ", v);
+    } else {
+      printf("%i ", v);
+    }
   }
   putchar('\n');
-  
+
+  stack_free(expr);
   return 0;
 }
