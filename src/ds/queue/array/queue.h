19a568edac0de267abdadf8620adb40825194466
南山竹 <junjiemars@gmail.com>
2020-07-01 22:48:11 +0800

    ds/queue: array

---
+++ src/ds/queue/array/queue.h
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct queue_s {
+  size_t n;
+  size_t size;
+  void *head;
+  void *tail;
+  void *data;
+} queue_s;
+
+
+static inline void*
+node_new(queue_s *const queue) {
+  return queue->data = realloc(queue->data, queue->n * queue->size);
+}
+
+static inline void
+node_free(queue_s *const queue) {
+  free(queue->data);
+}
+
+queue_s*
+queue_new(queue_s *queue, size_t n, size_t size) {
+  queue = calloc(1, sizeof(queue_s));
+  if (queue) {
+    queue->size = size;
+    queue->n = n;
+    node_new(queue);
+    queue->head = queue->tail = queue->data;
+  }
+  return queue;
+}
+
+void
+queue_free(queue_s *const queue) {
+  node_free(queue);
+  free(queue);  
+}
+
+int
+queue_empty(queue_s *const queue) {
+  return queue->head == queue->tail;
+}
+
+int
+queue_full(queue_s *const queue) {
+  size_t len = (char*)queue->tail - (char*)queue->data;
+  return queue->size * (queue->n - 1) == len;
+}
+
+void*
+queue_enq(queue_s *queue, void *val) {
+  if (queue_full(queue)) {
+    queue->n *= 2;
+    size_t offset = (char*)queue->tail - (char*)queue->data;
+    void *new_one = node_new(queue);
+    if (0 == new_one) {
+      return 0;
+    }
+    queue->tail = (char*)queue->data + offset;
+  }
+  memcpy(queue->tail, val, queue->size);
+  return queue->tail = (char*)queue->tail + queue->size;
+}
+
+void*
+queue_deq(queue_s *const queue, void *val) {
+  if (queue_empty(queue)) {
+    queue->head = queue->tail = queue->data;
+    return 0;
+  }
+  memcpy(val, queue->head, queue->size);
+  return queue->head = (char*)queue->head + queue->size;
+}
+
+void*
+queue_peek(queue_s *const queue, void *val) {
+  if (queue_empty(queue)) {
+    return 0;
+  }
+  memcpy(val, queue->head, queue->size);
+  return queue->head;
+}
---
+++ src/ds/queue/array/test.c
@@ -0,0 +1,104 @@
+#include "_ds_.h"
+#include "queue.h"
+#include <stdio.h>
+
+
+void
+test_queue_new_free_int(void) {
+  queue_s *s = queue_new(0, 4, sizeof(int));
+  queue_free(s);
+}
+
+void
+test_queue_enq_deq_int(void) {
+  queue_s *s = queue_new(0, 4, sizeof(int));
+  int n = s->n + 3, i = 0;
+
+  printf("queue int enq/deq [%zu/%i]\n", s->n, n);
+  printf("---------------------\n");
+
+  while (i++ < n) {
+    int *one = queue_enq(s, &i);
+    if (0 == one) {
+      fprintf(stderr, "enq failed\n");
+      break;
+    }
+    printf("%8s %16i\n", "enq", i);
+  }
+
+  printf("---------------------\n");
+  if (0 == queue_peek(s, &i)) {
+    fprintf(stderr, "peek failed\n");
+  }
+  printf("%8s %16i\n", "peek", i);
+
+  printf("---------------------\n");
+  i = 0;
+  while (i++ < 2*n) {
+    int k = 0;
+    int *empty = queue_deq(s, &k);
+    if (0 == empty) {
+      fprintf(stderr, "deq failed, empty\n");
+      break;
+    }
+    printf("%8s %16i\n", "deq", k);
+  }
+
+  queue_free(s);
+}
+
+void
+test_queue_new_free_str() {
+  queue_s *s = queue_new(0, 4, sizeof(char*));
+  queue_free(s);
+}
+
+void
+test_queue_enq_deq_str(void) {
+  queue_s *s = queue_new(0, 4, sizeof(char*));
+  char *ss[] = {"a", "bb", "ccc", "dddd", "eeeee", "ffffff", };
+  char *buf = 0;
+
+  printf("queue str enq/deq [%zu/%zu]\n", s->n, sizeof(ss)/sizeof(ss[0]));
+  printf("---------------------\n");
+  
+  for (size_t i = 0; i < sizeof(ss)/sizeof(ss[0]); i++) {
+    char *one = queue_enq(s, &ss[i]);
+    if (0 == one) {
+      fprintf(stderr, "enq failed\n");
+      break;
+    }
+    printf("%8s %16s\n", "enq", ss[i]);
+  }
+
+  printf("---------------------\n");
+  if (0 == queue_peek(s, &buf)) {
+    fprintf(stderr, "peek failed\n");
+  }
+  printf("%8s %16s\n", "peek", buf);
+
+  printf("---------------------\n");
+  for (size_t i = 0; i < sizeof(ss)/sizeof(ss[0]); i++) {
+    buf = 0;
+    char *empty = queue_deq(s, &buf);
+    if (0 == empty) {
+      fprintf(stderr, "deq failed, empty\n");
+      break;
+    }
+    printf("%8s %16s\n", "deq", buf);
+  }
+
+  queue_free(s);
+}
+
+
+int
+main(void) {
+  test_queue_new_free_int();
+  test_queue_enq_deq_int();
+  
+  test_queue_new_free_str();
+  test_queue_enq_deq_str();
+  
+  return 0;
+}
