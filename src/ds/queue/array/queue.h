d76aebe8c8483ad4bc961084a66cdaa235cde4f3
南山竹 <junjiemars@gmail.com>
2020-07-04 15:03:52 +0800

    ds/queue: array, realloc

--- src/ds/queue/array/queue.h
+++ src/ds/queue/array/queue.h
@@ -9,84 +9,92 @@ typedef struct queue_s {
   void *data;
 } queue_s;
 
+queue_s *queue_new(queue_s *q, size_t n, size_t size);
+void queue_free(queue_s *const q);
+int queue_empty(queue_s *q);
+int queue_full(queue_s *q);
+void *queue_enq(queue_s *const q, void *val);
+void *queue_deq(queue_s *const q, void *val);
+void *queue_peek(queue_s *const q, void *val);
+
 
 static inline void*
-queue_node_new(queue_s *const queue) {
-  return queue->data = realloc(queue->data, queue->n * queue->size);
+queue_node_new(queue_s *const q) {
+  return q->data = realloc(q->data, q->n * q->size);
 }
 
 static inline void
-queue_node_free(queue_s *const queue) {
-  free(queue->data);
+queue_node_free(queue_s *const q) {
+  free(q->data);
 }
 
 queue_s*
-queue_new(queue_s *queue, size_t n, size_t size) {
-  queue = calloc(1, sizeof(queue_s));
-  if (queue) {
-    queue->size = size;
-    queue->n = n;
-    queue_node_new(queue);
-    queue->head = queue->tail = queue->data;
+queue_new(queue_s *q, size_t n, size_t size) {
+  q = calloc(1, sizeof(queue_s));
+  if (q) {
+    q->size = size;
+    q->n = n;
+    queue_node_new(q);
+    q->head = q->tail = q->data;
   }
-  return queue;
+  return q;
 }
 
 void
-queue_free(queue_s *const queue) {
-  queue_node_free(queue);
-  free(queue);  
+queue_free(queue_s *const q) {
+  queue_node_free(q);
+  free(q);  
 }
 
 int
-queue_empty(queue_s *const queue) {
-  return queue->head == queue->tail;
+queue_empty(queue_s *const q) {
+  return q->head == q->tail;
 }
 
 int
-queue_full(queue_s *const queue) {
-  size_t len = (char*)queue->tail - (char*)queue->data;
-  return queue->size * queue->n == len;
+queue_full(queue_s *const q) {
+  size_t len = (char*)q->tail - (char*)q->data;
+  return q->size * q->n == len;
 }
 
 void*
-queue_enq(queue_s *queue, void *val) {
-  if (queue_full(queue)) {
-    size_t offset = (char*)queue->head - (char*)queue->data;
-    size_t len = (char*)queue->tail - (char*)queue->head;
+queue_enq(queue_s *const q, void *val) {
+  if (queue_full(q)) {
+    size_t offset = (char*)q->head - (char*)q->data;
+    size_t len = (char*)q->tail - (char*)q->head;
     if (offset > 0) {
       if (len > 0) {
-        memmove(queue->data, queue->head, len);
+        memmove(q->data, q->head, len);
       }
     } else {
-      queue->n = queue->n*2;
-      void *new_one = queue_node_new(queue);
+      q->n = q->n*2;
+      void *new_one = queue_node_new(q);
       if (0 == new_one) {
         return 0;
       }
     }
-    queue->head = queue->data;
-    queue->tail = (char*)queue->head + len;
+    q->head = q->data;
+    q->tail = (char*)q->head + len;
   }
-  memcpy(queue->tail, val, queue->size);
-  return queue->tail = (char*)queue->tail + queue->size;
+  memcpy(q->tail, val, q->size);
+  return q->tail = (char*)q->tail + q->size;
 }
 
 void*
-queue_deq(queue_s *const queue, void *val) {
-  if (queue_empty(queue)) {
-    queue->head = queue->tail = queue->data;
+queue_deq(queue_s *const q, void *val) {
+  if (queue_empty(q)) {
+    q->head = q->tail = q->data;
     return 0;
   }
-  memcpy(val, queue->head, queue->size);
-  return queue->head = (char*)queue->head + queue->size;
+  memcpy(val, q->head, q->size);
+  return q->head = (char*)q->head + q->size;
 }
 
 void*
-queue_peek(queue_s *const queue, void *val) {
-  if (queue_empty(queue)) {
+queue_peek(queue_s *const q, void *val) {
+  if (queue_empty(q)) {
     return 0;
   }
-  memcpy(val, queue->head, queue->size);
-  return queue->head;
+  memcpy(val, q->head, q->size);
+  return q->head;
 }
