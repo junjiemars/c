154538ba7e25b951d8932cf1b7e7fa41a4ded694
南山竹 <junjiemars@gmail.com>
2020-07-09 00:05:55 +0800

    feature: inline

--- src/feature/_feature_.h
+++ src/feature/_feature_.h
@@ -5,5 +5,18 @@
 
 #define _unused_(x) ((void)(x))
 
+#if defined(CLANG)
+#  if __has_attribute(noinline)
+#    define __noinline__ __attribute__((noinline))
+#  else
+#    define __noinline__
+#  endif
+#
+#  if __has_attribute(foreinline)
+#    define __always_inline__ __attribute__((always_inline))
+#  else
+#    define __always_inline__
+#  endif
+#endif
 
 #endif /* _FEATURE_H_ */
---
+++ src/feature/inline/always.c
@@ -0,0 +1,24 @@
+#include "_feature_.h"
+#include <stdio.h>
+
+__always_inline__
+int
+sum(int n, int acc) {
+  if (n < 1) return acc;
+  return sum(n-1, n+acc);
+}
+
+int
+main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("where the number?\n");
+    return 0;
+  }
+  
+  int n;
+  sscanf(argv[1], "%d", &n);
+  int x = sum(n, 0);
+  printf("sum(%d)=%d\n", n, x);
+
+  return 0;
+}
