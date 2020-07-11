199cb1891b7c83aac6ef035151bf623e9c8a8fb7
南山竹 <junjiemars@gmail.com>
2020-07-12 00:35:38 +0800

    lang: ctype.h

---
+++ src/lang/type/ct.c
@@ -0,0 +1,27 @@
+#include "_lang_.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+long
+atox(const char *s) {
+  static char const *x = "0123456789ABCDEF";
+  long sum;
+  while (isspace(*s++)) {
+    /* do nothing */
+  }
+  for (sum = 0L; isxdigit(*s); ++s) {
+    int d = strchr(x, toupper(*s)) - x;
+    sum = sum * 16L + d;
+  }
+  return sum;
+}
+
+int
+main(int argc, char **argv) {
+  if (argc > 1) {
+    long x = atox(argv[1]);
+    printf("0x%s = 0d%ld\n", argv[1], x);
+  }
+  return 0;
+}
