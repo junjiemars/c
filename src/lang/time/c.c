7a120353258e740753a24a1c174964c76c5f94c7
南山竹 <junjiemars@gmail.com>
2020-07-12 22:12:19 +0800

    lang/time: timer

--- src/lang/time/c.c
+++ src/lang/time/c.c
@@ -2,15 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
-void timer_reset(void);
+void timer_reset(time_t *epoch);
 void timer_wait(double secs);
-double timer_elapsed(void);
-
-static clock_t epoch = (clock_t)0;
+double timer_elapsed(const time_t *epoch);
 
 void
-timer_reset(void) {
-  epoch = clock();
+timer_reset(time_t *epoch) {
+  *epoch = clock();
 }
 
 void
@@ -22,8 +20,8 @@ timer_wait(double secs) {
 }
 
 double
-timer_elapsed(void) {
-  double elapsed =  (double)(clock() - epoch) / CLOCKS_PER_SEC;
+timer_elapsed(const time_t *epoch) {
+  double elapsed =  (double)(clock() - *epoch) / CLOCKS_PER_SEC;
   return elapsed;
 }
 
@@ -35,12 +33,14 @@ main(int argc, char **argv) {
 
   long max;
   sscanf(argv[1], "%ld", &max);
-  timer_reset();
+  
+  time_t epoch;
+  timer_reset(&epoch);
 
   for (long i = 0; i < max; ++i) {
     /* do nothing */
   }
-  double elapsed = timer_elapsed();
+  double elapsed = timer_elapsed(&epoch);
   printf("elapsed: %lf secs\n", elapsed);
 
 	return 0;
