6f2669c41d7db01407a96c9850d80a0e223948b9
南山竹 <junjiemars@gmail.com>
2020-07-18 21:39:32 +0800

    lang/math: float compare

--- src/lang/math/fcmp.c
+++ src/lang/math/fcmp.c
@@ -38,32 +38,48 @@ to_ul(double x) {
 
 void
 test_cmp_float(void) {
-  float d1 = 0.1, d2 = 0.2;
-  float r1 = 0.3;
+  float d1 = 0.1, d2 = 0.2, d3 = 0.3;
+  unsigned int u1 = to_ui(d1);
+  unsigned int u2 = to_ui(d2);
+  unsigned int u12 = to_ui(d1+d2);
+  unsigned int u3 = to_ui(d3);
 
   printf("compare float\n--------------------\n");
-  printf("%16f = 0x%X\n", d1, to_ui(d1));
-  printf("%16f = 0x%X\n", d2, to_ui(d2));
-  printf("%16f = 0x%X\n", r1, to_ui(r1));
+  printf("%16f = 0x%X\n", d1, u1);
+  printf("%16f = 0x%X\n", d2, u2);
+  printf("%16f = 0x%X\n", d1+d2, u12);
+  printf("%16f = 0x%X\n", d3, u3);
 
-  printf(BPRI32(to_ui(d1)));
-  printf(BPRI32(to_ui(d2)));
-  printf(BPRI32(to_ui(d1+d2)));
-  assert(r1 == d1 + d2);
+  printf(BPRI32(u1));
+  printf(BPRI32(u2));
+  printf(BPRI32(u12));
+  printf(BPRI32(u3));
+
+  assert(u3 == u12);
+  assert(d3 == d1 + d2);
 }
 
 void
 test_cmp_double(void) {
-  double d1 = 0.1, d2 = 0.2;
-  double r1 = 0.3;
+  double d1 = 0.1, d2 = 0.2, d3 = 0.3;
+  unsigned long u1 = to_ul(d1);  
+  unsigned long u2 = to_ul(d2);
+  unsigned long u12 = to_ul(d1+d2);
+  unsigned long u3 = to_ul(d3); 
+
   printf("compare double\n--------------------\n");
   printf("%16lf = 0x%lX\n", d1, to_ul(d1));
   printf("%16lf = 0x%lX\n", d2, to_ul(d2));
-  printf("%16lf = 0x%lX\n", r1, to_ul(r1));
-  printf(BPRI64(to_ul(d1)));
-  printf(BPRI64(to_ul(d2)));
-  printf(BPRI64(to_ul(d1+d2)));
-  assert(r1 != d1 + d2);
+  printf("%16lf = 0x%lX\n", d1+d2, to_ul(d1+d2));
+  printf("%16lf = 0x%lX\n", d3, to_ul(d3));
+
+  printf(BPRI64(u1));
+  printf(BPRI64(u2));
+  printf(BPRI64(u12));
+  printf(BPRI64(u3));
+
+  assert(u3 != u12);
+  assert(d3 != d1 + d2);
 }
 
 int
