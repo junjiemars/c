5d772779c4234742a5742dd95f2c64d72a70a0dc
南山竹 <junjiemars@gmail.com>
2020-07-25 11:46:44 +0800

    lang/typedef:

--- src/lang/typedef/t.c
+++ src/lang/typedef/t.c
@@ -1,4 +1,4 @@
-#include <_lang_.h>
+#include "_lang_.h"
 #include <stdio.h>
 
 
@@ -9,22 +9,43 @@ struct point_s /* tag: not type */ {
 	int x;
 	int y;
 };
-typedef struct point_s point_s;
+typedef struct point_s point_t;
 
 typedef struct point1_s {
 	int x;
 	int y;
-} point1_s /* type */;
+} point1_t /* type */;
 
+/* x_t alias of int */
+/* p_t alias of pointer to int */
+/* f_t as function accept int and return pointer to int */
+typedef int x_t, *p_t, *(*fn)(int);
+
+p_t fn(int n) {
+  x_t x = n + 10;
+  p_t p = &x;
+  return p;
+}
+
+void
+test_fn(void) {
+  p_t p = fn(3);
+  printf("test_fn(3)        = %4i\n", *p);
+}
 
 int
 main(int argc, char *argv[]) {
 	_unused_(argc);
 	_unused_(argv);
 
-	length_t len1 = 123;
-	printf("sizeof(length_t) = %zu\n", sizeof(len1));
+	printf("sizeof(length_t)  = %4zu\n", sizeof(length_t));
+  printf("sizeof(point_t)   = %4zu\n", sizeof(point_t));
+  printf("sizeof(point1_t)  = %4zu\n", sizeof(point1_t));
+  printf("sizeof(x_t)       = %4zu\n", sizeof(x_t));
+  printf("sizeof(p_t)       = %4zu\n", sizeof(p_t));
+  printf("sizeof(f_t)       = %4zu\n", sizeof(f_t));
   
-  printf("sizeof(point_s) = %zu\n", sizeof(point_s));
-  printf("sizeof(point1_s) = %zu\n", sizeof(point1_s));
+  test_fn();
+  
+  return 0;
 }
