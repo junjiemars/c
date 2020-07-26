c7ede3523a1a64f5e396f7f56a0f561ab323f52a
南山竹 <junjiemars@gmail.com>
2020-07-26 16:52:22 +0800

    lang/union: cast
    from the member of union to union, from union to the member.

--- src/lang/union/u.c
+++ src/lang/union/u.c
@@ -1,5 +1,6 @@
 #include "_lang_.h"
-#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
 #if defined(GCC) || defined(CLANG)
 union __attribute__((packed)) size_u {
@@ -22,17 +23,39 @@ union size_u {
 #endif
 
 void test_size_u(void);
+void test_cast(void);
 
 void
 test_size_u(void) {
   union size_u u1 = { .whole=0 };
-  printf("sizeof(u1) = %4zu\n", sizeof(u1));
+  assert(sizeof(u1) == sizeof(u1.whole));
 
   u1.split.high = 0x1122;
   u1.split.low = 0x3344;
-  printf("u1.split.high = 0x%x\n", u1.split.high);
-  printf("u1.split.low  = 0x%x\n", u1.split.low);
-  printf("u1.whole      = 0x%x\n", u1.whole);
+
+#if NM_CPU_LITTLE_ENDIAN
+  assert(u1.whole == 0x33441122); 
+#else
+  assert(u1.whole == 0x11223344);
+#endif
+}
+
+void
+test_cast(void) {
+  union size_u *u1 = malloc(sizeof(*u1));
+  u1->whole = 0x11223344;
+
+  union size_u *u2 = malloc(sizeof(*u2));
+  u2->whole = *(int*)u1;
+  assert(u2->whole == u1->whole);
+  
+  u2->whole++;
+  union size_u *u3;
+  u3 = (union size_u*)&u2->whole;
+  assert(u3->whole == u2->whole);
+  
+  free(u1);
+  free(u2);
 }
 
 int
@@ -41,6 +64,7 @@ main(int argc, char **argv) {
   _unused_(argv);
   
   test_size_u();
+  test_cast();
 
   return 0;
 }
