db4de0443b8db0f521672b289a9853286af4ab1b
南山竹 <junjiemars@gmail.com>
2020-08-03 09:41:24 +0800

    lang/type/int: on Linux

--- src/lang/type/int.c
+++ src/lang/type/int.c
@@ -12,16 +12,19 @@ main(void) {
 
 void
 test_signed(void) {
-  printf("%s: %02zu bytes\nMIN:%032" PRIi32 "\nMAX: %031" PRIi32 "\n",
+  printf("%s: %02zu bytes\nMIN:%032i\nMAX: %031i\n",
          "char", sizeof(char), CHAR_MIN, CHAR_MAX);
 
-  printf("%s: %02zu bytes\nMIN:%032" PRIi32 "\nMAX: %031" PRIi32 "\n",
+  printf("%s: %02zu bytes\nMIN:%032i\nMAX: %031i\n",
          "short", sizeof(short), SHRT_MIN, SHRT_MAX);
 
-  printf("%s: %02zu bytes\nMIN:%032" PRIi32 "\nMAX: %031" PRIi32 "\n",
+  printf("%s: %02zu bytes\nMIN:%032i\nMAX: %031i\n",
          "int", sizeof(int), INT_MIN, INT_MAX);
 
-  printf("%s: %02zu bytes\nMIN:%032" PRIi64 "\nMAX: %031" PRIi64 "\n",
+  printf("%s: %02zu bytes\nMIN:%032li\nMAX: %031li\n",
+         "long int", sizeof(long int), LONG_MIN, LONG_MAX);
+
+  printf("%s: %02zu bytes\nMIN:%032lli\nMAX: %031lli\n",
          "long long int", sizeof(long long int), LLONG_MIN, LLONG_MAX);
 }
 
