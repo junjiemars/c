41c8bc3f2a9560567261735de2a8905309b8b402
南山竹 <junjiemars@gmail.com>
2020-08-21 23:29:48 +0800

    lang/string: strnlen, strncpy, strncmp

--- src/lang/string/n.c
+++ src/lang/string/n.c
@@ -2,24 +2,35 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-#if defined(NM_HAVE_STRN_ANY_FN)
+size_t self_strnlen(const char *src, size_t len);
+char *self_strncpy(char *dst, const char *src, size_t len);
+
+typedef size_t (*strnlen_fn)(const char *src, size_t maxlen);
+typedef char *(*strncpy_fn)(char *dst, const char *src, size_t len);
+typedef int (*strncmp_fn)(const char *s1, const char *s2, size_t n);
+
+static void test_strnlen(strnlen_fn fn, const char *src, size_t maxlen);
+static void test_strncpy(strncpy_fn fn, char *src, size_t len);
+static void test_strncmp(strncmp_fn fn,
+                         const char *s1, const char *s2, size_t n);
 
 size_t
-self_strnlen(const char *s, size_t n) {
-	size_t len = 0;
-	while (*s++ && n--) {
-		len++;
+self_strnlen(const char *s, size_t maxlen) {
+	size_t n = 0;
+	while (*s++ && maxlen--) {
+		n++;
 	}
-	return len;
+	return n;
 }
 
 char *
 self_strncpy(char *dst, const char *src, size_t n) {
-	char *r = dst;
-	while (0 != (*dst++ = *src++) && n--);
-	return r;
+	char *p = dst;
+	while (0 != (*p++ = *src++) && n--) {
+    /* do nothing */
+  }
+	return dst;
 }
 
 int
@@ -27,89 +38,62 @@ self_strncmp(const char *s1, const char *s2, size_t n) {
 	while (*s1 && (*s1 == *s2) && n > 1) {
 		s1++, s2++, n--;
 	}
-	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+  
+	int cmp = *(const unsigned char*)s1 - *(const unsigned char*)s2;
+  if (cmp < 0) {
+    return -1;
+  } else if (cmp > 0) {
+    return 1;
+  }
+  return 0;
 }
 
 void
-test_strnlen(void) {
-	char buf[8];
-	memset(buf, 0, sizeof(buf)/sizeof(*buf));
-	size_t len, len1, len2;
-	len1 = strnlen(buf, sizeof(buf)/sizeof(*buf));
-	len2 = self_strnlen(buf, sizeof(buf)/sizeof(*buf));
-	assert(0 == len1 && 0 == len2);
-	len = strlen("abc");
-	strncpy(buf, "abc", len);
-	len1 = strnlen(buf, sizeof(buf)/sizeof(*buf));
-	len2 = self_strnlen(buf, sizeof(buf)/sizeof(*buf));
-	assert(len == len1 && len == len2);
-	len = strlen("abcdefgh");
-	strncpy(buf, "abcdefgh", len-1);
-	len1 = strnlen(buf, len-1);
-	len2 = self_strnlen(buf, len-1);
-	assert(len-1 == len1 && len-1 == len2);
+test_strnlen(strnlen_fn fn, const char *s, size_t maxlen) {
+  size_t n;
+  n = fn(s, maxlen);
+  printf("[%zu]: %s\n", n, s);
 }
 
 void
-test_strncpy(void) {
-	char buf[8];
-	memset(buf, 0x11, sizeof(buf)/sizeof(*buf));
-	assert(0x11 == buf[7] && "strncpy, 0x11 != <tailing>");
-	
-	/* if N > strlen(src) then padding 0 */
-	strncpy(buf, "abc", sizeof(buf)/sizeof(*buf) /* N */);
-	assert(0 == buf[7] && "strncpy(), N > strlen(src)");
-	char buf1[8];
-	memset(buf1, 0x11, sizeof(buf1)/sizeof(*buf1));
-	self_strncpy(buf1, "abc", sizeof(buf1)/sizeof(*buf1));
-	assert(0 == strcmp(buf, buf1) && "self_strncpy(), failed");
-
-	/* if N < strlen(src), N is reached before the entire array 
-		 src was copied, the tailing characters keep unchanged, 
-		 so the resulting character array is may not be null-terminated.
-	*/
-	memset(buf, 0x11, sizeof(buf)/sizeof(*buf));
-	strncpy(buf, "abc", 1 /* N */);
-	assert('a' == buf[0] && 0x11 == buf[7] && "strncpy(), N < strlen(src)");
-	memset(buf1, 0x11, sizeof(buf1)/sizeof(*buf1));
-	self_strncpy(buf1, "abc", 1);
-	assert(0 == strncmp(buf, buf1, 1) && "self_strncpy(), failed");
-
-	/* The behavior is undefined:
-		 1) if the character arrays overlap,
-		 2) if either dest or src is not a pointer to a character
-		 array (including if dest or src is a null pointer),
-		 3) if the size of the array pointed to by dest is
-		 less than N, or
-		 4) if the size of the array pointed to by src is less
-		 than N and it does not contain a null character 
-	*/
+test_strncpy(strncpy_fn fn, char *src, size_t n) {
+  char *dst;
+  dst = malloc(n + 1);
+  (void)fn(dst, src, n);
+  dst[n] = '\0';
+  printf("%s\n", dst);
+  free(dst);
 }
 
 void
-test_strncmp(void) {
-	char *s1 = "abcdefg", *s2 = "abcdefG";
-	int cmp1 = strncmp(s1, s2, strlen(s2));
-	int cmp2 = self_strncmp(s1, s2, strlen(s2));
-	assert(0 < cmp1 && 0 < cmp2 && "strncmp(), failed");
-	cmp1 = strncmp(s1, s2, strlen("abcdef"));
-	cmp2 = self_strncmp(s1, s2, strlen("abcdef"));
-	assert(0 == cmp1 && 0 == cmp2 && "strncmp(), failed");
+test_strncmp(strncmp_fn fn, const char *s1, const char *s2, size_t n) {
+  int cmp;
+  cmp = fn(s1, s2, n);
+  printf("==%i\n", cmp);
 }
 
 
-#endif /* end of NM_HAVE_STRN_ANY_FN */
+/* #endif /\* end of NM_HAVE_STRN_ANY_FN *\/ */
 
 int
 main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
+  if (argc < 4) {
+    printf("where the source string and length\n");
+    return 0;
+  }
+  
+  char *s1 = argv[1];
+  char *s2 = argv[2];
+  size_t n = atol(argv[3]);
+  
+  test_strnlen(strnlen, s1, n);
+  test_strnlen(self_strnlen, s1, n);
+  
+	test_strncpy(strncpy, s1, n);
+  test_strncpy(self_strncpy, s1, n);
 
-#if ! defined(NM_HAVE_STRN_ANY_FN)
-	printf("skip strn* fn testing\n");
-#else
-	test_strnlen();
-	test_strncpy();
-	test_strncmp();
-#endif	
+  test_strncmp(strncmp, s1, s2, n);
+  test_strncmp(self_strncmp, s1, s2, n);
+
+  return 0;
 }
