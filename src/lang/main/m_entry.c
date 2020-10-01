cd279030521a8cd15ac1c6b0784acb2e10f58c13
南山竹 <junjiemars@gmail.com>
2020-10-02 00:51:47 +0800

    ci: on Windows, msvc, entry

--- src/lang/main/m_entry.c
+++ src/lang/main/m_entry.c
@@ -1,9 +1,9 @@
 #include <_lang_.h>
 #include <stdio.h>
 
-/* #if (GCC) && (LINUX) */
-/* #  define entry main */
-/* #endif */
+#if (GCC) && (LINUX)
+#  define entry main
+#endif
 
 int
 entry(int argc, char **argv) {
@@ -15,11 +15,11 @@ entry(int argc, char **argv) {
   return 0;
 }
 
-/* #if (GCC) && (LINUX) */
-/* #  if defined(entry) */
-/* #    undef entry */
-/* #  endif */
-/* #endif */
+#if (GCC) && (LINUX)
+#  if defined(entry)
+#    undef entry
+#  endif
+#endif
 
 #ifdef main
 #undef main
