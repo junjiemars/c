641cb5409b740a75bfcd0bb02f39e137e0f58f80
南山竹 <junjiemars@gmail.com>
2020-10-01 21:54:13 +0800

    ci: on Windows, gcc and msvc

--- src/algo/_algo_.h
+++ src/algo/_algo_.h
@@ -10,13 +10,23 @@
 
 #define _unused_(x) ((void)(x))
 
+#if (MSVC)
+/* msvc does not support variable length array, C2057 */
+#define swap(a, b, w) do {                      \
+    unsigned char *_swap_1_ = malloc(w);        \
+		memcpy(_swap_1_, (a), (w));                 \
+		memmove((a), (b), (w));                     \
+		memcpy((b), _swap_1_, (w));                 \
+    free(_swap_1);                              \
+	} while (0)
+#else
 #define swap(a, b, w) do {                      \
 		unsigned char _swap_1_[w];                  \
 		memcpy(_swap_1_, (a), (w));                 \
 		memmove((a), (b), (w));                     \
 		memcpy((b), _swap_1_, (w));                 \
 	} while (0)
-
+#endif
 
 void list_array(const void *base, size_t nel, size_t width,
 								void (*print)(const void *el));
