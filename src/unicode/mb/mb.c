e23e541244af27ff7eb27f04151a043601ede43a
南山竹 <junjiemars@gmail.com>
2020-10-03 12:22:17 +0800

    unicode: multibytes

--- src/unicode/mb/mb.c
+++ src/unicode/mb/mb.c
@@ -6,10 +6,11 @@
 #include <string.h>
 
 #if MSVC
-#  pragma warning(disable : 4996)
+#  pragma warning(disable:4996)
 #endif
 
 void print_wc(const wchar_t *s);
+wchar_t *mbs_to_wcs(const char *s, size_t *n);
 void wc_to_mb(const wchar_t *s);
 void wcs_to_mbs(const wchar_t *s, size_t n);
 
@@ -20,6 +21,22 @@ print_wc(const wchar_t *s) {
 		printf("[%lc]", *p++);
 	}
 	putchar('\n');
+}
+
+wchar_t *
+mbs_to_wcs(const char *s, size_t *n) {
+  if (!s) {
+    return 0;
+  }
+
+  size_t ns = strlen(s);
+  wchar_t *d = malloc(sizeof(wchar_t) * ns);
+  *n = mbstowcs(d, s, ns);
+  if (n > 0) {
+    return d;
+  }
+  perror("!panic");
+  return 0;
 }
 
 void
@@ -43,9 +60,6 @@ wcs_to_mbs(const wchar_t *s, size_t n) {
 
 int
 main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
-
 	printf("current locale: %s\n", setlocale(LC_ALL, 0));
   printf("set locale to native: %s\n", setlocale(LC_ALL, ""));
 #if (WINNT)
@@ -55,12 +69,26 @@ main(int argc, char **argv) {
 	printf("MB_LEN_MAX: %i\n", MB_LEN_MAX);
 	printf("----------\n");
 
-	wchar_t s[] = L"abc中文def";
+  if (argc < 2) {
+    printf("where the input string?\n");
+    return 0;
+  }
+  
+  size_t n = 0;
+  wchar_t *s = mbs_to_wcs(argv[1], &n);
+
+  if (!s) {
+    return 0;
+  }
 	printf("%ls:\n", s);
 	printf("----------\n");
 	
 	print_wc(s);
 	wc_to_mb(s);
-	wcs_to_mbs(s, sizeof(s));
+	wcs_to_mbs(s, sizeof(wchar_t) * n);
+
+  if (s) {
+    free(s);
+  }
 	return 0;
 }
