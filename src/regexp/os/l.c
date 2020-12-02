3fcc4b891169774f08c527c6ec17b0cf6d79844e
南山竹 <junjiemars@gmail.com>
2020-12-02 22:47:37 +0800

    algo/regexp: on Darwin

--- src/regexp/os/l.c
+++ src/regexp/os/l.c
@@ -1,9 +1,46 @@
 #include <_regexp_.h>
-#include <sys/types.h>
+#if LINUX
+#  include <sys/types.h>
+#endif
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define ERRBUF_SIZE 1024
+static char errbuf[ERRBUF_SIZE];
+
+void
+test_basic(const char *pattern, const char *subject)
+{
+  regex_t re;
+  int errcode = 0;
+  regmatch_t match;
+
+  printf("----------\npattern = %s\nsubject = %s\n----------\n",
+         pattern, subject);
+  
+  errcode = regcomp(&re, pattern, REG_BASIC);
+  if (errcode)
+    {
+      regerror(errcode, &re, errbuf, ERRBUF_SIZE);
+      fprintf(stderr, "%s\n", errbuf);
+      return;
+    }
+
+  memset(&match, 0, sizeof(match));
+  errcode = regexec(&re, subject, 0, &match, 0);
+  if (REG_NOMATCH == errcode)
+    {
+      regerror(errcode, &re, errbuf, ERRBUF_SIZE);
+      fprintf(stderr, "%s\n", errbuf);
+      return;
+    }
+
+  printf("Matched: start = %i, end = %i\n",
+         (int)match.rm_so,
+         (int)match.rm_eo);
+}
 
 int
 main(int argc, char **argv)
@@ -11,44 +48,7 @@ main(int argc, char **argv)
   _unused_(argc);
   _unused_(argv);
 
-  if (argc < 3) {
-    fprintf(stderr, "%s %s\n", "pattern", "subject");
-    return 1;
-  }
-  
-  regex_t re;
-  int errcode = 0;
-  size_t errbuf_size = 1024;
-  char *errbuf;
-  regmatch_t match;
-  
-  errbuf = calloc(errbuf_size, 1);
-  if (!errbuf)
-    {
-      perror(NULL);
-      return 0;
-    }
-  
-  const char *pattern = argv[1];
-  const char *subject = argv[2];
-  
-  errcode = regcomp(&re, pattern, REG_ICASE);
-  if (errcode)
-    {
-      regerror(errcode, &re, errbuf, errbuf_size);
-      fprintf(stderr, "%s\n", errbuf);
-      return 0;
-    }
-
-  memset(&match, 0, sizeof(match));
-  errcode = regexec(&re, subject, 0, &match, 0);
-  if (REG_NOMATCH == errcode)
-    {
-      regerror(errcode, &re, errbuf, errbuf_size);
-      fprintf(stderr, "%s\n", errbuf);
-      return 0;
-    }
-  fprintf(stdout, "#matched\n");
+  test_basic("ca*r", "caaar");
     
   return 0;
 }
