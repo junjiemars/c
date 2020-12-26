308075884a4b38e44a588b6fd072c9c57b40fd89
南山竹 <junjiemars@gmail.com>
2020-12-26 20:32:24 +0800

    test: tar exclude

--- test.sh
+++ test.sh
@@ -83,15 +83,15 @@ test_do() {
 # basic test
 if [ "basic" = "$_TEST_" ]; then
   test_do --has-hi       || exit 1
-  # test_do --has-algo     || exit 1
-  # test_do --has-ds       || exit 1
-  # test_do --has-library  || exit 1
-  # test_do --has-lang     || exit 1
-  # test_do --has-memory   || exit 1
-  # test_do --has-regexp   || exit 1
-  # test_do --has-unicode  || exit 1
-  # test_do --has-x86      || exit 1
-  # test_do --has-os       || exit 1
+  test_do --has-algo     || exit 1
+  test_do --has-ds       || exit 1
+  test_do --has-library  || exit 1
+  test_do --has-lang     || exit 1
+  test_do --has-memory   || exit 1
+  test_do --has-regexp   || exit 1
+  test_do --has-unicode  || exit 1
+  test_do --has-x86      || exit 1
+  test_do --has-os       || exit 1
 fi
 
 # eof
