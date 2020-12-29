d92275db7c7afd978672a782a05f47f1e8986246
南山竹 <junjiemars@gmail.com>
2020-12-29 23:33:50 +0800

    io/scan

--- src/io/basic/b.c
+++ src/io/basic/b.c
@@ -1,21 +1,25 @@
 #include "_io_.h"
 #include <stdio.h>
 
-int 
-main(int argc, char *argv[]) {
+int
+main(int argc, char *argv[])
+{
 	_unused_(argc);
 	_unused_(argv);
 
 	int ch;
-	while (EOF != (ch = fgetc(stdin))) {
-		fputc(ch, stdout);
-	}
-	if (ferror(stdin)) {
-		perror("!panic");
-		return 1;
-	}
-    if (feof(stdin)) {
-      fprintf(stdout, "%s\n", "end of file");
+	while (EOF != (ch = fgetc(stdin)))
+    {
+      fputc(ch, stdout);
+    }
+	if (ferror(stdin))
+    {
+      perror("!panic");
+      return 1;
+    }
+  if (feof(stdin))
+    {
+      fprintf(stdout, "#%s\n", "end of file");
     }
 
 	return 0;
--- src/io/scan/free.c
+++ src/io/scan/free.c
@@ -2,34 +2,48 @@
 #include <stdio.h>
 
 int
-main(int argc, char **argv) {
-  if (argc < 2) {
-    fprintf(stderr, "where the free txt located?\n");
-    return 0;
-  }
+main(int argc, char **argv)
+{
+  if (argc < 2)
+    {
+      fprintf(stderr, "where the free txt located?\n");
+      return 0;
+    }
 
   int n = 0;
   int sum = 0;
   int num = 0;
 
   FILE *in = fopen(argv[1], "r");
-  if (!in) {
-    perror("!panic");
-    return 1;
-  }
+  if (!in)
+    {
+      perror("!panic");
+      return 1;
+    }
 
-  while (EOF != (fscanf(in, "%i", &num))) {
-    n += 1;
-    sum += num;
-  }
-  if (ferror(in)) {
-    perror("!panic");
-    goto clean_exit;
-  }
+  while (1 == (fscanf(in, "%d", &num)))
+    {
+      n += 1;
+      sum += num;
+    }
 
-  fprintf(stdout, "%dNo: numbers entered\n", n);
-  fprintf(stdout, "sum: %d\n", sum);
-  fprintf(stdout, "average: %3.2f\n", sum*1.0f/n);
+  if (ferror(in))
+    {
+      perror("!panic");
+      goto clean_exit;
+    }
+
+  if (!feof(in))
+    {
+      fprintf(stderr, "!panic, meet a no-number.");
+      goto clean_exit;
+    }
+  else
+    {
+      fprintf(stdout, "N: %d\n", n);
+      fprintf(stdout, "sum: %d\n", sum);
+      fprintf(stdout, "average: %3.2f\n", sum*1.0f/n);
+    }
 
  clean_exit:
   fclose(in);
