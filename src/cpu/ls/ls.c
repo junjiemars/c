78fb9f427b0fc35884f9849329fd2dd39b331936
南山竹 <junjiemars@gmail.com>
2020-12-29 18:55:01 +0800

    cpu

--- src/cpu/ls/ls.c
+++ src/cpu/ls/ls.c
@@ -6,8 +6,12 @@
 #  include <sys/sysctl.h>
 #elif defined (WINNT)
 #  include <windows.h>
+#  include <intrin.h>
+#  pragma warning(disable : 4996)
 #endif
 
+
+#include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 
@@ -16,11 +20,30 @@ typedef struct cpu_s {
 	int byte_order;
 	int ncpu;
 	size_t cache_line_size;
+  char vendor_id[64];
 } cpu_s;
 
+cpu_s *get_arch(cpu_s *cpu);
+cpu_s *get_byte_order(cpu_s *cpu);
+cpu_s *get_cache_line_size(cpu_s *cpu);
+cpu_s *get_vendor_id(cpu_s *cpu);
 
-cpu_s *
-lscpu(cpu_s *cpu) {
+cpu_s*
+get_arch(cpu_s *cpu)
+{
+  _unused_(cpu);
+  return 0;
+}
+
+cpu_s*
+get_byte_order(cpu_s *cpu)
+{
+  _unused_(cpu);
+  return 0;
+}
+
+cpu_s*
+get_cache_line_size(cpu_s *cpu) {
 
 #if defined (LINUX)
 	FILE *f = fopen("/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size", "r");
@@ -64,6 +87,49 @@ lscpu(cpu_s *cpu) {
 	return cpu;
 }
 
+cpu_s*
+get_vendor_id(cpu_s *cpu)
+{
+  uint32_t fn = 0;
+  uint32_t buf[5];
+  memset(buf, 0, sizeof(buf));
+  
+#if ( MSVC )
+
+  __cpuid((int32_t*) buf, fn);
+
+#elif ( __amd64__ ) || ( __x86_64__ )
+
+  uint32_t eax, ebx, ecx, edx;
+  __asm__ (
+           "cpuid"
+           : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+           : "a" (fn) );
+
+  buf[0] = eax;
+  buf[1] = ebx;
+  buf[2] = ecx;
+  buf[3] = edx;
+
+#else
+	
+  _unused_(fn);
+  _unused_(buf);
+  return NULL;
+	
+#endif
+
+  const size_t w = sizeof(uint32_t);
+  char *p = (char*)&cpu->vendor_id[0];
+
+  /* ebx, edx, ecx */
+  strncpy(p, (const char *) &buf[1], w);
+  strncpy(p += w, (const char *) &buf[3], w);
+  strncpy(p += w, (const char *) &buf[2], w);
+
+  return cpu;
+}
+
 int
 main(int argc, char **argv) {
 	_unused_(argc);
@@ -72,11 +138,15 @@ main(int argc, char **argv) {
 	cpu_s cpu;
 	memset(&cpu, 0, sizeof(cpu));
 
-	lscpu(&cpu);
+  get_arch(&cpu);
+  get_byte_order(&cpu);
+	get_cache_line_size(&cpu);
+  get_vendor_id(&cpu);
 
 	printf("Architecture: %s\n", cpu.machine);
 	printf("Byte Order: %s\n", 
 				 (1234 == cpu.byte_order ? "Little Endian" : "Big Endian"));
 	printf("Cache Line Size: %zu\n", cpu.cache_line_size);
+	printf("Vendor Id: %s\n", cpu.vendor_id);
 }
 
