92d18c743382a676ad2fa57626e7fc91b9d0bc3b
南山竹 <junjiemars@gmail.com>
2020-12-31 23:00:57 +0800

    io: seek

--- src/io/seek/s1.c
+++ src/io/seek/s1.c
@@ -3,21 +3,26 @@
 #include <string.h>
 
 #define NUM_LEN 7
-#define OUT_FMT "No.%d found\n"                 \
-"----\n"                                        \
-"num: %s\n"                                     \
-"name: %s\n"                                    \
-"stock: %d\n"                                   \
-"price: %lf\n"
+#define OUT_FMT                                 \
+  "> No.%d found\n"                             \
+  "------------\n"                              \
+  "  num: %s\n"                                 \
+  " name: %s\n"                                 \
+  "stock: %d\n"                                 \
+  "price: %lf\n"
 
-typedef struct record_s {
+#define PANIC "!panic"
+
+typedef struct record_s
+{
   char num[NUM_LEN];
   char name[25];
   int stock;
   double price;
 } record_s;
 
-typedef struct index_s {
+typedef struct index_s
+{
   char num[NUM_LEN];
   int idx;
 } index_s;
@@ -29,7 +34,7 @@ void out_records(const char *inpath,
 
 /* seek record by idx */
 
-void seek_record(const char *path, const int n);
+void seek_record(const char *path, int n);
 
 /* find record by num filed */
 void find_record(const char *binpath,
@@ -39,24 +44,28 @@ void find_record(const char *binpath,
 void
 out_records(const char *inpath,
             const char *binpath,
-            const char *idxpath) {
-  FILE *in = 0, *outbin = 0, *outidx = 0;
+            const char *idxpath)
+{
+  FILE *in = 0, *out_bin = 0, *out_idx = 0;
   in = fopen(inpath, "r");
-  if (!in) {
-    perror("!panic");
-    return;
-  }
-  outbin = fopen(binpath, "wb");
-  if (!outbin) {
-    perror("!panic");
-    goto clean_exit;
-  }
-  outidx = fopen(idxpath, "wb");
-  if (!outidx) {
-    perror("!panic");
-    goto clean_exit;
-  }
-  
+  if (!in)
+    {
+      perror(PANIC);
+      return;
+    }
+  out_bin = fopen(binpath, "wb");
+  if (!out_bin)
+    {
+      perror(PANIC);
+      goto clean_exit;
+    }
+  out_idx = fopen(idxpath, "wb");
+  if (!out_idx)
+    {
+      perror(PANIC);
+      goto clean_exit;
+    }
+
   record_s ss;
   index_s is;
   memset(&ss, 0, sizeof(record_s));
@@ -67,119 +76,160 @@ out_records(const char *inpath,
                      ss.num,
                      ss.name,
                      &ss.stock,
-                     &ss.price)) {
-    if (1 != fwrite(&ss, sizeof(record_s), 1, outbin)) {
-      perror("!panic");
-      goto clean_exit;
+                     &ss.price))
+    {
+      if (1 != fwrite(&ss, sizeof(record_s), 1, out_bin))
+        {
+          perror(PANIC);
+          goto clean_exit;
+        }
+      strncpy(is.num, ss.num, NUM_LEN);
+      is.idx = n;
+      if (1 != fwrite(&is, sizeof(index_s), 1, out_idx))
+        {
+          perror(PANIC);
+          goto clean_exit;
+        }
+      n++;
     }
-    strncpy(is.num, ss.num, NUM_LEN);
-    is.idx = n;
-    if (1 != fwrite(&is, sizeof(index_s), 1, outidx)) {
-      perror("!panic");
-      goto clean_exit;
-    }
-    n++;
-  }
-  
+
  clean_exit:
-  fclose(outidx);
-  fclose(outbin);
-  fclose(in);
+  if (out_idx)
+    {
+      fclose(out_idx);
+    }
+  if (out_bin)
+    {
+      fclose(out_bin);
+    }
+  if (in)
+    {
+      fclose(in);
+    }
 }
 
 void
-seek_record(const char *path, const int n) {
-  FILE *in = fopen(path, "rb");
-  if (!in) {
-    perror("!panic");
-    return;
-  }
+seek_record(const char *path, int n)
+{
+  fprintf(stdout, "\nseeking %i ...\n", n);
 
-  if (fseek(in, (n-1)*sizeof(record_s), SEEK_SET)) {
-    if (ferror(in)) {
-      perror("!panic");
+  FILE *in = fopen(path, "rb");
+  if (!in)
+    {
+      perror(PANIC);
+      return;
     }
-    goto clean_exit;
-  }
+
+  if (fseek(in, (n-1)*sizeof(record_s), SEEK_SET))
+    {
+      perror(PANIC);
+      goto clean_exit;
+    }
 
   record_s ss;
   memset(&ss, 0, sizeof(record_s));
-  if (1 != fread(&ss, sizeof(record_s), 1, in)) {
-    if (feof(in)) {
-      fprintf(stderr, "!No.%i no found\n----\n", n);
-    } else if (ferror(in)) {
-      perror("!panic");
+  if (1 != fread(&ss, sizeof(record_s), 1, in))
+    {
+      if (ferror(in))
+        {
+          perror(PANIC);
+        }
+      if (feof(in))
+        {
+          fprintf(stderr, "! No.%i no found\n------------\n", n);
+        } 
+      goto clean_exit;
     }
-    goto clean_exit;
-  }
   fprintf(stdout, OUT_FMT,
           n, ss.num, ss.name, ss.stock, ss.price);
 
  clean_exit:
-  fclose(in);
+  if (in)
+    {
+      fclose(in);
+    }
 }
 
 void
 find_record(const char *binpath,
             const char *idxpath,
-            const char *num) {
+            const char *num)
+{
+  fprintf(stdout, "\nfinding %s ...\n", num);
+
   FILE *idx = 0, *bin = 0;
   idx = fopen(idxpath, "rb");
-  if (!idx) {
-    perror("!panic");
-    return;
-  }
+  if (!idx)
+    {
+      perror(PANIC);
+      return;
+    }
   bin = fopen(binpath, "rb");
-  if (!bin) {
-    perror("!panic");
-    goto clean_exit;
-  }
+  if (!bin)
+    {
+      perror(PANIC);
+      goto clean_exit;
+    }
 
   index_s is;
   record_s ss;
   memset(&is, 0, sizeof(index_s));
   memset(&ss, 0, sizeof(record_s));
 
-  while (1 == fread(&is, sizeof(index_s), 1, idx)) {
-    if (0 == strncmp(num, is.num, NUM_LEN)) {
-      if (0 == fseek(bin, (is.idx)*sizeof(record_s), SEEK_SET)) {
-        if (1 == fread(&ss, sizeof(record_s), 1, bin)) {
-          fprintf(stdout, OUT_FMT,
-                  is.idx, ss.num, ss.name, ss.stock, ss.price);
-          goto clean_exit;
+  while (1 == fread(&is, sizeof(index_s), 1, idx))
+    {
+      if (0 == strncmp(num, is.num, NUM_LEN))
+        {
+          if (0 == fseek(bin, (is.idx)*sizeof(record_s), SEEK_SET))
+            {
+              if (1 == fread(&ss, sizeof(record_s), 1, bin))
+                {
+                  fprintf(stdout, OUT_FMT,
+                          is.idx, ss.num, ss.name, ss.stock, ss.price);
+                  goto clean_exit;
+                }
+            }
         }
-      } 
     }
-  }
 
-  if (ferror(idx)) {
-    perror("!panic");
-  }
-  if (feof(idx)) {
-    fprintf(stdout, "!Num.%s no found\n----\n", num);
-  }
+  if (ferror(idx))
+    {
+      perror(PANIC);
+    }
+  if (feof(idx))
+    {
+      fprintf(stdout, "! %s no found\n------------\n", num);
+    }
 
  clean_exit:
-  fclose(idx);
-  fclose(bin);
+  if (idx)
+    {
+      fclose(idx);
+    }
+  if (bin)
+    {
+      fclose(bin);
+    }
 }
 
 
 int
-main(int argc, char **argv) {
-  if (argc < 4) {
-    fprintf(stderr,
-            "where the records.txt/records.bin/records.idx"
-            "located?\n");
-    return 0;
-  }
+main(int argc, char **argv)
+{
+  if (argc < 4)
+    {
+      fprintf(stderr,
+              "where the records.txt/records.bin/records.idx"
+              "located?\n");
+      return 1;
+    }
 
   const char *txt = argv[1];
   const char *bin = argv[2];
   const char *idx = argv[3];
 
   out_records(txt, bin, idx);
-  
+
   seek_record(bin, 1);
   seek_record(bin, 3);
   seek_record(bin, 10);
@@ -187,6 +237,6 @@ main(int argc, char **argv) {
   find_record(bin, idx, "PKL070");
   find_record(bin, idx, "DKP080");
   find_record(bin, idx, "DKP081");
-  
+
   return 0;
 }
