31fb0ec91e8dfe171d61e89f7d47fc02b39c38de
南山竹 <junjiemars@gmail.com>
2021-01-17 01:15:40 +0800

    os: echo

---
+++ src/os/echo/e.c
@@ -0,0 +1,50 @@
+#include <_os_.h>
+#include <stdio.h>
+#include <posix/getopt.h>
+
+extern char *optarg;
+extern int errno;
+
+void usage(const char *echo);
+
+void
+usage(const char *pcre)
+{
+  printf("Usage: %s [-n]... [string ...]\n", pcre);
+	printf("\n");
+	printf("Write arguments to the standard output.\n");
+  printf("  -o            do not print the trailing newline character.\n");
+}
+
+int
+main(int argc, char **argv)
+{
+  int opt_n;
+  int ch;
+
+  while (EOF != (ch = getopt(argc, argv, "n")))
+    {
+      switch (ch)
+        {
+        case 'n':
+          opt_n = 1;
+          break;
+        default:
+          usage(argv[0]);
+          return 0;
+        }
+    }
+
+  char **p = argv + optind;
+  
+  while (*p)
+    {
+      fprintf(stdout, "%s", *p++);
+    }
+  if (!opt_n)
+    {
+      fprintf(stdout, "\n");
+    }
+
+  return 0;
+}
