21cb11c99d86ef44adebea039d6765b45f8d139c
南山竹 <junjiemars@gmail.com>
2021-01-18 18:13:46 +0800

    os: tcp wrapper

---
+++ src/os/tcpwrapper/tw.c
@@ -0,0 +1,12 @@
+#include <_os_.h>
+#include <tcpd.h>
+
+int
+main(int argc, char **argv) {
+  _unused_(argc);
+  _unused_(argv);
+  
+  hosts_access(0);
+
+  return 0;
+}
--- test.sh
+++ test.sh
@@ -26,7 +26,7 @@ cd "${_ROOT_DIR_}"
 
 # check nore
 if [ ! -f "${_ROOT_DIR_%/}/configure" ]; then
-  curl -O "https://raw.githubusercontent.com/junjiemars/nore/master/bootstrap.sh" -o bootstrap.sh
+  curl -O "https://raw.githubusercontent.com/junjiemars/nore/edge/bootstrap.sh" -o bootstrap.sh
   if [ 0 -ne $? ]; then
     echo "!panic: install Nore failed"
     exit 1
