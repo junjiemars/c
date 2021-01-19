f883a7364bd0a09e2cfb624e7301741c20ce1362
南山竹 <junjiemars@gmail.com>
2021-01-19 23:08:48 +0800

    parallel: pthread

--- src/parallel/pthread/b.c
+++ src/parallel/pthread/b.c
@@ -5,26 +5,30 @@
 #define N_THREADS 6
 
 void *
-echo(void *tid) {
+echo(void *tid)
+{
 	fprintf(stdout, "Hello, tid=%d\n", *(int*)tid);
 	pthread_exit(0);
 }
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	_unused_(argc);
 	_unused_(argv);
 
 	pthread_t t[N_THREADS];
 
-	for (long i = 0; i < N_THREADS; i++) {
-		fprintf(stdout, "creating thread %ld\n", i);
-		int r = pthread_create(&t[i], 0, echo, &i);
-		if (r) {
-			fprintf(stderr, "!panic, return code %d\n", r);
-			return -1;
-		}
-	}
+	for (long i = 0; i < N_THREADS; i++)
+    {
+      fprintf(stdout, "creating thread %ld\n", i);
+      int r = pthread_create(&t[i], 0, echo, &i);
+      if (r)
+        {
+          fprintf(stderr, "!panic, return code %d\n", r);
+          return -1;
+        }
+    }
 	pthread_exit(0);
 	
 	return 0;
