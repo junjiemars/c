b8afeaf92af5e9870374e3b0dea4533b58b496f9
南山竹 <junjiemars@gmail.com>
2021-01-19 17:49:42 +0800

    parallel: openmp

--- src/parallel/openmp/for.c
+++ src/parallel/openmp/for.c
@@ -6,26 +6,30 @@
 #  include <omp.h>
 #endif
 
+double calc_pi(long n);
+
 double
-calc_pi(int n) {
+calc_pi(long n)
+{
 	double pi, sum = 0.0, step;
 	double x;
-	int i;
+	long i;
 
-	step = 1.0 / (double) n;
+	step = 1.0 / (double)n;
 
 #ifdef NM_HAVE_OPENMP
-/* msvc error C3015: initialization in OpenMP 'for' statement has
-	 improper form.
-	 openmp does not know c99's for loop.
-*/
+  /* msvc error C3015: initialization in OpenMP 'for' statement has
+     improper form.
+     openmp does not know c99's for loop.
+  */
 #  pragma omp parallel for reduction(+:sum) private(x)
 #endif
-	
-	for (i = 1; i <= n; i++) {
-		x = (i - 0.5) * step;
-		sum = sum + 4.0 / (1.0 + x*x);
-	}
+
+	for (i = 1; i <= n; i++)
+    {
+      x = (i - 0.5) * step;
+      sum = sum + 4.0 / (1.0 + x*x);
+    }
 
 	_unused_(x);
 	pi = step * sum;
@@ -34,16 +38,17 @@ calc_pi(int n) {
 
 
 int
-main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
+main(int argc, char **argv)
+{
+	long n = 100L;
+	if (argc < 2 || 1 != sscanf(argv[1], "%li", &n))
+    {
+      perror(0);
+      return 1;
+    }
 
-	int n = 100;
-	if (argc > 1) {
-		n = atoi(argv[1]);
-	}
 	double pi = calc_pi(n);
-	printf("pi=%f via %i times calc\n", pi, n);
+	printf("pi=%3.10lf via %li times calc\n", pi, n);
 	
 	return 0;
 }
