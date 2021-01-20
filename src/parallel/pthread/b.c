d1d3eb7ec399deda41518e9b4a4e761656bea704
南山竹 <junjiemars@gmail.com>
2021-01-20 17:55:09 +0800

    parallel: pthread checking

--- src/parallel/pthread/b.c
+++ src/parallel/pthread/b.c
@@ -2,17 +2,13 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#if (MSVC)
-#  pragma warning (disable : 2220)
-#endif  /* end of MSVC */
-
 #define N_THREADS 6
 
 void *
 echo(void *tid)
 {
 	fprintf(stderr, "Hello, tid=%d\n", *(int*)tid);
-	pthread_exit(0);
+  return tid;
 }
 
 int
