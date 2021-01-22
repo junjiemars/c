d0c7e9edb17b66be4efac34d7155052d35b7ccc6
南山竹 <junjiemars@gmail.com>
2021-01-22 16:05:05 +0800

    parallel: pthread

--- src/parallel/pthread/b.c
+++ src/parallel/pthread/b.c
@@ -1,14 +1,22 @@
 #include "_parallel_.h"
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-#define N_THREADS 6
+#define N_THREADS 8
+
+typedef struct thread_info_s
+{
+  pthread_t tid;
+  long sn;
+} thread_info_t;
+
 
 void *
-echo(void *tid)
+echo(void *arg)
 {
-	fprintf(stderr, "Hello, tid=%d\n", *(int*)tid);
-  return tid;
+	fprintf(stderr, "Hello, sn=%li\n", ((thread_info_t*) arg)->sn);
+  return &(((thread_info_t*) arg)->sn);
 }
 
 int
@@ -17,19 +25,40 @@ main(int argc, char **argv)
 	_unused_(argc);
 	_unused_(argv);
 
-	pthread_t t[N_THREADS];
+  thread_info_t *tinfo = 0;
+  tinfo = calloc(N_THREADS, sizeof(thread_info_t));
+  if (!tinfo)
+    {
+      perror(0);
+      return 1;
+    }
 
 	for (long i = 0; i < N_THREADS; i++)
     {
       fprintf(stdout, "creating thread %ld\n", i);
-      int r = pthread_create(&t[i], 0, echo, &i);
+      tinfo[i].sn = i;
+      int r = pthread_create(&tinfo[i].tid, 0, echo, &tinfo[i]);
       if (r)
         {
-          fprintf(stderr, "!panic, return code %d\n", r);
-          return -1;
+          perror("!panic, when create");
+          goto clean_exit;
         }
     }
-	pthread_exit(0);
+
+  void *retval;
+  for (long i = 0; i < N_THREADS; i++)
+    {
+      int r = pthread_join(tinfo[i].tid, &retval);
+      if (r)
+        {
+          perror("!panic, when join");
+          continue;
+        }
+      fprintf(stderr, "tid=%li, return %li\n", tinfo[i].tid, *(long*) retval);
+    }
+  
+ clean_exit:
+  free(tinfo);
 	
 	return 0;
 }
