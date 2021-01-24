a55b6bcca7a8124c4659c867cf7697300f1a2b31
南山竹 <junjiemars@gmail.com>
2021-01-25 01:05:55 +0800

    parallel: pthread wait,

--- src/parallel/pthread/wait.c
+++ src/parallel/pthread/wait.c
@@ -9,15 +9,50 @@ typedef struct thread_state_s
   long sn;
 } thread_state_t;
 
-void *race(void *arg);
-
 static pthread_mutex_t mutex;
 static pthread_cond_t  cond;
+static long            race_counter = N_THREAD;
+
+void *race(void *arg);
 
 void *
 race(void *arg)
 {
-  
+  thread_state_t *state = (thread_state_t *) arg;
+  int             rc;
+
+  fprintf(stderr, "> #%02li\n", state->sn);
+
+  rc = pthread_mutex_lock(&mutex);
+  if (rc)
+    {
+      perror("!panic, pthread_mutex_lock");
+      goto exit;
+    }
+
+  sleep(1);
+  fprintf(stderr, ">> #%02li, waiting ...\n", state->sn);
+
+  rc = pthread_cond_wait(&cond, &mutex);
+  if (rc)
+    {
+      perror("!panic, pthread_cond_wait");
+      goto exit_lock;
+    }
+
+  sleep(1);
+  --race_counter;
+  fprintf(stderr, "<< #%02li, counter=%04li\n", state->sn, race_counter);
+
+ exit_lock:
+  rc = pthread_mutex_unlock(&mutex);
+  if (rc)
+    {
+      perror("!panic, pthread_mutex_unlock");
+    }
+  fprintf(stderr, "<< #%02li\n", state->sn);
+ exit:
+  fprintf(stderr, "< #%02li\n", state->sn);
   return arg;
 }
 
@@ -27,8 +62,11 @@ main(int argc, char **argv)
   _unused_(argc);
   _unused_(argv);
 
+  thread_state_t state[N_THREAD];
+  pthread_t thread[N_THREAD];
   int rc;
 
+  /* init mutex */
   rc = pthread_mutex_init(&mutex, 0);
   if (rc)
     {
@@ -36,6 +74,7 @@ main(int argc, char **argv)
       return 1;
     }
 
+  /* init cond */
   rc = pthread_cond_init(&cond, 0);
   if (rc)
     {
@@ -43,12 +82,50 @@ main(int argc, char **argv)
       return 1;
     }
 
+  /* create threads */
+  for (long i = 0; i < N_THREAD; i++)
+    {
+      state[i].sn = i+1;
+      rc = pthread_create(&thread[i], 0, race, &state[i]);
+      if (rc)
+        {
+          perror("!panic, pthread_create");
+          return 1;
+        }
+    }
+
+  /* while (race_counter > 0) */
+  /*   { */
+  /*     sleep(1); */
+  /*   } */
+  /* sleep(2); */
+
+  rc = pthread_cond_broadcast(&cond);
+  if (rc)
+    {
+      perror("!panic, pthread_cond_signal");
+      goto clean_exit;
+    }
+
+  /* /\* join threads *\/ */
+  /* for (long i = 0; i < N_THREAD; i++) */
+  /*   { */
+  /*     rc = pthread_join(thread[i], 0); */
+  /*     if (rc) */
+  /*       { */
+  /*         perror("!panic, pthread_join"); */
+  /*       } */
+  /*   } */
+
+clean_exit:
+  /* destroy cond */
   rc = pthread_cond_destroy(&cond);
   if (rc)
     {
       perror("!panic, pthread_cond_destroy");
     }
 
+  /* destroy mutex */
   rc = pthread_mutex_destroy(&mutex);
   if (rc)
     {
