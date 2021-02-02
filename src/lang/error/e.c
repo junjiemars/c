237d3c1b3f62ffa936001fb4cf1d9513fa38658f
南山竹 <junjiemars@gmail.com>
2021-02-02 21:36:37 +0800

    lang/error

--- src/lang/error/e.c
+++ src/lang/error/e.c
@@ -1,4 +1,4 @@
-#include "_lang_.h"
+#include <_lang_.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -12,43 +12,58 @@
 #  include <errno.h>
 #endif  /* LINUX */
 
-void
-test_open_file(const char *filename) {
-  FILE *out = fopen(filename, "r");
-  if (0 == out) {
-    int e = errno;
-  	if (e) {
-      perror(filename);
-      fprintf(stderr, "#open %s failed, caused by: %s\n",
-              filename, strerror(e));
-      
-      char buf[512];
-      int r = strerror_r(e, buf, sizeof(buf));
-      if (0 == r) {
-        fprintf(stderr, "#r\"%s\"\n", buf);
-      } else {
-        fprintf(stderr, "#strerror_r failed: %s\n", strerror(r));
-      }
-  	}
-  }
+#define BSIZE 512
 
-  if (out) {
-    fclose(out);
-  }
+void
+test_open_file(const char *filename) 
+{
+  FILE *out = fopen(filename, "r");
+  if (out)
+    {
+      fprintf(stderr, "#open %s success\n", filename);
+      goto clean_exit;
+    }
+
+  int e = errno;
+  if (e) 
+    {
+      char buf[BSIZE];
+      snprintf(buf, BSIZE, "!panic, open %s", filename);
+      perror(buf);
+      
+      int r = strerror_r(e, buf, sizeof(buf));
+      if (0 == r) 
+        {
+          fprintf(stderr, "!panic, strerror_r, \"%s\"\n", buf);
+        }
+      else 
+        {
+          fprintf(stderr, "#!panic, strerror, \"%s\"\n", strerror(r));
+        }
+    }
+
+ clean_exit:
+  if (out) 
+    {
+      fclose(out);
+    }
 }
 
 void
-test_sqrt(double x) {
+test_sqrt(double x) 
+{
   double d = sqrt(x);
   printf("%f\n", d);
 }
 
 int
-main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("input a filename\n");
-    return 0;
-  }
+main(int argc, char **argv) 
+{
+  if (argc < 2) 
+    {
+      printf("input a filename\n");
+      return 1;
+    }
 
   test_open_file(argv[1]);
   test_sqrt(-1 * argc);
