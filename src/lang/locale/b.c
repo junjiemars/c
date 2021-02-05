c6ffaa286156f8e0d17faa9ffb2a4d6c2d8719b9
南山竹 <junjiemars@gmail.com>
2021-02-05 21:24:33 +0800

    lang/locale

--- src/lang/locale/b.c
+++ src/lang/locale/b.c
@@ -2,36 +2,44 @@
 #include <locale.h>
 #include <stdio.h>
 #include <time.h>
+#include <assert.h>
+#include <string.h>
 
 
-void print_stuff(void);
-void test_locale(const char*);
+void print_LC_ALL(void);
+void test_setlocale(const char*);
+void test_LC_ALL(void);
 
 void
-print_stuff(void) {
+print_LC_ALL(void)
+{
   static char text[128];
   time_t t = time(0);
   
-  printf("%.2f\n", 1.2);
+  printf("%.3f\n", 31415.9265354);
   
-  strftime(text, sizeof(text), "%A, %B %d, %Y(%x)\n", localtime(&t));
+  strftime(text, sizeof(text), "%A, %B, %d, %Y(%x)\n", localtime(&t));
   puts(text);
 }
 
 void
-test_locale(const char *locale) {
-  printf("LC_ALL=%s:\n----------\n", locale);
+test_setlocale(const char *locale)
+{
+  char *curr;
   setlocale(LC_ALL, locale);
-  print_stuff();
+  curr = setlocale(LC_ALL, NULL);
+  printf("LC_ALL=%s\n----------\n", curr);
+  print_LC_ALL();
 }
 
 int
-main(void) {
-  test_locale("C");
-  test_locale("POSIX");
-  test_locale("en_US");
-  test_locale("zh_CN");
-  test_locale("zh_HK");
-
+main(int argc, char **argv)
+{
+  char *locale = "";
+  if (argc > 1)
+    {
+      locale = argv[1];
+    }
+  test_setlocale(locale);
   return 0;
 }
