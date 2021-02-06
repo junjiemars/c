8e2ff17e661bcb182db9baf85c56c1c3ea134c1d
南山竹 <junjiemars@gmail.com>
2021-02-06 21:18:42 +0800

    lang/signal: list signals

--- src/lang/signal/list/list-sig.c
+++ src/lang/signal/list/list-sig.c
@@ -3,18 +3,21 @@
 #include <stdio.h>
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	_unused_(argc);
 	_unused_(argv);
 
-	for (int i = 0; i < NSIG; i++) {
+	for (int i = 0; i < NSIG; i++)
+    {
 #ifdef DARWIN
-		printf("[%02i] [%s] %s\n", i, sys_signame[i], sys_siglist[i]);
+      printf("[%02i] [%s] %s\n", i, sys_signame[i], sys_siglist[i]);
 #elif LINUX
-		printf("[%02i] [%s]\n", i, sys_siglist[i]);
+      printf("[%02i] [%s]\n", i, sys_siglist[i]);
 #else
+      /* do nothing */
 #endif
-	}
+    }
 	
 	return 0;
 }
