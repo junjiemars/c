d79cbc72b605080b992ec12ac2514a0a9c3b2e42
南山竹 <junjiemars@gmail.com>
2021-02-06 21:28:30 +0800

    lang/signal: fork

--- src/lang/signal/fork/f.c
+++ src/lang/signal/fork/f.c
@@ -5,16 +5,23 @@
 #  include <signal.h>
 #  include <sys/wait.h>
 #  include <unistd.h>
+
+#define PRINT_FLAG(...) fprintf(stderr, "sig_flag = 0x%08x\n", __VA_ARGS__)
+
 static volatile int sig_flag = 0x11;
 
-void on_sigchld(int sig) {
+void on_sigchld(int sig)
+{
 	_unused_(sig);
+  fprintf(stderr, "^ on_sigchld\n");
+
 	sig_flag += 0x1;
 }
-#endif
+#endif  /* end of !MSVC */
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	_unused_(argc);
 	_unused_(argv);
 
@@ -22,13 +29,16 @@ main(int argc, char **argv) {
 	pid_t pid;
 	signal(SIGCHLD, on_sigchld);
 
-	if (0 == (pid = fork())) {
-		sig_flag += 0x2;
-		return 0;
-	}
+  PRINT_FLAG(sig_flag);
+
+	if (0 == (pid = fork()))
+    {
+      sig_flag += 0x2;
+      return 0;
+    }
 	waitpid(pid, 0, 0);
-	printf("sig_flag = 0x%08x\n", sig_flag);
-#endif
+	PRINT_FLAG(sig_flag);
+#endif  /* end of !MSVC */
 	
 	return 0;
 }
