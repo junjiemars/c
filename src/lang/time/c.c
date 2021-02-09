0712ae2a025052cb5f355ab18001a50049d18181
南山竹 <junjiemars@gmail.com>
2021-02-09 12:58:35 +0800

    lang: time

--- src/lang/time/c.c
+++ src/lang/time/c.c
@@ -7,29 +7,35 @@ void timer_wait(double secs);
 double timer_elapsed(const time_t *epoch);
 
 void
-timer_reset(time_t *epoch) {
+timer_reset(time_t *epoch) 
+{
   *epoch = clock();
 }
 
 void
-timer_wait(double secs) {
+timer_wait(double secs) 
+{
   clock_t end = clock() + (clock_t)(secs * CLOCKS_PER_SEC );
-  while (clock() < end) {
-    /* do nothing */
-  }
+  while (clock() < end) 
+    {
+      /* do nothing */
+    }
 }
 
 double
-timer_elapsed(const time_t *epoch) {
-  double elapsed =  (double)(clock() - *epoch) / CLOCKS_PER_SEC;
+timer_elapsed(const time_t *epoch) 
+{
+  double elapsed =  (double) (clock() - *epoch) / CLOCKS_PER_SEC;
   return elapsed;
 }
 
 int
-main(int argc, char **argv) {
-  if (argc < 2) {
-    return 0;
-  }
+main(int argc, char **argv) 
+{
+  if (argc < 2) 
+    {
+      return 0;
+    }
 
   long max;
   sscanf(argv[1], "%ld", &max);
@@ -37,9 +43,10 @@ main(int argc, char **argv) {
   time_t epoch;
   timer_reset(&epoch);
 
-  for (long i = 0; i < max; ++i) {
-    /* do nothing */
-  }
+  for (long i = 0; i < max; ++i) 
+    {
+      /* do nothing */
+    }
   double elapsed = timer_elapsed(&epoch);
   printf("elapsed: %lf secs\n", elapsed);
 
--- src/lang/time/conv.c
+++ src/lang/time/conv.c
@@ -1,20 +1,53 @@
 #include "_lang_.h"
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
+
+char *to_asctime(const time_t *epoch);
 
 char*
-to_asctime(const time_t *epoch) {
+to_asctime(const time_t *epoch) 
+{
   char *c = ctime(epoch);
+  if (!c)
+    {
+      perror(0);
+    }
   return c;
 }
 
 int
-main(int argc, char **argv) {
-  if (argc < 2) {
-    return 0;
-  }
+main(int argc, char **argv) 
+{
   time_t epoch;
-  sscanf(argv[1], "%lu", (unsigned long*)&epoch);
+  if (argc < 2) 
+    {
+      epoch = 0;
+      if ((time_t) -1 == time(&epoch))
+        {
+          perror(0);
+          return 1;
+        }
+      printf("%lu\n", epoch);
+      return 0;
+    }
+
+  if (0 == strncmp("-", argv[1], sizeof(int)))
+    {
+      if (EOF == fscanf(stdin, "%lu", (unsigned long*) &epoch))
+        {
+          perror(0);
+          return 1;
+        }
+      printf("%s", to_asctime(&epoch));      
+      return 0;
+    }
+
+  if (EOF == sscanf(argv[1], "%lu", (unsigned long*) &epoch))
+    {
+      perror(0);
+      return 1;
+    }
   printf("%s", to_asctime(&epoch));
 
   return 0;
