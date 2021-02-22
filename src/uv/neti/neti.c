7f3a09d56c8fb4f3cf29589046666333035871eb
南山竹 <junjiemars@gmail.com>
2021-02-22 18:16:18 +0800

    uv: neti

--- src/uv/neti/neti.c
+++ src/uv/neti/neti.c
@@ -1,8 +1,11 @@
 #include "_uv_.h"
 #include <stdio.h>
 
+static void test_neti(void);
+
 void
-test_neti(void) {
+test_neti(void)
+{
   char buf[512];
   uv_interface_address_t *info;
   int count, i;
@@ -10,26 +13,29 @@ test_neti(void) {
   uv_interface_addresses(&info, &count);
   i = count;
 
-  while (i--) {
-    uv_interface_address_t inet = info[i];
-    /* if (inet.is_internal) { */
-    /*   continue; */
-    /* } */
-
-    if (AF_INET == inet.address.address4.sin_family) {
-      uv_ip4_name(&inet.address.address4, buf, sizeof(buf));
-    } else if (AF_INET6 == inet.address.address4.sin_family) {
-      uv_ip6_name(&inet.address.address6, buf, sizeof(buf));
-    } else {
-      continue;
+  while (i--)
+    {
+      uv_interface_address_t inet = info[i];
+      if (AF_INET == inet.address.address4.sin_family)
+        {
+          uv_ip4_name(&inet.address.address4, buf, sizeof(buf));
+        }
+      else if (AF_INET6 == inet.address.address4.sin_family)
+        {
+          uv_ip6_name(&inet.address.address6, buf, sizeof(buf));
+        }
+      else
+        {
+          continue;
+        }
+      fprintf(stderr, "%s\n", buf);
     }
-    fprintf(stderr, "%s\n", buf);
-  }
 
   uv_free_interface_addresses(info, count);
 }
 
 int
-main(void) {
+main(void)
+{
   test_neti();
 }
