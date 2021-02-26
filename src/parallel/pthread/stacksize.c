f7723083950d1199515fbde0d0b8f222cc775b91
南山竹 <junjiemars@gmail.com>
2021-02-27 02:50:07 +0800

    parallel/pthread: stack size

--- src/parallel/pthread/stacksize.c
+++ src/parallel/pthread/stacksize.c
@@ -2,14 +2,8 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#if (DARWIN) || (LINUX)
-#  include <unistd.h>
-#  include <limits.h>
-#elif (WINNT)
-#  include <process.h>
-#  define sleep(x) Sleep((x) * 1000)
-#endif
 
 #define N_THREAD 4
 
@@ -17,48 +11,52 @@ typedef struct thread_state_s
 {
   long sn;
   pthread_attr_t attr;
-} thread_state_t;
+} thread_state_s;
 
 void *check_stacksize(void *arg);
 
 void*
 check_stacksize(void *arg)
 {
-  thread_state_t *state = (thread_state_t*) arg;
-  size_t stacksize;
-  int r = pthread_attr_getstacksize(&state->attr, &stacksize);
-  if (r)
+  thread_state_s *state = (thread_state_s*) arg;
+  size_t          stacksize;
+  int             rc;
+
+  rc = pthread_attr_getstacksize(&state->attr, &stacksize);
+  if (rc)
     {
       perror("!panic, check_stacksize");
     }
+
   fprintf(stderr, "#%02li, tid=0x%016zx, stacksize=0x%08zx\n",
           state->sn,
           (long) pthread_self(),
           stacksize);
+
   return state;
 }
 
 int
-main(int argc, char **argv) 
+main(int argc, char **argv)
 {
   _unused_(argc);
   _unused_(argv);
-  
-  pthread_t threads[N_THREAD];
-  thread_state_t states[N_THREAD];
+
+  pthread_t      threads[N_THREAD];
+  thread_state_s states[N_THREAD];
   pthread_attr_t attr;
-  size_t stacksize;
-  int r;
+  size_t         stacksize;
+  int            rc;
 
   /* check default stacksize */
-  r = pthread_attr_init(&attr);
-  if (r)
+  rc = pthread_attr_init(&attr);
+  if (rc)
     {
       perror(0);
       return 1;
     }
-  r = pthread_attr_getstacksize(&attr, &stacksize);
-  if (r)
+  rc = pthread_attr_getstacksize(&attr, &stacksize);
+  if (rc)
     {
       perror("!panic, pthread_attr_getstacksize");
       return 1;
@@ -66,33 +64,36 @@ main(int argc, char **argv)
   fprintf(stderr, "#00, tid=0x%016zx, stacksize=0x%08zx\n",
           (long) pthread_self(),
           stacksize);
-  r = pthread_attr_destroy(&attr);
-  if (r)
+  rc = pthread_attr_destroy(&attr);
+  if (rc)
     {
       perror("!panic, pthread_attr_destroy");
       return 1;
     }
-  
+
   /* create threads */
   for (long i = 0; i < N_THREAD; i++)
     {
       stacksize = PTHREAD_STACK_MIN * (i+1);
-      r = pthread_attr_init(&states[i].attr);
-      if (r)
+      rc = pthread_attr_init(&states[i].attr);
+      if (rc)
         {
           perror("!panic, pthread_attr_init");
           return 1;
         }
-      r = pthread_attr_setstacksize(&states[i].attr, stacksize);
-      if (r)
+      rc = pthread_attr_setstacksize(&states[i].attr, stacksize);
+      if (rc)
         {
           perror("!panic, pthread_attr_setstacksize");
           return 1;
         }
 
       states[i].sn = i+1;
-      r = pthread_create(&threads[i], &states[i].attr, check_stacksize, &states[i]);
-      if (r)
+      rc = pthread_create(&threads[i],
+                          &states[i].attr,
+                          check_stacksize,
+                          &states[i]);
+      if (rc)
         {
           perror("!panic, pthread_create");
           return 1;
@@ -102,13 +103,13 @@ main(int argc, char **argv)
   /* join threads */
   for (long i = 0; i < N_THREAD; i++)
     {
-      r = pthread_join(threads[i], 0);
-      if (r)
+      rc = pthread_join(threads[i], 0);
+      if (rc)
         {
           perror("!panic, pthread_join");
         }
-      r = pthread_attr_destroy(&states[i].attr);
-      if (r)
+      rc = pthread_attr_destroy(&states[i].attr);
+      if (rc)
         {
           perror("!panic, pthread_attr_destroy");
         }
