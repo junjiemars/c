f281955dab503b5888312dc14064c4a0d8282fa8
Ubuntu <findidiot@icloud.com>
2021-03-06 11:58:52 +0000

    io: _strncpy specified bound N equals destination size

--- src/lang/env/var/var.c
+++ src/lang/env/var/var.c
@@ -5,27 +5,33 @@
 
 
 int 
-main(int argc, const char **argv) {
+main(int argc, const char **argv) 
+{
 	printf("----------\n");
 	printf("#your can set COUNT, TEXT via command-line args or env vars\n");
 
 	int count = 3;
 	char text[32] = "Hello.";
 
-	for (int i=1; i < argc; ++i) {
-		sscanf(argv[i], "--COUNT=%i", &count);
-		sscanf(argv[i], "--TEXT=%s", text);
-	}
+	for (int i=1; i < argc; ++i) 
+    {
+      sscanf(argv[i], "--COUNT=%i", &count);
+      sscanf(argv[i], "--TEXT=%s", text);
+    }
 
 	char *c = getenv("COUNT");
 	if (c) count = atoi(c);
 
 	char *text_env = getenv("TEXT");
-	if (text_env) strncpy(text, text_env, sizeof(text)/sizeof(text[0]));
+	if (text_env)
+    {
+      strncpy(text, text_env, sizeof(text)/sizeof(text[0])-1);
+    }
 
-	for (int i=0; i < count; i++) {
-		printf("%s\n", text);
-	}
+	for (int i = 0; i < count; i++) 
+    {
+      printf("%s\n", text);
+    }
 	putchar('\n');
 
 	return 0;
