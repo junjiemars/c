bfb88e4ca61488b138fba15c42d422f2ebc11574
南山竹 <junjiemars@gmail.com>
2021-03-13 11:21:37 +0800

    nginx

--- src/nginx/hello/ngx_http_hello_module.c
+++ src/nginx/hello/ngx_http_hello_module.c
@@ -3,8 +3,7 @@
 #include <ngx_http.h>
 
 static ngx_int_t ngx_http_hello_handler(ngx_http_request_t *r);
-static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd,
-    void *conf);
+static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_command_t  ngx_http_hello_commands[] = {
     { ngx_string("hello"),
