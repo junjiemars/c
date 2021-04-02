10ea9cad65d5199a515353b660e1443799f3ac5d
南山竹 <junjiemars@gmail.com>
2021-04-02 16:24:18 +0800

    lang: restrict

--- src/lang/restrict/r1.c
+++ src/lang/restrict/r1.c
@@ -1,52 +1,53 @@
+/* configure --has-lang --with-optimize=-O1 */
+
 #include <_lang_.h>
 #include <stdio.h>
 
-/* ./configure --has-lang --with-optimize=-O1 */
 
-
-#if !( NM_HAVE_RESTRICT_KEYWORD )
-#  if ( NM_HAVE___RESTRICT_KEYWORD )
+#if !(NM_HAVE_RESTRICT)
+#  if (NM_HAVE___RESTRICT)
 #    define restrict __restrict
-#  else
-#    define restrict
 #  endif
-#endif
+#endif  /* NM_HAVE_RESTRICT */
 
+
+static void f1(int *p1, int *p2, int *val);
+static void f2(int *p1, int *p2, int *restrict val);
 
 void
 f1(int *p1, int *p2, int *val) {
-	*p1 += *val;
-	*p2 += *val;
+    *p1 += *val;
+    *p2 += *val;
 }
 
 void
 f2(int *p1, int *p2, int *restrict val) {
-	*p1 += *val;
-	*p2 += *val;
+    *p1 += *val;
+    *p2 += *val;
 }
 
 
 int
 main(int argc, char **argv) {
-	_unused_(argv);
+    _unused_(argv);
 
-	printf("support restrict keyword = %s\n----------\n",
-#if ( NM_HAVE_RESTRICT_KEYWORD ) || ( NM_HAVE___RESTRICT_KEYWORD )
-				 "yes"
+    printf("support restrict keyword = %s\n----------\n",
+#if (NM_HAVE_RESTRICT) || (NM_HAVE___RESTRICT)
+           "yes"
 #else
-				 "no"
+           "no"
 #endif
-				 );
+        );
 
-	int x = argc;
-	int y = argc * 10;
-	int z = argc * 100;
+    int x = argc;
+    int y = argc * 10;
+    int z = argc * 100;
 	
-	f1(&x, &y, &z);
-	printf("x=%d, y=%d, z=%d\n", x, y, z);
+    f1(&x, &y, &z);
+    printf("x=%d, y=%d, z=%d\n", x, y, z);
 
-	f2(&x, &y, &z);
-	printf("x=%d, y=%d, z=%d\n", x, y, z);
+    f2(&x, &y, &z);
+    printf("x=%d, y=%d, z=%d\n", x, y, z);
 
-	return 0;
+    return 0;
 }
