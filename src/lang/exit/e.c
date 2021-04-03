69722ea2be338be63605dd16fd0cd6b4e9a6187b
南山竹 <junjiemars@gmail.com>
2021-04-03 20:39:27 +0800

    lang: exit

--- src/lang/exit/e.c
+++ src/lang/exit/e.c
@@ -2,26 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+static void on_exit_first(void);
+static void on_exit_second(void);
+
+
 void
-on_exit_first(void) {
-	printf("----------\n");
-	printf("existing first ...\n");
+on_exit_first(void)
+{
+    printf("----------\n");
+    printf("existing first ...\n");
 }
 
 void 
-on_exit_second(void) {
-	printf("----------\n");
-	printf("existing second ...\n");
+on_exit_second(void)
+{
+    printf("----------\n");
+    printf("existing second ...\n");
 }
 
+
 int
-main(int argc, char **argv) {
-  _unused_(argc);
-  _unused_(argv);
+main(int argc, char **argv)
+{
+    _unused_(argc);
+    _unused_(argv);
 
-  atexit(&on_exit_second);
-  atexit(&on_exit_first);
+    if (atexit(&on_exit_first)) {
+        perror("1st");
+        return 1;
+    }
 
-  printf("calling exit() ...\n");
-  exit(EXIT_SUCCESS);
+    if (atexit(&on_exit_second)) {
+        perror("2nd");
+        return 1;
+    }
+
+    printf("calling exit() ...\n");
+    exit(EXIT_SUCCESS);
 }
