ca220690cebf30808a1bed53f2b33220901bbaa2
南山竹 <junjiemars@gmail.com>
2021-04-12 23:04:51 +0800

    lang: unsetenv, putenv

--- src/lang/env/set.c
+++ src/lang/env/set.c
@@ -11,9 +11,10 @@ static void test_setenv(const char *name, const char *value);
 void
 test_setenv(const char *name, const char *value)
 {
-    int          rc;
-    char        *env;
-    char        new_value[N] = {0};
+    int    rc;
+    char  *env;
+    char   new_value[N]  =  {0};
+    char   new_env[N]    =  {0};
     
     rc = setenv(name, value, 0);
     if (rc) {
@@ -23,15 +24,31 @@ test_setenv(const char *name, const char *value)
     env = getenv(name);
     printf("%s=%s\n", name, env);
 
-    strcpy(&new_value[0], value);
-    strcat(&new_value[0], "Zzz");
+    strcpy(new_value, value);
+    strcat(new_value, "Zzz");
     rc = setenv(name, new_value, 1);
     if (rc) {
         perror(0);
         return;
     }
-
     printf("%s=%s\n", name, new_value);
+
+    sprintf(new_env, "%s=%s123", name, new_value);
+    rc = putenv(new_env);
+    if (rc) {
+        perror(0);
+        return;
+    }
+    env = getenv(name);
+    printf("%s=%s\n", name, env);
+
+    rc = unsetenv(name);
+    if (rc) {
+        perror(0);
+        return;
+    }
+    env = getenv(name);
+    printf("%s=%s\n", name, env);
 }
 
 int 
