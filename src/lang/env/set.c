162eb116a3a7b962a06a4b4b3bf0c72ca68cf292
南山竹 <junjiemars@gmail.com>
2021-04-12 22:17:43 +0800

    lang: setenv

--- src/lang/env/set.c
+++ src/lang/env/set.c
@@ -3,40 +3,55 @@
 #include <stdio.h>
 #include <string.h>
 
+#define N  32
 
-static void test_setenv(const char *name, const char *value, int overwrite);
+static void test_setenv(const char *name, const char *value);
 
 
 void
-test_setenv(const char *name, const char *value, int overwrite)
+test_setenv(const char *name, const char *value)
 {
-    int    rc;
-    char  *var;
+    int          rc;
+    char        *env;
+    char        *new_value  =  0;
     
-    rc = setenv(name, value, overwrite);
+    rc = setenv(name, value, 0);
+    if (rc) {
+        perror(0);
+        return;
+    }
+    env = getenv(name);
+    printf("%s=%s\n", name, env);
+
+    new_value = malloc(N + 1);
+    if (!new_value) {
+        perror(0);
+        goto clean_exit;
+    }
+
+    strcpy(&new_value[0], value);
+    strcat(&new_value[0], "Zzz");
+    rc = setenv(name, new_value, 1);
     if (rc) {
         perror(0);
         return;
     }
 
-    var = getenv(name);
-    printf("%s=%s\n", name, var);
+    printf("%s=%s\n", name, new_value);
+
+clean_exit:
+    free(new_value);
 }
 
 int 
 main(int argc, const char **argv) 
 {
-    const char  *name, *value;
-
     if (argc < 3) {
         fprintf(stderr, "please, input an env, such as NAME VALUE\n");
         return 1;
     }
 
-    name = argv[1];
-    value = argv[2];
+    test_setenv(argv[1], argv[2]);
 
-    test_setenv(name, value, 0);
-  
     return 0;
 }
