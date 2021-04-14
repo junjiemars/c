2ec461d97564a91e339b2e496522359212fab0de
南山竹 <junjiemars@gmail.com>
2021-04-14 14:25:18 +0800

    ffi: on Windows

--- src/lang/env/set.c
+++ src/lang/env/set.c
@@ -5,6 +5,10 @@
 
 #define N  32
 
+#if (MSVC) && (WINNT)
+#  define setenvx(name, value, _)  _putenv_s(name, value)
+#endif  /* MSVC */
+
 static void test_setenv(const char *name, const char *value);
 
 
