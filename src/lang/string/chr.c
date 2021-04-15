a64b38060837ea8d288c96ba5a6669174cd2129b
南山竹 <junjiemars@gmail.com>
2021-04-15 20:35:53 +0800

    lang: string/strchr

--- src/lang/string/chr.c
+++ src/lang/string/chr.c
@@ -3,66 +3,79 @@
 #include <stdio.h>
 
 
-#if MSVC
-#  pragma warning(disable : 4996)
-#endif
+#if (MSVC)
+#pragma warning(disable : 4996)
+#endif  /* MSVC */
+
 
 typedef char *(*strchr_fn)(const char *, int);
 typedef char *(*strrchr_fn)(const char *, int);
 
-char *self_strchr(const char *, int);
-char *self_strrchr(const char *, int);
+
+static char *self_strchr(const char *, int);
+static char *self_strrchr(const char *, int);
 
 static void test_strchr(strchr_fn, char *, int);
 
+
 void
-test_strchr(strchr_fn fn, char *ss, int c) {
-  char *s1 = fn(ss, c);
-  if (s1) {
-    fprintf(stdout, "%s\n", s1);
-  } else {
-    fprintf(stdout, "%c: no found\n", c);
-  }
+test_strchr(strchr_fn fn, char *ss, int c)
+{
+    char  *s1;
+
+    s1 = fn(ss, c);
+    if (s1) {
+        fprintf(stdout, "%s\n", s1);
+
+    } else {
+        fprintf(stdout, "%c: no found\n", c);
+    }
 }
 
 char *
-self_strchr(const char *s, int c) {
-  while (*s) {
-    if (c == *s) {
-      return (char *)s;
+self_strchr(const char *s, int c)
+{
+    while (*s) {
+        if (c == *s) {
+            return (char *) s;
+        }
+        s++;
     }
-    s++;
-  }
-  return 0;
+    return 0;
 }
 
 char *
 self_strrchr(const char *s, int c) {
-  const char *pre = 0;
-  while (*s) {
-    if (c == *s) {
-      pre = s;
+    const char  *pre  =  0;
+
+    while (*s) {
+        if (c == *s) {
+            pre = s;
+        }
+        s++;
     }
-    s++;
-  }
-  return (char *)pre;
+    return (char *) pre;
 }
 
 int
-main(int argc, char **argv) {
-	if (argc < 2) {
-		printf("what string and found what\n");
-		return 0;
-	}
+main(int argc, char **argv)
+{
+    char  *ss;
+    int    c;
 
-  char *ss = argv[1];
-  int c = argv[2][0];
-  
-  test_strchr(strchr, ss, c);
-  test_strchr(strrchr, ss, c);
-  
-  test_strchr(self_strchr, ss, c);
-  test_strchr(self_strrchr, ss, c);
+    if (argc < 2) {
+        printf("what string and found what\n");
+        return 0;
+    }
 
-  return 0;
+    ss = argv[1];
+    c = argv[2][0];
+  
+    test_strchr(strchr, ss, c);
+    test_strchr(strrchr, ss, c);
+  
+    test_strchr(self_strchr, ss, c);
+    test_strchr(self_strrchr, ss, c);
+
+    return 0;
 }
