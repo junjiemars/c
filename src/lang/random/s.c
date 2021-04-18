a6c59c386016846e50def9237c03f414a29d34d0
南山竹 <junjiemars@gmail.com>
2021-04-18 20:39:55 +0800

    lang: secure random

--- src/lang/random/s.c
+++ src/lang/random/s.c
@@ -4,39 +4,69 @@
 #include <string.h>
 #include <limits.h>
 
-#if NM_HAVE_RAND_S_FN
-#  if MSVC
-#    define _CRT_RAND_S
-#  endif
+
+#if (NM_HAVE_ARC4RANDOM_UNIFORM) || (NM_HAVE_CRT_RAND_S)
+#  define NM_HAVE_RAND_S 1
 #endif
+
+
+#if (NM_HAVE_CRT_RAND_S)
+#  define _CRT_RAND_S
+#endif  /* NM_HAVE_CRT_RAND_S */
+
+
 #include <stdlib.h>
 
-#if NM_HAVE_RAND_S_FN
-int
-ranged_randomize(int min, int max) {
-	unsigned int x;
-	errno_t e = rand_s(&x);
-	if (e) {
-		return UINT_MAX;
-	}
-	x = (unsigned int)(x / (UINT_MAX + 1.0) * (max - min) + min);
-	return x;
+
+static unsigned int __rand_s(unsigned int *u);
+
+
+unsigned int
+__rand_s(unsigned int *u)
+{
+#if (NM_HAVE_ARC4RANDOM_UNIFORM)
+    return arc4random_uniform(*u);
+
+#endif  /* NM_HAVE_ARC4RANDOM_UNIFORM */
+
+
+#if (NM_HAVE_CRT_RAND_S)
+    (error_t) rand_s(u);
+    return *u;
+
+#endif  /* NM_HAVE_RAND_S */ 
+
+    return (unsigned int) 0;
 }
-#endif
+
 
 int
-main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
+main(int argc, char **argv)
+{
+    size_t        n;
+    unsigned int  u = 10, r;
 
-#if NM_HAVE_RAND_S_FN
-	printf("rand_s [-10, 10)\n");
-	printf("--------------------\n");
-	for (int i = 0; i < 10; i++) {
-		printf("%i ", ranged_randomize(-10, 10));
-	}
-	putchar('\n');
-#endif
+    if (argc < 2) {
+        fprintf(stderr, "please input: <N>\n");
+        return 1;
+    }
+
+    n = atoi(argv[1]);
+
+
+#if (NM_HAVE_RAND_S)
+
+    printf("raw random [%i, %zu)\n", 0, (size_t) u);
+    printf("--------------------\n");
+
+    for (size_t i = 0; i < n; i++) {
+        r = __rand_s(&u);
+        printf("%i ", r);
+    }
+    putchar('\n');
+
+
+#endif  /* NM_HAVE_RAND_S */
 	
-	return 0;
+    return 0;
 }
