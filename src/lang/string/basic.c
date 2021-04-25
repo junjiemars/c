1152c9eeb9016b20d37f6fba12851656f65ba997
南山竹 <junjiemars@gmail.com>
2021-04-25 14:49:06 +0800

    lang/string

--- src/lang/string/basic.c
+++ src/lang/string/basic.c
@@ -20,7 +20,7 @@ self_strcmp(const char *s1, const char *s2)
     while (*s1 && (*s1 == *s2)) {
         s1++, s2++;
     }
-    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+    return *(const unsigned char*) s1 - *(const unsigned char*) s2;
 }
 
 char *
@@ -41,8 +41,8 @@ self_strcat(char *s1, const char *s2)
 void
 test_empty(void)
 {
-    char *s = "";
-    char a[] = "";
+    char  *s  =  "";
+    char a[]  =  "";
     assert(0 == strlen(s));
     assert(0 == strlen(a));
 
@@ -55,7 +55,7 @@ test_empty(void)
 void
 test_static_len(void)
 {
-    assert((sizeof("abc") - 1)== strlen("abc"));
+    assert((sizeof("abc") - 1) == strlen("abc"));
 }
 
 void
@@ -63,6 +63,7 @@ test_strlen(char *s)
 {
     size_t len1 = strlen(s);
     size_t len2 = self_strlen(s);
+
     assert((len1 == len2) && "self_strlen() failed");
 #if NDEBUG
     _unused_(len1);
@@ -73,42 +74,38 @@ test_strlen(char *s)
 void
 test_strcpy(void)
 {
-    char buf1[8];
-    int len = sizeof(buf1)/sizeof(*buf1);
+    char  buf1[8], buf2[8];
+    int   len  =  sizeof(buf1)/sizeof(*buf1);
+
     memset(buf1, 0x11, len);
-    assert(0x11 == buf1[len-1]
-           && "strcpy(), 0x11 != <tailing>");
+    assert(0x11 == buf1[len-1] && "strcpy(), 0x11 != <tailing>");
 
     strcpy(buf1, "");
-    assert(0 == strlen(buf1)
-           && "strcpy(), copy empty string failed");
+    assert(0 == strlen(buf1) && "strcpy(), copy empty string failed");
 
-    /* does not pad tailing 0 */
     strcpy(buf1, "abc");
-    assert(0 == buf1[strlen(buf1)]
-           && "strcpy(), 0 != <tailing>");
-    assert(0x11 == buf1[len-1]
-           && "strcpy(), 0x11 != <tailing>");
-
-    char buf2[8];
+    assert(0 == buf1[strlen(buf1)] && "strcpy(), 0 != <tailing>");
+    assert(0x11 == buf1[len-1] && "strcpy(), 0x11 != <tailing>");
+    
     self_strcpy(buf2, "abc");
-    assert(0 == strncmp(buf1, buf2, len)
-           && "self_strcpy(), 0 != <tailing>");
+    assert(0 == strncmp(buf1, buf2, len) && "self_strcpy(), 0 != <tailing>");
 }
 
 void
 test_strcmp(char *s)
 {
-    size_t len = strlen(s);
-    char *d1 = malloc(len+1);
-    char *d2 = strcpy(d1, s);
+    size_t   len  =  strlen(s);
+    char    *d1   =  malloc(len+1);
+    char    *d2   =  strcpy(d1, s);
+    char     buf[8];
+
     assert(d1 == d2 && "strcpy(), diff between dest and retval");
     assert(0 == strcmp(s, d1)
            && 0 == self_strcmp(s, d1)
            && 0 == self_strcmp(d1, s)
            && "self_strcpy(), failed");
 
-    char buf[8];
+
     d2 = buf;
     strcpy(d1, "abc");
     strcpy(d2, "abx");
@@ -126,21 +123,22 @@ void
 test_strcat(void)
 {
     char buf1[8], buf2[8];
+
     memset(buf1, 0, sizeof(buf1)/sizeof(*buf1));
     memset(buf2, 0, sizeof(buf2)/sizeof(*buf2));
+
     strcat(&buf1[0], "abc");
-    assert(0 == strcmp(buf1, "abc")
-           && "strcat(), failed");
+    assert(0 == strcmp(buf1, "abc") && "strcat(), failed");
     _unused_(buf2);
+
     self_strcat(buf2, "abc");
-    assert(0 == strcmp(buf2, buf1)
-           && "self_strcat(), failed");
+    assert(0 == strcmp(buf2, buf1) && "self_strcat(), failed");
+
     self_strcat(buf2, "");
-    assert(0 == strcmp(buf2, buf1)
-           && "self_strcat(), failed");
+    assert(0 == strcmp(buf2, buf1) && "self_strcat(), failed");
+
     self_strcat(buf2, "x");
-    assert(0 == strcmp(buf2, "abcx")
-           && "self_strcat(), failed");
+    assert(0 == strcmp(buf2, "abcx") && "self_strcat(), failed");
 }
 
 int
