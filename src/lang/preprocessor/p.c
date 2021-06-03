6c79e1cd6f2c3261f3acc431d5c0f4b715cf6c61
南山竹 <junjiemars@gmail.com>
2021-06-03 21:40:45 +0800

    lang: preprocessor

--- src/lang/preprocessor/p.c
+++ src/lang/preprocessor/p.c
@@ -1,10 +1,16 @@
 #include "_lang_.h"
 #include <stdio.h>
 
-#ifdef MAX
-#  undef MAX
+#ifdef max
+#  undef max
 #endif
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+#ifdef min
+#  undef min
+#endif
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
 
 #ifdef E
 #  undef E
@@ -13,27 +19,32 @@
 double e_squared = E * E;
 #undef E
 
-#ifdef MIN
-#  undef MIN
-#endif
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 #if defined( _ERROR_ ) && ( 1 == _ERROR_ )
 #  error "compile failed: because _ERROR_==1 is true"
 #endif
 
+
 #if defined(MSVC)
 #  pragma warning(disable:4996) /*_CRT_SECURE_NO_WARNINGS*/
 #endif
+
 
 #if (GCC)
 #  pragma GCC diagnostic ignored "-Wstrict-aliasing"  /* (unsigned init*)&f: */
 #endif
 
-void
-_log_(const char *header, const char *message) {
-	fprintf(stderr, "%s: %s\n", header, message);
-}
+
+#if defined(str)
+#  undef str
+#endif
+#define str(x)  #x
+
+#if defined(log)
+#  undef log
+#endif
+#define log(h, m) fprintf(stderr, "%s: %s\n", (h), (str(m)));
+
 
 enum color {
 	RED,
@@ -42,17 +53,19 @@ enum color {
 };
 #define COLOR_STR(x) #x
 
-#define DEFVAR(type, var, val) type var_##var = val
+
+#define defvar(type, var, val) type var_##var = val
 
 
 int
-main(int argc, char *argv[]) {
+main(int argc, char *argv[])
+{
 	_unused_(argc);
 	_unused_(argv);
 
 	char *platform = 
 #if defined(LINUX)
-  "Linux";
+    "Linux";
 #elif defined(DARWIN)
 	"Darwin";
 #elif defined(WINNT)
@@ -63,7 +76,7 @@ main(int argc, char *argv[]) {
 
   char *compiler =
 #if (CLANG)
-  "clang";
+    "clang";
 #elif (GCC)
   "gcc";
 #elif (MSVC)
@@ -74,27 +87,28 @@ main(int argc, char *argv[]) {
 
 	printf("platform: %s\n", platform);
   printf("compiler: %s\n", compiler);
-	printf("max: %i in [%i %i]\n", MAX(0,1), 0, 1);
+	printf("max: %i in [%i %i]\n", max(0, 1), 0, 1);
+	printf("min: %i in [%i %i]\n", min(0, 1), 0, 1);
+  printf("min + max: %i in [%i %i]\n", min(0, 1) + max(0, 1), 0, 1);
 	printf("E^2: %f\n", e_squared);
-	printf("min: %i in [%i %i]\n", MIN(0,1), 0, 1);
 
-	_log_("__FILE__", __FILE__);
+	log("__FILE__", __FILE__);
 
-  char s[8] = {0};
-	snprintf(s, sizeof(s), "%i", __LINE__);
-	_log_("__LINE__", s);
+  /* char s[8] = {0}; */
+	/* snprintf(s, sizeof(s), "%i", __LINE__); */
+	log("__LINE__", __LINE__);
 
-	_log_("__DATE__", __DATE__);
-	_log_("__TIME__", __TIME__);
-	_log_("__TIMESTAMP__", __TIMESTAMP__);
+	log("__DATE__", __DATE__);
+	log("__TIME__", __TIME__);
+	log("__TIMESTAMP__", __TIMESTAMP__);
 
 	printf("color: %s\n", COLOR_STR(RED));
 	printf("color: %s\n", COLOR_STR(BLUE));
 
-	DEFVAR(int, x, 1);
-	DEFVAR(double, y, e_squared);
-	printf("defvar x=%i\n", var_x);
-	printf("defvar y=%f\n", var_y);
+	defvar(int, x, 1);
+	defvar(double, y, e_squared);
+	printf("defvar var_x=%i\n", var_x);
+	printf("defvar var_y=%f\n", var_y);
 
 	return 0;
 }
