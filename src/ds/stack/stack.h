b349b0ccd49b878afea18ae04bb4ee8fb7120f98
南山竹 <junjie.mars@icloud.com>
2021-07-29 19:42:05 +0800

    ds: stack refine

---
+++ src/ds/stack/stack.h
@@ -0,0 +1,111 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define stack_empty(s)  (s->top == s->data)
+#define stack_full(s)                                                 \
+  ((s->n * s->width) == (size_t) ((char*) s->top - (char*) s->data))
+
+
+typedef void  (*free_fn)(void *ptr);
+
+
+typedef struct {
+  size_t    n;
+  size_t    width;
+  free_fn   free;
+  void     *top;
+  void     *data;
+} stack_s;
+
+
+stack_s *stack_new(stack_s *stack, size_t n, size_t width, free_fn free);
+void stack_free(stack_s *const stack);
+void *stack_push(stack_s *const stack, void *item);
+void *stack_pop(stack_s *const stack, void *item);
+void *stack_peek(stack_s *const stack, void *item);
+
+
+stack_s *
+stack_new(stack_s *stack, size_t n, size_t width, free_fn free)
+{
+  if (NULL == stack)
+    {
+      return NULL;
+    }
+
+  stack->n = n;
+  stack->width = width;
+  stack->free = free;
+  stack->top = stack->data = malloc(n * width);
+  
+  if (stack->top)
+    {
+      return stack;
+    }
+  return NULL;
+}
+
+void
+stack_free(stack_s *const stack)
+{
+  if (NULL == stack)
+    {
+      return;
+    }
+
+  if (stack->free)
+    {
+      char *base = (char *) stack->data;
+      char *top = (char *) stack->top;
+      for (char **p = &base; *p < top; *p += stack->width)
+        {
+          stack->free(*p);
+        }
+    }
+  free(stack->data);
+}
+
+void *
+stack_push(stack_s *const stack, void *item)
+{
+  if (stack_full(stack))
+    {
+      size_t offset = (char *) stack->top - (char *) stack->data;
+      stack->data = realloc(stack->data, stack->n * stack->width * 2);
+      stack->top = (char *) stack->data + offset;
+    }
+  memcpy(stack->top, item, stack->width);
+  stack->top = (char *) stack->top + stack->width;
+  return item;
+}
+
+void *
+stack_pop(stack_s *const stack, void *item)
+{
+  if (stack_empty(stack))
+    {
+      return NULL;
+    }
+  stack->top = (char *) stack->top - stack->width;
+  memcpy(item, stack->top, stack->width);
+  return item;
+}
+
+void *
+stack_peek(stack_s *const stack, void *item)
+{
+  if (stack_empty(stack))
+    {
+      return NULL;
+    }
+  memcpy(item, (char *) stack->top - stack->width, stack->width);
+  return item;
+}
+
+
+#endif /* end of _STACK_H_ */
---
+++ src/ds/stack/test.c
@@ -0,0 +1,201 @@
+#include "_ds_.h"
+#include "stack.h"
+#include <stdio.h>
+
+#if (MSVC)
+#  pragma warning(disable:4267) /* conversion from 'size_t' to 'int' */
+#endif
+
+
+static void test_stack_new_free_int(void);
+static void test_stack_push_pop_int(stack_s *s);
+
+static void test_stack_push_pop_int(stack_s *s);
+static void test_stack_push_pop_str(stack_s *s);
+
+static void test_stack_push_pop_ptr(stack_s *s);
+
+void
+test_stack_new_free_int(void)
+{
+  stack_s   s1;
+  stack_s  *s2;
+
+  s2 = stack_new(NULL, 4, sizeof(int), NULL);
+  stack_free(s2);
+
+  stack_new(&s1, 4, sizeof(int), NULL);
+  stack_free(&s1);
+}
+
+void
+test_stack_push_pop_int(stack_s *s)
+{
+  int         n;
+  int         i;
+
+  stack_new(s, 4, sizeof(int), NULL);
+  n = s->n + 3;
+  i = 0;
+
+  printf("stack int push/pop [%zu/%i]\n", s->n, n);
+  printf("---------------------\n");
+
+  while (i++ < n)
+    {
+      int *one = stack_push(s, &i);
+      if (0 == one)
+        {
+          fprintf(stderr, "push failed\n");
+          break;
+        }
+      printf("%8s %16i\n", "push", i);
+    }
+
+  printf("---------------------\n");
+  if (0 == stack_peek(s, &i))
+    {
+      fprintf(stderr, "peek failed\n");
+    }
+  printf("%8s %16i\n", "peek", i);
+
+  printf("---------------------\n");
+  i = 0;
+  while (i++ < 2*n) {
+    int k = 0;
+    int *empty = stack_pop(s, &k);
+    if (0 == empty)
+      {
+        fprintf(stderr, "pop failed, empty\n");
+        break;
+      }
+    printf("%8s %16i\n", "pop", k);
+  }
+
+  stack_free(s);
+}
+
+void
+test_stack_new_free_str()
+{
+  stack_s s1;
+  stack_s *s2;
+
+  s2 = stack_new(NULL, 4, sizeof(char*), NULL);
+  stack_free(s2);
+  
+  stack_new(&s1, 4, sizeof(char*), NULL);
+  stack_free(&s1);
+}
+
+void
+test_stack_push_pop_str(stack_s *s)
+{
+  char *ss[] = {"a", "bb", "ccc", "dddd", "eeeee", "ffffff", };
+  char *buf = 0;
+
+  stack_new(s, 4, sizeof(char*), NULL);
+  printf("stack str push/pop [%zu/%zu]\n", s->n, sizeof(ss)/sizeof(ss[0]));
+  printf("---------------------\n");
+  
+  for (size_t i = 0; i < sizeof(ss)/sizeof(ss[0]); i++)
+    {
+      char *one = stack_push(s, &ss[i]);
+      if (0 == one)
+        {
+          fprintf(stderr, "push failed\n");
+          break;
+        }
+      printf("%8s %16s\n", "push", ss[i]);
+    }
+
+  printf("---------------------\n");
+  if (0 == stack_peek(s, &buf))
+    {
+      fprintf(stderr, "peek failed\n");
+    }
+  printf("%8s %16s\n", "peek", buf);
+
+  printf("---------------------\n");
+  for (size_t i = 0; i < sizeof(ss)/sizeof(ss[0]); i++)
+    {
+      buf = 0;
+      char *empty = stack_pop(s, &buf);
+      if (0 == empty)
+        {
+          fprintf(stderr, "pop failed, empty\n");
+          break;
+        }
+      printf("%8s %16s\n", "pop", buf);
+    }
+
+  stack_free(s);
+}
+
+
+void
+test_stack_push_pop_ptr(stack_s *s)
+{
+  int   n;
+  int   i;
+  int  *k;
+  int  *one;
+
+  stack_new(s, 4, sizeof(int*), free);
+  n = s->n;
+  i = 0;
+  k = 0;
+
+  printf("stack int* push/pop [%zu/%i]\n", s->n, n);
+  printf("---------------------\n");
+
+  while (i++ < n)
+    {
+      one = malloc(sizeof(int));
+      *one = i;
+
+      if (NULL == stack_push(s, &one))
+        {
+          fprintf(stderr, "push failed\n");
+          break;
+        }
+      printf("%8s %16i\n", "push", i);
+    }
+
+  printf("---------------------\n");
+  if (NULL == stack_peek(s, &k))
+    {
+      fprintf(stderr, "peek failed\n");
+    }
+  printf("%8s %16i\n", "peek", *k);
+
+  printf("---------------------\n");
+  i = 0;
+  while (i++ < 2*n)
+    {
+      if (NULL == stack_pop(s, &k))
+        {
+          fprintf(stderr, "pop failed, empty\n");
+          break;
+        }
+      printf("%8s %16i\n", "pop", *k);
+    }
+
+  stack_free(s);
+}
+
+int
+main(void)
+{
+  stack_s   ss;
+
+  test_stack_new_free_int();
+  test_stack_new_free_str();
+
+  test_stack_push_pop_int(&ss);
+  test_stack_push_pop_str(&ss);
+
+  test_stack_push_pop_ptr(&ss);
+
+  return 0;
+}
