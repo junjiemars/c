03aa9fdc4ee26747e9df4d548c6cfd9fd86e1237
南山竹 <junjie.mars@icloud.com>
2021-10-07 21:41:53 +0800

    src/x86: all stages for call

--- src/x86/call/call.c
+++ src/x86/call/call.c
@@ -1,74 +1,74 @@
-#include <_x86_.h>
+#include "_x86_.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 static int
 adder(int one, int two)
 {
-	int sum;
-	sum = one + two;
-	return sum;
+  int sum;
+  sum = one + two;
+  return sum;
 }
 
 static void
 caller(void)
 {
-	int n = 0x22;
-	n = adder(n, 0x1100);
-	n = 0x1f;
+  int n = 0x22;
+  n = adder(n, 0x1100);
+  n = 0x1f;
 }
 
 static void
 ptr_callee_p(int *p)
 {
-	*p += 0x1100;
+  *p += 0x1100;
 }
 
 static void
 ptr_caller_p(void)
 {
-	int i;
-	i = 0x22;
-	ptr_callee_p(&i);
-	i += 1;
+  int i;
+  i = 0x22;
+  ptr_callee_p(&i);
+  i += 1;
 }
 
 static void
 ptr_callee_pp(int **pp)
 {
-	*pp = malloc(sizeof(int*));
-	**pp = 0x1133;
+  *pp = malloc(sizeof(int*));
+  **pp = 0x1133;
 }
 
 static void
 ptr_caller_pp(void)
 {
-	int i;
-	int *p;
+  int i;
+  int *p;
 
-	i = 0x22;
-	p = &i;
-	ptr_callee_pp(&p);
-	*p += 0x1;
-	free(p);
+  i = 0x22;
+  p = &i;
+  ptr_callee_pp(&p);
+  *p += 0x1;
+  free(p);
 }
 
 static void
 struct_callee(struct fraction i)
 {
-	struct fraction local;
-	local.denominator = 0x33;
-	i.denominator = 0x44;
+  struct fraction local;
+  local.denominator = 0x33;
+  i.denominator = 0x44;
 }
 
 static void
 struct_caller(void)
 {
-	struct fraction actual =
+  struct fraction actual =
     {
       0x11, 0x22
     };
-	struct_callee(actual);
+  struct_callee(actual);
 }
 
 int
@@ -79,18 +79,15 @@ fact(int n)
 }
 
 int
-main(int argc, char* argv[])
+main(void)
 {
-	_unused_(argc);
-	_unused_(argv);
+  adder(0x1100, 0x22);
+  caller();
 
-	adder(0x1100, 0x22);
-	caller();
+  ptr_caller_p();
+  ptr_caller_pp();
 
-	ptr_caller_p();
-	ptr_caller_pp();
-
-	struct_caller();
+  struct_caller();
 
   fact(3);
 }
