f16c336b0e165c3fef347c386bece985942fa721
南山竹 <junjie.mars@icloud.com>
2021-10-07 21:23:26 +0800

    src/x86: retval for pointer

--- src/x86/retval/retval.c
+++ src/x86/retval/retval.c
@@ -1,7 +1,9 @@
-#include <_x86_.h>
+#include "_x86_.h"
+#include <stdlib.h>
 
 int
-foo() {
+foo()
+{
 	int a;
 	int b;
 	b = 1;
@@ -9,7 +11,19 @@ foo() {
 	return a;
 }
 
+int *
+bar()
+{
+  int *a;
+  a = malloc(sizeof(int) * 4);
+  return a;
+}
+
 int 
-main() {
-	return foo();
+main()
+{
+	int a;
+  int *b;
+  a = foo();
+  b = bar();
 }
