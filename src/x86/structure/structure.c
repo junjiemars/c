00d836db07e016723311758954db6ce0add35a5a
南山竹 <junjiemars@gmail.com>
2021-10-08 14:01:51 +0800

    src/x86: NO_FRAME_POINTER

--- src/x86/structure/structure.c
+++ src/x86/structure/structure.c
@@ -1,22 +1,21 @@
-#include <_x86_.h>
+#include "_x86_.h"
 #include <stdio.h>
 
 void
-structure() {
-	struct binky x;
-	x.a = 0x22334441;
-	x.b = 'B';
-	x.c = 'C';
-	x.d = 'D';
-	x.e = 'E';
-	x.f = x.a;
+structure()
+{
+  struct binky x;
+  x.a = 0x22334441;
+  x.b = 'B';
+  x.c = 'C';
+  x.d = 'D';
+  x.e = 'E';
+  x.f = x.a;
 }
 
-int 
-main(int argc, const char *argv[]) {
-	_unused_(argc);
-	_unused_(argv);
-
-	printf("sizeof(struct binky)=%zu\n", sizeof(struct binky));
-	structure();
+int
+main(void)
+{
+  printf("sizeof(struct binky)=%zu\n", sizeof(struct binky));
+  structure();
 }
