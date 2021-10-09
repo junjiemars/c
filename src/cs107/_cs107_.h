8dc24dbab93baab32475bc4ff33541176d050e11
南山竹 <junjie.mars@icloud.com>
2021-10-09 21:45:18 +0800

    src/cs107

---
+++ src/cs107/_cs107_.h
@@ -0,0 +1,12 @@
+#ifndef _CS107_H_
+#define _CS107_H_
+
+/* https://see.stanford.edu/Course/CS107 */
+
+
+#include "nore.h"
+
+#define _unused_(x) ((void)(x))
+
+
+#endif /* end of _CS107_H_ */
---
+++ src/cs107/handout10/h10.c
@@ -0,0 +1,31 @@
+#include "_cs107_.h"
+#include <string.h>
+
+typedef struct rubble
+{
+  int betty;
+  char barney[4];
+  struct rubble *bammbamm;
+} rubble;
+
+typedef struct
+{
+  short *wilma[2];
+  short fred[2];
+  rubble dino;
+} flintstone;
+
+int
+main(void)
+{
+  rubble      *simpsons;
+  flintstone   jetsons[4];
+  
+  simpsons = &jetsons[0].dino;
+  jetsons[1].wilma[3] = (short *) &simpsons;
+  strcpy(simpsons[2].barney, "Bugs Bunny");
+  ((flintstone *) (jetsons->fred))->dino.bammbamm = simpsons;
+  *(char **) jetsons[4].fred = simpsons->barney + 4;
+}
+
+/* eof */
