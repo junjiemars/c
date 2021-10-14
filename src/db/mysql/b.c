92f22d31f3a583d36bf6dae700060c9fa7515e4f
南山竹 <junjiemars@gmail.com>
2021-10-14 13:58:20 +0800

    src/db: mysql

--- src/db/mysql/b.c
+++ src/db/mysql/b.c
@@ -1,5 +1,6 @@
 #include "_db_.h"
 #include <mysql.h>
+#include <stdio.h>
 
 int
 main(int argc, char **argv)
@@ -7,4 +8,7 @@ main(int argc, char **argv)
   _unused_(argc);
   _unused_(argv);
  
+  printf("mysqlclient info: %s\n", mysql_get_client_info());
+  
+  return 0;
 }
