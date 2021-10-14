1bd9c2c7288b6d4e3382351717c977720687a6e2
南山竹 <junjiemars@gmail.com>
2021-10-14 13:14:21 +0800

    src/db

---
+++ src/db/_db_.h
@@ -0,0 +1,9 @@
+#ifndef _DB_H_
+#define _DB_H_
+
+#include "nore.h"
+
+#define _unused_(x)  ((void)(x))
+
+
+#endif /* end of _DB_H_ */
---
+++ src/db/mysql/b.c
@@ -0,0 +1,10 @@
+#include "_db_.h"
+#include <mysql.h>
+
+int
+main(int argc, char **argv)
+{
+  _unused_(argc);
+  _unused_(argv);
+ 
+}
