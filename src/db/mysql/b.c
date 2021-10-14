127bdf0fccf7515b2f163fc92aa1a36cd6cda738
南山竹 <junjiemars@gmail.com>
2021-10-14 17:55:26 +0800

    src/mysql: connect

--- src/db/mysql/b.c
+++ src/db/mysql/b.c
@@ -1,14 +1,96 @@
 #include "_db_.h"
 #include <mysql.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+
+static void
+usage(const char *bin)
+{
+  printf("Usage: %s [OPTION]...", bin);
+  printf("\n");
+  printf("Play with MySQL\n");
+  printf("  -h, --help             print this message\n");
+  printf("  -H, --host             connect to host\n");
+  printf("  -P, --port             port number of the host\n");
+  printf("  -u, --user             user for login\n");
+  printf("  -p, --password         password for login\n");
+  printf("  -t, --sqltext          sql text to execute\n");
+}
+
+static struct option long_options[] =
+  {
+    {"help",    no_argument,          0, 'h'},
+    {"host",    required_argument,    0, 'H'},
+    {"port",    optional_argument ,   0, 'p'},
+    {"user",    required_argument,    0, 'u'},
+    {"port",    optional_argument,    0, 'p'},
+    {"sqltext", optional_argument,    0, 't'},
+    {0,         0,                    0,  0 },
+  };
+
+
+static char  opt_host[64];
+static int   opt_port = 3306;
+static char  opt_user[64];
+static char  opt_password[64];
+static char  opt_sqltext[1024];
+
 
 int
 main(int argc, char **argv)
 {
-  _unused_(argc);
-  _unused_(argv);
- 
-  printf("mysqlclient info: %s\n", mysql_get_client_info());
-  
+  int     ch;
+  MYSQL  *mysql;
+
+  while (-1 != (ch = getopt_long(argc, argv, "hH:P:u:p:t:", long_options, 0)))
+    {
+      switch (ch)
+        {
+        case 'h':
+          usage(argv[0]);
+          return 0;
+        case 'H':
+          strncpy(opt_host, optarg, strlen(optarg));
+          break;
+        case 'P':
+          opt_port = (uint16_t)atoi(optarg);
+          break;
+        case 'u':
+          strncpy(opt_user, optarg, strlen(optarg));
+          break;
+        case 'p':
+          strncpy(opt_password, optarg, strlen(optarg));
+          break;
+        case 't':
+          strncpy(opt_password, optarg, strlen(optarg));
+          break;
+        default:
+          usage(argv[0]);
+          return -1;
+        }
+    }
+
+  mysql = mysql_init(NULL);
+
+  if(NULL == mysql_real_connect(mysql,
+                        opt_host,
+                        opt_user,
+                        opt_password,
+                        opt_sqltext,
+                        3306,
+                        NULL,
+                        0))
+    {
+      fprintf(stderr, "%s\n", mysql_error(mysql));
+      goto clean_exit;
+    }
+  fprintf(stderr, "connected.\n");
+
+ clean_exit:
+  mysql_close(mysql);
+
   return 0;
 }
