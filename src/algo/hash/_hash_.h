7bf13ebebb35a9eb9dcb7dec1b2ca86890bc885a
南山竹 <junjiemars@gmail.com>
2021-10-15 17:18:31 +0800

    src/algo: hash

--- src/algo/hash/_hash_.h
+++ src/algo/hash/_hash_.h
@@ -3,6 +3,8 @@
 
 #include "_algo_.h"
 
+unsigned long hash(const char *ss);
+
 void print_hash(const char *name, const char *ss, unsigned long hash);
 
 #endif /* end of _HASH__H_ */
--- src/algo/hash/djb2.c
+++ src/algo/hash/djb2.c
@@ -3,30 +3,35 @@
 #include "_hash_.h"
 
 
-/* Dan Bernstein */
+/*
+ * Written by Daniel J. Bernstein (aka. djb)
+ *
+ * https://en.wikipedia.org/wiki/Daniel_J._Bernstein
+ * https://theartincode.stanis.me/008-djb2/
+ */
 unsigned long
 hash(const char *str)
 {
-	unsigned long h = 5381;
-	int c;
-	while (0 != (c = *str++))
+  unsigned long h = 5381;
+  int c;
+  while (0 != (c = *str++))
     {
-      h = ((h << 5) + h) + c;
+      h = ((h << 5) + h) + c;       /* h * 33 + c */
     }
-	return h;
+  return h;
 }
 
 int
 main(int argc, char **argv)
 {
-  const char *ss;
-  unsigned long h;
+  const char     *ss;
+  unsigned long   h;
 
-	if (argc > 1)
+  if (argc > 1)
     {
       ss = argv[1];
       h = hash(ss);
       print_hash("djb2 hash", ss, h);
     }
-	return 0;
+  return 0;
 }
