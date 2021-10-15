0bd1d540976f4c7e5da1fae4a6fe7974591dc82c
南山竹 <junjiemars@gmail.com>
2021-10-15 17:37:45 +0800

    src/algo: hash

--- src/algo/hash/_hash_.c
+++ src/algo/hash/_hash_.c
@@ -1,10 +1,11 @@
 #include "_hash_.h"
 #include <stdio.h>
+#include <bits.h>
 
 
 void 
 print_hash(const char *name, const char *ss, unsigned long hash)
 {
-  printf("%s\n------------\n", name);
-  printf("%16lu: %s\n", hash, ss);
+  printf("%s\n------------\n%s -> %16lu\n", name, ss, hash);
+  printf(BPRI64(hash));
 }
