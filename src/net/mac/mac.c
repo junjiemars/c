69d90cc7098c2ec23c008ea059ea2e70978e5e8e
南山竹 <junjie.mars@icloud.com>
2021-10-17 13:47:32 +0800

    src/net

--- src/net/mac/mac.c
+++ src/net/mac/mac.c
@@ -1,4 +1,4 @@
-#include <_net_.h>
+#include "_net_.h"
 #include <stdio.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
@@ -6,21 +6,25 @@
 
 
 void
-get_mac_address(const char *interface) {
+get_mac_address(const char *interface)
+{
 	int sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-	if (-1 == sd) {
-		return;
-	}
+	if (-1 == sd)
+    {
+      return;
+    }
 	struct ifreq ifr;
 	memset(&ifr, 0, sizeof(ifr));
 	snprintf(ifr.ifr_name, IFNAMSIZ, "%s", interface);
-	if (ioctl(sd, SIOCGIFHWADDR, &ifr) < 0) {
-		return;
-	}
+	if (ioctl(sd, SIOCGIFHWADDR, &ifr) < 0)
+    {
+      return;
+    }
 }
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	_unused_(argc);
 	_unused_(argv);
 	return 0;
