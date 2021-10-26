7247101c82324337ab565aa3542c7e4c9d488ca5
南山竹 <junjiemars@gmail.com>
2021-10-26 11:21:12 +0800

    format

--- src/lang/macro/nested.c
+++ src/lang/macro/nested.c
@@ -1,17 +1,18 @@
 #include <_lang_.h>
 #include <stdio.h>
 
-#define SQUARE(x) ((x)*(x))
-#define CUBE(x) (SQUARE(x)*(x))
+#define SQUARE(x)  ((x)*(x))
+#define CUBE(x)    (SQUARE(x)*(x))
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	_unused_(argc);
 	_unused_(argv);
 
 	int x = SQUARE(3+3);
 	int y = CUBE(3+3);
 	printf("square(3)=%i, cube(3)=%i\n", x, y);
-	
+
 	return 0;
 }
--- src/lang/macro/sa.c
+++ src/lang/macro/sa.c
@@ -1,15 +1,16 @@
 #include <_lang_.h>
 
 #ifdef static_assert
-#undef static_assert
+#  undef static_assert
 #endif
 
 #define sa_cat_(a, b) a##b
 #define sa_cat(a, b) sa_cat_(a, b)
-#define static_assert(e, m) enum {sa_cat(line_, __LINE__) = 1/!!(e)}
+#define static_assert(e, m) enum {sa_cat(line_, __LINE__)  =  1/!!(e)}
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	_unused_(argc);
 	_unused_(argv);
 
