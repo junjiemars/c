78036d7beb0d865a0a46f03fa1cca5b53ce10e7f
南山竹 <junjiemars@gmail.com>
2021-10-29 11:28:51 +0800

    src/net: dns raw

--- src/net/dns/raw.c
+++ src/net/dns/raw.c
@@ -35,10 +35,10 @@
 
 
 /* class */
-#define DNS_CLASS_IN  0x0001
-#define DNS_CLASS_CS  0x0002
-#define DNS_CLASS_CH  0x0003
-#define DNS_CLASS_HS  0x0004
+#define DNS_CLASS_IN  0x01
+#define DNS_CLASS_CS  0x02
+#define DNS_CLASS_CH  0x03
+#define DNS_CLASS_HS  0x04
 
 /* length */
 #define DNS_QNAME_MAX_LEN  255
@@ -105,6 +105,8 @@ typedef struct s_dns_rr
 static void query(void);
 static void make_label(uint8_t *dst, size_t *dst_len, uint8_t *name);
 static void parse_label(uint8_t *buf, uint8_t *name, size_t *n);
+static int make_request(uint8_t **req, size_t *req_len, uint16_t *req_id);
+static void parse_response(uint16_t id, uint8_t *res, size_t res_len);
 static void dump(uint8_t *buf, size_t n, const char *where);
 
 
@@ -123,7 +125,7 @@ static int              opt_dump           =  0;
 static char            *opt_dump_req       =  ".request";
 static char            *opt_dump_res       =  ".response";
 static uint16_t         opt_port           =  53;
-static struct timeval   opt_timeout        =  { .tv_sec = 3, };
+static struct timeval   opt_timeout        =  { .tv_sec = 15, 0 };
 static char opt_query[DNS_QNAME_MAX_LEN]   =  { 0, };
 static char opt_server[DNS_QNAME_MAX_LEN]  =  "127.0.0.53";
 
@@ -135,6 +137,13 @@ static char *dns_type_str[] = {
   "HINFO",  "MX",    "TXT",
   0
 };
+
+static char *dns_class_str[] = {
+  0,
+  "IN",      "CS",    "CH",   "HS",
+  0
+};
+
 
 static void
 usage(const char *p)
@@ -199,32 +208,131 @@ parse_label(uint8_t *buf, uint8_t *name, size_t *n)
       memcpy(d, p + 1, len);
       d += len;
       *d++ = '.';
-      *n += 1 + len;
       p += 1 + len;
     }
-  *n -= 1;
   *--d = 0;
+  *n = d - name + 2;
+}
+
+int
+make_request(uint8_t **req, size_t *req_len, uint16_t *req_id)
+{
+  s_dns_hs   hs;
+  s_dns_qs   qs;
+  uint8_t   *msg  =  0;
+  uint8_t    qname[DNS_QNAME_MAX_LEN];
+  size_t     qname_len;
+
+  /* make header */
+  memset(&hs, 0, sizeof(hs));
+  hs.id = htons(*req_id = getpid());
+  hs.h_flags.rd = (uint8_t) htons(1u);
+  hs.qdcount = htons(1u);
+
+  /* make question */
+  memset(&qs, 0, sizeof(qs));
+  make_label(&qname[0], &qname_len, (uint8_t*) opt_query);
+  qs.qtype = htons(DNS_TYPE_A);
+  qs.qclass = htons(DNS_CLASS_IN);
+
+  /* make message */
+  *req_len = sizeof(s_dns_hs) + qname_len + sizeof(s_dns_qs);
+  msg = calloc(1, *req_len);
+  if (0 == msg)
+    {
+      fprintf(stderr, "!calloc: %s\n", strerror(errno));
+      return -1;
+    }
+  memcpy(msg, &hs, sizeof(hs));
+  memcpy(msg + sizeof(hs), qname, qname_len);
+  memcpy(msg + sizeof(hs) + qname_len, &qs, sizeof(qs));
+
+  *req = msg;
+  return 0;
+}
+
+void
+parse_response(uint16_t id, uint8_t *res, size_t res_len)
+{
+  s_dns_hs  *hs;
+  s_dns_qs  *qs;
+  ssize_t    n;
+  uint8_t    qname[DNS_QNAME_MAX_LEN];
+  size_t     qname_len;
+  uint8_t   *offset;
+
+  hs = (s_dns_hs *) res;
+
+  if (id != ntohs(hs->id))
+    {
+      fprintf(stderr, "!response: id is unmatched\n");
+      goto clean_exit;
+    }
+
+  if (1 != hs->h_flags.qr)
+    {
+      fprintf(stderr, "!response: qr is not response\n");
+      goto clean_exit;
+    }
+
+  n = (ssize_t) ntohs(hs->qdcount);
+  offset = res + sizeof(*hs);
+  while (n-- > 0)
+    {
+      parse_label(offset, qname, &qname_len);
+      offset += qname_len;
+      qs = (s_dns_qs *) offset;
+
+      fprintf(stdout, "# question section:\n -> %s  %s  %s\n",
+              qname,
+              dns_type_str[ntohs(qs->qtype)],
+              dns_class_str[ntohs(qs->qclass)]);
+      offset += sizeof(*qs);
+    }
+
+  n = (ssize_t) ntohs(hs->ancount);
+  if (0 == n)
+    {
+      fprintf(stderr, "!response: no answer\n");
+      goto clean_exit;
+    }
+
+  /* offset = res + sizeof(s_dns_hs) + qname_len + sizeof(s_dns_qs); */
+  /* while (n-- > 0) */
+  /*   { */
+  /*     rr = (s_dns_rr *) offset; */
+  /*     if (DNS_PTR_NAME == dns_ptr_type(rr->name)) */
+  /*       { */
+  /*         parse_label(res + dns_ptr_offset(rr->name), qname, &qname_len); */
+  /*       } */
+  /*     fprintf(stdout, "# %s\n", qname); */
+  /*     fprintf(stdout, "-> %s\n", dns_type_str[ntohs(rr->type)]); */
+
+  /*     offset += sizeof(*rr) + rr->rdlength; */
+  /*   } */
+ clean_exit:
+  _unused_(res_len);
+  _unused_(parse_label);
+  _unused_(dns_type_str);
+  return;
 }
 
 void
 query(void)
 {
   int                  rc;
-  s_dns_hs             header;
-  uint8_t              qname[DNS_QNAME_MAX_LEN];
-  size_t               qname_len;
-  s_dns_qs             question;
-  sockfd_t             sfd = 0;
-  uint8_t             *msg = 0;
-  size_t               msg_len;
+  sockfd_t             sfd  =  0;
+  uint8_t             *req  =  0;
+  size_t               req_len;
+  uint16_t             req_id;
+  uint8_t             *res  =  0;
+  size_t               res_len;
   struct in_addr       host;
   struct sockaddr_in   dst;
   socklen_t            dst_len;
   ssize_t              n;
-  uint8_t             *offset;
-  s_dns_rr            *rr;
 
-#ifdef WINNT
+#if (WINNT)
   WSADATA wsa;
   rc= WSAStartup(MAKEWORD(2, 2), &wsa);
   if (rc)
@@ -234,30 +342,12 @@ query(void)
     }
 #endif
 
-  /* make header */
-  memset(&header, 0, sizeof(header));
-  header.id = htons(getpid());
-  header.h_flags.rd = (uint8_t) htons(1u);
-  header.qdcount = htons(1u);
-
-  /* make question */
-  memset(&question, 0, sizeof(question));
-  make_label(qname, &qname_len, (uint8_t*) opt_query);
-  question.qtype = htons(DNS_TYPE_A);
-  question.qclass = htons(DNS_CLASS_IN);
-
-  /* make message */
-  msg_len = sizeof(s_dns_hs) + qname_len + sizeof(s_dns_qs);
-  msg = malloc(DNS_UDP_MAX_LEN);
-  if (0 == msg)
+  rc = make_request(&req, &req_len, &req_id);
+  if (-1 == rc)
     {
-      fprintf(stderr, "!malloc: %s\n", strerror(errno));
+      fprintf(stderr, "!make_request: failed\n");
       goto clean_exit;
     }
-  memset(msg, 0, DNS_UDP_MAX_LEN);
-  memcpy(msg, &header, sizeof(header));
-  memcpy(msg + sizeof(header), qname, qname_len);
-  memcpy(msg + sizeof(header) + qname_len, &question, sizeof(question));
 
   /* socket */
   sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -283,18 +373,18 @@ query(void)
 
   if (opt_dump)
     {
-      dump(msg, msg_len, opt_dump_req);
+      dump(req, req_len, opt_dump_req);
     }
 
-  /* rc = setsockopt(sfd, SOL_SOCKET, SO_SNDTIMEO, &opt_timeout, */
-  /*                 sizeof(opt_timeout)); */
-  /* if (-1 == rc) */
-  /*   { */
-  /*     fprintf(stderr, "!setsockopt: SO_SNDTIMEO %s\n", strerror(errno)); */
-  /*     goto clean_exit; */
-  /*   } */
+  rc = setsockopt(sfd, SOL_SOCKET, SO_SNDTIMEO, &opt_timeout,
+                  sizeof(opt_timeout));
+  if (-1 == rc)
+    {
+      fprintf(stderr, "!setsockopt: SO_SNDTIMEO %s\n", strerror(errno));
+      goto clean_exit;
+    }
 
-  n = sendto(sfd, msg, msg_len, 0, (const struct sockaddr*) &dst, dst_len);
+  n = sendto(sfd, req, req_len, 0, (const struct sockaddr*) &dst, dst_len);
   if (-1 == n)
     {
       fprintf(stderr, "!sendto: %s\n", strerror(errno));
@@ -302,69 +392,46 @@ query(void)
     }
 
   /* receive */
-  /* rc = setsockopt(sfd, SOL_SOCKET, SO_RCVTIMEO, &opt_timeout, */
-  /*                 sizeof(opt_timeout)); */
-  /* if (-1 == rc) */
-  /*   { */
-  /*     fprintf(stderr, "!setsockopt: SO_RCVTIMEO %s\n", strerror(errno)); */
-  /*     goto clean_exit; */
-  /*   } */
-  memset(msg, 0, DNS_UDP_MAX_LEN);
+  rc = setsockopt(sfd, SOL_SOCKET, SO_RCVTIMEO, &opt_timeout,
+                  sizeof(opt_timeout));
+  if (-1 == rc)
+    {
+      fprintf(stderr, "!setsockopt: SO_RCVTIMEO %s\n", strerror(errno));
+      goto clean_exit;
+    }
 
-  rc = recvfrom(sfd, msg, DNS_UDP_MAX_LEN, 0, (struct sockaddr *) &dst,
+  res = calloc(1, DNS_UDP_MAX_LEN);
+  if (!res)
+    {
+      fprintf(stderr, "!calloc: %s\n", strerror(errno));
+      goto clean_exit;
+    }
+
+  rc = recvfrom(sfd, res, DNS_UDP_MAX_LEN, 0, (struct sockaddr *) &dst,
                 &dst_len);
   if (-1 == rc)
     {
       fprintf(stderr, "!recvfrom: %s\n", strerror(errno));
       goto clean_exit;
     }
-  msg_len = rc;
+  res_len = rc;
 
   if (opt_dump)
     {
-      dump(msg, msg_len, opt_dump_res);
+      dump(req, req_len, opt_dump_res);
     }
 
-  if (header.id != ((s_dns_hs *) msg)->id)
-    {
-      fprintf(stderr, "!response: id is %d unmatched\n", ntohs(header.id));
-      goto clean_exit;
-    }
-  if (1 != ((s_dns_hs *) msg)->h_flags.qr)
-    {
-      fprintf(stderr, "!response: qr is not response\n");
-      goto clean_exit;
-    }
-
-  n = ntohs(((s_dns_hs*) msg)->ancount);
-  if (0 == n)
-    {
-      fprintf(stderr, "!response: no answer\n");
-      goto clean_exit;
-    }
-
-  offset = msg + sizeof(s_dns_hs) + qname_len + sizeof(s_dns_qs);
-  while (n-- > 0)
-    {
-      rr = (s_dns_rr *) offset;
-      if (DNS_PTR_NAME == dns_ptr_type(rr->name))
-        {
-          parse_label(msg + dns_ptr_offset(rr->name), qname, &qname_len);
-        }
-      fprintf(stdout, "# %s\n", qname);
-      fprintf(stdout, "-> %s\n", dns_type_str[ntohs(rr->type)]);
-
-      offset += sizeof(*rr) + rr->rdlength;
-    }
+  parse_response(req_id, res, res_len);
 
  clean_exit:
   if (sfd)
     {
       close(sfd);
     }
-  free(msg);
+  free(res);
+  free(req);
 
-#ifdef WINNT
+#if (WINNT)
   WSACleanup();
 #endif
 }
@@ -392,8 +459,8 @@ dump(uint8_t *b, size_t n, const char *w)
     }
   if (len < n)
     {
-       fprintf(stderr, "!fwrite: %zu/%zu\n", len, n);
-       goto clean_exit;
+      fprintf(stderr, "!fwrite: %zu/%zu\n", len, n);
+      goto clean_exit;
     }
 
  clean_exit:
@@ -410,7 +477,6 @@ main(int argc, char* argv[])
       usage(argv[0]);
       goto clean_exit;
     }
-
 
   while (-1 != (ch = getopt_long(argc, argv, "hs:p:q:t:d", longopts, 0)))
     {
