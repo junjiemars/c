4d6f7d7db61cc6e25872365435ade88ce00ba840
南山竹 <junjiemars@gmail.com>
2021-11-11 10:57:00 +0800

    msvc C4293

--- src/algo/hash/_hash_.c
+++ src/algo/hash/_hash_.c
@@ -7,5 +7,5 @@ void
 print_hash(const char *name, const char *ss, unsigned long hash)
 {
   printf("%s\n------------\n%s -> %16lu\n", name, ss, hash);
-  printf(BPRI64(hash));
+  printf(BPRI64((uint64_t)hash));
 }
