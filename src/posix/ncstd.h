d896230719af5d159baf045158462038cd214190
南山竹 <junjie.mars@icloud.com>
2021-11-12 23:36:18 +0800

    ncstd.h

---
+++ src/posix/ncstd.h
@@ -0,0 +1,18 @@
+/**
+ * Nore C standard
+ */
+
+#ifndef _NCSTD_H_
+#define _NCSTD_H_
+
+
+#include <nore.h>
+
+#include <defs.h>
+#include <ints.h>
+#include <strs.h>
+#include <ioss.h>
+
+
+
+#endif /* _NCSTD_H_ */
---
+++ src/posix/ncstd_test.c
@@ -0,0 +1,11 @@
+#include "ncstd.h"
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+  _unused_(argc);
+  _unused_(argv);
+
+  return 0;
+}
