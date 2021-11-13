b1fc77dea9dbc390d50ec91c53db507ab8af031c
南山竹 <junjie.mars@icloud.com>
2021-11-13 17:05:23 +0800

    io: line

--- src/io/line/l.c
+++ src/io/line/l.c
@@ -1,310 +1,102 @@
 #include "_io_.h"
+#include <nio.h>
 #include <nint.h>
-
-#if (LINUX)
-#  ifndef __USE_GNU
-#    define __USE_GNU 1
-#  endif
-#  ifndef _GNU_SOURCE
-#    define _GNU_SOURCE
-#  endif
-#endif
-
-#include <stdio.h>
-
 #include <stdlib.h>
 #include <string.h>
 
 
+#define LINE_LEN_MAX  (sizeof(void*))
 
-/* #include <unistd.h> */
+static void test_getline(const char *where);
+static void test_fgets(const char *where);
 
-#define LESS_LEN 7
-#define REGU_LEN 8
+void
+test_getline(const char *where) 
+{
+	FILE     *file;
+	char     *line  =  0;
+	size_t    n;
+	ssize_t   len;
+  
+  file = fopen(where, "r");
+	if (!file) 
+    {
+      perror(0);
+      return;
+    }
 
-ssize_t self_getline(char* *restrict, size_t *restrict, FILE *restrict);
-ssize_t self_getdelim(char* *restrict, size_t *restrict, int, FILE *restrict);
-#if !(MSVC)
-void test_getline(const char*);
-void test_getline1(const char*);
-void test_getline2(const char*);
-#endif
-void test_getdelim(const char*);
-void test_self_getdelim(const char*);
-void test_self_getline(const char*);
+  n = LINE_LEN_MAX;
+  line = malloc(n);
+  if (!line)
+    {
+      perror(0);
+      return;
+    }
+	
+	while ((len = getline(&line, &n, file)) > 0)
+    {
+      fwrite(line, len, 1, stdout);
+    }
+  if (ferror(file))
+    {
+      perror(0);
+    }
+
+  fclose(file);
+  free(line);
+}
+
+void
+test_fgets(const char *where) 
+{
+	FILE    *file;
+	char    *line  =  0;
+	size_t   n;
+	char    *s     =  0;
+  
+  file = fopen(where, "r");
+	if (!file) 
+    {
+      perror(0);
+      return;
+    }
+
+  n = LINE_LEN_MAX;
+  line = malloc(n);
+  if (!line)
+    {
+      perror(0);
+      return;
+    }
+	
+	while ((s = fgets(line, n, file)) != NULL)
+    {
+      printf("%s", s);
+    }
+  if (ferror(file))
+    {
+      perror(0);
+    }
+
+  fclose(file);
+  free(line);
+}
+
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
+  if (argc < 2)
+    {
+      printf("where the file located?\n");
+      return 0;
+    }
 
-  if (argc < 2) {
-    printf("where the file located?\n");
-    return 0;
-  }
-
-  char *f = malloc(strlen(argv[1]) + 1);
-  if (0 == f) {
-    perror(argv[1]);
-    return 0;
-  }
-  strcpy(f, argv[1]);
-	
-#if !(MSVC)	
-  test_getline(f);
-  test_getline1(f);
-  test_getline2(f);
-#endif
-  test_self_getline(f);
-  fprintf(stdout, "##########\n");
-		
-#if !(MSVC)
-  test_getdelim(f);
-  fprintf(stdout, "##########\n");
-#endif
-		
-  test_self_getdelim(f);
-  free(f);
+  test_getline(argv[1]);
+  test_fgets(argv[1]);
 
 	return 0;
 }
 
-ssize_t
-self_getline(char ** restrict linep,
-						 size_t * restrict linecapp,
-						 FILE * restrict stream) {
-  if (0 == linep) {
-    return EOF;
-  }
-	if (0 == *linep) {
-		*linecapp = REGU_LEN;
-		*linep = malloc(*linecapp);
-		if (0 == *linep) {
-			return EOF;
-		}
-	}
-	
-	ssize_t linelen = 0;
-	int c = 0;
-	char *p = *linep;
-	
-	while (EOF != (c = fgetc(stream))) {
-		if (linelen == (ssize_t) (*linecapp - 1)) {
-			*linecapp <<= 1;
-			char *p1 = realloc(*linep, *linecapp);
-			if (0 == p1) {
-				return EOF;
-			}
-      *linep = p1;
-			p = p1 + linelen;
-		}
-		*p++ = c;
-		linelen++;
-
-		if ('\n' == c) {
-			*p = 0;
-			return linelen;
-		}
-	}
-	return EOF == c ? EOF : linelen;
-}
-
-ssize_t
-self_getdelim(char ** restrict linep,
-							size_t * restrict linecapp,
-							int delimiter,
-							FILE * restrict stream) {
-  if (0 == linep) {
-    return EOF;
-  }
-	if (0 == *linep) {
-		*linecapp = REGU_LEN;
-		*linep = malloc(*linecapp);
-		if (0 == *linep) {
-			return EOF;
-		}
-	}
-
-	ssize_t linelen = 0;
-	int c = 0;
-	char *p = *linep;
-	
-	while (EOF != (c = fgetc(stream))) {
-		if (linelen == (ssize_t) *linecapp - 1) {
-			*linecapp <<= 1;
-			char *p1 = realloc(*linep, *linecapp);
-			if (0 == *p1) {
-				return EOF;
-			}
-      *linep = p1;
-			p = p1 + linelen;
-		}
-		*p++ = c;
-		linelen++;
-		
-		if (delimiter == c) {
-			*p = 0;
-			return linelen;
-		}
-	}
-	return EOF == c ? EOF : linelen;
-}
-
-#if !(MSVC)
-void
-test_getline(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		perror(filename);
-		return;
-	}
-
-  char *block = 0;
-	char **line = &block;
-	size_t linecap = 0;
-	ssize_t linelen = 0;
-	
-	while (0 < (linelen = getline(line, &linecap, file))) {
-		fwrite(*line, linelen, 1, stdout);
-	}
-	
-	if (ferror(file)) {
-		perror(filename);
-	}
-	fclose(file);
-  
-  free(*line);
-}
-#endif
-
-#if !(MSVC)
-void
-test_getline1(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		perror(filename);
-		return;
-	}
-
-	char *block = malloc(LESS_LEN);
-  char **line = &block;
-	size_t linecap = LESS_LEN;
-	ssize_t linelen = 0;
-	
-	while (0 < (linelen = getline(line, &linecap, file))) {
-		fwrite(*line, linelen, 1, stdout);
-	}
-	
-	if (ferror(file)) {
-		perror(filename);
-	}
-  fclose(file);
-
-  free(*line);
-}
-#endif
-
-#if !(MSVC)
-void
-test_getline2(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		perror(filename);
-		return;
-	}
-
-  char *block = 0;
-	char **line = &block;
-	size_t linecap = 64;
-	ssize_t linelen = 0;
-	
-	while (0 < (linelen = getline(line, &linecap, file))) {
-		fwrite(*line, linelen, 1, stdout);
-	}
-	
-	if (ferror(file)) {
-		perror(filename);
-	}
-  fclose(file);
-	
-  free(*line);
-}
-#endif
-
-void
-test_self_getline(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		perror(filename);
-		return;
-	}
-
-	char *block = malloc(LESS_LEN);
-  char **line = &block;
-	size_t linecap = LESS_LEN;
-	ssize_t linelen = 0;
-	
-	while (0 < (linelen = self_getline(line, &linecap, file))) {
-		fwrite(*line, linelen, 1, stdout);
-	}
-	
-	if (ferror(file)) {
-		perror(filename);
-	}
-	fclose(file);
-
-  free(*line);
-}
-
-
-#if !(MSVC)
-void
-test_getdelim(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		perror(filename);
-		return;
-	}
-
-  char *block = 0;
-	char **line = &block;
-	size_t linecap = 0;
-	ssize_t linelen;
-	
-	while (0 < (linelen = getdelim(line, &linecap, '\n', file))) {
-		fwrite(*line, linelen, 1, stdout);
-	}
-
-  if (ferror(file)) {
-    perror(filename);
-  }
-  fclose(file);
-
-  free(*line);
-}
-#endif
-
-void
-test_self_getdelim(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		perror(filename);
-		return;
-	}
-
-	char *block = 0;
-  char **line = &block;
-	size_t linecap = 0;
-	ssize_t linelen;
-	
-	while (0 < (linelen = self_getdelim(line, &linecap, '\n', file))) {
-		fwrite(*line, linelen, 1, stdout);
-	}
-
-  if (ferror(file)) {
-    perror(filename);
-  }
-  fclose(file);
-  
-  free(*line);
-}
 
 /* eof */
