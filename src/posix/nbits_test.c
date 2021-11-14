f48add8956200c6854c017687207d913788b08bc
南山竹 <junjie.mars@icloud.com>
2021-11-14 10:03:32 +0800

    nbits.h

--- src/posix/nbits_test.c
+++ src/posix/nbits_test.c
@@ -1,13 +1,33 @@
 #include "ndef.h"
 #include <stdio.h>
 
+#include "nbits.h"
+
+#if defined(BIT8_SEP)
+#  undef BIT8_SEP
+#endif
 #define BIT8_SEP  ","
+
+#if defined(BIT16_SEP)
+#  undef BIT16_SEP
+#endif
 #define BIT16_SEP "|"
+
+#if defined(BIT32_SEP)
+#  undef BIT32_SEP
+#endif
 #define BIT32_SEP "\n                         "
+
+#if defined(BIT8_PRE)
+#  undef BIT8_PRE
+#endif
 #define BIT8_PRE  "0b: "
+
+#if defined(BIT8_SUF)
+#  undef BIT8_SUF
+#endif
 #define BIT8_SUF  "\n"
 
-#include "nbits.h"
 
 int
 main(int argc, char **argv) 
