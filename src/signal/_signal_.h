0ba2b86dff12bf514868a3724204d6d929ca3997
南山竹 <junjie.mars@icloud.com>
2022-02-02 22:23:52 +0800

    signal: pause

---
+++ src/signal/_signal_.h
@@ -0,0 +1,8 @@
+#ifndef _SIGNAL_H_
+#define _SIGNAL_H_
+
+#include <nore.h>
+#include <unistd.h>
+#include <signal.h>
+
+#endif /* end of _SIGNAL_H_ */
---
+++ src/signal/pause/p1.c
@@ -0,0 +1,37 @@
+#include "_signal_.h"
+#include <stdio.h>
+
+static void on_sig_usr1(int signo);
+
+
+static void
+on_sig_usr1(int signo)
+{
+  if (signo == SIGUSR1)
+    {
+      printf("#received: SIGUSR1\n");
+    }
+  else
+    {
+      printf("#received: %d\n", signo);
+    }
+}
+
+int
+main(void)
+{
+  printf("%d\n", getpid());
+
+  if (SIG_ERR == signal(SIGUSR1, on_sig_usr1))
+    {
+      perror(NULL);
+      return -1;
+    }
+
+  for (;;)
+    {
+      pause();
+    }
+
+  return 0;
+}
