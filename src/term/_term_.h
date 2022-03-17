2efb630afa4a0bd026815752fce1b993f95a6615
南山竹 <junjie.mars@icloud.com>
2022-03-17 19:59:29 +0800

    term: raw mode

--- src/term/_term_.h
+++ src/term/_term_.h
@@ -2,14 +2,10 @@
 #define _TERM_H_
 
 #include <nustd.h>
-#include <nio.h>
-#include <limits.h>
-#include <stdlib.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <libgen.h>
 #include <termios.h>
+#include <stdlib.h>
+#include <errno.h>
 
 
 
