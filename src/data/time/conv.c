5bf99580b555a374e5f9fa13c453e526c6aa2fde
南山竹 <junjie.mars@icloud.com>
2022-03-18 22:20:09 +0800

    data: time conversion

--- src/data/time/conv.c
+++ src/data/time/conv.c
@@ -11,39 +11,49 @@ int
 main(void)
 {
   char       *s;
-  time_t      t;
-  struct tm  *bt;
-
+  char        buf[64];
+  time_t      tt_utc, tt_loc;
+  struct tm  *bt_loc, *bt_gmt;
 
   /* get time_t of UTC */
-  t = time(NULL);
-  pr_time_t("time", &t);
+  tt_utc = time(NULL);
+  pr_time_t("time", &tt_utc);
 
 
   /* localtime: time_t -> tm (broken-down time)  */
-  bt = localtime(&t);
-  pr_tm("localtime", bt);
+  bt_loc = localtime(&tt_utc);
+  pr_tm("localtime", bt_loc);
 
 
   /* gmtime: time_t -> tm (UTC) */
-  bt = gmtime(&t);
-  pr_tm("gmtime", bt);
+  bt_gmt = gmtime(&tt_utc);
+  pr_tm("gmtime", bt_gmt);
 
 
   /* ctime: time_t -> string */
-  s = ctime(&t);
+  s = ctime(&tt_utc);
   pr_str("ctime", s);
 
 
-  /* mktime: tm -> time_t */
-  t = mktime(bt);
-  pr_time_t("mktime", &t);
-
+  /* mktime: tm (local time) -> time_t */
+  tt_loc = mktime(bt_loc);
+  pr_time_t("mktime", &tt_loc);
+  assert(tt_utc == tt_loc);
 
   /* asctime: tm -> string */
-  s = asctime(bt);
-  pr_str("asctime", s);
+  s = asctime(bt_loc);
+  pr_str("asctime (loc)", s);
+  s = asctime(bt_gmt);
+  pr_str("asctime (gmt)", s);
 
+  /* format */
+  if (strftime(buf, sizeof(buf), "%c", bt_loc) > 0)
+    {
+      pr_str("strftime", buf);
+    }
+
+  /* time zone */
+  /* tzset(); */
 
   exit(EXIT_SUCCESS);
 }
---
+++ src/data/time/high.c
@@ -0,0 +1,21 @@
+#include "_data_.h"
+#include <sys/time.h>
+#include <time.h>
+
+int
+main(void)
+{
+  time_t          utc;
+  struct timeval  tv;
+
+  if (gettimeofday(&tv, NULL) == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  time(&utc);
+  assert(utc >= tv.tv_sec);
+
+  exit(EXIT_SUCCESS);
+}
