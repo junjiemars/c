af2dfaad8fea9601143dd2bd94baa46b80e36432
南山竹 <junjie.mars@icloud.com>
2022-03-19 18:33:59 +0800

    posix: nint.h

--- src/posix/nint.h
+++ src/posix/nint.h
@@ -3,24 +3,22 @@
 
 #include <nore.h>
 
+
 #if !defined(NM_HAVE_STDINT_H)
+#  error "<stdint.h> no found"
+#endif
+
+
+#include <stdint.h>
+
+
+#if (NM_HAVE_SSIZE_T)
 #  if (MSVC)
-#    include <stddef.h>
 #    include <BaseTsd.h>
-typedef __int8            int8_t;
-typedef unsigned __int8   uint8_t;
-typedef __int16           int16_t;
-typedef unsigned __int16  uint16_t;
-typedef __int32           int32_t;
-typedef unsigned __int32  uint32_t;
-typedef __int64           int64_t;
-typedef unsigned __int64  uint64_t;
-typedef SSIZE_T           ssize_t;
-#  else
-#    error "<stdint.h> no found"
+typedef SSIZE_T ssize_t
 #  endif  /* MSVC */
 #else
-#  include <stdint.h>
+typedef long ssize_t
 #endif
 
 
