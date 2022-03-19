ae58db4b93ddc618e53cd7d0c99b8e1ae58e82fb
南山竹 <junjie.mars@icloud.com>
2022-03-19 20:23:05 +0800

    posix: nustd.h

--- src/posix/nustd_test.c
+++ src/posix/nustd_test.c
@@ -7,40 +7,25 @@ static void test_getpid(void);
 void
 test_sleep(void)
 {
+  printf("sleep(1) ...\n");
   sleep(1);
 }
 
 void
 test_getpid(void)
 {
-  getpid();
+  printf("pid: %d\n", getpid());
 }
 
 
 int
-main(int argc, char **argv)
+main(void)
 {
-  _unused_(argc);
-  _unused_(argv);
 
-#if (LINUX)
 #if (_POSIX_C_SOURCE)
   printf("_POSIX_C_SOURCE: %ldL\n", (long) _POSIX_C_SOURCE);
-#elif defined(_POSIX_C_SOURCE)
-  printf("_POSIX_C_SOURCE: defined\n");
 
 #endif  /* _POSIX_C_SOURCE */
-
-#if (_XOPEN_SOURCE)
-  printf("_XOPEN_SOURCE: %d\n", (int) _XOPEN_SOURCE);
-#elif defined(_XOPEN_SOURCE)
-  printf("_XOPEN_SOURCE: defined\n");
-
-#endif  /* _XOPEN_SOURCE */
-
-#endif  /* LINUX */
-
-
 
   test_sleep();
   test_getpid();
