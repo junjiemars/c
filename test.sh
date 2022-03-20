46b74156c7fe0b4490add8bf5b4d09e69345b85f
南山竹 <junjie.mars@icloud.com>
2022-03-20 21:30:34 +0800

    open

--- test.sh
+++ test.sh
@@ -94,10 +94,10 @@ if [ "basic" = "$_TEST_" ]; then
   # test_do --has-cio
   # test_do --has-cpu
   # test_do --has-ctl
-  # test_do --has-ds
+  test_do --has-ds
   # test_do --has-ffi
   # test_do --has-flex
-  # test_do --has-hi
+  test_do --has-hi
   # test_do --has-io
   # test_do --has-lang
   # test_do --has-leptonica
