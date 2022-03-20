6e6d872528bd3917dd090847ed018d6f12896517
南山竹 <junjie.mars@icloud.com>
2022-03-20 22:10:19 +0800

    posix: nustd

--- src/posix/nustd_test.c
+++ src/posix/nustd_test.c
@@ -3,6 +3,36 @@
 
 static void test_sleep(void);
 static void test_getpid(void);
+static void test_conf(void);
+
+int
+main(void)
+{
+  test_conf();
+
+  test_sleep();
+  test_getpid();
+
+  return 0;
+}
+
+void
+test_conf(void)
+{
+#if (_POSIX_VERSION)
+  printf("%s: %ld\n", _str_(_POSIX_VERSION), _POSIX_VERSION);
+#endif
+
+#if (_XOPEN_VERSION)
+  printf("%s: %d\n", _str_(_XOPEN_VERSION), _XOPEN_VERSION);
+#endif
+
+#if (_POSIX_C_SOURCE)
+  printf("%s: %ld\n", _str_(_POSIX_C_SOURCE), _POSIX_C_SOURCE);
+
+#endif  /* _POSIX_C_SOURCE */
+
+}
 
 void
 test_sleep(void)
@@ -15,20 +45,4 @@ void
 test_getpid(void)
 {
   printf("pid: %d\n", getpid());
-}
-
-
-int
-main(void)
-{
-
-#if (_POSIX_C_SOURCE)
-  printf("_POSIX_C_SOURCE: %ldL\n", (long) _POSIX_C_SOURCE);
-
-#endif  /* _POSIX_C_SOURCE */
-
-  test_sleep();
-  test_getpid();
-
-  return 0;
 }
