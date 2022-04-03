9068a2fda8c1235e1c5923538a153fe7fa00bcf6
南山竹 <junjie.mars@icloud.com>
2022-04-03 23:28:55 +0800

    io: temp

--- src/cio/temp/stdtmp.c
+++
@@ -1,7 +0,0 @@
-#include "_cio_.h"
-
-int
-main(void)
-{
-  return 0;
-}
---
+++ src/io/temp/t.c
@@ -0,0 +1,45 @@
+#include "_io_.h"
+
+/*
+ * 1. tmpname in stdio.h had been deprecated.
+ *
+ */
+
+
+int
+main(int argc, char **argv)
+{
+  int   rc;
+  int   fd;
+  char  tmp[NAME_MAX];
+  char  buf[NAME_MAX];
+
+  if (argc < 2)
+    {
+      fprintf(stderr, "usage: %s <pathname>\n", basename(argv[0]));
+      exit(EXIT_FAILURE);
+    }
+
+  snprintf(tmp, NAME_MAX, "%s/nore-XXXXXX", argv[1]);
+
+  fd = mkstemp(tmp);
+  if (fd == -1)
+    {
+      perror("!panic");
+      exit(EXIT_FAILURE);
+    }
+
+  printf("open %s for write ...\n", tmp);
+
+  strncpy(buf, "one line to output", NAME_MAX);
+
+  rc = write(fd, buf, strlen(buf));
+  if (rc == -1)
+    {
+      perror("!panic");
+      exit(EXIT_FAILURE);
+    }
+  printf("wrote %d bytes\n", rc);
+
+  exit(EXIT_SUCCESS);
+}
