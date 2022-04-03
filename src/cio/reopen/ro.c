26e617666412a679e67816afa7aadac230fc5995
南山竹 <junjie.mars@icloud.com>
2022-04-03 20:53:18 +0800

    cio: clean

--- src/cio/reopen/ro.c
+++ src/cio/reopen/ro.c
@@ -1,45 +1,56 @@
-#include <_cio_.h>
-#include <stdio.h>
+#include "_cio_.h"
 #include <string.h>
 
 
+static void change_mode(const char *);
+
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 2) {
+    fprintf(stderr, "usage: <filename>\n");
+    return 1;
+	}
+  change_mode(argv[1]);
+
+	return 0;
+}
+
 void
-change_mode(const char *name) {
+change_mode(const char *name)
+{
 	FILE *f1 = fopen(name, "r");
-	if (!f1) {
-		perror("!panic");
-		return;
-	}
-	for (int i = 0; i < 3; i++) {
-		int ch = fgetc(f1);
-		if (EOF == ch) {
-			break;
-		}
-		fputc(ch, stdout);
-	}
+	if (!f1)
+    {
+      perror("!panic");
+      return;
+    }
+	for (int i = 0; i < 3; i++)
+    {
+      int ch = fgetc(f1);
+      if (EOF == ch)
+        {
+          break;
+        }
+      fputc(ch, stdout);
+    }
 	fputc('\n', stdout);
+
 	FILE *f2 = freopen(name, "w", f1);
-	if (!f2) {
-		perror("!panic");
-		return;
-	}
+	if (!f2)
+    {
+      perror("!panic");
+      return;
+    }
 	char *s = "|mode to 'w'|";
 	size_t n = fwrite(s, sizeof(*s), strlen(s), f2);
 	fprintf(stdout, "wrote %zu bytes into %s\n", n, name);
-	if (ferror(f2)) {
-		perror("!panic");
-	}
+	if (ferror(f2))
+    {
+      perror("!panic");
+    }
 	fclose(f2);
-}
 
-int
-main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
 
-	if (argc > 1) {
-		change_mode(argv[1]);
-	}
-
-	return 0;
 }
