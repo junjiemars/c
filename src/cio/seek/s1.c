3822d9290788217d2e72c87adf8d49939ee1cc06
南山竹 <junjie.mars@icloud.com>
2022-04-06 00:05:56 +0800

    cio: seek

--- src/cio/seek/s1.c
+++ src/cio/seek/s1.c
@@ -10,6 +10,7 @@ main(int argc, char **argv)
 {
   char    *ss  =  "abcdefghij";
   char    *us  =  "ABC";
+  char     buf[NM_LINE_MAX];
   size_t   len, n;
   fpos_t   pos;
   FILE    *fp;
@@ -20,14 +21,14 @@ main(int argc, char **argv)
       return 1;
     }
 
-  fp = fopen(argv[1], "w");
+  fp = fopen(argv[1], "w+");
   if (!fp)
     {
       perror("!panic");
       return 1;
     }
 
-  printf("write %s into %s\n", ss, argv[1]);
+  printf("write \"%s\" into %s\n", ss, argv[1]);
 
   len = strlen(ss);
   if ((n = fwrite(ss, sizeof(*ss), len, fp)) != len)
@@ -58,6 +59,30 @@ main(int argc, char **argv)
       perror("!panic");
       exit(EXIT_FAILURE);
     }
+
+  if (fflush(fp))
+    {
+      perror("!panic");
+      clearerr(fp);
+    }
+
+  pos = 0;
+  if (fsetpos(fp, &pos) == -1)
+    {
+      perror("!panic");
+      exit(EXIT_FAILURE);
+    }
+  printf("position reset to %lld\n", pos);
+
+  if ((n = fread(buf, sizeof(*buf), NM_LINE_MAX, fp)) == 0)
+    {
+      if (ferror(fp))
+        {
+          perror("!panic");
+          exit(EXIT_FAILURE);
+        }
+    }
+  printf("read \"%s\" from %s\n", buf, argv[1]);
 
 
   exit(EXIT_SUCCESS);
