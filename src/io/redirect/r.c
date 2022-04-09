bfecc0545e03d86ae58e454ecd56df220d938f6d
南山竹 <junjie.mars@icloud.com>
2022-04-09 21:00:36 +0800

    io/redirect

--- src/io/redirect/r.c
+++ src/io/redirect/r.c
@@ -1,13 +1,40 @@
 #include "_io_.h"
 
+/*
+ * Q1: d1 > f1 d2>&d1, shell process command line from left to right.
+ * A1:
+ * 1. dup2(f1, d1)
+ * 2. dup2(d1, d2)
+ * so fd(1) and fd(2) point to same f1's file table.
+ *
+ * Q2: d2>&d1 > f1
+ * A2:
+ * 1. dup2(d1, d2)
+ * 2. dup2(f1, d1)
+ * so fd(2) point to d1's file table and fd(1) point to f1's file table.
+ *
+ */
+
+
 int
 main(void)
 {
   char  out[]  =  "stdout output\n";
   char  err[]  =  "stderr output\n";
 
-  write(STDOUT_FILENO, out, sizeof(out) - 1);
-  write(STDERR_FILENO, err, sizeof(err) - 1);
+  if (write(STDOUT_FILENO, out, sizeof(out)-1) != sizeof(out)-1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
 
-  return 0;
+
+  if (write(STDERR_FILENO, err, sizeof(err)-1) != sizeof(out)-1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  exit(EXIT_SUCCESS);
+
 }
