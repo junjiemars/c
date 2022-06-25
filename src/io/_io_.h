845ebb563dcc7409779b6bdbfc769a9b65f32f55
南山竹 <junjiemars@gmail.com>
2022-06-25 14:57:26 +0800

    io: clean

--- src/io/_io_.h
+++ src/io/_io_.h
@@ -2,13 +2,13 @@
 #define _IO_H_
 
 #include <nustd.h>
-#include <limits.h>
+#include <ncstd.h>
+
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <libgen.h>
 #include <assert.h>
-
+#include <fcntl.h>
+#include <sys/stat.h>
 
 
 #endif /* end of _IO_H_ */
