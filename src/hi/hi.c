0350b30360c35a3af7be41b8f34fbe3e53ddc30e
南山竹 <junjiemars@gmail.com>
2022-06-27 16:37:17 +0800

    hi: clean

--- src/hi/hi.c
+++ src/hi/hi.c
@@ -6,28 +6,33 @@
 #define HI_VERSION_PATCH 1
 
 unsigned long
-hi_version(void) {
+hi_version(void)
+{
   return HI_VERSION_MAJOR * 0x10000 |
          HI_VERSION_MINOR * 0x00100 |
          HI_VERSION_PATCH * 0x00001;
 }
 
-int 
-main(int argc, const char *argv[]) {
-  if (argc > 1) {
-		printf("Hi, %s, Version:0x%x\n",
-					 argv[1], 
-					 HI_VERSION/* defined in nore.h */);
-  } else {
-    unsigned long ver = hi_version();
-    printf("Hello, world!, "
-					 "Version:0x%lx(%lu)<=[major:%lu minor:%lu patch:%lu]\n"
-					 , ver
-					 , ver
-					 , (ver >> 16) & 0xff
-					 , (ver >> 8) & 0xff
-					 , ver & 0xff);
-  }
+int
+main(int argc, const char *argv[])
+{
+  if (argc > 1)
+    {
+      printf("Hi, %s, Version:0x%x\n",
+             argv[1],
+             HI_VERSION/* defined in nore.h */);
+    }
+  else
+    {
+      unsigned long ver = hi_version();
+      printf("Hello, world!, "
+             "Version:0x%lx(%lu)<=[major:%lu minor:%lu patch:%lu]\n"
+             , ver
+             , ver
+             , (ver >> 16) & 0xff
+             , (ver >> 8) & 0xff
+             , ver & 0xff);
+    }
 
   return 0;
 }
