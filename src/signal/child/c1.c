4fb70c62ad07586a8dc072e85fa0feaef8366bfe
南山竹 <junjie.mars@icloud.com>
2022-07-05 20:50:55 +0800

    signal: child

---
+++ src/signal/child/c1.c
@@ -0,0 +1,52 @@
+#include <_signal_.h>
+
+/*
+ * The child inherits the parent's signal dispositions when a process
+ * calls fork.
+ *
+ */
+
+static void on_sig_alrm(int);
+
+
+int
+main(void)
+{
+  int    stat;
+  pid_t  pid;
+
+  signal(SIGALRM, on_sig_alrm);
+
+  pid = fork();
+  if (-1 == pid)
+    {
+      perror(NULL);
+      exit(errno);
+    }
+  else if (0 == pid)
+    {
+      printf("%-8d: pause...\n", getpid());
+      pause();
+    }
+  else
+    {
+      printf("%-8d: sleep(1)...\n", getpid());
+
+      sleep(1);
+      kill(pid, SIGALRM);
+
+      waitpid(pid, &stat, 0);
+    }
+
+  exit(EXIT_SUCCESS);
+
+}
+
+void
+on_sig_alrm(int signo)
+{
+  if (SIGALRM == signo)
+    {
+      printf("%-8d: %s\n", getpid(), _str_(SIGALRM));
+    }
+}
