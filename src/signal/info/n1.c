421e155b1bac078bfb694312c715008c78f222e8
南山竹 <junjie.mars@icloud.com>
2022-07-10 00:35:35 +0800

    signal: info

--- src/signal/info/n1.c
+++ src/signal/info/n1.c
@@ -1,16 +1,33 @@
 #include <_signal_.h>
 #include <ctype.h>
 
+#define N  16
+
+static void strupper(const char*, char*);
+
+extern const char * const  sys_siglist[];
+extern const char * const  sys_signame[];
+
+
 int
 main(void)
 {
-  extern const char * const sys_siglist[];
-  extern const char * const sys_signame[];
+  char  name[N];
 
   for (unsigned i = 1; i < NSIG; i++)
     {
-      printf("sig%s: %s\n", sys_signame[i], sys_siglist[i]);
+      strupper(sys_signame[i], name);
+      printf("%-16s: %s\n", name, sys_siglist[i]);
     }
 
   exit(EXIT_SUCCESS);
+}
+
+void
+strupper(const char *s, char *u)
+{
+  while ((*u++ = (char) toupper(*s++)) != 0)
+    {
+      /* void */
+    }
 }
