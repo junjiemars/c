80e9e7ed77c3ea7cb0fcfc5ed5c342acb86edad3
南山竹 <junjiemars@gmail.com>
2022-07-20 15:43:01 +0800

    lang: posix

--- src/lang/exit/e.c
+++ src/lang/exit/e.c
@@ -6,43 +6,42 @@
 static void at_exit_first(void);
 static void at_exit_second(void);
 
-
-void
-at_exit_first(void)
-{
-    printf("----------\n");
-    printf("existing first ...\n");
-}
-
-void 
-at_exit_second(void)
-{
-    printf("----------\n");
-    printf("existing second ...\n");
-}
-
-
 int
-main(int argc, char **argv)
+main(void)
 {
-    _unused_(argc);
-    _unused_(argv);
-
-    if (atexit(&at_exit_first)) {
-        perror("1st");
-        return 1;
+  if (atexit(&at_exit_first))
+    {
+      perror("1st");
+      return 1;
     }
 
-    if (atexit(&at_exit_second)) {
-        perror("2nd");
-        return 1;
+  if (atexit(&at_exit_second))
+    {
+      perror("2nd");
+      return 1;
     }
 
-    if (atexit(at_exit_second)) {
+  if (atexit(at_exit_second))
+    {
       perror("3rd");
       return 1;
     }
 
-    printf("calling exit() ...\n");
-    exit(EXIT_SUCCESS);
+  printf("calling exit() ...\n");
+  exit(EXIT_SUCCESS);
+}
+
+
+void
+at_exit_first(void)
+{
+  printf("----------\n");
+  printf("existing first ...\n");
+}
+
+void
+at_exit_second(void)
+{
+  printf("----------\n");
+  printf("existing second ...\n");
 }
--- src/lang/fn/f.c
+++ src/lang/fn/f.c
@@ -1,63 +1,25 @@
 #include <_lang_.h>
 #include <stdio.h>
 
-int
-add1(int n) {
-	return (n+1);
-}
-
-int
-sub1(int n) {
-	return (n-1);
-}
-
 typedef int (*op)(int n);
 
-int
-fn1(int n, op fn) {
-	return fn(n);
-}  
+enum
+{ ADD1, SUB1 };
 
-enum { ADD1, SUB1 };
 
-op
-fn2(int op) {
-	if (ADD1 == op) return &add1;
-	if (SUB1 == op) return &sub1;
-	return 0;
-}
+static int add1(int);
+static int sub1(int);
+static int fn1(int, op);
+static op fn2(int);
+static int recursive_factorial(int);
+static inline int _iter_fact(int, int);
+static int iterative_factorial(int);
 
 int
-recursive_factorial(int n) {
-	if (n < 0) return 0;
-	if (n < 2) {
-		return 1;
-	} else {
-		return n*recursive_factorial(n-1);
-	}
-}
+main(void)
+{
+	int  i1  =  0x11223344;
 
-static inline int 
-_iter_fact(int n, int acc) {
-	if (n == 1) {
-		return acc;
-	} else {
-		return _iter_fact(n-1, n*acc);
-	}
-}
-
-int
-iterative_factorial(int n) {
-	if (n < 0) return 0;
-	return _iter_fact(n, 1);
-}
-
-int
-main(int argc, char* argv[]) {
-	_unused_(argc);
-	_unused_(argv);
-
-	int i1 = 0x11223344;
 	printf("fn1(add1:%d)=%d\n", i1, fn1(i1, &add1));
 	printf("fn1(sub1:%d)=%d\n", i1, fn1(i1, &sub1));
 
@@ -68,4 +30,65 @@ main(int argc, char* argv[]) {
 	printf("recursive_factorial(%i)=%i\n", f1, recursive_factorial(f1));
 	printf("iterative_factorial(%i)=%i\n", f1, iterative_factorial(f1));
 
+}
+
+int
+add1(int n)
+{
+	return (n+1);
+}
+
+int
+sub1(int n)
+{
+	return (n-1);
+}
+
+
+
+int
+fn1(int n, op fn)
+{
+	return fn(n);
+}
+
+
+op
+fn2(int op)
+{
+	if (ADD1 == op) return &add1;
+	if (SUB1 == op) return &sub1;
+	return 0;
+}
+
+int
+recursive_factorial(int n)
+{
+	if (n < 0) return 0;
+	if (n < 2)
+{
+		return 1;
+	} else
+{
+		return n*recursive_factorial(n-1);
+	}
+}
+
+int
+_iter_fact(int n, int acc)
+{
+	if (n == 1)
+{
+		return acc;
+	} else
+{
+		return _iter_fact(n-1, n*acc);
+	}
+}
+
+int
+iterative_factorial(int n)
+{
+	if (n < 0) return 0;
+	return _iter_fact(n, 1);
 }
