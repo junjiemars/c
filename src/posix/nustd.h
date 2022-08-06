72ccebcaa14150a035a0f1ad654ddcfaaf496ef6
南山竹 <junjie.mars@icloud.com>
2022-08-06 12:19:47 +0800

    posix: msvc c11

--- src/posix/nustd.h
+++ src/posix/nustd.h
@@ -27,9 +27,15 @@
 
 
 #if (MSVC)
-#  if defined(__attribute__) && defined(unused)
-#    define __attribute____unused__  unused
-#    undef unused
+#  if defined(__attribute__)
+#    if defined(unused)
+#      define __attribute____unused__  unused
+#      undef unused
+#    endif
+#    if defined(fallthrough)
+#      define __attribute____fallthrough__  fallthrough
+#      undef fallthrough
+#    endif
 #  endif
 #  include <BaseTsd.h>
 #  include <windows.h>
@@ -37,6 +43,10 @@
 #  if defined(__attribute____unused__)
 #    define unused  __attribute____unused__
 #    undef __attribute____unused__
+#  endif
+#  if defined(__attribute____fallthrough__)
+#    define fallthrough  __attribute____fallthrough__
+#    undef __attribute____fallthrough__
 #  endif
 #else
 #  include <sys/types.h>
