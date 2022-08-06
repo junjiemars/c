a5758cadcb848c7cbe77c03d84c1fbb3e8da122e
南山竹 <junjie.mars@icloud.com>
2022-08-06 09:50:57 +0800

    posix: msvc c11

--- src/posix/ncstd.h
+++ src/posix/ncstd.h
@@ -6,10 +6,10 @@
 #define _NCSTD_H_
 
 
-#include <ndef.h>
-#include <nasrt.h>
-#include <nint.h>
-#include <nstr.h>
+/* #include <ndef.h> */
+/* #include <nasrt.h> */
+/* #include <nint.h> */
+/* #include <nstr.h> */
 /* #include <nio.h> */
 
 
