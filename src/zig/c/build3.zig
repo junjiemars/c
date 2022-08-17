ec429a3d078a8385df3c752ed974a1a76f0cca5e
南山竹 <junjiemars@gmail.com>
2022-08-17 18:18:41 +0800

    zig

--- src/zig/c/build3.zig
+++ src/zig/c/build3.zig
@@ -1,14 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    const obj = b.addObject("c", "c.zig");
     const exe = b.addExecutable("zig_z3", null);
-    exe.addObjectSource("c.o", "c.zig", null);
+
     exe.addCSourceFile("m.c", &[_][]const u8{});
+    exe.addObject(obj);
 
     exe.setTarget(b.standardTargetOptions(.{}));
     exe.setBuildMode(b.standardReleaseOptions());
     exe.addIncludeDir("./");
-    exe.addObjectFile("c.o");
+
     exe.linkLibC();
     exe.install();
 
--- src/zig/c/c.zig
+++ src/zig/c/c.zig
@@ -2,6 +2,6 @@ const c = @cImport({
     @cInclude("c.h");
 });
 
-pub fn incr1(a: c_int) c_int {
+export fn incr1(a: c_int) c_int {
     return a + 1;
 }
