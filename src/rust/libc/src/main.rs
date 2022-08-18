dd7093580a0b8930c54750f2108dedc45b6f1835
南山竹 <junjiemars@gmail.com>
2022-08-18 16:16:50 +0800

    rust

--- src/rust/libc/src/main.rs
+++ src/rust/libc/src/main.rs
@@ -1,3 +1,16 @@
+use std::ffi::CString;
+
+extern {
+    static stdout: *mut libc::FILE;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // getpid
+    let pid = unsafe { libc::getpid() };
+    println!("pid = {}", pid);
+		
+		// fprintf
+		let cstr = CString::new("inside C, %d\n").expect("CString::new failed");
+		unsafe { libc::fprintf(stdout, cstr.as_ptr(), pid); };
 }
