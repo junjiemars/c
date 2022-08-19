97891bf45f089b535c0ae31bb5d4d1c71188a8a8
南山竹 <junjiemars@gmail.com>
2022-08-19 17:34:17 +0800

    zig

--- src/zig/game/src/main.zig
+++ src/zig/game/src/main.zig
@@ -6,26 +6,29 @@ pub fn main() !void {
 
     var rand = std.rand.DefaultPrng.init(0);
     const secret_number = rand.random().intRangeAtMostBiased(i32, 1, 100);
-    try stdout.print("The secret number is: {}\n", .{secret_number});
+    // try stdout.print("The secret number is: {}\n", .{secret_number});
 
-    const stdin = std.io.getStdIn().reader();
-    var buf = [_]u8{0} ** 16;
-    const n = stdin.read(&buf) catch |err| return err;
+    while (true) {
+        const stdin = std.io.getStdIn().reader();
+        var buf = [_]u8{0} ** 16;
+        const n = stdin.read(&buf) catch |err| return err;
 
-    if ('Q' == buf[0]) {
-        try stdout.print("Quit!\n", .{});
-        return;
-    }
+        if ('Q' == buf[0]) {
+            try stdout.print("Quit!\n", .{});
+            return;
+        }
 
-    const line = std.mem.trim(u8, buf[0 .. n - 1], " \n");
-    try stdout.print("You guessed: {s}\n", .{line});
+        const line = std.mem.trim(u8, buf[0 .. n - 1], " \n");
+        try stdout.print("You guessed: {s}\n", .{line});
 
-    var guess = std.fmt.parseInt(i32, line, 10) catch |err| return err;
-    if (guess < secret_number) {
-        try stdout.print("Too small!\n", .{});
-    } else if (guess > secret_number) {
-        try stdout.print("Too big!\n", .{});
-    } else {
-        try stdout.print("You win!\n", .{});
+        var guess = std.fmt.parseInt(i32, line, 10) catch |err| return err;
+        if (guess < secret_number) {
+            try stdout.print("Too small!\n", .{});
+        } else if (guess > secret_number) {
+            try stdout.print("Too big!\n", .{});
+        } else {
+            try stdout.print("You win!\n", .{});
+            break;
+        }
     }
 }
