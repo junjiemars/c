6c3ad27d75d9dd391b20971958529665f596cc7d
南山竹 <junjie.mars@icloud.com>
2022-08-20 22:22:34 +0800

    lang: translation

--- src/lang/stage/s.c
+++ src/lang/stage/s.c
@@ -1,18 +1,36 @@
 #include <_lang_.h>
 
-int g_x1 = 0x11223344;
-int g_x2;
+/*
+ * 1. translation environment.
+ * preprocessing translation unit -> translation unit -> C program.
+ *
+ * 2. execution environment.
+ * hosted environment:
+ * a) int main(void);
+ * b) int main(int argc, char *argv[]);
+ *
+ */
+
+extern char **environ;
+
+int  g_x1  =  0x11223344;
+int  g_x2;
+
+
 
 int
 main(void)
 {
-	static int s_x3 = 0x44002211;
-	int i = 1;
-	int a[] = { 1, 2, 3 };
+	static int  s_x3  =  0x44002211;
+	int         i     =  1;
+	int         a[]   =  { 1, 2, 3 };
+
 	a[i] = (i + 4) * (2 + 6);
 	g_x2 = a[i+1];
 
-	printf("Hello, %s: %d\n", "stage",
-         a[i] + g_x1 + g_x2 + s_x3);
+	printf("Hello, stage: %d\n, env[0]: %s",
+         a[i] + g_x1 + g_x2 + s_x3,
+         environ[0]);
+
 	return 0;
 }
