e670194ba483d12af9bf067512a925e37e7505ad
南山竹 <junjie.mars@icloud.com>
2022-08-21 12:23:17 +0800

    rust: ownership

--- src/rust/owner/src/main.rs
+++ src/rust/owner/src/main.rs
@@ -3,13 +3,24 @@ fn main() {
     let (s11, len) = calculate_length1(s1);
     println!("len({}) = {}", s11, len);
 
-		let s2 = String::from("def");
-		let len2 = calculate_length2(&s2);
-		println!("len({}) = {}", s2, len2);
+    let s2 = String::from("def");
+    let len2 = calculate_length2(&s2);
+    println!("len({}) = {}", s2, len2);
 
-		let mut s3 = String::from("123");
-		let len3 = calculate_length3(&mut s3);
-		println!("len({}) = {}", s3, len3);
+    let mut s3 = String::from("123");
+    let len3 = calculate_length3(&mut s3);
+    println!("len({}) = {}", s3, len3);
+
+    let mut s4 = String::from("hello world");
+    let w4 = first_word1(&s4);
+    println!("word(\"{}\") = {}", s4, w4);
+    s4.clear();
+    println!("word(\"{}\") = {}", s4, w4);
+
+		let /* mut */ s5 = String::from("hello world");
+    let w5 = first_word2(&s5);
+    println!("word(\"{}\") = {}", s5, w5);
+    // s5.clear();
 }
 
 fn calculate_length1(s: String) -> (String, usize) {
@@ -18,10 +29,34 @@ fn calculate_length1(s: String) -> (String, usize) {
 }
 
 fn calculate_length2(s: &String) -> usize {
-		return s.len();
+    return s.len();
 }
 
 fn calculate_length3(s: &mut String) -> usize {
-		s.push_str(",abc");
-		return s.len();
+    s.push_str(",abc");
+    return s.len();
+}
+
+fn first_word1(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    return s.len();
+}
+
+fn first_word2(s: &String) -> &str {
+		let bytes = s.as_bytes();
+
+		for (i, &item) in bytes.iter().enumerate() {
+				if item == b' ' {
+						return &s[0..i];
+				}
+		}
+
+		return &s[..];
 }
