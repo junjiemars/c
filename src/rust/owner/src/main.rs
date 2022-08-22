0ea44358f4472faaef9f0acfe2ba80821b94b58d
南山竹 <junjiemars@gmail.com>
2022-08-22 11:29:13 +0800

    rust: owner

--- src/rust/owner/src/main.rs
+++ src/rust/owner/src/main.rs
@@ -1,4 +1,7 @@
+#![allow(unused)]
 fn main() {
+    short();
+
     let s1 = String::from("abc");
     let (s11, len) = calculate_length1(s1);
     println!("len({}) = {}", s11, len);
@@ -17,10 +20,50 @@ fn main() {
     s4.clear();
     println!("word(\"{}\") = {}", s4, w4);
 
-		let /* mut */ s5 = String::from("hello world");
+    let /* mut */ s5 = String::from("hello world");
     let w5 = first_word2(&s5);
     println!("word(\"{}\") = {}", s5, w5);
     // s5.clear();
+
+    let l1 = String::from("abcd");
+    let l11 = "xyz";
+    let l111 = longest1(l1.as_str(), l11);
+    println!("longest1({}, {}) = {}", l1, l11, l111);
+
+    let l21 = String::from("long long string");
+    let result;
+    {
+        let l211 = String::from("abc");
+        result = longest1(l21.as_str(), l211.as_str());
+        println!("longest1 = {}", result);
+    }
+    // println!("longest1 = {}", result);
+    let l3 = 29;
+    let l31 = Twin { age: &l3 };
+    assert_eq!(29, *l31.age);
+    assert_eq!(29, *l31.return_age());
+
+    let l4 = String::from("Julius");
+    let l41 = Twin1 {
+        first_name: l4.as_str(),
+    };
+    println!("twin1.first_name = {}", l41.first_name);
+
+    let l5 = lifetime2();
+    println!("static lifetime: {}", l5);
+
+    let l6 = lifetime3();
+    println!("ownership move: {}", l6);
+
+    let m1 = String::from("abcd");
+    let m11 = "abc";
+    let m1r = longest_with_announcement(m1.as_str(), m11, "who care");
+    println!("longest_with_announcement: {}", m1r);
+}
+
+fn short() {
+    let s = String::from("abc");
+    println!("short: {}", s);
 }
 
 fn calculate_length1(s: String) -> (String, usize) {
@@ -50,13 +93,73 @@ fn first_word1(s: &String) -> usize {
 }
 
 fn first_word2(s: &String) -> &str {
-		let bytes = s.as_bytes();
+    let bytes = s.as_bytes();
 
-		for (i, &item) in bytes.iter().enumerate() {
-				if item == b' ' {
-						return &s[0..i];
-				}
-		}
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
 
-		return &s[..];
+    return &s[..];
+}
+
+fn lifetime1() {
+    // let r;
+
+    // // E0597
+    // {
+    // 		let x = 5;
+    // 		r = &x;
+    // }
+
+    let x = 5;
+    let r = &x;
+
+    assert_eq!(*r, 5);
+}
+
+fn longest1<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+pub struct Twin<'a> {
+    age: &'a i32,
+}
+
+impl<'a> Twin<'a> {
+    fn return_age(&self) -> &i32 {
+        return self.age;
+    }
+}
+
+pub struct Twin1<'a> {
+    first_name: &'a str,
+}
+
+fn lifetime2() -> &'static str {
+    let s: &'static str = "elder, partiarch";
+    return s;
+}
+
+fn lifetime3() -> String {
+    let s = String::from("MiIlroy");
+    return s;
+}
+
+use std::fmt::Display;
+fn longest_with_announcement<'a, T>(a: &'a str, b: &'a str, ann: T) -> &'a strc
+where
+    T: Display,
+{
+    println!("ann: {}", ann);
+    if a.len() > b.len() {
+        return a;
+    } else {
+        return b;
+    }
 }
