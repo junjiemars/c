8e9399bfb559bf464f2b6ee03bbd7b96f51f5784
南山竹 <junjiemars@gmail.com>
2022-08-23 11:03:34 +0800

    rust: grep

--- src/rust/grep/src/main.rs
+++ src/rust/grep/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
     println!("in file {}", config.file_path);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
