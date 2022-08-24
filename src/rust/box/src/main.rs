ed9dff4fbbed1bb0360f884c43e06f787f0a0870
南山竹 <junjiemars@gmail.com>
2022-08-24 16:52:29 +0800

    rust: clean

--- src/rust/box/src/main.rs
+++ src/rust/box/src/main.rs
@@ -1,19 +1,18 @@
 #![allow(unused)]
 
 fn main() {
-		let b1 = Box::new(5);
-		println!("b1 = {}", b1);
-		
-		use crate::List::{Cons, Nil};
+    let b1 = Box::new(5);
+    println!("b1 = {}", b1);
 
-		let l1 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-		
-		println!();
-		println!("ddd");
+    use crate::List::{Cons, Nil};
+
+    let l1 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!();
+    println!("ddd");
 }
 
-
 enum List {
-		Cons(i32, Box<List>),
-		Nil,
+    Cons(i32, Box<List>),
+    Nil,
 }
