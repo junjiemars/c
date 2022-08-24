3b02416e903a7e8e1313344a9515d7155a9e2857
南山竹 <junjiemars@gmail.com>
2022-08-24 17:59:56 +0800

    rust: box

--- src/rust/box/src/main.rs
+++ src/rust/box/src/main.rs
@@ -7,12 +7,18 @@ fn main() {
     use crate::List::{Cons, Nil};
 
     let l1 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
-    println!();
-    println!("ddd");
+    println!("Cons: {}", l1);
 }
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+use std::fmt;
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+				write!(f, "XXX")
+    }
 }
