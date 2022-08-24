f3ec1d786a21bc3f3e6f42203bd1f5fe8fdc2d35
南山竹 <junjiemars@gmail.com>
2022-08-24 16:31:51 +0800

    rust: box

---
+++ src/rust/box/src/main.rs
@@ -0,0 +1,16 @@
+#![allow(unused)]
+
+fn main() {
+		let b1 = Box::new(5);
+		println!("b1 = {}", b1);
+		
+		use crate::List::{Cons, Nil};
+
+		let l1 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
+
+enum List {
+		Cons(i32, Box<List>),
+		Nil,
+}
