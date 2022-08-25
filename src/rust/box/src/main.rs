5b61c1251adeba4a8f1cccf27ebabd6924a55304
南山竹 <junjiemars@gmail.com>
2022-08-25 14:48:50 +0800

    rust: box

--- src/rust/box/src/main.rs
+++ src/rust/box/src/main.rs
@@ -6,6 +6,14 @@ fn main() {
     let b1 = Box::new(5);
     println!("b1 = {}", b1);
 
+    let mut b2 = Box::new(0);
+    *b2 = *b2 + 1;
+
+    let a1 = BoxedArray {
+        aa: Box::new([1, 2, 3]),
+    };
+    println!("a1 = {}", a1);
+
     let l1 = Cons(
         1,
         Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Nil))))))),
@@ -22,10 +30,10 @@ use std::fmt;
 
 impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut lst = &*self;
+        let mut cons = &*self;
         let mut tail = String::new();
         loop {
-            match lst {
+            match cons {
                 Nil => {
                     write!(f, "Nil");
                     break;
@@ -33,10 +41,24 @@ impl fmt::Display for List {
                 Cons(car, cdr) => {
                     write!(f, "({}, ", car);
                     tail.push(')');
-                    lst = &*cdr;
+                    cons = &*cdr;
                 }
             }
         }
         write!(f, "{}", tail)
+    }
+}
+
+struct BoxedArray<T> {
+    aa: Box<[T]>,
+}
+
+impl<T: fmt::Display> fmt::Display for BoxedArray<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "_[");
+        for a in &self.aa[..] {
+            write!(f, "{}", *a);
+        }
+        write!(f, "]_")
     }
 }
