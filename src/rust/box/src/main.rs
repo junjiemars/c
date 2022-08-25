c985fc4cb4f9f40e26b4c7359b5630f28c2dded3
南山竹 <junjiemars@gmail.com>
2022-08-25 09:19:07 +0800

    rust: box

--- src/rust/box/src/main.rs
+++ src/rust/box/src/main.rs
@@ -1,12 +1,15 @@
 #![allow(unused)]
 
+use crate::List::{Cons, Nil};
+
 fn main() {
     let b1 = Box::new(5);
     println!("b1 = {}", b1);
 
-    use crate::List::{Cons, Nil};
-
-    let l1 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let l1 = Cons(
+        1,
+        Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Nil))))))),
+    );
     println!("Cons: {}", l1);
 }
 
@@ -19,6 +22,21 @@ use std::fmt;
 
 impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-				write!(f, "XXX")
+        let mut lst = &*self;
+        let mut tail = String::new();
+        loop {
+            match lst {
+                Nil => {
+                    write!(f, "Nil");
+                    break;
+                }
+                Cons(car, cdr) => {
+                    write!(f, "({}, ", car);
+                    tail.push(')');
+                    lst = &*cdr;
+                }
+            }
+        }
+        write!(f, "{}", tail)
     }
 }
