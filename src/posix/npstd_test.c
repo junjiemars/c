fb2701419fa6b0a3212a681bb1a962422f05e0a7
南山竹 <junjiemars@gmail.com>
2022-08-29 15:49:36 +0800

    variadic macro

--- src/posix/npstd_test.c
+++ src/posix/npstd_test.c
@@ -7,15 +7,23 @@ main(void)
 {
 
 #if (_POSIX_C_SOURCE)
-  printf("_POSIX_C_SOURCE=%luL\n", _POSIX_C_SOURCE);
+  printf("_POSIX_C_SOURCE=%luL\n", (unsigned long) _POSIX_C_SOURCE);
 #else
   printf("_POSIX_C_SOURCE=(no symbol)\n");
 
+#endif
+
+
+#if (__STDC_HOSTED__)
+  printf("__STDC_HOSTED__=%i\n", (int) __STDC_HOSTED__);
+#else
+  printf("__STDC_HOSTED__=(no symbol)\n");
 
 #endif
 
+
 #if (__STDC_VERSION__)
-  printf("__STDC_VERSION__=%luL\n", __STDC_VERSION__);
+  printf("__STDC_VERSION__=%luL\n", (unsigned long) __STDC_VERSION__);
 #else
   printf("__STDC_VERSION__=(no symbol)\n");
 
