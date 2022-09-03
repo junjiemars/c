305425844628f0316674d7a1ebbd611d755ea988
南山竹 <junjie.mars@icloud.com>
2022-09-04 03:15:33 +0800

    memory: q

--- src/memory/byte/q.c
+++ src/memory/byte/q.c
@@ -12,14 +12,14 @@
 
 typedef union
 {
-	float     f;
-	uint32_t  u;
-	struct
+  float     f;
+  uint32_t  u;
+  struct
   {
-		uint32_t sign:      1;      /* (u & 0x007fffff) */
-		uint32_t exponent:  8;      /* ((u & 0x07800000) >> 23) */
-		uint32_t mantissa: 23;      /* (u >> 31) */
-	} layout;
+    uint32_t sign:      1;      /* (u & 0x007fffff) */
+    uint32_t exponent:  8;      /* ((u & 0x07800000) >> 23) */
+    uint32_t mantissa: 23;      /* (u >> 31) */
+  } layout;
 } ufloat32_t;
 
 
