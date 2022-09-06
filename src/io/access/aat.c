0cb4d133ccde6179bc8d511834a5a6a96cba9216
南山竹 <junjie.mars@icloud.com>
2022-09-06 21:47:51 +0800

    io: access

---
+++ src/io/access/aat.c
@@ -0,0 +1,47 @@
+#include "_io_.h"
+#include <fcntl.h>
+
+
+int
+main(int argc, char **argv)
+{
+  int    rc;
+  uid_t  uid, euid;
+
+  if (argc < 2)
+    {
+      fprintf(stderr, "usage: %s <pathname> [pathname...]\n",
+             basename(argv[0]));
+      exit(EXIT_FAILURE);
+    }
+
+  uid = getuid();
+  euid = geteuid();
+
+  printf("uid: %i, euid: %i\n------------\n", uid, euid);
+
+  for (int i = 1; i < argc; i++)
+    {
+      /* same as access(argv[i], F_OK) */
+      rc = faccessat(AT_FDCWD, argv[i], F_OK, AT_SYMLINK_FOLLOW);
+      if (rc == -1)
+        {
+          perror("!exist access");
+        }
+
+      rc = faccessat(AT_FDCWD, argv[i], R_OK, AT_SYMLINK_FOLLOW);
+      if (rc == -1)
+        {
+          perror("!read access");
+        }
+
+      rc = open(argv[i], O_RDONLY);
+      if (rc == -1)
+        {
+          perror("!open for read");
+        }
+
+    }
+
+  exit(EXIT_SUCCESS);
+}
