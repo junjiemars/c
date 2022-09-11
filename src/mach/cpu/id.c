38a3fd0a41c26b7e3cc48e2baf8e1d37deb252a9
Ubuntu <findidiot@icloud.com>
2022-09-11 01:38:01 +0000

    migrate src/cpu/id to src/mach/cpu/

---
+++ src/mach/cpu/id.c
@@ -0,0 +1,16 @@
+#include <_mach_.h>
+
+/*
+ *
+ *
+ * Refs:
+ * 1. https://www.amd.com/system/files/TechDocs/25481.pdf
+ * 2. https://developer.arm.com
+ *
+ */
+
+int
+main(void)
+{
+  return 0;
+}
