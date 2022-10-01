eb7feacb551bc058ac9074d51018c48a73416bc1
南山竹 <junjie.mars@icloud.com>
2022-10-01 19:20:35 +0800

    io: sync

---
+++ src/io/sync/c1.c
@@ -0,0 +1,58 @@
+#include <_io_.h>
+
+#define BLKSIZ  4096
+
+void copy_block(int, int);
+void copy_block_sync(int, int);
+
+static char  blk[BLKSIZ];
+
+void
+copy_block(int src, int dst)
+{
+  int n;
+
+  while ((n = read(src, blk, BLKSIZ)) > 0)
+    {
+      if (write(dst, blk, n) != n)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void
+copy_block_sync(int src, int dst)
+{
+  int  n, fl;
+
+  fl = fcntl(dst, F_GETFL);
+  if (fl == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  fl |= O_SYNC;
+  if (fcntl(dst, F_SETFL, fl) == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  while ((n = read(src, blk, BLKSIZ)) > 0)
+    {
+      if (write(dst, blk, n) != n)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
+    }
+
+  if (fsync(dst) == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+}
--- src/io/sync/n1.c
+++ src/io/sync/n1.c
@@ -1,15 +1,8 @@
-#include "_io_.h"
+#include <_io_.h>
 #include <time.h>
 
-/*
- * 1. Tranditional implementations of the UNIX system have a buffer
- * cache or page cache in the kernel through which most disk I/O
- * passes.
- *
- *
- */
 
-extern void copy_block_sync(int, int);
+extern void copy_block(int, int);
 
 int
 main(int argc, char **argv)
@@ -30,16 +23,17 @@ main(int argc, char **argv)
       exit(EXIT_FAILURE);
     }
 
-  fd_dst = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR, S_IWUSR);
+  fd_dst = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
   if (fd_dst == -1)
     {
       perror(NULL);
       exit(EXIT_FAILURE);
     }
 
-  _time_(copy_block_sync(fd_src, fd_dst), elapsed);
+  _time_(copy_block(fd_src, fd_dst), elapsed);
 
   printf("elapsed: %16lfs\n", elapsed);
+
 
   exit(EXIT_SUCCESS);
 }
