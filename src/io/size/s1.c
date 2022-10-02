b4dccb69de8c98bbbff0eeb7fc5cca6b84e352d7
南山竹 <junjie.mars@icloud.com>
2022-10-02 22:38:20 +0800

    io

--- src/io/size/s1.c
+++ src/io/size/s1.c
@@ -1,4 +1,22 @@
-#include "_io_.h"
+#include <_io_.h>
+
+/*
+ * A) size:
+ * 1. regular file: actual bytes.
+ *
+ * 2. symbol link: the bytes of the absolute pathname of the link
+ * point to.
+ *
+ * 3. device: 0.
+ *
+ * 4. directory: a multiple bytes of sizeof(short) or sizeof(int).
+ *
+ * B) block size: preferred for I/O for the file.
+ *
+ * C) blocks: 512-byte blocks that are allocated.
+ *
+ */
+
 
 int
 main(int argc, char **argv)
@@ -7,20 +25,25 @@ main(int argc, char **argv)
 
   if (argc < 2)
     {
-      fprintf(stderr, "usage: %s <pathname>\n", basename(argv[0]));
+      fprintf(stderr, "usage: %s <pathname> [pathname...]\n",
+              basename(argv[0]));
       exit(EXIT_FAILURE);
     }
 
-  if (lstat(argv[1], &buf) == -1)
+  for (int i = 1; i < argc; i++)
     {
-      perror(NULL);
-      exit(EXIT_FAILURE);
-    }
+      if (lstat(argv[i], &buf) == -1)
+        {
+          perror(argv[i]);
+          continue;
+        }
 
-  printf("size: %lld\nblock size: %lld\nblocks: %lld\n",
-         (long long int) buf.st_size,
-         (long long int) buf.st_blksize,
-         (long long int) buf.st_blocks);
+      printf("%s: size(%lld), block size(%lld), blocks(%lld)\n",
+             argv[i],
+             (long long int) buf.st_size,
+             (long long int) buf.st_blksize,
+             (long long int) buf.st_blocks);
+    }
 
 
   exit(EXIT_SUCCESS);
