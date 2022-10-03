3141aa4581dddd1e596b5231e05011b3cc6ff5ff
南山竹 <junjie.mars@icloud.com>
2022-10-03 16:09:30 +0800

    io: link

--- src/io/link/lk1.c
+++ src/io/link/lk1.c
@@ -1,7 +1,27 @@
 #include <_io_.h>
 
+/*
+ * 1. Make a hard link.
+ *
+ * 2. Because hard links can cause loops in the file system, so hard
+ * links to directories is restricted to only the superuser.
+ *
+ */
+
 int
-main(void)
+main(int argc, char **argv)
 {
+  if (argc < 3)
+    {
+      fprintf(stderr, "usage: <existing> <link>\n");
+      exit(EXIT_FAILURE);
+    }
+
+  if (linkat(AT_FDCWD, argv[1], AT_FDCWD, argv[2], AT_SYMLINK_FOLLOW) == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
   exit(EXIT_SUCCESS);
 }
---
+++ src/io/link/ulk1.c
@@ -0,0 +1,56 @@
+#include <_io_.h>
+
+int
+main(int argc, char **argv)
+{
+  int          fd;
+  struct stat  ss;
+
+  if (argc < 2)
+    {
+      fprintf(stderr, "usage: <path|file>\n");
+      exit(EXIT_FAILURE);
+    }
+
+  fd = open(argv[1], O_RDONLY);
+  if (fd == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  if (fstat(fd, &ss) == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  if (S_ISREG(ss.st_mode))
+    {
+      if (unlink(argv[1]) == -1)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
+    }
+  else if (S_ISDIR(ss.st_mode))
+    {
+      if (unlinkat(AT_FDCWD, argv[1], AT_REMOVEDIR) == -1)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
+    }
+  else
+    {
+      printf("# skip ...\n");
+      exit(EXIT_SUCCESS);
+    }
+
+  printf("unlink %s(%c)... ", argv[1], S_ISREG(ss.st_mode) ? 'f' : 'd');
+  sleep(5);
+  printf("done\n");
+
+
+  exit(EXIT_SUCCESS);
+}
