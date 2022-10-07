d42e47445f49e63ad825a20f8030d929ac30a8a0
南山竹 <junjie.mars@icloud.com>
2022-10-07 18:13:45 +0800

    ld

--- src/ld/asm/a1.c
+++ src/ld/asm/a1.c
@@ -1,11 +1,26 @@
 #include <_ld_.h>
 
+size_t strlen1(const char*);
+
 int
 main(void)
 {
   static char *ss = "Hi, ASM\n";
 
-  fprintf(stdout, "%s", ss);
+  write(STDOUT_FILENO, ss, strlen1(ss));
 
   return 0;
+}
+
+size_t
+strlen1(const char *ss)
+{
+  size_t  n  =  0;
+
+  while (*ss++)
+    {
+      n++;
+    }
+
+  return n;
 }
