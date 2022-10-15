e678df339304b944238a032826e3637483cc03dc
Ubuntu <findidiot@icloud.com>
2022-10-15 04:07:58 +0000

    io: open

---
+++ src/io/open/o2.c
@@ -0,0 +1,32 @@
+#include <_io_.h>
+
+
+static char  buf[BUFSIZ];
+
+
+int
+main(int argc, char **argv)
+{
+  int  fd;
+
+  if (argc < 2)
+    {
+      fprintf(stderr, "usage: <pathname>\n");
+      exit(EXIT_FAILURE);
+    }
+
+  fd = openat(AT_FDCWD, argv[1], O_RDONLY | O_DIRECTORY);
+  if (fd == -1)
+    {
+      perror("open");
+      exit(EXIT_FAILURE);
+    }
+
+  if (read(fd, buf, BUFSIZ) == -1)
+    {
+      perror("read");
+      exit(EXIT_FAILURE);
+    }
+
+  exit(EXIT_SUCCESS);
+}
