6e0fa047f9e0db220f15792f35cec28c82e5d815
南山竹 <junjie.mars@icloud.com>
2022-10-17 22:10:10 +0800

    io: data

---
+++ src/io/data/ppwd.c
@@ -0,0 +1,22 @@
+#include <_io_.h>
+#include <pwd.h>
+
+void
+print_passwd(const struct passwd *pwd)
+{
+  printf("name: %s\n"
+         "passwd: %s\n"
+         "uid: %li\n"
+         "gid: %li\n"
+         "comment: %s\n"
+         "dir: %s\n"
+         "shell: %s\n",
+         pwd->pw_name,
+         pwd->pw_passwd,
+         (long) pwd->pw_uid,
+         (long) pwd->pw_gid,
+         pwd->pw_gecos,
+         pwd->pw_dir,
+         pwd->pw_shell);
+
+}
---
+++ src/io/data/pwnam.c
@@ -0,0 +1,32 @@
+#include <_io_.h>
+#include <pwd.h>
+
+extern void print_passwd(const struct passwd*);
+
+int
+main(int argc, char **argv)
+{
+  int             err;
+  struct passwd  *pwd  =  NULL;
+
+  if (argc < 2)
+    {
+      fprintf(stderr, "usage: <login>\n");
+      exit(EXIT_FAILURE);
+    }
+
+  errno = err = 0;
+  if ((pwd = getpwnam(argv[1])) == NULL)
+    {
+      err = errno;
+      if (err)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
+    }
+  print_passwd(pwd);
+
+
+  exit(EXIT_SUCCESS);
+}
