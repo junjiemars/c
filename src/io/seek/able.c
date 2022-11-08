e860ca3aac1eb9e7db69fc4699446816f8c5f3bc
南山竹 <junjiemars@gmail.com>
2022-11-08 17:37:35 +0800

    io: seek

--- src/io/seek/able.c
+++ src/io/seek/able.c
@@ -1,5 +1,5 @@
 #include "_io_.h"
-#include <errno.h>
+
 
 /*
  * 1. It's unseekable when fd point to a pipe, FIFO, or socket.
@@ -7,59 +7,76 @@
  */
 
 
+static char get_filetype(mode_t);
+
+
 int
 main(int argc, char **argv)
 {
-
-  int          fd;
-  off_t        offset;
-  struct stat  buf;
-
-
-  if (argc < 2)
+  if (argc < 1)
     {
-      fprintf(stderr, "usage: %s <fileno>\n", basename(argv[0]));
+      fprintf(stderr, "usage: %s <fileno...>\n", basename(argv[0]));
       exit(EXIT_FAILURE);
     }
 
-  fd = atoi(argv[1]);
+  for (int i = 0; i < argc; i++)
+    {
+      int          fd;
+      struct stat  ss;
 
-  if (fstat(fd, &buf) == -1)
-    {
-      perror("!panic");
-      exit(EXIT_FAILURE);
-    }
+      fd = atoi(argv[i]);
 
-  if (S_ISFIFO(buf.st_mode))
-    {
-      printf("named pipe (fifo) file\n");
-    }
-  else if (S_ISCHR(buf.st_mode))
-    {
-      printf("character special file\n");
-    }
-  else if (S_ISBLK(buf.st_mode))
-    {
-      printf("block special file\n");
-    }
-  else if (S_ISREG(buf.st_mode))
-    {
-      printf("regular file\n");
-    }
-  else
-    {
-      printf("st_mode = %d\n", buf.st_mode);
-    }
+      if (fstat(fd, &ss) == -1)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
 
-  offset = lseek(fd, 0, SEEK_CUR);
-  if (offset == -1)
-    {
-      printf("cannot seek\n");
-    }
-  else
-    {
-      printf("seekable\n");
+      if (fstat(fd, &ss) == -1)
+        {
+          perror(NULL);
+          continue;
+        }
+
+      char ft = get_filetype(ss.st_mode);
+      off_t offset = lseek(fd, 0, SEEK_CUR);
+
+      printf("%d: %c(%c)\n", fd, ft, (offset == -1) ? '_' : '*');
     }
 
   exit(EXIT_SUCCESS);
+}
+
+
+char
+get_filetype(mode_t m)
+{
+  if (S_ISREG(m))
+    {
+      return 'r';
+    }
+  else if (S_ISDIR(m))
+    {
+      return 'd';
+    }
+  else if (S_ISCHR(m))
+    {
+      return 'c';
+    }
+  else if (S_ISBLK(m))
+    {
+      return 'b';
+    }
+  else if (S_ISFIFO(m))
+    {
+      return 'p';
+    }
+  else if (S_ISSOCK(m))
+    {
+      return 's';
+    }
+  else
+    {
+      return '?';
+    }
 }
