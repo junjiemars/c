02c129c8eb41a7817d82b923dc5e7836abf2aa70
南山竹 <junjie.mars@icloud.com>
2022-11-11 23:43:00 +0800

    io: share

--- src/io/share/s2.c
+++ src/io/share/s2.c
@@ -9,6 +9,15 @@
  * 4. pwrite should ignore offset when O_APPEND had been specified.
  * 5. pwrite same as write open with O_APPEND on Linux.
  *
+ *
+ * References:
+ *
+ * POSIX requires that opening a file with the O_APPEND flag should
+ * have no affect on the location at which pwrite() writes
+ * data. However, on Linux, if a file is opened with O_APPEND,
+ * pwrite() appends data to the end of the file, regardless of the
+ * value of offset.
+ *
  */
 
 
@@ -69,8 +78,21 @@ main(int argc, char **argv)
           perror(NULL);
           exit(EXIT_FAILURE);
         }
+
+#if defined(_PWRITE_)
+
+#  if (LINUX)
+      assert(ss.st_size == (_N_ * 2));
+#  endif
+
+#  if (DARWIN)
+      assert(ss.st_size == 1);
+#  endif
+
+#else
       /* O_APPEND ensure */
       assert(ss.st_size == (_N_ * 2));
+#endif
 
     }
 }
@@ -85,8 +107,11 @@ out(int fd, char w)
     {
 
 #if defined(_PWRITE_)
+
       n = pwrite(fd, &w, sizeof(char), 0 /* offset */);
+
 #else
+
       n = write(fd, &w, sizeof(char));
 
 #endif
@@ -96,5 +121,6 @@ out(int fd, char w)
           perror(NULL);
           break;
         }
+
     }
 }
