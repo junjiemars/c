9062eab2d7e8c36c135afd0f2a4a3153d6013ae9
南山竹 <junjiemars@gmail.com>
2022-11-14 12:15:20 +0800

    io: duff

--- src/io/open/duff.c
+++ src/io/open/duff.c
@@ -1,8 +1,9 @@
 #include "_io_.h"
 
 /*
- * Tom Duff is the author of /dev/fd feature and Duff Device.
+ * 1. Tom Duff is the author of /dev/fd feature and Duff Device.
  *
+ * 2. open(/dev/fd/0) has the same manner as dup(0).
  *
  */
 
@@ -10,18 +11,54 @@
 int
 main(int argc, char *argv[])
 {
-  int           fd;
+  int           fd, fl;
   char         *dev  =  "/dev/fd/0";
+  struct stat   ss;
 
   if (argc > 1)
     {
       dev = argv[1];
     }
 
-  if ((fd = open(dev, O_RDONLY)) == -1)
+  if (lstat(dev, &ss) == -1)
     {
       perror(NULL);
       exit(EXIT_FAILURE);
+    }
+  printf("link?(%s): %d\n", dev, S_ISLNK(ss.st_mode));
+
+  printf("try to open with O_RDWR\n");
+  if ((fd = open(dev, O_RDWR)) == -1)
+    {
+      perror(NULL);
+
+      printf("try to open with O_RDONLY\n");
+      if ((fd = open(dev, O_RDONLY)) == -1)
+        {
+          perror(NULL);
+          exit(EXIT_FAILURE);
+        }
+    }
+
+  if ((fl = fcntl(fd, F_GETFL)) == -1)
+    {
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+  switch (fl & O_ACCMODE)
+    {
+    case O_RDONLY:
+      printf("readonly?(%s): 1\n", dev);
+      break;
+
+    case O_WRONLY:
+      printf("writeonly?(%s): 1\n", dev);
+      break;
+
+    case O_RDWR:
+      printf("readwrite?(%s): 1\n", dev);
+      break;
     }
 
   exit(EXIT_SUCCESS);
