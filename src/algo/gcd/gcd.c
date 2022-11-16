5b70fb4c176e87aedc99eb393d4442df0ad81b38
南山竹 <junjiemars@gmail.com>
2022-11-16 16:50:23 +0800

    clean

--- src/algo/gcd/gcd.c
+++ src/algo/gcd/gcd.c
@@ -1,4 +1,4 @@
-#include <_algo_.h>
+#include "_algo_.h"
 
 int ecluid(int a, int b);
 int ecluid_tr(int a, int b);
@@ -9,15 +9,19 @@ main(int argc, char **argv)
 {
 	if (argc < 3)
     {
-      printf("ecluid(a, b)\n");
+      fprintf(stderr, "ecluid(a, b)\n");
       return 1;
     }
 
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
+  int c;
 
-	printf("gcd: ecluid(%i, %i) = %i\n", a, b, ecluid(a, b));
-	printf("gcd: ecluid_tr(%i, %i) = %i\n", a, b, ecluid_tr(a, b));
+  c = ecluid(a, b);
+	printf("gcd: ecluid(%i, %i) = %i\n", a, b, c);
+
+  c = ecluid_tr(a, b);
+	printf("gcd: ecluid_tr(%i, %i) = %i\n", a, b, c);
 
 	return 0;
 }
