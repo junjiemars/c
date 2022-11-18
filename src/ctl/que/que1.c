307d629948532f89e57eeea119e7f082fb5cc69e
南山竹 <junjiemars@gmail.com>
2022-11-18 16:02:54 +0800

    ctl: que

--- src/ctl/que/que1.c
+++ src/ctl/que/que1.c
@@ -1,5 +1,6 @@
 #include "_ctl_.h"
 #include <stdio.h>
+#include <assert.h>
 
 
 #define P
@@ -90,15 +91,29 @@ image image_copy(image* self)
 void
 test_que_int(void)
 {
+  int  d[]  =  {9,1,8,3,4};
+
   que_int a = que_int_init();
 
-  que_int_push(&a, 9);
-  que_int_push(&a, 1);
-  que_int_push(&a, 8);
-  que_int_push(&a, 3);
-  que_int_push(&a, 4);
+  assert(que_int_empty(&a));
+
+  for (size_t i = 0; i < _nof_(d); i++)
+    {
+      que_int_push(&a, d[i]);
+    }
+
+  assert(a.size == _nof_(d));
+  assert(*que_int_front(&a) == d[0]);
+  assert(*que_int_back(&a) == d[_nof_(d)-1]);
 
   printf("que<int>\n------------\n");
+  while (!que_int_empty(&a))
+    {
+      printf("%d\n", *que_int_front(&a));
+      que_int_pop(&a);
+    }
+
+  assert(que_int_empty(&a));
 
   que_int_free(&a);
 }
@@ -106,15 +121,21 @@ test_que_int(void)
 void
 test_que_double(void)
 {
+  double  d[]  =  {9.0,1.0,8.0,3.0,4.0};
+
   que_double a = que_double_init();
 
-  que_double_push(&a, 9);
-  que_double_push(&a, 1);
-  que_double_push(&a, 8);
-  que_double_push(&a, 3);
-  que_double_push(&a, 4);
+  for (size_t i = 0; i < _nof_(d); i++)
+    {
+      que_double_push(&a, d[i]);
+    }
 
   printf("que<double>\n------------\n");
+  while (!que_double_empty(&a))
+    {
+      printf("%lf\n", *que_double_front(&a));
+      que_double_pop(&a);
+    }
 
   que_double_free(&a);
 }
@@ -122,15 +143,22 @@ test_que_double(void)
 void
 test_que_str(void)
 {
+  char *d[] = {"9a", "1a", "8a", "3a", "4a"};
+
   que_str a = que_str_init();
 
-  que_str_push(&a, str_init("9aaa"));
-  que_str_push(&a, str_init("1aaa"));
-  que_str_push(&a, str_init("8aaa"));
-  que_str_push(&a, str_init("3aaa"));
-  que_str_push(&a, str_init("4aaa"));
+  for (size_t i = 0; i < _nof_(d); i++)
+    {
+      que_str_push(&a, str_init(d[i]));
+    }
 
   printf("que<str>\n------------\n");
+
+  while (!que_str_empty(&a))
+    {
+      printf("\"%s\"\n", str_c_str(que_str_front(&a)));
+      que_str_pop(&a);
+    }
 
   que_str_free(&a);
 }
@@ -139,6 +167,7 @@ void
 test_que_image(void)
 {
   que_image a = que_image_init();
+
   for(size_t i = 0; i < 5; i++)
     {
       que_image_push(&a, image_read());
@@ -146,7 +175,16 @@ test_que_image(void)
 
   printf("que<image>\n------------\n");
 
+  while (!que_image_empty(&a))
+    {
+      image *it = que_image_front(&a);
+
+      printf("{ %d, %zu }\n", *it->data, it->size);
+      que_image_pop(&a);
+    }
+
   que_image b = que_image_copy(&a);
+
   que_image_free(&a);
   que_image_free(&b);
 }
