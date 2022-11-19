ca98efc0c719fd8d81fbb607edd3b275241e2bb9
南山竹 <junjie.mars@icloud.com>
2022-11-20 00:46:49 +0800

    posix: nio

--- src/posix/nio.h
+++ src/posix/nio.h
@@ -22,7 +22,7 @@ ssize_t getdelim(char ** restrict lineptr, size_t * restrict n, int delimiter,
 #  if defined(getline)
 #    undef getline
 #  endif
-#
+
 #  define getline(lp, n, f)  getdelim((lp), (n), 0x0a, (f))
 
 #endif  /* end of getdelim */
@@ -30,19 +30,19 @@ ssize_t getdelim(char ** restrict lineptr, size_t * restrict n, int delimiter,
 
 #if (MSVC) && (NM_HAVE_FILENO)
 #  define fileno  _fileno
-#
+
 #endif  /* fileno */
 
 
 #if (MSVC) && (NM_HAVE_FDOPEN)
 #  define fdopen  _fdopen
-#
+
 #endif  /* fdopen */
 
 
 #if (NM_HAVE_DIRNAME_GETFULLPATHNAME)
-#  char *dirname(char *);
-#
+char *dirname(char *);
+
 #endif  /* dirname */
 
 
