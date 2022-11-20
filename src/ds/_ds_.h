b2956978454dd638a56a8b826d48c136ec9d25cc
南山竹 <junjie.mars@icloud.com>
2022-11-20 22:10:09 +0800

    posix

--- src/ds/_ds_.h
+++ src/ds/_ds_.h
@@ -1,11 +1,10 @@
 #ifndef _DS_H_
 #define _DS_H_
 
-#include "nore.h"
-#include <ndef.h>
+#include <ncstd.h>
 
 #ifdef MSVC
-#pragma warning(disable:4996) /*_CRT_SECURE_NO_WARNINGS*/
+#  pragma warning(disable: 4996) /*_CRT_SECURE_NO_WARNINGS*/
 #endif
 
 /* #if NDEBUG */
