a5d159f2420cf14a901e1fc35e60185feec65513
南山竹 <junjie.mars@icloud.com>
2022-12-12 21:36:10 +0800

    lang

--- src/lang/pointer/basic.c
+++ src/lang/pointer/basic.c
@@ -1,14 +1,18 @@
-#include <_lang_.h>
+#include "_lang_.h"
 
 
 int
 main(void)
 {
-	int    i    =  1, j=2;
-	int   *pi   =  &i, *pj = &j;
-	int  **ppi  =  &pi;
+	int    i      =  1;
+	int   *pi     =  &i;
+	int  **ppi    =  &pi;
+  int    ii[]   =  {0x11, 0x22, 0x33};
+  int (*pii)[]  =  &ii;
+  int   *iip[]  =  {&i};
 
-	*ppi = pj;
+  i += 0x10;
 
-	printf("ppi@%p=%i\n", ppi, **ppi);
+	printf("ppi@%p=%i, ii[1]=0x%x, iip[0]=0x%x\n",
+         ppi, **ppi, (*pii)[1], *(iip[0]));
 }
