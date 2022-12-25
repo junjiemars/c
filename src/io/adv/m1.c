57c69a80af6445dc80dc8e32cf7ffbaf7448848a
南山竹 <junjie.mars@icloud.com>
2022-12-25 18:37:05 +0800

    io: mmap

--- src/io/adv/m1.c
+++ src/io/adv/m1.c
@@ -7,7 +7,6 @@ main(int argc, char **argv)
   char         *src, *dst;
   int           rfd, wfd;
   char         *rptr, *wptr;
-  size_t        size;
   struct stat   ss;
 
   if (argc < 3)
@@ -31,7 +30,7 @@ main(int argc, char **argv)
       exit(EXIT_FAILURE);
     }
 
-  wfd = open(dst, O_WRONLY | O_CREAT |O_TRUNC, 0666);
+  wfd = open(dst, O_RDWR | O_CREAT | O_TRUNC, 0666);
   if (wfd == -1)
     {
       perror(NULL);
@@ -40,35 +39,34 @@ main(int argc, char **argv)
 
   rptr = NULL;
   wptr = NULL;
-  size = ((ss.st_size + NM_PAGESIZE - 1)/NM_PAGESIZE) * NM_PAGESIZE;
 
-  rptr = mmap(0, size, PROT_READ, MAP_SHARED, rfd, 0);
+  rptr = mmap(0, ss.st_size, PROT_READ, MAP_SHARED, rfd, 0);
   if (rptr == MAP_FAILED)
     {
       perror(NULL);
       goto clean_exit;
     }
 
-  wptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, wfd, 0);
+  wptr = mmap(0, ss.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, wfd, 0);
   if (wptr == MAP_FAILED)
     {
       perror(NULL);
       goto clean_exit;
     }
 
-  memcpy(wptr, rptr, ss.st_size);
+  /* memcpy(wptr, rptr, ss.st_size); */
 
  clean_exit:
   if (wptr != NULL && wptr != MAP_FAILED)
     {
-      if (munmap(wptr, size) == -1)
+      if (munmap(wptr, ss.st_size) == -1)
         {
           perror(NULL);
         }
     }
   if (rptr != NULL && rptr != MAP_FAILED)
     {
-      if (munmap(rptr, size) == -1)
+      if (munmap(rptr, ss.st_size) == -1)
         {
           perror(NULL);
         }
