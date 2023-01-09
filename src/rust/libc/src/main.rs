1cf80034d81de0917f1dd998dcd8bc0224620f60
南山竹 <junjiemars@gmail.com>
2023-01-09 17:38:07 +0800

    rust: libc

--- src/rust/libc/src/main.rs
+++ src/rust/libc/src/main.rs
@@ -2,17 +2,17 @@ use libc::{getpid, printf, strlen};
 use std::ffi::CString;
 
 fn main() {
-    // getpid
+    // getpid(2)
     let pid = unsafe { getpid() };
     println!("pid = {}", pid);
 
-    // printf
+    // printf(3)
     unsafe {
         let cstr = CString::new("Inside C, %d\n").expect("XX");
         printf(cstr.as_ptr(), pid);
     }
 
-    // strlen
+    // strlen(3)
     unsafe {
         let cstr = CString::new("Inside C").expect("XX");
         let fmt = CString::new("strlen(\"%s\") = %d\n").expect("XX");
