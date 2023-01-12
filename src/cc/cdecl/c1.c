811f8370a71fcc5bace58eeded8200f18f58f294
南山竹 <junjiemars@gmail.com>
2023-01-12 09:58:32 +0800

    cc

--- src/cc/cdecl/c1.c
+++ src/cc/cdecl/c1.c
@@ -5,89 +5,111 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define BSIZE 128
-#define MAX_TOKEN_LEN 32
-#define MAX_TOKEN_NUM 32
-#define EOS '\0'
+#define BSIZE          128
+#define MAX_TOKEN_LEN  32
+#define MAX_TOKEN_NUM  32
+#define EOS            '\0'
 
-enum token_t {
-	IDENTIFIER = 256,
-	QUALIFIER,
-	TYPE
-};
 
-struct token_s {
+enum token_t
+  {
+    IDENTIFIER  =  256,
+    QUALIFIER,
+    TYPE
+  };
+
+struct token_s
+{
 	int type;
 	char string[MAX_TOKEN_LEN];
 };
 
-int top = -1;
-struct token_s stack[MAX_TOKEN_NUM];
-struct token_s token;
 
-char inbuf[BSIZE];
-char *inptr;
-char outbuf[BSIZE*2];
-char *outptr;
-
-
-int lookahead(void);
-void pushback(void);
-void _vsprintf_(char *, ...);
-void get_token(void);
-enum token_t classify_string(void);
-void on_first_identifier(void);
-void on_function_args(void);
-void on_array(void);
-void on_pointer(void);
-void on_declarator(void);
+int           lookahead(void);
+void          pushback(void);
+void          _vsprintf_(char *, ...);
+void          get_token(void);
+void          on_first_identifier(void);
+void          on_function_args(void);
+void          on_array(void);
+void          on_pointer(void);
+void          on_declarator(void);
+enum token_t  classify_string(void);
 
 
-#define pop stack[top--]
-#define push(s) stack[++top] = s
+static struct token_s  stack[MAX_TOKEN_NUM];
+static struct token_s  token;
+
+static char   inbuf[BSIZE];
+static char  *inptr;
+static char   outbuf[BSIZE*2];
+static char  *outptr;
+static int    top  =  -1;
+
+#define pop      stack[top--]
+#define push(s)  stack[++top]  =  (s)
 
 
 int
-lookahead(void) {
-	if (EOS == *inptr || inptr >= inbuf + BSIZE - 1) {
-		return EOF;
-	}
+lookahead(void)
+{
+	if (EOS == *inptr || inptr >= inbuf + BSIZE - 1)
+    {
+      return EOF;
+    }
 	return *inptr++;
 }
 
 void
-pushback(void) {
-	if (inbuf < inptr) {
-		inptr--;
-	}
+pushback(void)
+{
+	if (inbuf < inptr)
+    {
+      inptr--;
+    }
 }
 
 void
-_vsprintf_(char *fmt, ...) {
-	va_list args;
+_vsprintf_(char *fmt, ...)
+{
+  int      len;
+	va_list  args;
+
 	va_start(args, fmt);
-	int len = vsprintf(outptr, fmt, args);
+  len = vsprintf(outptr, fmt, args);
 	outptr += len;
 	va_end(args);
 }
 
 void
-get_token(void) {
-	char *p = token.string;
-	while (isblank(*p = (char)lookahead())) ;
-	if (isalnum(*p)) {
-		while (isalnum(*++p = (char)lookahead())) ;
-		pushback();
-		*p = EOS;
-		token.type = classify_string();
-		return;
-	}
+get_token(void)
+{
+	char  *p  =  token.string;
 
-	if ('*' == *p) {
-		strcpy(token.string, "pointer to");
-		token.type = '*';
-		return;
-	}
+	while (isblank(*p = (char)lookahead()))
+    {
+      /* void */
+    }
+
+	if (isalnum(*p))
+    {
+      while (isalnum(*++p = (char)lookahead()))
+        {
+          /* void */
+        }
+
+      pushback();
+      *p = EOS;
+      token.type = classify_string();
+      return;
+    }
+
+	if ('*' == *p)
+    {
+      strcpy(token.string, "pointer to");
+      token.type = '*';
+      return;
+    }
 
 	token.string[1] = EOS;
 	token.type = *p;
@@ -95,7 +117,8 @@ get_token(void) {
 }
 
 enum token_t
-classify_string(void) {
+classify_string(void)
+{
 	char *s = token.string;
 	if (0 == strcmp(s, "const")) return QUALIFIER;
 	if (0 == strcmp(s, "volatile")) return QUALIFIER;
@@ -115,104 +138,120 @@ classify_string(void) {
 }
 
 void
-on_first_identifier(void) {
+on_first_identifier(void)
+{
 	get_token();
-	while (IDENTIFIER != token.type) {
-		push(token);
-		get_token();
-		/* error: no identifier found, such as cast case */
-	}
+	while (IDENTIFIER != token.type)
+    {
+      push(token);
+      get_token();
+      /* error: no identifier found, such as cast case */
+    }
 	_vsprintf_("declare %s as ", token.string);
 	get_token();
 }
 
 void
-on_array(void) {
-	while ('[' == token.type) {
-		_vsprintf_("%s ", "array");
-		get_token();
-		if (isdigit(token.string[0])) {
-			_vsprintf_("0..%d ", atoi(token.string)-1);
-			get_token();
-		}
-		get_token();
-		_vsprintf_("%s ", "of");
-	}
+on_array(void)
+{
+	while ('[' == token.type)
+    {
+      _vsprintf_("%s ", "array");
+      get_token();
+      if (isdigit(token.string[0]))
+        {
+          _vsprintf_("0..%d ", atoi(token.string)-1);
+          get_token();
+        }
+      get_token();
+      _vsprintf_("%s ", "of");
+    }
 }
 
 void
-on_function_args(void) {
-	while ('(' == token.type) {
-		_vsprintf_("function ");
-		char buf[MAX_TOKEN_LEN*4];
-		int idx = 0;
-		char c;
-		int depth = 0;
-		buf[idx++] = '(';
-	nested:
-		while (')' != (c = (char)lookahead())) {
-			if ('(' == c) depth++;
-			buf[idx++] = c;
-		}
-		if (depth) {
-			depth--;
-			buf[idx++] = ')';
-			goto nested;
-		}
-		buf[idx++] = ')';
-		buf[idx++] = EOS;
-		pushback();
-		if (0 != strcmp("()", buf)) {
-			_vsprintf_("%s ", buf);
-		}
-		get_token();
-	}
+on_function_args(void)
+{
+	while ('(' == token.type)
+    {
+      _vsprintf_("function ");
+      char buf[MAX_TOKEN_LEN*4];
+      int idx = 0;
+      char c;
+      int depth = 0;
+      buf[idx++] = '(';
+    nested:
+      while (')' != (c = (char)lookahead()))
+        {
+          if ('(' == c) depth++;
+          buf[idx++] = c;
+        }
+      if (depth)
+        {
+          depth--;
+          buf[idx++] = ')';
+          goto nested;
+        }
+      buf[idx++] = ')';
+      buf[idx++] = EOS;
+      pushback();
+      if (0 != strcmp("()", buf))
+        {
+          _vsprintf_("%s ", buf);
+        }
+      get_token();
+    }
 	get_token();
 	_vsprintf_("returning ");
 }
 
 void
-on_pointer(void) {
-	while ('*' == stack[top].type) {
-		_vsprintf_("%s ", pop.string);
-	}
+on_pointer(void)
+{
+	while ('*' == stack[top].type)
+    {
+      _vsprintf_("%s ", pop.string);
+    }
 }
 
 void
-on_declarator(void) {
-	switch (token.type) {
-	case '[':
-		on_array();
-		break;
-	case '(':
-		on_function_args();
-		break;
-	}
+on_declarator(void)
+{
+	switch (token.type)
+    {
+    case '[':
+      on_array();
+      break;
+    case '(':
+      on_function_args();
+      break;
+    }
 	on_pointer();
-	while (top >= 0) {
-		if ('(' == stack[top].type) {
-			(void)pop;
-			get_token();
-			on_declarator();
-		} else {
-			_vsprintf_("%s ", pop.string);
-		}
-	}
+	while (top >= 0)
+    {
+      if ('(' == stack[top].type)
+        {
+          (void)pop;
+          get_token();
+          on_declarator();
+        } else
+        {
+          _vsprintf_("%s ", pop.string);
+        }
+    }
 }
 
-int main(int argc, char **argv) {
-	_unused_(argc);
-	_unused_(argv);
-
-	if (argc > 1) {
-		strcpy(inbuf, argv[1]);
-		inptr = inbuf;
-		outptr = outbuf;
-		on_first_identifier();
-		on_declarator();
-		_vsprintf_("%s", "\n");
-		fprintf(stdout, "%s", outbuf);
-	}
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+    {
+      strcpy(inbuf, argv[1]);
+      inptr = inbuf;
+      outptr = outbuf;
+      on_first_identifier();
+      on_declarator();
+      _vsprintf_("%s", "\n");
+      fprintf(stdout, "%s", outbuf);
+    }
 
 	return 0;
 }
