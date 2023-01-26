f10aa69e85bfd8d14aff83c2fbbb592d39d2c276
南山竹 <junjie.mars@icloud.com>
2023-01-26 19:13:36 +0800

    algo: coroutine

--- src/algo/coroutine/cr1.c
+++ src/algo/coroutine/cr1.c
@@ -3,10 +3,12 @@
 /*
  *
  * https://www.chiark.greenend.org.uk/~sgtatham/coroutines.html
+ * https://www.chiark.greenend.org.uk/~sgtatham/coroutine.h
  *
  */
 
 static int  range1(int, int);
+static int  range2(int, int);
 
 
 static void  test(int (*)(int, int), int, int);
@@ -16,6 +18,7 @@ int
 main(void)
 {
   test(range1, 1, 5);
+  test(range2, 1, 5);
 
   return 0;
 }
@@ -50,4 +53,25 @@ range1(int a, int b)
     }
 
   return -1;
+}
+
+
+#define _cr_begin_  static int state=0; switch(state) { case 0:
+#define _cr_return_(x)                                          \
+do { state = __LINE__; return (x); case __LINE__:; } while (0)
+#define _cr_finish_(z)  } return (z)
+
+int
+range2(int a, int b)
+{
+  static int  i;
+
+  _cr_begin_;
+
+  for (i = a; i < b; i++)
+    {
+      _cr_return_(i);
+    }
+
+  _cr_finish_(-1);
 }
