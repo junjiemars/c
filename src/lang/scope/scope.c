7a153239d5d61ccf3db47da62a15b2b6e7ae8886
南山竹 <junjiemars@gmail.com>
2023-01-29 17:01:08 +0800

    lang: scope

--- src/lang/scope/scope.c
+++ src/lang/scope/scope.c
@@ -1,7 +1,5 @@
-#include <_lang_.h>
+#include "_lang_.h"
 #include "scope.h"
-#include <stdio.h>
-#include <assert.h>
 
 
 #if MSVC
@@ -10,116 +8,106 @@
 #endif
 
 
-/* file scope, declaration, definition with initializer  */
-int i0 = 123;
+/*
+ * file scope
+ */
 
-/* file scope, declaration, tentative definition with initializer to
-	 0 */
-int i1;
+/* declaration, definition with initializer  */
+int  i0  =  123;
 
-/* file scope, extern declaration, must without initializer */
-extern int i2;
+/* declaration, tentative definition without initializer to 0 */
+int  i1;
 
-/* function sub forward declaration */
-/* extern keyword is optional */
-static void sub(int, int);
+/* extern declaration, must without initializer */
+extern int  i2;
+
+
+/*
+ * function `sub' forward declaration
+ */
+static void  sub(int, int);
+static void  lab(int);
 
 
 int
-inc() {
-	static int i=0;
+inc()
+{
+	static int  i  =  0;
 	return i++;
 }
 
-#ifdef MSVC
-int map(int (*fn)(int n), int *a) {
+#if defined(MSVC)
+int map(int (*fn)(int n), int *a)
+{
 #else
-  int map(int (*fn)(int n), int a[static 4]) {
+int map(int (*fn)(int n), int a[static 4])
+{
 #endif
-    for (int i=0; i<4; ++i) {
-      a[i] = (*fn)(a[i]);
+  for (int i0 = 0; i0 < 4; ++i0)
+    {
+      a[i0] = (*fn)(a[i0]);
     }
-    return 4;
-  }
+  return 4;
+
+  /*
+   * `i0' be shadowed by the file scope `i0'.
+   */
+}
 
 int
-main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+main(void)
 {
-  printf("epoch: i0=%i, i1=%i, i2=%i\n", i0, i1, i2);
-
-  printf("----------file scope----------\n");
-  printf("=>main{i0}=%i\n", i0);
-
-
-  printf("----------block scope----------\n");
-  int i0 = 321;
-  printf("=>main{i0}=%i\n", i0);
-
-  {
-    int i0 = 123+321;
-    printf("=>main{{i0}}=%i\n", i0);
-  }
-
-  printf("=>main{i0}=%i\n", i0);
-
-  printf("=>main{argc}=%i\n", argc);
-
-  {
-    int argc = 123;
-    printf("=>main{{argc}}=%i\n", argc);
-  }
-
-  if (i0 > 0) {
-    printf("=>main{{goto i0}}=>{{i0:}}\n");
-    goto i0;
-    printf("this line bypassed via goto\n");
-  }
-
-  {
-    /* goto label has different namespace than objects,
-     * so u can use i0 here
-     */
-  i0:
-    printf("=>main{{i0:}}\n");
-  }
-
-  printf("----------fn scope----------\n");
-  sub(i0, i0+1);
-
-  printf("----------block scope----------\n");
-  {
-    auto int i0 = 1;
-    printf("=>main{{auto i0}}=%i\n", i0);
-  }
-
-  printf("----------file scope----------\n");
-  printf("=>main{i1}=%i\n", i1);
-
-  printf("=>main{extern int}:i2=%i\n", i2);
-
-  printf("=>main=>{extern inline fn}:sqr(%i)=%i\n", i2, sqr(i2));
-
-  printf("=>main=>{fn}:inc()=%i\n", inc());
-  printf("=>main=>{fn}:inc()=%i\n", inc());
+  printf("i0 = %i, i1 = %i, i2 = %i\n", i0, i1, i2);
 
   int a[] = { 1, 2, 3, 4 };
-  printf("=>main=>{fn}:map({1,2,3,4},&sqr)=%i\n", map(&sqr, a));
-  for (size_t i=0; i<sizeof(a)/sizeof(a[0]); ++i)
+  map(&sqr, a);
+  for (size_t i = 0; i < sizeof(a)/sizeof(*a); ++i)
     {
       printf("%i ", a[i]);
     }
   putchar('\n');
 
-  register int k, sum;
-  for (k=1, sum=0; k < 6; sum += k, k++);
-  printf("=>main{for:register:1+2...+5}=%i\n", sum);
+  sub(i0, i1);
+  sub(i0+1, i1+2);
+
+  lab(0);
+  lab(1);
 }
 
 void
 sub(int i0, int i1)
 {
-  printf("=>sub{i0=%i+i1=%i}=%i\n", i0, i1, i0+i1);
+  printf("i0 = %i, i1 = %i\n", i0, i1);
 
-  /* well, the i0 from function parameter i0, so u can't redefined it */
-  /* int i0 += 1; */
+  /*
+   * well, function parameter `i0' has the entire function scope, so u
+   * can't:
+   *
+   * 1. redefined it `int i0 += 1`;
+   *
+   * 2. but function paramter `i0' can be shadowed by the file scope
+   * `i0'.
+   */
+}
+
+void
+lab(int a)
+{
+  if (a == 0)
+    {
+      goto label_1;
+    label_0:
+      printf("label_0 <- %d\n", a);
+    }
+  else if (a == 1)
+    {
+      goto label_0;
+
+    label_1:
+      printf("label_1 <- %d\n", a);
+    }
+
+  /*
+   * `label' has function scope.
+   */
 }
--- src/lang/scope/scope.h
+++ src/lang/scope/scope.h
@@ -1,10 +1,13 @@
-int i2=12345;
+int  i2  =  12345;
 
-inline int sqr(int n) {
+inline int sqr(int n)
+{
 	return n*n;
 }
 
-/* without it, `sqr' would only be an inline fn, 
-	 so other files would not be able to call it.
+/*
+ * without `extern', `sqr' would only be an inline fn, so other files
+ * would not be able to call it.
+ *
  */
 extern int sqr(int);
