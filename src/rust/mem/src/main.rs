81f24f9da74210095c58bd4e966d67a943c6ca88
南山竹 <junjiemars@gmail.com>
2023-02-02 10:56:13 +0800

    rust: memory model

---
+++ src/rust/mem/src/main.rs
@@ -0,0 +1,25 @@
+/*
+ * https://doc.rust-lang.org/stable/reference/variables.html
+ *
+ */
+
+#![allow(unused)]
+fn main() {
+    let init_should_be;
+    let init_may_be;
+
+    if random_bool() {
+        init_should_be = ();
+        init_may_be = ();
+    } else {
+        init_should_be = ();
+    }
+
+    println!("{:?}", init_should_be);
+    // `E0381' use of possibly-uninitialized `init_may_be`
+    // println!("{:?}", init_may_be);
+}
+
+fn random_bool() -> bool {
+    true
+}
