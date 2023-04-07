055e9bd6b24887e6749d0f336d17690fbfa6ec2f
南山竹 <junjiemars@gmail.com>
2023-04-07 13:48:14 +0800

    rust: gtk

---
+++ src/rust/gtk/src/main.rs
@@ -0,0 +1,24 @@
+// use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Show the window.
+        window.show();
+    });
+
+    app.run()
+}
