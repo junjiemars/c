6964f78867b527c1ffb2e4b20a7af7321a1eeba9
Junjie Mars <junjie@Air.local>
2023-04-19 21:52:24 +0800

    process: group

--- src/process/group/ss1.c
+++ src/process/group/ss1.c
@@ -1,5 +1,12 @@
 #include "_process_.h"
 
+#define print_ids(h, s, p, g, pp)                                       \
+  do                                                                    \
+    {                                                                   \
+      printf("%s: sid=%d, pid=%d, pgid=%d, ppid=%d\n", h, s, p, g, pp); \
+    } while (0)
+
+
 int
 main(void)
 {
@@ -10,7 +17,7 @@ main(void)
   ppid = getppid();
   sid = getsid(0);
 
-  printf("sid=%d, pid=%d, pgid=%d, ppid=%d\n", sid, pid, pgid, ppid);
+  print_ids(">", sid, pid, pgid, ppid);
 
   if ((sid = setsid()) == -1)
     {
@@ -19,9 +26,41 @@ main(void)
     }
   else
     {
+      sid = getsid(0);
       pgid = getpgid(0);
-      printf("setsid() -> sid=%d, pid=%d, pgid=%d, ppid=%d\n",
-             sid, pid, pgid, ppid);
+      print_ids("setsid()", sid, pid, pgid, ppid);
+    }
+
+  if ((pid = fork()) == -1)
+    {
+      perror(NULL);
+    }
+  else if (pid == 0)
+    {
+      pid_t  cid  =  getpid();
+      sid = getsid(0);
+      pgid = getpgid(0);
+      ppid = getppid();
+
+      print_ids(">", sid, cid, pgid, ppid);
+
+      if ((sid = setsid()) == -1)
+        {
+          perror("setsid() in child");
+        }
+      else
+        {
+          sid = getsid(0);
+          pgid = getpgid(0);
+          print_ids("setsid()", sid, cid, pgid, ppid);
+        }
+      exit(0);
+    }
+
+  if (waitpid(pid, 0, 0) < 0)
+    {
+      perror(NULL);
+      exit(1);
     }
 
   exit(0);
