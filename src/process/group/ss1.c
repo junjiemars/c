504862680cc8c045459ad6ae5f07e5441535b515
Junjie Mars <junjie@Air.local>
2023-04-19 20:25:29 +0800

    process: group

--- src/process/group/ss1.c
+++ src/process/group/ss1.c
@@ -3,16 +3,24 @@
 int
 main(void)
 {
-  pid_t  sid, pid;
+  pid_t  sid, pid, pgid, ppid;
+
+  pid = getpid();
+  pgid = getpgid(0);
+  ppid = getppid();
+  sid = getsid(0);
+
+  printf("sid=%d, pid=%d, pgid=%d, ppid=%d\n", sid, pid, pgid, ppid);
 
   if ((sid = setsid()) == -1)
     {
       perror(NULL);
       exit(1);
     }
-  pid = getpid();
-
-  printf("pid=%d, sid=%d\n", pid, sid);
+  sid = getsid(0);
+  pgid = getpgid(0);
+  printf("setsid() -> sid=%d, pid=%d, pgid=%d, ppid=%d\n",
+         sid, pid, pgid, ppid);
 
   exit(0);
 }
