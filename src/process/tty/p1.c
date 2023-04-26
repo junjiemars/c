47f49f7a76d22117ac378425f75a1a6cd8c43a3b
Junjie Mars <junjie@Air.local>
2023-04-26 23:06:54 +0800

    process: tty

--- src/process/tty/p1.c
+++ src/process/tty/p1.c
@@ -3,5 +3,31 @@
 int
 main(void)
 {
+  int    err;
+  char  *name;
+
+  err = 0;
+  if (isatty(STDIN_FILENO) == 0)
+    {
+      if (err)
+        {
+          perror("isatty");
+        }
+      else
+        {
+          name = ttyname(STDIN_FILENO);
+          if (name == NULL)
+            {
+              perror("ttyname");
+              exit(1);
+            }
+          printf("stdin is a tty: %s\n", name);
+        }
+    }
+  else
+    {
+      printf("stdin is not a tty\n");
+    }
+
   exit(0);
 }
