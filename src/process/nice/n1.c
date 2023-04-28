65e4f8582ae3f4298c0936823ef8625f4c1b77c1
Junjie Mars <junjie@Air.local>
2023-04-29 07:50:06 +0800

    process: nice

--- src/process/nice/n1.c
+++ src/process/nice/n1.c
@@ -1,41 +1,78 @@
 #include "_process_.h"
 
+/*
+ * XSI:
+ *
+ */
+
 int
 main(int argc, char *argv[])
 {
-  id_t  nice, old;
+  int   n1;
+  id_t  n2, o2;
+  pid_t pid;
 
   if (argc < 2)
     {
-      nice = -20;
+      n2 = -NZERO;
     }
   else
     {
-      nice = (id_t) atoi(argv[1]);
+      n2 = (id_t) atoi(argv[1]);
     }
 
   errno = 0;
-  old = getpriority(PRIO_PROCESS, 0);
+  n1 = nice(0);
+  if (errno)
+    {
+      perror(NULL);
+    }
+  assert(n1 == 0);
+  printf("nice(%d) = %d\n", 0, n1);
+
+  errno = 0;
+  o2 = getpriority(PRIO_PROCESS, 0);
   if (errno)
     {
       perror(NULL);
       exit(1);
     }
 
-  if (setpriority(PRIO_PROCESS, 0, nice) == -1)
+  if (setpriority(PRIO_PROCESS, 0, n2) == -1)
     {
       perror(NULL);
       exit(1);
     }
 
-  errno = 0;
-  nice = getpriority(PRIO_PROCESS, 0);
-  if (errno)
+  if ((pid = fork()) == -1)
     {
       perror(NULL);
       exit(1);
     }
-  printf("nice from %d to %d\n", old, nice);
+  else if (pid == 0)
+    {
+      id_t  c;
+      errno = 0;
+      c = getpriority(PRIO_PROCESS, 0);
+      if (errno)
+        {
+          perror(NULL);
+          exit(1);
+        }
+      printf("child's nice is %d\n", c);
+      exit(0);
+    }
+  else
+    {
+      errno = 0;
+      n2 = getpriority(PRIO_PROCESS, 0);
+      if (errno)
+        {
+          perror(NULL);
+          exit(1);
+        }
+      printf("nice from %d to %d\n", o2, n2);
+    }
 
   exit(0);
 }
