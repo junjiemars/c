fe1dccb90b3c6b54a46fcbffa0da6ba8b8efc64a
Junjie Mars <junjie@Air.local>
2023-04-30 00:09:19 +0800

    rust: rest

--- src/rust/rest/src/main.rs
+++ src/rust/rest/src/main.rs
@@ -2,7 +2,7 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let root = warp::path::end().map(|| "Welcome to RESTfull!");
+    let root = warp::path::end().map(|| "Welcome to RESTful!");
     let routes = root.with(warp::cors().allow_any_origin());
 
     warp::serve(routes).run(([127, 0, 0, 1], 9090)).await;
