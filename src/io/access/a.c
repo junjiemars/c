23575f123efa78381952a0fb9ba82466c6075c32
Junjie Mars <junjie@Air.local>
2023-05-04 00:18:51 +0800

    io

--- src/io/access/a.c
+++ src/io/access/a.c
@@ -1,61 +1,59 @@
 #include "_io_.h"
 
-
 /*
  * 1. access just check real user/group-id.
  * 2. open check effective user/group-id.
  *
  * References:
- * 1. https://pubs.opengroup.org/onlinepubs/9699919799/functions/access.html#tag_16_09
+ * 1.
+ * https://pubs.opengroup.org/onlinepubs/9699919799/functions/access.html#tag_16_09
  *
  */
 
-
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
-  int    rc;
-  uid_t  uid, euid;
+  int rc;
+  uid_t uid, euid;
 
   if (argc < 2)
     {
-      fprintf(stderr, "usage: %s <pathname> [pathname...]\n",
-             basename(argv[0]));
-      exit(EXIT_FAILURE);
+      fprintf (stderr, "usage: %s <pathname> [pathname...]\n",
+               basename (argv[0]));
+      exit (EXIT_FAILURE);
     }
 
-  uid = getuid();
-  euid = geteuid();
+  uid = getuid ();
+  euid = geteuid ();
 
-  printf("uid: %i, euid: %i\n------------\n", uid, euid);
+  printf ("uid: %i, euid: %i\n------------\n", uid, euid);
 
   for (int i = 1; i < argc; i++)
     {
-      rc = access(argv[i], F_OK);
+      rc = access (argv[i], F_OK);
       if (rc == -1)
         {
-          perror("!exist access");
+          perror ("!exist access");
         }
 
-      rc = access(argv[i], X_OK);
+      rc = access (argv[i], X_OK);
       if (rc == -1)
         {
-          perror("!execute access");
+          perror ("!execute access");
         }
 
-      rc = access(argv[i], R_OK);
+      rc = access (argv[i], R_OK);
       if (rc == -1)
         {
-          perror("!read access");
+          perror ("!read access");
         }
 
-      rc = open(argv[i], O_RDONLY);
+      rc = open (argv[i], O_RDONLY);
       if (rc == -1)
         {
-          perror("!open for read");
+          perror ("!open for read");
         }
-
     }
 
-  exit(EXIT_SUCCESS);
+  exit (EXIT_SUCCESS);
 }
--- src/io/access/aat.c
+++ src/io/access/aat.c
@@ -9,52 +9,50 @@
  * faccessat(,,,flag=AT_EACCESS) using effective uid/gid.
  */
 
-
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
-  int    rc;
-  uid_t  uid, euid;
+  int rc;
+  uid_t uid, euid;
 
   if (argc < 2)
     {
-      fprintf(stderr, "usage: %s <pathname> [pathname...]\n",
-              basename(argv[0]));
-      exit(EXIT_FAILURE);
+      fprintf (stderr, "usage: %s <pathname> [pathname...]\n",
+               basename (argv[0]));
+      exit (EXIT_FAILURE);
     }
 
-  uid = getuid();
-  euid = geteuid();
+  uid = getuid ();
+  euid = geteuid ();
 
-  printf("uid(%i),euid(%i)\n------------\n", uid, euid);
+  printf ("uid(%i),euid(%i)\n------------\n", uid, euid);
 
   for (int i = 1; i < argc; i++)
     {
-      rc = faccessat(AT_FDCWD, argv[i], F_OK, 0);
+      rc = faccessat (AT_FDCWD, argv[i], F_OK, 0);
       if (rc == -1)
         {
-          perror("!exist access");
+          perror ("!exist access");
         }
 
-      rc = faccessat(AT_FDCWD, argv[i], R_OK, 0);
+      rc = faccessat (AT_FDCWD, argv[i], R_OK, 0);
       if (rc == -1)
         {
-          perror("!read access");
+          perror ("!read access");
         }
 
-      rc = faccessat(AT_FDCWD, argv[i], R_OK, AT_EACCESS);
+      rc = faccessat (AT_FDCWD, argv[i], R_OK, AT_EACCESS);
       if (rc == -1)
         {
-          perror("!read access (effective)");
+          perror ("!read access (effective)");
         }
 
-      rc = open(argv[i], O_RDONLY);
+      rc = open (argv[i], O_RDONLY);
       if (rc == -1)
         {
-          perror("!open for read");
+          perror ("!open for read");
         }
-
     }
 
-  exit(EXIT_SUCCESS);
+  exit (EXIT_SUCCESS);
 }
