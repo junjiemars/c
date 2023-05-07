9f7684223f38fc6121299b926f29b5db8ebeb23e
Junjie Mars <junjie@Air.local>
2023-05-07 13:19:40 +0800

    rust: iced

--- src/rust/iced/src/main.rs
+++ src/rust/iced/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Settings;
+use iced::Sandbox;
+
+pub mod Tour;
+
+pub fn main() -> iced::Result {
+    env_logger::init();
+
+    Tour::run(Settings::default())
 }
