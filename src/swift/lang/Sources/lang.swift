7a76521fcd1145856f06bf861b8bdf0051c73a86
junjie <junjie.mars@icloud.com>
2023-06-10 20:48:26 +0800

    swift

--- src/swift/lang/Sources/lang.swift
+++ src/swift/lang/Sources/lang.swift
@@ -140,6 +140,77 @@ func functionsAndClosures() {
     assert([1,5,9,2,1].sorted { $0 > $1 }.count == 5)
 }
 
+func objectsAndClasses() {
+    let shape1 = Shape(name: "unnamed")
+    assert(shape1.name == "unnamed")
+
+    let square1 = Square(sideLength: 2.0, name: "Square")
+    assert(square1.area() == 4.0)
+
+    let triangle1 = EquilateralTriangle(sideLength: 3.0, name: "Triangle")
+    assert(triangle1.perimeter == 3.0 * 3.0)
+    triangle1.perimeter = 24.0
+    assert(triangle1.sideLength == 8.0)
+
+    let st1 = SquareAndTriangle(size: 3, name: "square & triangle")
+    assert(st1.square.sideLength == 3.0 && st1.triangle.sideLength == 3.0)
+    st1.square = Square(sideLength: 4.0, name: "larger square")
+    assert(st1.triangle.sideLength == 4.0)
+}
+
+func enumerationsAndStructures() {
+    let ace = Rank.ace
+    assert(ace.rawValue == 1)
+
+    if let r1 = Rank(rawValue: 9) {
+        assert(r1 == Rank.nine)
+    }
+
+    let spades3 = Card(rank: .three, suit: .spades)
+    assert(spades3.rank == Rank.three && spades3.suit == Suit.spades)
+}
+
+func concurrency() async {
+    let r1 = await slowFetch(from: "xxx")
+    assert(r1 == 501)
+
+    let r2 = await slowFetch(from: "primary")
+    assert(r2 == 200)
+}
+
+func protocolsAndExtensions() {
+}
+
+func errorHandling() {
+    do {
+        let p1 = try send(job: 101, toPrinter: "HP2210")
+
+        defer {
+            assert(p1.count > 0)
+        }
+
+        if let p3 = try? send(job: 103, toPrinter: "HP2210") {
+            assert(p3.count > 0)
+        }
+
+        let p4 = try? send(job: 104, toPrinter: "HP2211")
+        assert(p4 == nil)
+
+    } catch {
+        print(error)
+    }
+}
+
+func generics() {
+    let a1 = makeArray(repeating: "A", numberOfTimes: 3)
+    assert(a1.count == 3 && a1[0] == "A")
+
+    var o1: OptionalValue<Int> = .none
+    o1 = .some(100)
+
+    assert(anyOverlapped([1,2,3], [3]))
+    assert(anyOverlapped(["A","B","C"], ["B"]))
+}
 
 func functionReturnTuple(a: Int) -> (name: String, value: Int) {
     return (name: "Integer", value: a)
@@ -165,4 +236,141 @@ func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
 
 func greatThan20(n: Int) -> Bool {
     return n > 20
+}
+
+
+class Shape  {
+    var name: String
+    var numberOfSides = 0
+    init(name: String) {
+        self.name = name
+    }
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Square: Shape {
+    var sideLength: Double
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+class EquilateralTriangle: Shape {
+    var sideLength: Double
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+class SquareAndTriangle {
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, teen
+    case jack, queen, king
+    func simpleDescription() -> String {
+        switch self {
+        case .ace: return "ace"
+        case .jack: return "jack"
+        case .queen: return "queen"
+        case .king: return "king"
+        default: return String(self.rawValue)
+        }
+    }
+}
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+}
+struct Card {
+    var rank: Rank
+    var suit: Suit
+}
+
+// concurrency
+func slowFetch(from server: String) async -> Int {
+    if server == "primary" {
+        return 200
+    }
+    return 501
+}
+
+// errorHandling
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "HP2211" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+
+// generics
+func makeArray<T>(repeating item: T, numberOfTimes: Int) -> [T] {
+    var result: [T] = []
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+
+func anyOverlapped<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+  where T.Element: Equatable, T.Element == U.Element
+{
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
 }
