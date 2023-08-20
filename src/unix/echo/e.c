2d770e7dc5ff0d758cf9625fa014f831cc812bd7
junjie <junjie.mars@icloud.com>
2023-08-20 21:08:39 +0800

    unix: echo

--- src/unix/echo/e.c
+++ src/unix/echo/e.c
@@ -1,27 +1,28 @@
-#include <_os_.h>
+#include <_unix_.h>
 #include <stdio.h>
 #include <getopt.h>
 
 extern char *optarg;
+extern int   optind;
 
-void usage(const char *echo);
+void usage (const char *echo);
 
 void
-usage(const char *pcre)
+usage (const char *pcre)
 {
-  printf("Usage: %s [-n]... [string ...]\n", pcre);
-	printf("\n");
-	printf("Write arguments to the standard output.\n");
-  printf("  -o            do not print the trailing newline character.\n");
+  printf ("Usage: %s [-n]... [string ...]\n", pcre);
+	printf ("\n");
+	printf ("Write arguments to the standard output.\n");
+  printf ("  -n            do not print the trailing newline character.\n");
 }
 
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
   int opt_n = 0;
   int ch;
 
-  while (EOF != (ch = getopt(argc, argv, "hn")))
+  while (-1 != (ch = getopt(argc, argv, "hn")))
     {
       switch (ch)
         {
@@ -30,20 +31,20 @@ main(int argc, char **argv)
           break;
         case 'h':
         default:
-          usage(argv[0]);
+          usage (argv[0]);
           return 0;
         }
     }
 
-  char **p = argv + optind;
-  
-  while (*p)
+  for (char **p = argv + optind; *p != 0; p++)
     {
-      fprintf(stdout, "%s", *p++);
+      char sp = (optind == argc - 1) ? 0x0 : 0x20;
+      fprintf (stdout, "%s%c", *p, sp);
     }
+
   if (!opt_n)
     {
-      fprintf(stdout, "\n");
+      fprintf (stdout, "\n");
     }
 
   return 0;
