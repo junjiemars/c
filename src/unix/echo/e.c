df658a4024af47fb5b553c579db31900caf99e78
junjie <junjie.mars@icloud.com>
2023-08-30 22:47:51 +0800

    unix/echo

--- src/unix/echo/e.c
+++ src/unix/echo/e.c
@@ -1,6 +1,6 @@
 #include "_unix_.h"
 #include <stdio.h>
-#include <string.h>
+
 
 int
 main (int argc, char **argv)
@@ -8,25 +8,21 @@ main (int argc, char **argv)
   int opt_n = 0;
   int opt_ind = 1;
 
+  if (argc > 1)
+    {
+      opt_n = *argv[1] == '-' && *(argv[1]+1) == 'n';
+      opt_ind += opt_n;
+    }
+  else
+    {
+      fprintf(stdout, "\n");
+      return 0;
+    }
+
   for (int i = opt_ind; i < argc; i++)
     {
-      if (strcmp ("-n", argv[i]) == 0)
-        {
-           opt_n = 1;
-           opt_ind = i+1;
-           break;
-        }
-    }
-
-  for (char **p = argv + opt_ind; *p != 0; p++)
-    {
-      char sp = (opt_ind == argc - 1) ? 0x0 : 0x20;
-      fprintf (stdout, "%s%c", *p, sp);
-    }
-
-  if (opt_n == 0)
-    {
-      fprintf (stdout, "\n");
+      char *s = i+1 < argc ? " " : opt_n == 0 ? "\n" : "";
+      fprintf (stdout, "%s%s", argv[i], s);
     }
 
   return 0;
