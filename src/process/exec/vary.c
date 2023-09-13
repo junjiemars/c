90202fb8cf101e4e31d265fdcce00ed9965169b1
南山竹 <junjiemars@gmail.com>
2023-09-13 13:29:31 +0800

    process: execve

--- src/process/exec/vary.c
+++ src/process/exec/vary.c
@@ -1,4 +1,5 @@
 #include "_process_.h"
+#include <ctype.h>
 
 /*
  * Exhibit how `exec' sequence functions been called.
@@ -14,40 +15,109 @@ int
 main (int argc, char *argv[])
 {
   int rc = 0;
-  char *const vargv[] = { _FILE_NAME_, "a", "b", "c", 0 };
   extern char **environ;
 
   if (argc < 2)
     {
-      printf ("usage: argv[1] == exec...\n");
+      printf ("usage: argv[1] == exec\n");
+      return 0;
+    }
+  else if (argc > 5)
+    {
+      printf ("usage: argc > 5\n");
       return 0;
     }
   printf ("%s------------\n", argv[1]);
 
   if (0 == strcmp ("execl", argv[1]))
     {
-      rc = execl (_PATH_NAME_, _FILE_NAME_, "a", "b", "c", (char *)0);
+      switch (argc)
+        {
+        case 5:
+          rc = execl (_PATH_NAME_, _FILE_NAME_, argv[2], argv[3], argv[4],
+                      (char *)0);
+          break;
+        case 4:
+          rc = execl (_PATH_NAME_, _FILE_NAME_, argv[2], argv[3], (char *)0);
+          break;
+        case 3:
+          rc = execl (_PATH_NAME_, _FILE_NAME_, argv[2], (char *)0);
+          break;
+        default:
+          rc = execl (_PATH_NAME_, _FILE_NAME_, (char *)0);
+          break;
+        }
     }
   else if (0 == strcmp ("execlp", argv[1]))
     {
-      rc = execlp (_FILE_NAME_, _FILE_NAME_, "a", "b", "c", (char *)0);
+      switch (argc)
+        {
+        case 5:
+          rc = execlp (_FILE_NAME_, _FILE_NAME_, argv[2], argv[3], argv[4],
+                       (char *)0);
+          break;
+        case 4:
+          rc = execl (_FILE_NAME_, _FILE_NAME_, argv[2], argv[3], (char *)0);
+          break;
+        case 3:
+          rc = execl (_PATH_NAME_, _FILE_NAME_, argv[2], (char *)0);
+          break;
+        default:
+          rc = execlp (_FILE_NAME_, _FILE_NAME_, (char *)0);
+          break;
+        }
     }
   else if (0 == strcmp ("execle", argv[1]))
     {
-      rc = execle (_PATH_NAME_, _FILE_NAME_, "a", "b", "c", (char *)0,
-                   environ);
+      switch (argc)
+        {
+        case 5:
+          rc = execle (_PATH_NAME_, _FILE_NAME_, argv[2], argv[3], argv[4],
+                       (char *)0, environ);
+          break;
+        case 4:
+          rc = execle (_PATH_NAME_, _FILE_NAME_, argv[2], argv[3], (char *)0,
+                       environ);
+          break;
+        case 3:
+          rc = execle (_PATH_NAME_, _FILE_NAME_, argv[2], (char *)0, environ);
+          break;
+        default:
+          rc = execle (_PATH_NAME_, _FILE_NAME_, (char *)0, environ);
+          break;
+        }
     }
   else if (0 == strcmp ("execv", argv[1]))
     {
-      rc = execv (_PATH_NAME_, vargv);
+      char **args = 0;
+      if (argc > 2)
+        {
+          args = &argv[2];
+        }
+      rc = execv (_PATH_NAME_, args);
     }
   else if (0 == strcmp ("execvp", argv[1]))
     {
-      rc = execvp (_FILE_NAME_, vargv);
+      char **args = 0;
+      if (argc > 2)
+        {
+          args = &argv[2];
+        }
+      rc = execvp (_FILE_NAME_, args);
     }
   else if (0 == strcmp ("execve", argv[1]))
     {
-      rc = execve (_PATH_NAME_, vargv, environ);
+      char **args = 0;
+      char *xxx = 0;
+      if (argc > 2)
+        {
+          args = &argv[2];
+        }
+      if ((xxx = getenv ("XXX")) != 0)
+        {
+          setenv ("YYY", xxx, 0);
+        }
+      rc = execve (_PATH_NAME_, args, environ);
     }
   else
     {
