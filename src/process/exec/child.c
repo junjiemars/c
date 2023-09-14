afcd7e5094528cc4847a1b4bd041d40f9a89461b
南山竹 <junjiemars@gmail.com>
2023-09-14 17:37:40 +0800

    process/exec: child

--- src/process/exec/child.c
+++ src/process/exec/child.c
@@ -6,11 +6,12 @@
  */
 
 int
-main (void)
+main (int argc, char **argv)
 {
   int rc = 0;
   pid_t pid;
-  char *const env[] = { "XXX=xxx", 0 };
+  char **args = 0;
+  char **env = 0;
 
   if ((pid = fork ()) == -1)
     {
@@ -19,12 +20,35 @@ main (void)
     }
   else if (pid == 0)
     {
-      rc = execle (_PATH_NAME_, _FILE_NAME_, "a", "b", "c", (char *)0, env);
+      int env_n = 0;
+      extern char **environ;
+
+      for (char **p = environ; *p != 0; p++)
+        {
+          env_n++;
+        }
+      env = (char **)malloc (sizeof (char *) * (env_n + 1));
+      for (int i = 0; i < env_n; i++)
+        {
+          env[i] = strdup (environ[i]);
+        }
+      env[env_n] = 0;
+
+      args = (char **)malloc (sizeof (char *) * (argc + 1));
+      args[0] = strdup (_FILE_NAME_);
+      for (int i = 1; i < argc; i++)
+        {
+          args[i] = strdup (argv[i]);
+        }
+      args[argc] = 0;
+
+      rc = execve (_PATH_NAME_, args, env);
       if (rc == -1)
         {
           perror (NULL);
           exit (EXIT_FAILURE);
         }
+      /* never return */
       exit (EXIT_SUCCESS);
     }
 
