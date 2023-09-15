250dcc377c5293727d676942ff90397c633b52c9
南山竹 <junjiemars@gmail.com>
2023-09-15 14:08:19 +0800

    process/rlimit: prlimit

---
+++ src/process/rlimit/p1.c
@@ -0,0 +1,38 @@
+#include "_process_.h"
+#include <stdlib.h>
+
+/*
+ * Simulate `prlimit(1)' command.
+ *
+ *
+ */
+
+static void pr_rlimit (struct rlimit *);
+
+int
+main (int argc, char **argv)
+{
+  pid_t pid = 0;
+  struct rlimit orl;
+
+  if (argc > 1)
+    {
+      pid = atoi (argv[1]);
+    }
+
+  if (prlimit (pid, RLIMIT_STACK, NULL, &orl) == -1)
+    {
+      perror (NULL);
+      exit (EXIT_FAILURE);
+    }
+  pr_rlimit (&orl);
+
+  exit (EXIT_SUCCESS);
+}
+
+void
+pr_rlimit (struct rlimit *rl)
+{
+  printf ("{rlim_cur=%ld, rlim_max=%ld}\n", (long)rl->rlim_cur,
+          (long)rl->rlim_max);
+}
