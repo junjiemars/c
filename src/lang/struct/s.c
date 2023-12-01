3c8d1c1bed439a2874f100f9066ed27c6adcc518
junjie <junjie.mars@icloud.com>
2023-12-01 23:03:18 +0800

    lang: struct

--- src/lang/struct/s.c
+++ src/lang/struct/s.c
@@ -1,83 +1,94 @@
 #include "_lang_.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 
 #if (MSVC)
-#  pragma warning(disable: 4200)
+#pragma warning(disable : 4200)
 #endif
 
-struct flex1_s {
+struct flex1_s
+{
   int n;
-  long n2[];  /* flexibile member */
+  long n2[]; /* flexibile member */
 };
 
 #if defined(GCC) || defined(CLANG)
-struct __attribute__((packed)) flex2_s {
+struct __attribute__ ((packed)) flex2_s
+{
   int n;
   long n2[]; /* flexibile member */
 };
 #elif defined(MSVC)
-#  pragma pack(push, 1)
-struct flex2_s {
+#pragma pack(push, 1)
+struct flex2_s
+{
   int n;
   long n2[]; /* flexibile member */
 };
-#  pragma pack(pop)
+#pragma pack(pop)
 #endif
 
-struct noname1_s {
+struct noname1_s
+{
   uint16_t n;
-  struct { /* anonymous struct */
+  struct
+  { /* anonymous struct */
     uint16_t a;
     uint16_t b;
   } noname;
 };
 
-
 #if defined(GCC) || defined(CLANG)
-struct __attribute__((packed)) noname2_s {
+struct __attribute__ ((packed)) noname2_s
+{
   uint16_t n;
-  struct { /* anonymous struct */
+  struct
+  { /* anonymous struct */
     uint16_t a;
     uint16_t b;
   } noname;
 };
 #elif defined(MSVC)
-#  pragma pack(push, 1)
-struct noname2_s {
+#pragma pack(push, 1)
+struct noname2_s
+{
   uint16_t n;
-  struct { /* anonymous struct */
+  struct
+  { /* anonymous struct */
     uint16_t a;
     uint16_t b;
   } noname;
 };
-#  pragma pack(pop)
+#pragma pack(pop)
 #endif
 
 struct cyclic1_y;
 
-struct cyclic1_x {
+struct cyclic1_x
+{
   int nx;
   struct cyclic1_y *y; /* if not pointer type that is incomplete type */
 };
 
-struct cyclic1_y {
+struct cyclic1_y
+{
   int ny;
   struct cyclic1_x x;
 };
 
-
 #if defined(GCC) || defined(CLANG)
 struct cyclic2_y;
 
-struct __attribute__((packed)) cyclic2_x {
+struct __attribute__ ((packed)) cyclic2_x
+{
   int nx;
   struct cyclic2_y *y; /* if not pointer type that is incomplete type */
 };
 
-struct __attribute__((packed)) cyclic2_y {
+struct __attribute__ ((packed)) cyclic2_y
+{
   int ny;
   struct cyclic2_x x;
 };
@@ -85,107 +96,128 @@ struct __attribute__((packed)) cyclic2_y {
 #pragma pack(push, 1)
 struct cyclic2_y;
 
-struct cyclic2_x {
+struct cyclic2_x
+{
   int nx;
   struct cyclic2_y *y; /* if not pointer type that is incomplete type */
 };
 
-struct cyclic2_y {
+struct cyclic2_y
+{
   int ny;
   struct cyclic2_x x;
 };
 #pragma pack(pop)
 #endif
 
-#define _OVERLAP_                               \
-  char *base;                                   \
+#define _OVERLAP_                                                             \
+  char *base;                                                                 \
   size_t len;
 
-struct nest1_s {
+struct nest1_s
+{
   _OVERLAP_
   uint16_t x;
 };
 
-struct nest2_s {
+struct nest2_s
+{
   _OVERLAP_
   char y[16];
 };
 
-struct padding_s {
+struct padding_s
+{
   int a;
   char b;
 };
 
 #if defined(GCC) || defined(CLANG)
-struct __attribute__((packed)) packed_s {
+struct __attribute__ ((packed)) packed_s
+{
   int a;
   char b;
 };
 #elif defined(MSVC)
 #pragma pack(push, 1)
-struct packed_s {
+struct packed_s
+{
   int a;
   char b;
 };
 #pragma pack(pop)
 #endif
 
-void test_flex1_s(void);
-void test_flex2_s(void);
-void test_noname1_s(void);
-void test_noname2_s(void);
-void test_cyclic1_s(void);
-void test_cyclic2_s(void);
-void test_nest_s(void);
-void test_padding_s(void);
-void test_packed_s(void);
+void test_flex1_s (void);
+void test_flex2_s (void);
+void test_noname1_s (void);
+void test_noname2_s (void);
+void test_cyclic1_s (void);
+void test_cyclic2_s (void);
+void test_nest_s (void);
+void test_padding_s (void);
+void test_packed_s (void);
+void test_empty_s (void);
 
 void
-test_flex1_s(void) {
+test_flex1_s (void)
+{
   struct flex1_s fs1 = { 0 };
-  printf("sizeof(fs1) = %zu\n", sizeof(fs1));
+
+  printf ("struct flex1_s:\n------------\n");
+  printf ("sizeof(fs1) = %zu\n", sizeof (fs1));
   /* fs1.n2[0] = 0x1; */
   /* fs1.n2 flexibile member, no space had been allocated */
 
   /* initialization of flexible array member is not allowed */
   /* struct flex1_s fs2 = { .n = 1, .n2= { 0x11, 0x22 } }; */
   struct flex1_s fs3 = { .n = 1 };
-  printf("sizeof(fs3) = %zu\n", sizeof(fs3));
+  printf ("sizeof(fs3) = %zu\n", sizeof (fs3));
 
   /* n2[8] */
-  struct flex1_s *fs64 = malloc(sizeof(struct flex1_s) + sizeof(long)*8);
+  struct flex1_s *fs64 = malloc (sizeof (struct flex1_s) + sizeof (long) * 8);
   /* n2[1] */
-  struct flex1_s *fsd2 = malloc(sizeof(struct flex1_s) + sizeof(long)+2);
+  struct flex1_s *fsd2 = malloc (sizeof (struct flex1_s) + sizeof (long) + 2);
 
-  for (int i = 0; i < 8; i++) {
-    fs64->n2[i] = i;
-  }
-  for (int i = 0; i < 8; i++) {
-    printf("fs64->n2[%d] = %8li\n", i, fs64->n2[i]);
-  }
+  for (int i = 0; i < 8; i++)
+    {
+      fs64->n2[i] = i;
+    }
+  for (int i = 0; i < 8; i++)
+    {
+      printf ("fs64->n2[%d] = %8li\n", i, fs64->n2[i]);
+    }
 
   /* head buffer overflow */
-  for (int i = 0; i < 1; i++) {
-    fsd2->n2[i] = i;
-  }
-  for (int i = 0; i < 1; i++) {
-    printf("fsd2->n2[%d] = %8li\n", i, fsd2->n2[i]);
-  }
+  for (int i = 0; i < 1; i++)
+    {
+      fsd2->n2[i] = i;
+    }
+  for (int i = 0; i < 1; i++)
+    {
+      printf ("fsd2->n2[%d] = %8li\n", i, fsd2->n2[i]);
+    }
 
-  free(fs64);
-  free(fsd2);
+  free (fs64);
+  free (fsd2);
 }
 
 void
-test_flex2_s(void) {
-  struct flex2_s fs1 = {0};
-  printf("sizeof(fs1) = %4zu\n", sizeof(fs1));
+test_flex2_s (void)
+{
+  struct flex2_s fs1 = { 0 };
+
+  printf ("struct flex2_s:\n------------\n");
+  printf ("sizeof(fs1) = %4zu\n", sizeof (fs1));
 }
 
 void
-test_noname1_s(void) {
-  struct noname1_s nn1 = {0};
-  printf("sizeof(nn1) = %4zu\n", sizeof(nn1));
+test_noname1_s (void)
+{
+  struct noname1_s nn1 = { 0 };
+
+  printf ("struct noname1_s:\n------------\n");
+  printf ("sizeof(nn1) = %4zu\n", sizeof (nn1));
 
   nn1.n = 0x1;
   nn1.noname.a = 0x2;
@@ -193,9 +225,12 @@ test_noname1_s(void) {
 }
 
 void
-test_noname2_s(void) {
-  struct noname2_s nn1 = {0};
-  printf("sizeof(nn1) = %4zu\n", sizeof(nn1));
+test_noname2_s (void)
+{
+  struct noname2_s nn1 = { 0 };
+
+  printf ("struct noname2_s:\n------------\n");
+  printf ("sizeof(nn1) = %4zu\n", sizeof (nn1));
 
   nn1.n = 0x1;
   nn1.noname.a = 0x2;
@@ -203,72 +238,102 @@ test_noname2_s(void) {
 }
 
 void
-test_cyclic1_s(void) {
+test_cyclic1_s (void)
+{
   struct cyclic1_x x = { 0 };
   struct cyclic1_y y = { 0 };
-  printf("sizeof(x) = %4zu\n", sizeof(x));
-  printf("sizeof(y) = %4zu\n", sizeof(y));
+
+  printf ("struct cyclic1_y:\n------------\n");
+  printf ("sizeof(x) = %4zu\n", sizeof (x));
+  printf ("sizeof(y) = %4zu\n", sizeof (y));
 }
 
 void
-test_cyclic2_s(void) {
+test_cyclic2_s (void)
+{
   struct cyclic2_x x = { 0 };
-  printf("sizeof(x) = %4zu\n", sizeof(x));
+
+  printf ("struct cyclic2_x:\n------------\n");
+  printf ("sizeof(x) = %4zu\n", sizeof (x));
 
   struct cyclic2_y y = { 0 };
   y.ny = 0x11223344;
   y.x.nx = 0xaabbccdd;
-  printf("sizeof(y) = %4zu\n", sizeof(y));
+  printf ("sizeof(y) = %4zu\n", sizeof (y));
 }
 
 void
-test_nest_s(void) {
-  struct nest1_s *n1 = malloc(sizeof(struct nest1_s));
-  printf("sizeof(nest1_s) = %4zu\n", sizeof(struct nest1_s));
+test_nest_s (void)
+{
+  struct nest1_s *n1 = malloc (sizeof (struct nest1_s));
+
+  printf ("struct nest1_s:\n------------\n");
+  printf ("sizeof(nest1_s) = %4zu\n", sizeof (struct nest1_s));
 
   n1->base = "abc";
-  n1->len = strlen(n1->base);
+  n1->len = strlen (n1->base);
 
-  struct nest1_s *n11 = (struct nest1_s*)&n1->base;
-  printf("n11->base = %s, %zu\n", n11->base, n11->len);
+  struct nest1_s *n11 = (struct nest1_s *)&n1->base;
+  printf ("n11->base = %s, %zu\n", n11->base, n11->len);
 
-  struct nest2_s *n2 = (struct nest2_s*)n11;
-  printf("n2->base = %s, %zu\n", n2->base, n2->len);
+  struct nest2_s *n2 = (struct nest2_s *)n11;
+  printf ("n2->base = %s, %zu\n", n2->base, n2->len);
 
-  struct nest2_s *n21 = (struct nest2_s*)&n1->base;
-  printf("n21->base = %s, %zu\n", n21->base, n21->len);
+  struct nest2_s *n21 = (struct nest2_s *)&n1->base;
+  printf ("n21->base = %s, %zu\n", n21->base, n21->len);
 
-  free(n1);
+  free (n1);
 }
 
 void
-test_padding_s(void) {
-  struct padding_s *p1 = malloc(sizeof(struct padding_s));
-  printf("sizeof(padding_s) = %zu\n", sizeof(struct padding_s));
+test_padding_s (void)
+{
+  struct padding_s *p1 = malloc (sizeof (struct padding_s));
 
-  free(p1);
+  printf ("sturct padding_s:\n------------\n");
+  printf ("sizeof(padding_s) = %zu\n", sizeof (struct padding_s));
+
+  free (p1);
 }
 
 void
-test_packed_s(void) {
-  struct packed_s *p1 = malloc(sizeof(struct packed_s));
-  printf("sizeof(packed_s) = %zu\n", sizeof(struct packed_s));
+test_packed_s (void)
+{
+  struct packed_s *p1 = malloc (sizeof (struct packed_s));
 
-  free(p1);
+  printf ("struct packed_s:\n------------\n");
+  printf ("sizeof(packed_s) = %zu\n", sizeof (struct packed_s));
+
+  free (p1);
+}
+
+void
+test_empty_s (void)
+{
+#if defined(GCC) || defined(CLANG)
+  struct empty_s
+  {
+    /* void */
+  };
+  printf ("struct empty_s:\n------------\n");
+  printf ("sizeof(empty_s) = %zu\n", sizeof (struct empty_s));
+#endif
 }
 
 int
-main(void) {
+main (void)
+{
 
-  test_flex1_s();
-  test_flex2_s();
-  test_noname1_s();
-  test_noname2_s();
-  test_cyclic1_s();
-  test_cyclic2_s();
-  test_nest_s();
-  test_padding_s();
-  test_packed_s();
+  test_flex1_s ();
+  test_flex2_s ();
+  test_noname1_s ();
+  test_noname2_s ();
+  test_cyclic1_s ();
+  test_cyclic2_s ();
+  test_nest_s ();
+  test_padding_s ();
+  test_packed_s ();
+  test_empty_s ();
 
   return 0;
 }
--- src/lang/typedef/t.c
+++ src/lang/typedef/t.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 #if (MSVC)
-#  pragma warning(disable: 4172)
+#pragma warning(disable : 4172)
 #endif
 
 #if (GCC)
-#  pragma GCC diagnostic ignored "-Wreturn-local-addr"
+#pragma GCC diagnostic ignored "-Wreturn-local-addr"
 #endif
 
 /* syntax: typedef type declaration */
@@ -14,23 +14,24 @@ typedef int length_t;
 
 struct point_s /* tag: not type */
 {
-	int x;
-	int y;
+  int x;
+  int y;
 };
 typedef struct point_s point_t;
 
 typedef struct point1_s
 {
-	int x;
-	int y;
+  int x;
+  int y;
 } point1_t /* type */;
 
 /* x_t alias of int */
 /* p_t alias of pointer to int */
 /* f_t as function accept int and return pointer to int */
-typedef int x_t, *p_t, *(*fn_t)(int);
+typedef int x_t, *p_t, *(*fn_t) (int);
 
-p_t fn(int n)
+p_t
+fn (int n)
 {
   x_t x = n + 10;
   p_t p = &x;
@@ -40,25 +41,25 @@ p_t fn(int n)
 }
 
 void
-test_fn(void)
+test_fn (void)
 {
   fn_t f1 = fn;
-  p_t p = f1(3);
-  printf("test_fn(3)        = %4i\n", *p);
+  p_t p = f1 (3);
+  printf ("test_fn(3)        = %4i\n", *p);
 }
 
 int
-main(void)
+main (void)
 {
 
-	printf("sizeof(length_t)  = %4zu\n", sizeof(length_t));
-  printf("sizeof(point_t)   = %4zu\n", sizeof(point_t));
-  printf("sizeof(point1_t)  = %4zu\n", sizeof(point1_t));
-  printf("sizeof(x_t)       = %4zu\n", sizeof(x_t));
-  printf("sizeof(p_t)       = %4zu\n", sizeof(p_t));
-  printf("sizeof(f_t)       = %4zu\n", sizeof(fn_t));
+  printf ("sizeof(length_t)  = %4zu\n", sizeof (length_t));
+  printf ("sizeof(point_t)   = %4zu\n", sizeof (point_t));
+  printf ("sizeof(point1_t)  = %4zu\n", sizeof (point1_t));
+  printf ("sizeof(x_t)       = %4zu\n", sizeof (x_t));
+  printf ("sizeof(p_t)       = %4zu\n", sizeof (p_t));
+  printf ("sizeof(f_t)       = %4zu\n", sizeof (fn_t));
 
-  test_fn();
+  test_fn ();
 
   return 0;
 }
