dac8afa3f1d400ad733bc6e175a3d2b5b53aafbb
junjie <junjie.mars@icloud.com>
2023-12-06 22:45:58 +0800

    lang: clean

--- src/lang/volatile/basic.c
+++ src/lang/volatile/basic.c
@@ -3,54 +3,68 @@
 static int n;
 static volatile int n1;
 
-void
-bar(void)
+static void bar (void);
+static void bar1 (void);
+static int incr (void);
+
+int
+main (void)
 {
-	/* with clang -O2 optimized will keep function skeleton, just
-		 like:
-		 void bar(void)
-{}
-		 because n has ever changed
- */
-	n = 0;
-	int i = 0;
-	while (n != 255)
-{
-		if (++i > 3)
-{
-			break;
-		}
-	}
+  bar ();
+  bar1 ();
+  incr ();
+
+  return 0;
 }
 
 void
-bar1(void)
+bar (void)
 {
-	n1 = 0;
-	int i = 0;
-	while (n1 != 255)
+  /* with clang -O2 optimized will keep function skeleton, just
+     like:
+     void bar(void)
+     {}
+     because n has ever changed
+  */
+  n = 0;
+  int i = 0;
+  while (n != 254)
+    {
+      if (++i > 3)
+        {
+          break;
+        }
+    }
+}
+
+void
+bar1 (void)
 {
-		if (++i > 3)
-{
-			break;
-		}
-	}
+  /*
+   * With -O2/-O2:
+   * 1. bar1 may be inline into main routine
+   * 2. but, load n1 from memory never disappear
+   */
+  n1 = 0;
+  int i = 0;
+  while (n1 != 255)
+    {
+      if (++i > 3)
+        {
+          break;
+        }
+    }
 }
 
 int
-incr(void)
+incr (void)
 {
-	n1 += 0xff;
-	return n1;
-}
-
-int
-main(void)
-{
-
-	bar();
-	bar1();
-	incr();
-
-	return 0;
+  /*
+   * Always:
+   * 1. load from memory
+   * 2. add
+   * 3. store back to memory
+   */
+  n1 += 0xff;
+  return n1;
 }
