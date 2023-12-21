126d53dedaffed81c5bab37ece4d251e0b60986e
junjie <junjiemars@gmail.com>
2023-12-21 14:27:20 +0800

    clean

--- src/lang/type/ct.c
+++ src/lang/type/ct.c
@@ -1,34 +1,35 @@
 #include "_lang_.h"
 
-
 #if (MSVC)
-#  pragma warning(disable: 4244)
+#pragma warning(disable : 4244)
 #endif
 
-long
-atox(const char *s)
-{
-  static char const *x = "0123456789ABCDEF";
-  long sum;
-  while (isspace(*s++))
-    {
-      /* do nothing */
-    }
-  for (sum = 0L; isxdigit(*s); ++s)
-    {
-      int d = strchr(x, toupper(*s)) - x;
-      sum = sum * 16L + d;
-    }
-  return sum;
-}
+static long atox (const char *);
 
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
   if (argc > 1)
     {
-      long x = atox(argv[1]);
-      printf("0x%s = 0d%ld\n", argv[1], x);
+      long x = atox (argv[1]);
+      printf ("0x%s = 0d%ld\n", argv[1], x);
     }
   return 0;
+}
+
+long
+atox (const char *s)
+{
+  static char const *x = "0123456789ABCDEF";
+  long sum;
+  while (isspace (*s++))
+    {
+      /* do nothing */
+    }
+  for (sum = 0L; isxdigit (*s); ++s)
+    {
+      int d = strchr (x, toupper (*s)) - x;
+      sum = sum * 16L + d;
+    }
+  return sum;
 }
