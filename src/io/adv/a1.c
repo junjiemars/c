4dbabb973fb17e0af1c48aa1bf926896181afa26
junjie <junjie.mars@icloud.com>
2023-12-27 19:59:13 +0800

    io: posix_fadvise

--- src/io/adv/a1.c
+++ src/io/adv/a1.c
@@ -7,11 +7,13 @@ main (void)
   int n;
   char buf [64];
 
+#if (NM_HAVE_POSIX_FADVISE)
   if (posix_fadvise (fd, 0, 0, POSIX_FADV_SEQUENTIAL) < 0)
     {
       perror (NULL);
       exit (EXIT_FAILURE);
     }
+#endif
 
   while ((n = read (fd, buf, sizeof (buf))) > 0)
     {
