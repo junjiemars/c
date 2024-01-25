c347799687046f02b809e07a8e354d4552c86c58
junjie <junjiemars@gmail.com>
2024-01-25 17:11:50 +0800

    cxx

--- src/cxx/std14/main.cpp
+++ src/cxx/std14/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+
 auto add(int a, int b) {
 	return a + b;
 }
@@ -6,13 +8,13 @@ auto add(int a, int b) {
 template<typename T>
 constexpr T pi = T(3.14159265358979323846);
 
-
 int main() {
 	std::cout << add(3,4) << std::endl;
 	auto times = [](auto a, auto b) {
 		return a * b;
 	};
 	std::cout << times(3,4) << std::endl;
-	std::cout << pi<long double> << "," << pi<float> << std::endl;
+  std::cout << pi<float> << std::endl;
+	std::cout << std::setprecision(10) << pi<long double> << std::endl;
 	return 0;
 }
