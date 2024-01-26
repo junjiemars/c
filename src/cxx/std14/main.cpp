ee56803ccad13bbb08dd11d34e9cd64c86d02ae7
junjie <junjiemars@gmail.com>
2024-01-26 10:47:18 +0800

    cxx

--- src/cxx/std14/main.cpp
+++ src/cxx/std14/main.cpp
@@ -10,8 +10,9 @@ constexpr T pi = T(3.14159265358979323846);
 
 int main() {
 	std::cout << add(3,4) << std::endl;
-	auto times = [](auto a, auto b) {
-		return a * b;
+	int c = 5;
+	auto times = [c](auto a, auto b) {
+		return a * b * c;
 	};
 	std::cout << times(3,4) << std::endl;
   std::cout << pi<float> << std::endl;
