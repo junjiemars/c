61a118c47eb1e99dc865d24e64a1891b4313e281
junjie <junjiemars@gmail.com>
2024-01-26 17:36:37 +0800

    cxx and rust

--- src/cxx/std14/main.cpp
+++ src/cxx/std14/main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <iomanip>
 
-auto add(int a, int b) {
-	return a + b;
+auto
+add (int a, int b)
+{
+  return a + b;
 }
 
-template<typename T>
-constexpr T pi = T(3.14159265358979323846);
+template <typename T> constexpr T pi = T (3.14159265358979323846);
 
-int main() {
-	std::cout << add(3,4) << std::endl;
-	int c = 5;
-	auto times = [c](auto a, auto b) {
-		return a * b * c;
-	};
-	std::cout << times(3,4) << std::endl;
+int
+main ()
+{
+  std::cout << add (3, 4) << std::endl;
+  int c = 5;
+  auto times = [c] (auto a, auto b) { return a * b * c; };
+  std::cout << times (3, 4) << std::endl;
   std::cout << pi<float> << std::endl;
-	std::cout << std::setprecision(10) << pi<long double> << std::endl;
-	return 0;
+  std::cout << std::setprecision (10) << pi<long double> << std::endl;
+  return 0;
 }
