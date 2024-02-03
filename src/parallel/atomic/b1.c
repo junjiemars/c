00408ec29cfb847b09ca8ed5e0b6d12667c11251
南山竹 <junjie.mars@icloud.com>
2024-02-03 13:29:44 +0800

    c11 atomic

---
+++ src/parallel/atomic/b1.c
@@ -0,0 +1,20 @@
+#include "_parallel_.h"
+#include <stdatomic.h>
+#include <stdio.h>
+
+int
+main (void)
+{
+	atomic_int x;
+
+	atomic_init (&x, 0x1234);
+	printf ("%#0x\n", x);
+
+	atomic_fetch_add (&x, 1);
+	printf ("%#0x\n", x);
+
+	atomic_fetch_add (&x, 2);
+	printf ("%#0x\n", x);
+
+  return 0;
+}
