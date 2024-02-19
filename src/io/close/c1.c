fb793e649f11702bc333321b3b6445952c2fdcb0
南山竹 <junjie.mars@icloud.com>
2024-02-19 21:52:17 +0800

    clean

--- src/io/close/c1.c
+++ src/io/close/c1.c
@@ -1,4 +1,4 @@
-#include <_io_.h>
+#include "_io_.h"
 
 /*
  * 1. close(fd) just remove the fd's entry from the process table.
@@ -6,46 +6,40 @@
  * 2. The kernel decrements the reference count of the file table
  * entry if the count > 1 or removes the entry when count = 1.
  *
- * 3. The kernel decrements the reference count of the inode table
- * entry if the count > 1 or frees the entry when count = 1.
- *
- * 4. Close all fd's entries in the process table when the process
+ * 3. Close all fd's entries in the process table when the process
  * exits.
  *
  */
 
-
-#define ALPHA_L  "abc\n"
-
+#define ALPHA_L "abc\n"
 
 int
-main(void)
+main (void)
 {
   int fd;
 
-  fd = dup(STDOUT_FILENO);
-  if (fd == -1)
+  if ((fd = dup (STDOUT_FILENO)) == -1)
     {
-      perror(NULL);
-      exit(EXIT_FAILURE);
+      perror (NULL);
+      exit (EXIT_FAILURE);
     }
 
-  if (close(STDOUT_FILENO) == -1)
+  if (close (STDOUT_FILENO) == -1)
     {
-      perror(NULL);
-      exit(EXIT_FAILURE);
+      perror (NULL);
+      exit (EXIT_FAILURE);
     }
 
-  if (write(STDOUT_FILENO, ALPHA_L, _nof_(ALPHA_L)-1) != _nof_(ALPHA_L)-1)
+  if (write (STDOUT_FILENO, ALPHA_L, sizeof (ALPHA_L) - 1)
+      != sizeof (ALPHA_L) - 1)
     {
-      perror("stdout");
+      perror ("stdout");
     }
 
-  if (write(fd, ALPHA_L, _nof_(ALPHA_L)-1) != _nof_(ALPHA_L)-1)
+  if (write (fd, ALPHA_L, sizeof (ALPHA_L) - 1) != sizeof (ALPHA_L) - 1)
     {
-      perror("stdout(dup)");
+      perror ("stdout(dup)");
     }
 
-
-  exit(EXIT_SUCCESS);
+  exit (EXIT_SUCCESS);
 }
