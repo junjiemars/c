9689ea3076267fe87ae05c0ffc89dce2f00c2bd4
junjie <junjie.mars@icloud.com>
2024-08-11 22:08:54 +0800

    src/ipc: sock addr

--- src/ipc/sock/addr1.c
+++ src/ipc/sock/addr1.c
@@ -15,18 +15,26 @@ main (void)
   struct sockaddr_in in4 = { 0 };
   in4.sin_family = AF_INET;
   in4.sin_port = 0x1234;
-  in4.sin_addr.s_addr = inet_addr ("127.0.0.1");
+  char in4_addr_ss[128] = "127.0.0.1";
+  in4.sin_addr.s_addr = inet_addr (in4_addr_ss);
+  struct in_addr in4_addr = { 0 };
+  inet_pton (AF_INET, in4_addr_ss, &in4_addr);
+  assert (in4.sin_addr.s_addr == in4_addr.s_addr);
   pr_addr ((struct sockaddr *)&in4, "in4");
   printf ("in4.sin_addr.s_addr = %s\n", inet_ntoa (in4.sin_addr));
+  char in4_addr_ss1[128] = { 0 };
+  inet_ntop (AF_INET, &in4.sin_addr.s_addr, in4_addr_ss1,
+             sizeof (in4_addr_ss1));
+  assert (strcmp (in4_addr_ss, in4_addr_ss1) == 0);
 
   /* internet v6 address */
   struct sockaddr_in6 in6 = { 0 };
   in6.sin6_family = AF_INET6;
   in6.sin6_port = 0x1234;
   struct in6_addr in6_addr_n;
-  char in6_addr_ss[128] = {0};
+  char in6_addr_ss[128] = { 0 };
   strcpy (in6_addr_ss, "240e:3a2:e7f:1410:84eb:7d13:9c3a:e7f5");
-  inet_pton(AF_INET6, in6_addr_ss, &in6_addr_n);
+  inet_pton (AF_INET6, in6_addr_ss, &in6_addr_n);
   in6.sin6_addr = in6_addr_n;
   pr_addr ((struct sockaddr *)&in6, "in6");
   inet_ntop (AF_INET6, &in6.sin6_addr, in6_addr_ss, sizeof (in6_addr_ss));
