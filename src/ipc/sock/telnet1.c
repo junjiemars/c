0ef9206a9f47ec6cc575bfc412a7c7339baad3e9
junjie <junjie.mars@icloud.com>
2024-08-21 21:27:00 +0800

    src/ipc: telnet

---
+++ src/ipc/sock/telnet1.c
@@ -0,0 +1,39 @@
+#include "_ipc_.h"
+
+int
+main (int argc, char **argv)
+{
+  int sfd;
+  int status = 0;
+  uint16_t port;
+  struct sockaddr_in in4 = { 0 };
+
+  if (argc < 3)
+    {
+      fprintf (stderr, "usage: %s <node> <port>\n", argv[0]);
+      exit (EXIT_FAILURE);
+    }
+
+  in4.sin_family = AF_INET;
+  sscanf (argv[2], "%hu", &port);
+  in4.sin_port = htons (port);
+  in4.sin_addr.s_addr = inet_addr (argv[1]);
+
+  sfd = socket (AF_INET, SOCK_STREAM, 0);
+  if (sfd == -1)
+    {
+      perror (0);
+      exit (EXIT_FAILURE);
+    }
+
+  if (connect (sfd, (struct sockaddr *)&in4, sizeof (in4)) < 0)
+    {
+      status = errno;
+      perror (0);
+      goto clean_exit;
+    }
+
+ clean_exit:
+  close (sfd);
+  exit (status);
+}
