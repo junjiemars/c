5fd803e3fb9becb46ab283b524e52f22d921974b
junjie <junjie.mars@icloud.com>
2024-08-24 10:17:27 +0800

    src/ipc: telnet

--- src/ipc/sock/telnet1.c
+++ src/ipc/sock/telnet1.c
@@ -5,41 +5,50 @@ main (int argc, char **argv)
 {
   int sfd;
   int status = 0;
-  char *node;
-  uint16_t port;
-  struct sockaddr_in in4 = { 0 };
+  char *node, *service = 0;
+  struct addrinfo hint = { 0 }, *in4;
 
   if (argc < 3)
     {
-      fprintf (stderr, "usage: %s <node> <port>\n", argv[0]);
+      fprintf (stderr, "usage: %s <node> [service]\n", argv[0]);
       exit (EXIT_FAILURE);
     }
   node = argv[1];
-  sscanf (argv[2], "%hu", &port);
+  service = argv[2];
 
-  in4.sin_family = AF_INET;
-  in4.sin_port = htons (port);
-  in4.sin_addr.s_addr = inet_addr (node);
+  hint.ai_family = AF_INET;
+  hint.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo (node, service, &hint, &in4) != 0)
+    {
+      status = errno;
+      fprintf (stderr, "%s\n", gai_strerror (status));
+      goto clean_exit;
+    }
 
   if ((sfd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
     {
+      status = errno;
       perror (0);
-      exit (EXIT_FAILURE);
+      goto clean_addr;
     }
 
-  if (connect (sfd, (struct sockaddr *)&in4, sizeof (in4)) < 0)
+  if (connect (sfd, in4->ai_addr, in4->ai_addrlen) < 0)
     {
       status = errno;
       if (status != ECONNREFUSED)
         {
           perror (0);
-          goto clean_exit;
+          goto clean_sock;
         }
     }
 
-  printf ("#successed");
+  printf ("#successed\n");
 
- clean_exit:
+clean_sock:
   close (sfd);
+clean_addr:
+  freeaddrinfo (in4);
+clean_exit:
   exit (status);
 }
