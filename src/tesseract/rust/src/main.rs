9fd3fa3f13edada4e1af909dff603742a37bf0c4
junjie <junjiemars@gmail.com>
2024-09-05 01:30:51 +0800

    src/tesseract/rust: test

--- src/tesseract/rust/src/main.rs
+++ src/tesseract/rust/src/main.rs
@@ -1,30 +1,55 @@
-use clap::Parser;
+use clap::{ArgAction, Parser};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-pub struct Args {
+pub struct Argv {
     /// source
     #[arg(short, long)]
-    source: String,
+    source: Option<String>,
 
     /// dest
-    #[arg(short, long, default_value = "spec.json")]
+    #[arg(short, long)]
     dest: Option<String>,
+
+    /// list languages
+    #[arg(long, action = ArgAction::SetTrue)]
+    list_languages: bool,
+
+    /// list parameters
+    #[arg(long, action = ArgAction::SetTrue)]
+    list_parameters: bool,
 }
 
 fn main() {
-
+    // parsing args
+    let argv = Argv::parse();
+    if argv.list_languages {
+        if let Ok(langs) = rusty_tesseract::get_tesseract_langs() {
+            for lang in langs.into_iter() {
+                println!("{lang}")
+            }
+        }
+        return;
+    }
+    if argv.list_parameters {
+        if let Ok(config) = rusty_tesseract::get_tesseract_config_parameters() {
+						for param in config.config_parameters.into_iter() {
+								println!("{}", param)
+						}
+        }
+        return;
+    }
 }
 
 mod tests {
     #[test]
     fn test_list_langs() {
         let langs = rusty_tesseract::get_tesseract_langs();
-				println!("{:#?}", langs)
+        println!("{langs:#?}")
     }
-		#[test]
-		fn test_list_params() {
-				let params = rusty_tesseract::get_tesseract_config_parameters();
-				println!("{:#?}", params)
-		}
+    #[test]
+    fn test_list_params() {
+        let params = rusty_tesseract::get_tesseract_config_parameters();
+        println!("{params:#?}")
+    }
 }
