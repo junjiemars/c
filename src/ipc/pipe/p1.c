897ae7207b7a3f8be411d50e577d3e1b792c4260
junjie <junjie.mars@icloud.com>
2024-10-20 14:47:32 +0800

    src/ipc: pipe chain

--- src/ipc/pipe/p1.c
+++ src/ipc/pipe/p1.c
@@ -1,20 +1,35 @@
 #include <_ipc_.h>
 #include <string.h>
 
-#define _TXT_ "hello pipe"
+#define _TXT_ "hello pipe\n"
 
 int
 main (void)
 {
   ssize_t n;
-  int fd[2];
+  int fildes[2];
+  struct stat fs;
   pid_t pid;
   char line[NM_LINE_MAX];
 
-  if (pipe (fd) == -1)
+  if (pipe (fildes) == -1)
     {
       perror (0);
       exit (1);
+    }
+
+  for (int i = 0; i < (int)_nof_ (fildes); i++)
+    {
+      if (fstat (fildes[i], &fs) == -1)
+        {
+          perror (NULL);
+          exit (1);
+        }
+      if (S_ISFIFO (fs.st_mode))
+        {
+          printf ("fildes[%i]=%i fifo\n", i, fildes[i]);
+        }
+      printf ("fildes[%i]=%i size %zu\n", i, fildes[i], (size_t)fs.st_size);
     }
 
   if ((pid = fork ()) == -1)
@@ -24,17 +39,17 @@ main (void)
     }
   else if (pid > 0)
     {
-      close (fd[0]); /* close read fd */
-      write (fd[1], _TXT_, sizeof (_TXT_) - 1);
+      close (fildes[0]); /* close read fd */
+      write (fildes[1], _TXT_, sizeof (_TXT_) - 1);
       waitpid (pid, NULL, 0);
     }
   else
     {
-      close (fd[1]); /* close write fd */
+      close (fildes[1]); /* close write fd */
       n = snprintf (line, NM_LINE_MAX,
                     "%zi received from %zi: ", (size_t)getpid (),
                     (size_t)getppid ());
-      n += read (fd[0], line + n, NM_LINE_MAX);
+      n += read (fildes[0], line + n, NM_LINE_MAX);
       write (STDOUT_FILENO, line, n);
     }
 
---
+++ src/ipc/pipe/p2.c
@@ -0,0 +1,67 @@
+#include <_ipc_.h>
+#include <string.h>
+
+#define _TXT_ "hello pipe\n"
+
+int
+main (void)
+{
+  ssize_t n;
+  int fildes1[2], fildes2[2];
+  pid_t pid;
+  char line[NM_LINE_MAX];
+
+  if (pipe (fildes1) == -1)
+    {
+      perror (0);
+      exit (1);
+    }
+
+  if ((pid = fork ()) == -1)
+    {
+      perror (0);
+      exit (1);
+    }
+  else if (pid > 0)
+    {
+      close (fildes1[0]);
+      write (fildes1[1], _TXT_, sizeof (_TXT_) - 1);
+      waitpid (pid, NULL, 0);
+    }
+  else
+    {
+      close (fildes1[1]);
+
+      if (pipe (fildes2) == -1)
+        {
+          perror (NULL);
+          exit (1);
+        }
+
+      if ((pid = fork ()) == -1)
+        {
+          perror (NULL);
+          exit (1);
+        }
+      else if (pid > 0)
+        {
+          close (fildes2[0]);
+          n = snprintf (line, NM_LINE_MAX,
+                        "%zi received from %zi: ", (size_t)getpid (),
+                        (size_t)getppid ());
+          n += read (fildes1[0], line + n, NM_LINE_MAX);
+          write (fildes2[1], line, n);
+        }
+      else
+        {
+          close (fildes2[1]);
+          n = snprintf (line, NM_LINE_MAX,
+                        "%zi received from %zi: ", (size_t)getpid (),
+                        (size_t)getppid ());
+          n += read (fildes2[0], line + n, NM_LINE_MAX);
+          write (STDOUT_FILENO, line, n);
+        }
+    }
+
+  exit (0);
+}
