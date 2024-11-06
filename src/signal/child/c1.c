79573e566965bfaa5ee0b5fe85826cea60953d38
findidiot <junjie.mars@icloud.com>
2024-11-06 16:55:21 +0800

    src/signal: siginfo_t

--- src/signal/child/c1.c
+++ src/signal/child/c1.c
@@ -1,64 +1,62 @@
-#include <_signal_.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "_signal_.h"
 
-static void on_sig_chld(int);
+static void on_sig_chld (int, siginfo_t *, void *);
 
-static int  N  =  5;
+static int seconds = 1;
+static int sa_flags = 0;
 
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
-  pid_t  pid;
+  pid_t pid;
+  struct sigaction act;
 
-  printf("%d\n", getpid());
+  printf ("%d\n", getpid ());
 
   if (argc > 1)
     {
-      N = atoi(argv[1]);
+      seconds = atoi (argv[1]);
     }
-
-  if (SIG_ERR == signal(SIGCHLD, on_sig_chld))
+  if (argc > 2)
     {
-      perror(NULL);
-      exit(EXIT_FAILURE);
+      sscanf (argv[2], "%x", &sa_flags);
     }
 
-  pid = fork();
-  if (-1 == pid)
+  act.sa_sigaction = on_sig_chld;
+  act.sa_flags = SA_SIGINFO | sa_flags;
+  sigemptyset (&act.sa_mask);
+  if (sigaction (SIGCHLD, &act, NULL) == -1)
     {
-      perror(NULL);
+      perror (NULL);
+      _exit (1);
     }
-  else if (0 == pid)
+
+  if ((pid = fork ()) == -1)
     {
-      sleep(N);
-      _exit(0);
+      perror (NULL);
+      _exit (1);
+    }
+  else if (0 == pid) /* in child process */
+    {
+      sleep (seconds);
+      _exit (0x11223344);
     }
 
-  pause();
+  pause ();
 
-  exit(EXIT_SUCCESS);
+  _exit (0);
 }
 
-
 void
-on_sig_chld(int signo)
+on_sig_chld (int signo, siginfo_t *info, void *ctx)
 {
-  pid_t  pid;
-  int    status;
-
   if (SIGCHLD == signo)
     {
-      if (SIG_ERR == signal(SIGCHLD, on_sig_chld))
-        {
-          perror(NULL);
-        }
+      assert (info->si_code == CLD_EXITED && "should child has exited");
 
-      if (-1 == (pid = wait(&status)))
-        {
-          perror(NULL);
-        }
-
-      printf("# %s %d\n", _str_(SIGCHLD), pid);
+      printf ("# %s signo=%d "
+              "pid=%d uid=%d status=0x%0x errno=%d code=%d ctx=%p\n",
+              _str_ (SIGCHLD), info->si_signo, info->si_pid, info->si_uid,
+              info->si_status, info->si_errno, info->si_code, ctx);
     }
 }
