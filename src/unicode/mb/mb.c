2307775aaba99f8053973b7f9017c61b351858f9
findidiot <junjie.mars@icloud.com>
2024-12-12 19:51:20 +0800

    clean

--- src/unicode/mb/mb.c
+++ src/unicode/mb/mb.c
@@ -1,119 +1,133 @@
-#include <_unicode_.h>
-#include <wchar.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "../_unicode_.h"
 #include <limits.h>
 #include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 
 #if MSVC
-#  pragma warning(disable:4996)
+#pragma warning(disable : 4996)
 #endif
 
-size_t mbstrlen(const char *s);
-void print_wcs(const wchar_t *ws);
-size_t mbs_to_wcs(wchar_t *ws, const char *s, size_t ns);
-void wc_to_mb(const wchar_t *ws);
-void wcs_to_mbs(const wchar_t *ws, size_t n);
+size_t mbstrlen (const char *s);
+void print_wcs (const wchar_t *ws);
+size_t mbs_to_wcs (wchar_t *ws, const char *s, size_t ns);
+void wc_to_mb (const wchar_t *ws);
+void wcs_to_mbs (const wchar_t *ws, size_t n);
 
 size_t
-mbstrlen(const char *s) {
+mbstrlen (const char *s)
+{
   size_t mblen;
   size_t n = 0;
   mbstate_t mbs;
 
-  memset(&mbs, 0, sizeof(mbs));
-  while ((mblen = mbrlen(s, MB_CUR_MAX, &mbs)) != 0
-         && mblen != (size_t)-1
-         && mblen != (size_t)-2) {
-    s += mblen;
-    n++;
-  }
+  memset (&mbs, 0, sizeof (mbs));
+  while ((mblen = mbrlen (s, MB_CUR_MAX, &mbs)) != 0 && mblen != (size_t)-1
+         && mblen != (size_t)-2)
+    {
+      s += mblen;
+      n++;
+    }
   return n;
 }
 
 void
-print_wcs(const wchar_t *ws) {
-	const wchar_t *p = ws;
-	while (*p) {
-		printf("[%lc]", *p++);
-	}
-	putchar('\n');
+print_wcs (const wchar_t *ws)
+{
+  const wchar_t *p = ws;
+  while (*p)
+    {
+      printf ("[%lc]", *p++);
+    }
+  putchar ('\n');
 }
 
 size_t
-mbs_to_wcs(wchar_t *ws, const char *s, size_t ns) {
-  if (!s) {
-    return 0;
-  }
+mbs_to_wcs (wchar_t *ws, const char *s, size_t ns)
+{
+  if (!s)
+    {
+      return 0;
+    }
 
-  size_t n = mbstowcs(ws, s, ns);
-  if (n == (size_t)-1) {
-    perror("!panic");
-    return 0;
-  }
+  size_t n = mbstowcs (ws, s, ns);
+  if (n == (size_t)-1)
+    {
+      perror ("!panic");
+      return 0;
+    }
   return n;
 }
 
 void
-wc_to_mb(const wchar_t *ws) {
-	char buf[MB_LEN_MAX];
-	while (*ws) {
-		memset(buf, 0, MB_LEN_MAX);
-		int len = wctomb(buf, *ws);
-    if (-1 == len) {
-      perror("!panic");
-      return;
+wc_to_mb (const wchar_t *ws)
+{
+  char buf[MB_LEN_MAX];
+  while (*ws)
+    {
+      memset (buf, 0, MB_LEN_MAX);
+      int len = wctomb (buf, *ws);
+      if (-1 == len)
+        {
+          perror ("!panic");
+          return;
+        }
+      printf ("[%s/%i]", buf, len);
+      ws++;
     }
-		printf("[%s/%i]", buf, len);
-		ws++;
-	}
-	putchar('\n');
+  putchar ('\n');
 }
 
 void
-wcs_to_mbs(const wchar_t *ws, size_t n) {
-	char *buf = calloc(n, sizeof(*buf));
-	size_t len = wcstombs(buf, ws, n);
-  if ((size_t)-1 == len) {
-    perror("!panic");
-  }
-	printf("mbstr=%s/%zu\n", buf, len);
-  free(buf);
+wcs_to_mbs (const wchar_t *ws, size_t n)
+{
+  char *buf = calloc (n, sizeof (*buf));
+  size_t len = wcstombs (buf, ws, n);
+  if ((size_t)-1 == len)
+    {
+      perror ("!panic");
+    }
+  printf ("mbstr=%s/%zu\n", buf, len);
+  free (buf);
 }
 
 int
-main(int argc, char **argv) {
-	printf("current locale: %s\n", setlocale(LC_ALL, 0));
-  printf("set locale to native: %s\n", setlocale(LC_ALL, ""));
-/* #if (WINNT) */
-/*   printf("set locale to native: %s\n", setlocale(LC_ALL, "English_United States.65001")); */
-/* #endif */
-	printf("sizeof(wchar_t): %zu bytes\n", sizeof(wchar_t));
-	printf("MB_LEN_MAX: %zu\n", (size_t)MB_LEN_MAX);
-	printf("MB_CUR_MAX: %zu\n", (size_t)MB_CUR_MAX);
+main (int argc, char **argv)
+{
+  printf ("current locale: %s\n", setlocale (LC_ALL, 0));
+  printf ("set locale to native: %s\n", setlocale (LC_ALL, ""));
+  /* #if (WINNT) */
+  /*   printf("set locale to native: %s\n", setlocale(LC_ALL, "English_United
+   * States.65001")); */
+  /* #endif */
+  printf ("sizeof(wchar_t): %zu bytes\n", sizeof (wchar_t));
+  printf ("MB_LEN_MAX: %zu\n", (size_t)MB_LEN_MAX);
+  printf ("MB_CUR_MAX: %zu\n", (size_t)MB_CUR_MAX);
 
-	printf("----------\n");
+  printf ("----------\n");
 
-  if (argc < 2) {
-    printf("where the input string?\n");
-    return 0;
-  }
-  printf("%s\n", argv[1]);
-	printf("----------\n");
+  if (argc < 2)
+    {
+      printf ("where the input string?\n");
+      return 0;
+    }
+  printf ("%s\n", argv[1]);
+  printf ("----------\n");
 
-  size_t sstrlen = strlen(argv[1]);
-  size_t smblen = mbstrlen(argv[1]);
-  printf("strlen=%zu, mbstrlen=%zu\n", sstrlen, smblen);
-  
-  wchar_t *ws = calloc(sstrlen + 1, sizeof(wchar_t));
-  size_t n = mbs_to_wcs(&ws[0], argv[1], sstrlen + 1);
-	printf("wcs=%ls\n", ws);
+  size_t sstrlen = strlen (argv[1]);
+  size_t smblen = mbstrlen (argv[1]);
+  printf ("strlen=%zu, mbstrlen=%zu\n", sstrlen, smblen);
 
-	print_wcs(ws);
-	wc_to_mb(ws);
-	wcs_to_mbs(ws, sizeof(wchar_t) * n);
+  wchar_t *ws = calloc (sstrlen + 1, sizeof (wchar_t));
+  size_t n = mbs_to_wcs (&ws[0], argv[1], sstrlen + 1);
+  printf ("wcs=%ls\n", ws);
 
-  free(ws);
-	return 0;
+  print_wcs (ws);
+  wc_to_mb (ws);
+  wcs_to_mbs (ws, sizeof (wchar_t) * n);
+
+  free (ws);
+  return 0;
 }
--- src/unicode/utf8/utf8.c
+++ src/unicode/utf8/utf8.c
@@ -1,74 +1,83 @@
-#include <_unicode_.h>
-#include <stdio.h>
+#include "../_unicode_.h"
 #include <locale.h>
+#include <stdio.h>
 
-
-#define TO_BASE_N (sizeof(unsigned)*8 + 1)
-#define TO_BASE(n, b) to_base((unsigned char [TO_BASE_N]){""}, (n), (b))
-#define OUT_SIZE(t)                                      \
-	do {                                                   \
-    printf("sizeof(%s) \t= %zu bytes\n", #t, sizeof(t)); \
-	} while (0)
-
+#define TO_BASE_N (sizeof (unsigned) * 8 + 1)
+#define TO_BASE(n, b) to_base ((unsigned char[TO_BASE_N]){ "" }, (n), (b))
+#define OUT_SIZE(t)                                                           \
+  do                                                                          \
+    {                                                                         \
+      printf ("sizeof(%s) \t= %zu bytes\n", #t, sizeof (t));                  \
+    }                                                                         \
+  while (0)
 
 #if MSVC
-#  pragma warning(disable:4244)
+#pragma warning(disable : 4244)
 #endif
 
+const unsigned char *
+to_base (unsigned char *o, unsigned i, unsigned base)
+{
+  unsigned char *s = &o[TO_BASE_N - 1];
+  *s = 0;
+  do
+    {
+      s--;
+      *s = "0123456789ABCDEF"[i % base];
+      i /= base;
+    }
+  while (i);
 
-const unsigned char*
-to_base(unsigned char *o, unsigned i, unsigned base) {
-	unsigned char *s = &o[TO_BASE_N - 1];
-	*s = 0;
-	do {
-		s--;
-		*s = "0123456789ABCDEF"[i % base];
-		i /= base;
-	} while (i);
-
-	return s;
+  return s;
 }
 
-void out(const char *s, unsigned c) {
-	if (c < 0x0080) {
-		unsigned char o[1+1] = {0};
-		*o = c;
-		printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(c, 2), o);
-	} else if (c < 0x0800) {
-		unsigned char o[2+1] = {0};
-		unsigned u2;
-		o[0] = u2 = 0xc0 | ((c >> 6) & 0x1f);
-		o[1] = u2 = (u2 << 8) | (0x80 | (c & 0x3f));
-		printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(u2, 2), o);
-	} else if (c < 0x10000) {
-		unsigned char o[3+1] = {0};
-		unsigned u3;
-		o[0] = u3 = 0xe0 | ((c >> 12) & 0x0f);
-		o[1] = u3 = (u3 << 8) | (0x80 | ((c >> 6) & 0x3f));
-		o[2] = u3 = (u3 << 8) | (0x80 | ((c & 0x3f)));
-		printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(u3, 2), o);
-	} else if (c < 0x200000) {
-		unsigned char o[4+1] = {0};
-		unsigned u4;
-		o[0] = u4 = 0xf0 | ((c >> 18) & 0x07);
-		o[1] = u4 = (u4 << 8) | (0x80 | ((c >> 12) & 0x3f));
-		o[2] = u4 = (u4 << 8) | (0x80 | ((c >> 6) & 0x3f));
-		o[3] = u4 = (u4 << 8) | (0x80 | (c & 0x3f));
-		printf("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE(u4, 2), o);
-	}
+void
+out (const char *s, unsigned c)
+{
+  if (c < 0x0080)
+    {
+      unsigned char o[1 + 1] = { 0 };
+      *o = c;
+      printf ("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE (c, 2), o);
+    }
+  else if (c < 0x0800)
+    {
+      unsigned char o[2 + 1] = { 0 };
+      unsigned u2;
+      o[0] = u2 = 0xc0 | ((c >> 6) & 0x1f);
+      o[1] = u2 = (u2 << 8) | (0x80 | (c & 0x3f));
+      printf ("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE (u2, 2), o);
+    }
+  else if (c < 0x10000)
+    {
+      unsigned char o[3 + 1] = { 0 };
+      unsigned u3;
+      o[0] = u3 = 0xe0 | ((c >> 12) & 0x0f);
+      o[1] = u3 = (u3 << 8) | (0x80 | ((c >> 6) & 0x3f));
+      o[2] = u3 = (u3 << 8) | (0x80 | ((c & 0x3f)));
+      printf ("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE (u3, 2), o);
+    }
+  else if (c < 0x200000)
+    {
+      unsigned char o[4 + 1] = { 0 };
+      unsigned u4;
+      o[0] = u4 = 0xf0 | ((c >> 18) & 0x07);
+      o[1] = u4 = (u4 << 8) | (0x80 | ((c >> 12) & 0x3f));
+      o[2] = u4 = (u4 << 8) | (0x80 | ((c >> 6) & 0x3f));
+      o[3] = u4 = (u4 << 8) | (0x80 | (c & 0x3f));
+      printf ("%s: 0x%08x %32s \t%s\n", s, c, TO_BASE (u4, 2), o);
+    }
 }
 
-int 
-main(int argc, const char *argv[]) {
-	_unused_(argc);
-	_unused_(argv);
+int
+main (void)
+{
+  OUT_SIZE (unsigned);
+  OUT_SIZE (unsigned char);
+  out ("$", 0x0024u);
+  out ("¢", 0x00a2u);
+  out ("€", 0x20acu);
+  out ("¥", 0x00a5);
 
-	OUT_SIZE(unsigned);
-	OUT_SIZE(unsigned char);
-	out("$", 0x0024u);
-	out("¢", 0x00a2u);
-	out("€", 0x20acu);
-	out("¥", 0x00a5);
-
-	return 0;
+  return 0;
 }
