d544a9f0df70e3813912497b76b7288c85c590ea
findidiot <junjie.mars@icloud.com>
2024-12-31 20:54:38 +0800

    algo/hash: bone

--- src/algo/hash/bone.c
+++ src/algo/hash/bone.c
@@ -4,25 +4,58 @@
 #define M 20
 #define N 23
 
+#define H(x) ((x) % N)
+#define H1(x) (((x) + 1) % N)
 #define EMPTY 0
 
-int hash (int);
-int insert (int);
+#ifndef FN_RD
+#define FN_RD 0
+#endif
+
+#ifndef FN_LOC
+#define FN_LOC 0
+#endif
+
+typedef int (*fn_read_t) (int *);
+typedef int (*fn_probe_t) (int);
+
+int read_number (int *);
+int read_string (int *);
+int insert (int, fn_probe_t, int *);
+int find_loc_linear_probing (int);
+int find_loc_quadratic_probing (int);
+int find_loc_double_hashing (int);
 void dump (void);
 
-int table[N + 1] = { EMPTY };
-int count = 0;
+int hash_table[N + 1];
+
+fn_read_t read_table[] = {
+  read_number,
+  read_string,
+  (fn_read_t)0,
+};
+
+fn_probe_t probe_table[] = {
+  find_loc_linear_probing,
+  find_loc_quadratic_probing,
+  find_loc_double_hashing,
+  (fn_probe_t)0,
+};
 
 int
 main (void)
 {
   int n;
   int i = 0;
+  memset (hash_table, EMPTY, sizeof (hash_table));
 
-  while (scanf ("%d", &n) == 1)
+  while (read_table[FN_RD](&n) == 1)
     {
-      insert (n);
-      i++;
+      if (insert (n, *probe_table[FN_LOC], &i))
+        {
+          perror ("!panic");
+          break;
+        }
     }
 
   dump ();
@@ -31,48 +64,107 @@ main (void)
 }
 
 int
-hash (int n)
+read_number (int *out)
 {
-  return n % N + 1;
+  return scanf ("%d", out);
 }
 
 int
-insert (int n)
+read_string (int *out)
 {
-  int h;
-  static int count = 0;
-
-  h = hash (n);
-
-  while (table[h] != EMPTY && table[h] == n)
+  int h = 0;
+  char buf[64], *p;
+  int c = scanf ("%s", &buf[0]);
+  if (c == 1)
     {
-      h = hash (h);
+      for (p = &buf[0], h = 0; *p; p++)
+        {
+          h = *p + 31 * h;
+        }
+    }
+  *out = h;
+  return c;
+}
+
+int
+insert (int key, fn_probe_t find_loc, int *n)
+{
+  int loc;
+
+  loc = find_loc (key);
+
+  if (*n >= M)
+    {
+      errno = EOVERFLOW;
+      return -1;
     }
 
-  if (count >= M)
+  if (hash_table[loc] == EMPTY)
     {
-      return EOVERFLOW;
+      (*n)++;
     }
 
-  count++;
-  table[h] = n;
+  hash_table[loc] = key;
 
   return 0;
+}
+
+int
+find_loc_linear_probing (int key)
+{
+  int loc = H (key);
+  while (hash_table[loc] != EMPTY && hash_table[loc] != key)
+    {
+      loc = loc % N + 1;
+    }
+  return loc;
+}
+
+int
+find_loc_quadratic_probing (int key)
+{
+  int i = 0;
+  int loc = H (key);
+  while (hash_table[loc] != EMPTY && hash_table[loc] != key)
+    {
+      i++;
+      loc = loc + 2 * i + 3 * i * i;
+      while (loc > N)
+        {
+          loc -= N;
+        }
+    }
+  return loc;
+}
+
+int
+find_loc_double_hashing (int key)
+{
+  int loc = H (key);
+  int loc1 = H1 (key);
+  while (hash_table[loc] != EMPTY && hash_table[loc] != key)
+    {
+      loc += loc1;
+      if (loc > N)
+        {
+          loc -= N;
+        }
+    }
+  return loc;
 }
 
 void
 dump (void)
 {
   int cnt = 0;
-  size_t total = sizeof (table) / sizeof (table[0]);
-
+  size_t total = sizeof (hash_table) / sizeof (hash_table[0]);
   for (size_t i = 0; i < total; i++)
     {
-      if (table[i] != EMPTY)
+      if (hash_table[i] != EMPTY)
         {
           cnt++;
-          printf ("%4zu  %4d\n", i, table[i]);
+          printf ("%4zu  %4d\n", i, hash_table[i]);
         }
     }
-  printf ("------------\ntable (%d/%zu)\n", cnt, total);
+  printf ("------------\ntable %d/(%d,%zu)\n", cnt, M, total);
 }
--- src/lang/math/f.c
+++ src/lang/math/f.c
@@ -1,277 +1,272 @@
-#include "_lang_.h"
+#include "../_lang_.h"
 #include <float.h>
 #include <math.h>
-
 
 #define BIT8_SUF "\n"
 #include <nbits.h>
 
 #if (MSVC)
-#  pragma warning(disable: 4293)
+#pragma warning(disable : 4293)
 #endif
 
 #if defined(BIT8_SEP)
-#  undef  BIT8_SEP
-#  define BIT8_SEP " "
+#undef BIT8_SEP
+#define BIT8_SEP " "
 #endif
 
 #if defined(BIT16_SEP)
-#  undef  BIT16_SEP
-#  define BIT16_SEP " "
+#undef BIT16_SEP
+#define BIT16_SEP " "
 #endif
 
-static unsigned int to_ui(float);
-static unsigned long to_ul(double);
+static unsigned int to_ui (float);
+static unsigned long to_ul (double);
 
-static void test_cmp_float(void);
-static void test_cmp_double(void);
-static void test_asm_double(void);
-static void test_asm_long_double(void);
-static void test_epsilon_double(void);
-static void test_double_zero(void);
-static void test_double_equal(void);
-static void test_double_classify(void);
-static void test_double_round(void);
+static void test_cmp_float (void);
+static void test_cmp_double (void);
+static void test_asm_double (void);
+static void test_asm_long_double (void);
+static void test_epsilon_double (void);
+static void test_double_zero (void);
+static void test_double_equal (void);
+static void test_double_classify (void);
+static void test_double_round (void);
 
 int
-main(void)
+main (void)
 {
-  test_cmp_float();
-  test_cmp_double();
-  test_asm_double();
-  test_asm_long_double();
-  test_epsilon_double();
-  test_double_zero();
-  test_double_equal();
-  test_double_classify();
-  test_double_round();
+  test_cmp_float ();
+  test_cmp_double ();
+  test_asm_double ();
+  test_asm_long_double ();
+  test_epsilon_double ();
+  test_double_zero ();
+  test_double_equal ();
+  test_double_classify ();
+  test_double_round ();
 
   return 0;
 }
 
-
 unsigned int
-to_ui(float x)
+to_ui (float x)
 {
   unsigned int u;
-  memcpy((char*)&u, (char*)&x, sizeof(x));
+  memcpy ((char *)&u, (char *)&x, sizeof (x));
   return u;
 }
 
 unsigned long
-to_ul(double x)
+to_ul (double x)
 {
   unsigned long u;
-  memcpy((char*)&u, (char*)&x, sizeof(x));
+  memcpy ((char *)&u, (char *)&x, sizeof (x));
   return u;
 }
 
 void
-test_cmp_float(void)
+test_cmp_float (void)
 {
   float d1 = 0.1f, d2 = 0.2f, d3 = 0.3f;
-  unsigned int u1 = to_ui(d1);
-  unsigned int u2 = to_ui(d2);
-  unsigned int u12 = to_ui(d1+d2);
-  unsigned int u3 = to_ui(d3);
+  unsigned int u1 = to_ui (d1);
+  unsigned int u2 = to_ui (d2);
+  unsigned int u12 = to_ui (d1 + d2);
+  unsigned int u3 = to_ui (d3);
 
-  printf("compare float\n--------------------\n");
-  printf("%16f = 0x%X\n", d1, u1);
-  printf("%16f = 0x%X\n", d2, u2);
-  printf("%16f = 0x%X\n", d1+d2, u12);
-  printf("%16f = 0x%X\n", d3, u3);
+  printf ("compare float\n--------------------\n");
+  printf ("%16f = 0x%X\n", d1, u1);
+  printf ("%16f = 0x%X\n", d2, u2);
+  printf ("%16f = 0x%X\n", d1 + d2, u12);
+  printf ("%16f = 0x%X\n", d3, u3);
 
-  printf(BPRI32, BIT32(u1));
-  printf(BPRI32, BIT32(u2));
-  printf(BPRI32, BIT32(u12));
-  printf(BPRI32, BIT32(u3));
+  printf (BPRI32, BIT32 (u1));
+  printf (BPRI32, BIT32 (u2));
+  printf (BPRI32, BIT32 (u12));
+  printf (BPRI32, BIT32 (u3));
 
-  assert(u3 == u12);
-  assert(d3 == d1 + d2);
+  assert (u3 == u12);
+  assert (d3 == d1 + d2);
 
-  __attribute__((unused)) int  less;
-  __attribute__((unused)) int  greater;
-  __attribute__((unused)) int  lessgreater;
-  __attribute__((unused)) int  unordered;
-  __attribute__((unused)) int  lessequal;
-  __attribute__((unused)) int  greaterequal;
+  __attribute__ ((unused)) int less;
+  __attribute__ ((unused)) int greater;
+  __attribute__ ((unused)) int lessgreater;
+  __attribute__ ((unused)) int unordered;
+  __attribute__ ((unused)) int lessequal;
+  __attribute__ ((unused)) int greaterequal;
 
-  less = isless(d3, d1+d2);
-  greater = isgreater(d3, d1+d2);
-  lessgreater = islessgreater(d3, d1+d2);
-  lessequal = islessequal(d3, d1+d2);
-  greaterequal = isgreaterequal(d3, d1+d2);
-  unordered = isunordered(d3, d1+d2);
+  less = isless (d3, d1 + d2);
+  greater = isgreater (d3, d1 + d2);
+  lessgreater = islessgreater (d3, d1 + d2);
+  lessequal = islessequal (d3, d1 + d2);
+  greaterequal = isgreaterequal (d3, d1 + d2);
+  unordered = isunordered (d3, d1 + d2);
 
-  assert(!unordered);
-  assert(!less);
-  assert(!greater);
-  assert(!lessgreater);
-  assert((less || greater) == lessgreater);
-  assert(lessequal && greaterequal);
+  assert (!unordered);
+  assert (!less);
+  assert (!greater);
+  assert (!lessgreater);
+  assert ((less || greater) == lessgreater);
+  assert (lessequal && greaterequal);
 
-  __attribute__((unused)) int  less1;
-  __attribute__((unused)) int  greater1;
-  __attribute__((unused)) int  lessgreater1;
+  __attribute__ ((unused)) int less1;
+  __attribute__ ((unused)) int greater1;
+  __attribute__ ((unused)) int lessgreater1;
 
   float lg1 = 0.1f, lg2 = 0.2f, lg3 = 0.3001f;
-  less1 = isless(lg3, lg1+lg2);
-  greater1 = isgreater(lg3, lg1+lg2);
-  lessgreater1 = islessgreater(lg3, lg1+lg2);
+  less1 = isless (lg3, lg1 + lg2);
+  greater1 = isgreater (lg3, lg1 + lg2);
+  lessgreater1 = islessgreater (lg3, lg1 + lg2);
 
-  assert(less1 || greater1);
-  assert(lessgreater1);
-  assert((less1 || greater1) == lessgreater1);
+  assert (less1 || greater1);
+  assert (lessgreater1);
+  assert ((less1 || greater1) == lessgreater1);
 }
 
 void
-test_cmp_double(void)
+test_cmp_double (void)
 {
   double d1 = 0.1, d2 = 0.2, d3 = 0.3;
-  unsigned long u1 = to_ul(d1);
-  unsigned long u2 = to_ul(d2);
-  unsigned long u12 = to_ul(d1+d2);
-  unsigned long u3 = to_ul(d3);
+  unsigned long u1 = to_ul (d1);
+  unsigned long u2 = to_ul (d2);
+  unsigned long u12 = to_ul (d1 + d2);
+  unsigned long u3 = to_ul (d3);
 
-  printf("compare double\n--------------------\n");
-  printf("%16lf = 0x%lX\n", d1, to_ul(d1));
-  printf("%16lf = 0x%lX\n", d2, to_ul(d2));
-  printf("%16lf = 0x%lX\n", d1+d2, to_ul(d1+d2));
-  printf("%16lf = 0x%lX\n", d3, to_ul(d3));
+  printf ("compare double\n--------------------\n");
+  printf ("%16lf = 0x%lX\n", d1, to_ul (d1));
+  printf ("%16lf = 0x%lX\n", d2, to_ul (d2));
+  printf ("%16lf = 0x%lX\n", d1 + d2, to_ul (d1 + d2));
+  printf ("%16lf = 0x%lX\n", d3, to_ul (d3));
 
-  printf(BPRI64, BIT64(u1));
-  printf(BPRI64, BIT64(u2));
-  printf(BPRI64, BIT64(u12));
-  printf(BPRI64, BIT64(u3));
+  printf (BPRI64, BIT64 (u1));
+  printf (BPRI64, BIT64 (u2));
+  printf (BPRI64, BIT64 (u12));
+  printf (BPRI64, BIT64 (u3));
 
-  assert(u3 != u12);
-  assert(d3 != d1 + d2);
+  assert (u3 != u12);
+  assert (d3 != d1 + d2);
 
-  __attribute__((unused)) int  less;
-  __attribute__((unused)) int  greater;
-  __attribute__((unused)) int  lessgreater;
-  __attribute__((unused)) int  unordered;
-  __attribute__((unused)) int  lessequal;
-  __attribute__((unused)) int  greaterequal;
-  less = isless(d3, d1+d2);
-  greater = islessgreater(d3, d1+d2);
-  lessgreater = islessgreater(d3, d1+d2);
-  lessequal = islessequal(d3, d1+d2);
-  greaterequal = isgreaterequal(d3, d1+d2);
-  unordered = isunordered(d3, d1+d2);
+  __attribute__ ((unused)) int less;
+  __attribute__ ((unused)) int greater;
+  __attribute__ ((unused)) int lessgreater;
+  __attribute__ ((unused)) int unordered;
+  __attribute__ ((unused)) int lessequal;
+  __attribute__ ((unused)) int greaterequal;
+  less = isless (d3, d1 + d2);
+  greater = islessgreater (d3, d1 + d2);
+  lessgreater = islessgreater (d3, d1 + d2);
+  lessequal = islessequal (d3, d1 + d2);
+  greaterequal = isgreaterequal (d3, d1 + d2);
+  unordered = isunordered (d3, d1 + d2);
 
-  assert(!unordered);
-  assert(less);
-  assert(greater);
-  assert(lessgreater);
-  assert(lessequal || greaterequal);
-  assert(less && greater);
+  assert (!unordered);
+  assert (less);
+  assert (greater);
+  assert (lessgreater);
+  assert (lessequal || greaterequal);
+  assert (less && greater);
 
-  __attribute__((unused)) int  less1;
-  __attribute__((unused)) int  greater1;
-  __attribute__((unused)) int  lessgreater1;
+  __attribute__ ((unused)) int less1;
+  __attribute__ ((unused)) int greater1;
+  __attribute__ ((unused)) int lessgreater1;
   double lg1 = 0.1, lg2 = 0.2, lg3 = 0.3001;
-  less1 = isless(lg3, lg1+lg2);
-  greater1 = isgreater(lg3, lg1+lg2);
-  lessgreater1 = islessgreater(lg3, lg1+lg2);
+  less1 = isless (lg3, lg1 + lg2);
+  greater1 = isgreater (lg3, lg1 + lg2);
+  lessgreater1 = islessgreater (lg3, lg1 + lg2);
 
-  assert(less1 || greater1);
-  assert(lessgreater1);
-  assert((less1 || greater1) == lessgreater1);
+  assert (less1 || greater1);
+  assert (lessgreater1);
+  assert ((less1 || greater1) == lessgreater1);
 }
 
 void
-test_asm_double(void)
+test_asm_double (void)
 {
-  double                       d1  =  0.1, d2 = 0.2, d3 = 0.3;
-  __attribute__((unused)) int  x   =  (int) ((d1+d2) - d3);
+  double d1 = 0.1, d2 = 0.2, d3 = 0.3;
+  __attribute__ ((unused)) int x = (int)((d1 + d2) - d3);
 }
 
 void
-test_asm_long_double(void)
+test_asm_long_double (void)
 {
-  long double                  d1  =  0.1L, d2 = 0.2L, d3 = 0.3L;
-  __attribute__((unused)) int  x   =  (int) ((d1+d2) - d3);
+  long double d1 = 0.1L, d2 = 0.2L, d3 = 0.3L;
+  __attribute__ ((unused)) int x = (int)((d1 + d2) - d3);
 }
 
 void
-test_epsilon_double(void)
+test_epsilon_double (void)
 {
   double d1 = 0.1, d2 = 0.2, d3 = 0.3;
   double epsilon = DBL_EPSILON;
-  __attribute__((unused)) int x = fabs((d1+d2) - d3) < epsilon;
-  assert(x);
+  __attribute__ ((unused)) int x = fabs ((d1 + d2) - d3) < epsilon;
+  assert (x);
 }
 
 void
-test_double_zero(void)
+test_double_zero (void)
 {
   double d1 = 0.0, d2 = 0.0;
-  __attribute__((unused)) int  le;
-  __attribute__((unused)) int  ge;
+  __attribute__ ((unused)) int le;
+  __attribute__ ((unused)) int ge;
 
-  le = islessequal(d1, d2);
-  ge = isgreaterequal(d1, d2);
+  le = islessequal (d1, d2);
+  ge = isgreaterequal (d1, d2);
 
-  assert(FP_ZERO == fpclassify(d1));
-  assert(d1 == d2);
-  assert(le && ge);
+  assert (FP_ZERO == fpclassify (d1));
+  assert (d1 == d2);
+  assert (le && ge);
 }
 
 void
-test_double_equal(void)
+test_double_equal (void)
 {
   double d1 = 0.1, d2 = 0.2, d3 = 0.3;
-  __attribute__((unused)) int  le;
-  __attribute__((unused)) int  ge;
-  __attribute__((unused)) int  l;
-  __attribute__((unused)) int  g;
+  __attribute__ ((unused)) int le;
+  __attribute__ ((unused)) int ge;
+  __attribute__ ((unused)) int l;
+  __attribute__ ((unused)) int g;
 
-  le = islessequal(d1+d2, d3);
-  ge = isgreaterequal(d1+d2, d3);
-  l = isless(d1+d2, d3);
-  g = isgreater(d1+d2, d3);
+  le = islessequal (d1 + d2, d3);
+  ge = isgreaterequal (d1 + d2, d3);
+  l = isless (d1 + d2, d3);
+  g = isgreater (d1 + d2, d3);
 
-  assert((le || ge) && (l || g));
-  assert(g && ge);
+  assert ((le || ge) && (l || g));
+  assert (g && ge);
 }
 
 void
-test_double_classify(void)
+test_double_classify (void)
 {
-  assert(FP_ZERO == fpclassify(0.0));
-  assert(FP_ZERO == fpclassify(0.0/DBL_MAX));
-  assert(FP_NORMAL == fpclassify(1.0));
-  assert(FP_NORMAL != fpclassify(0.0));
+  assert (FP_ZERO == fpclassify (0.0));
+  assert (FP_ZERO == fpclassify (0.0 / DBL_MAX));
+  assert (FP_NORMAL == fpclassify (1.0));
+  assert (FP_NORMAL != fpclassify (0.0));
 
 #if !defined(MSVC)
-  assert(FP_INFINITE == fpclassify(1.0/0.0));
-  assert(FP_INFINITE == fpclassify(-1.0/0.0));
-  assert(FP_NAN == fpclassify((1.0L/0.0) / (1.0/0.0)));
+  assert (FP_INFINITE == fpclassify (1.0 / 0.0));
+  assert (FP_INFINITE == fpclassify (-1.0 / 0.0));
+  assert (FP_NAN == fpclassify ((1.0L / 0.0) / (1.0 / 0.0)));
 #endif
 
-  assert(isfinite(0.0));
+  assert (isfinite (0.0));
 #if !defined(MSVC)
-  assert(isinf(1.0/0.0));
-  assert(isnan((1.0/0.0)/(1.0/0.0)));
+  assert (isinf (1.0 / 0.0));
+  assert (isnan ((1.0 / 0.0) / (1.0 / 0.0)));
 #endif
-  assert(isnormal(0.1));
+  assert (isnormal (0.1));
 }
 
 void
-test_double_round(void)
+test_double_round (void)
 {
-  char decimal[][64] =
-    {
-      "3.1415926535897932384626433832795028841971693993751058209749"
-    };
+  char decimal[][64]
+      = { "3.1415926535897932384626433832795028841971693993751058209749" };
 
-  printf("round double\n------------\n");
+  printf ("round double\n------------\n");
 
-  for (size_t i = 0; i < _nof_(decimal); i++)
+  for (size_t i = 0; i < _nof_ (decimal); i++)
     {
-      printf("decimal:     %s\n", decimal[i]);
-      printf("hexadecimal: %a\n", strtod(decimal[i], NULL));
+      printf ("decimal:     %s\n", decimal[i]);
+      printf ("hexadecimal: %a\n", strtod (decimal[i], NULL));
     }
-
 }
