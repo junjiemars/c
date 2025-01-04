e1b4af40b16bbaee375be89558e79e3bef1adea9
junjie <junjie.mars@icloud.com>
2025-01-04 14:36:21 +0800

    clean

--- src/library/math/math1.c
+++ src/library/math/math1.c
@@ -1,46 +1,37 @@
 #include "math1.h"
+#include <assert.h>
 #include <geo.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 
-#if (GCC && WINNT)
-extern double trunc (double);
-extern double floor (double);
-extern double ceil (double);
-extern double round (double);
-#endif
-
 int
 main (void)
 {
   double a = area_of_rect (4., 5.);
-
-  printf ("area of rect(%f, %f) = %f\n", 4., 5., a);
+  assert (a == 20 && "aref of rect 4*5 should be 20");
 
   // trunc toward to zero
-  printf ("trunc(%f) = %f\n", 1.5, trunc (1.5));
-  printf ("trunc(%f) = %f\n", -1.5, trunc (-1.5));
+  assert (1 == trunc (1.5) && -1 == trunc (-1.5)
+          && "trunc should toward to zero");
 
   // floor toward to -infinite
-  printf ("floor(%f) = %f\n", 1.5, floor (1.5));
-  printf ("floor(%f) = %f\n", -1.5, floor (-1.5));
+  assert (1 == floor (1.5) && -2 == floor (-1.5)
+          && "floor should to -infinite");
 
   // ceil toward to +infinite
-  printf ("ceil(%f) = %f\n", 1.5, ceil (1.5));
-  printf ("ceil(%f) = %f\n", -1.5, ceil (-1.5));
+  assert (2 == ceil (1.5) && -1 == ceil (-1.5)
+          && "ceil should toward to +infinite");
 
   /*
     round: return the integral value nearest to x rounding
     half-way cases away from zero, regardless of the
     current rounding direction.
   */
-  printf ("round(%f) = %f\n", 1.4, round (1.4));
-  printf ("round(%f) = %f\n", -1.4, round (-1.4));
-  printf ("round(%f) = %f\n", 1.5, round (1.5));
-  printf ("round(%f) = %f\n", -1.5, round (-1.5));
-  printf ("round(%f) = %f\n", 2.5, round (2.5));
-  printf ("round(%f) = %f\n", -2.5, round (-2.5));
+  assert (1 == round (1.4) && -1 == round (-1.4)
+          && "nearest and rounding half-away");
+  assert (2 == round (1.5) && -2 == round (-1.5)
+          && "nearest and rounding half-away");
 
   return 0;
 }
