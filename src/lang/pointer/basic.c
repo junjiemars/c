5f3cda7aeda4313ed158db1dade7f5dd66d0bcf8
junjie <junjie.mars@icloud.com>
2025-01-04 23:30:54 +0800

    lang: compatibility on Windows

--- src/lang/pointer/basic.c
+++ src/lang/pointer/basic.c
@@ -1,18 +1,21 @@
-#include "_lang_.h"
+#include "../_lang_.h"
 
+#if (MSVC)
+#pragma warning(disable: 4048)
+#endif
 
 int
-main(void)
+main (void)
 {
-	int    i      =  1;
-	int   *pi     =  &i;
-	int  **ppi    =  &pi;
-  int    ii[]   =  {0x11, 0x22, 0x33};
-  int (*pii)[]  =  &ii;
-  int   *iip[]  =  {&i};
+  int i = 1;
+  int *pi = &i;
+  int **ppi = &pi;
+  int ii[] = { 0x11, 0x22, 0x33 };
+  int (*pii)[] = &ii;
+  int *iip[] = { &i };
 
   i += 0x10;
 
-	printf("pi@%p=%i, ppi@%p=%i, ii[1]=0x%x, iip[0]=0x%x\n",
-         pi, *pi, ppi, **ppi, (*pii)[1], *(iip[0]));
+  printf ("pi@%p=%i, ppi@%p=%i, ii[1]=0x%x, iip[0]=0x%x\n", pi, *pi, ppi,
+          **ppi, (*pii)[1], *(iip[0]));
 }
