48ceaa47312bfff21712204c91ab500e73d0da41
junjie <junjie.mars@icloud.com>
2025-01-05 22:36:53 +0800

    NSIG

--- src/signal/info/r1.c
+++ src/signal/info/r1.c
@@ -6,14 +6,13 @@
 int
 main (void)
 {
+#if (NM_SYS_SIGNAME)
   for (int i = 1; i < NSIG; i++)
     {
-      char *s = NULL;
-#if (NM_SYS_SIGNAME)
-      s = (char *)sys_signame[i];
-#endif
+      char *s = (char *)sys_signame[i];
       psignal (i, s);
     }
+#endif
 
 #if (NM_SYS_SIGLIST)
   for (int i = 1; i < NSIG; i++)
