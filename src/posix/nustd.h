b9cc92237442dbe531b64421581718ac5de0ed13
findidiot <junjie.mars@icloud.com>
2025-01-06 18:23:32 +0800

    posix,lang: fallthrough

--- src/posix/nustd.h
+++ src/posix/nustd.h
@@ -27,9 +27,27 @@
 
 
 #if (MSVC)
+#  if defined(__attribute__)
+#    if defined(unused)
+#      define __attribute____unused__  unused
+#      undef unused
+#    endif
+#    if defined(fallthrough)
+#      define __attribute____fallthrough__  fallthrough
+#      undef fallthrough
+#    endif
+#  endif
 #  include <BaseTsd.h>
 #  include <windows.h>
 #  include <process.h>
+#  if defined(__attribute____unused__)
+#    define unused  __attribute____unused__
+#    undef __attribute____unused__
+#  endif
+#  if defined(__attribute____fallthrough__)
+#    define fallthrough  __attribute____fallthrough__
+#    undef __attribute____fallthrough__
+#  endif
 #else
 #  include <sys/types.h>
 #  include <unistd.h>
