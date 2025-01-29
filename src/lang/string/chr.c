bc1f92e373a0946e3862b4c0afcef8243d1ade77
junjie <junjie.mars@icloud.com>
2025-01-30 00:44:14 +0800

    lang: string, chr

--- src/lang/string/chr.c
+++ src/lang/string/chr.c
@@ -1,6 +1,4 @@
 #include "../_lang_.h"
-#include <stdio.h>
-#include <string.h>
 
 typedef char *(*strchr_fn) (const char *s, int);
 typedef char *(*strrchr_fn) (const char *s, int);
@@ -10,61 +8,15 @@ static char *self_strrchr (const char *s, int);
 
 static void test_strchr (strchr_fn, char *s, int);
 
-void
-test_strchr (strchr_fn fn, char *ss, int c)
-{
-  char *s1;
-
-  s1 = fn (ss, c);
-  if (s1)
-    {
-      fprintf (stdout, "%s\n", s1);
-    }
-  else
-    {
-      fprintf (stdout, "%c: no found\n", c);
-    }
-}
-
-char *
-self_strchr (const char *s, int c)
-{
-  while (*s)
-    {
-      if (c == *s)
-        {
-          return (char *)s;
-        }
-      s++;
-    }
-  return 0;
-}
-
-char *
-self_strrchr (const char *s, int c)
-{
-  const char *pre = 0;
-
-  while (*s)
-    {
-      if (c == *s)
-        {
-          pre = s;
-        }
-      s++;
-    }
-  return (char *)pre;
-}
-
 int
 main (int argc, char **argv)
 {
   char *ss;
   int c;
 
-  if (argc < 2)
+  if (argc < 3)
     {
-      printf ("what string and found what\n");
+      fprintf (stderr, "usage: <src> <find>\n");
       return 1;
     }
 
@@ -78,4 +30,57 @@ main (int argc, char **argv)
   test_strchr (self_strrchr, ss, c);
 
   return 0;
+}
+
+void
+test_strchr (strchr_fn fn, char *s, int c)
+{
+  char *s1;
+
+  s1 = fn (s, c);
+  if (s1)
+    {
+      fprintf (stdout, "%s\n", s1);
+    }
+  else
+    {
+      fprintf (stdout, "%c: no found\n", c);
+    }
+}
+
+char *
+self_strchr (const char *s, int c)
+{
+  if (!s)
+    {
+      return NULL;
+    }
+
+  do
+    {
+      if (*s == c)
+        return (char *)s;
+    }
+  while (*s++);
+  return NULL;
+}
+
+char *
+self_strrchr (const char *s, int c)
+{
+  char const *pre = NULL;
+  if (!s)
+    {
+      return NULL;
+    }
+
+  do
+    {
+      if (*s == c)
+        {
+          pre = s;
+        }
+    }
+  while (*s++);
+  return (char *)pre;
 }
