703a9dfe15cba8db47fe22a61b55970c7776ed10
findidiot <junjie.mars@icloud.com>
2025-01-30 20:52:15 +0800

    lang: string, strpbrk

--- src/lang/string/pbrk.c
+++ src/lang/string/pbrk.c
@@ -1,25 +1,18 @@
 #include "../_lang_.h"
-#include <stdio.h>
-#include <string.h>
 
-typedef char *(*strpbrk_fn) (const char *ss, const char *d);
+char *self_strpbrk (const char *, const char *);
 
-char *self_strpbrk (const char *ss, const char *d);
+typedef char *(*strpbrk_fn) (const char *, const char *);
 
-static void test_strpbrk (strpbrk_fn, const char *ss, const char *d);
+static void test_strpbrk (strpbrk_fn);
 
-void
-test_strpbrk (strpbrk_fn fn, const char *ss, const char *d)
+int
+main (void)
 {
-  char *s1 = fn (ss, d);
-  if (s1)
-    {
-      fprintf (stdout, "|%s|\n", s1);
-    }
-  else
-    {
-      fprintf (stdout, "%s: no found\n", d);
-    }
+  test_strpbrk (self_strpbrk);
+  test_strpbrk (strpbrk);
+
+  return 0;
 }
 
 char *
@@ -39,20 +32,10 @@ self_strpbrk (const char *s, const char *d)
   return 0;
 }
 
-int
-main (__attribute__ ((unused)) int argc, char **argv)
+void
+test_strpbrk (strpbrk_fn fn)
 {
-
-  if (argc < 3)
-    {
-      printf ("where the source string and delim?\n");
-      return 0;
-    }
-
-  const char *ss = argv[1];
-  const char *d = argv[2];
-  test_strpbrk (strpbrk, ss, d);
-  test_strpbrk (self_strpbrk, ss, d);
-
-  return 0;
+	char *p;
+	p = fn ("a", "abc");
+	assert (*p == 'a');
 }
