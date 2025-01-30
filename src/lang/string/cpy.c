6bc7af8dc797ae94d298b886edb4ef35ea8aa581
findidiot <junjie.mars@icloud.com>
2025-01-30 20:21:00 +0800

    lang: string, strcpy

--- src/lang/string/cpy.c
+++ src/lang/string/cpy.c
@@ -1,24 +1,25 @@
 #include "../_lang_.h"
 
 char *self_strcpy (char *, char const *);
-char *self_strpcpy (char *, char const *);
+char *self_stpcpy (char *, char const *);
 char *self_strncpy (char *, char const *, size_t);
 
 typedef char *(*strcpy_fn) (char *, char const *);
-typedef char *(*strpcpy_fn) (char *, char const *);
+typedef char *(*stpcpy_fn) (char *, char const *);
 typedef char *(*strncpy_fn) (char *, char const *, size_t);
 
-static void test_strcpy (char const *);
-static void test_strpcpy (char const *);
-static void test_strncpy (char const *);
+static void test_strcpy (strcpy_fn);
+static void test_stpcpy (stpcpy_fn);
+static void test_strncpy (strncpy_fn);
 
 int
 main (void)
 {
-  test_strcpy ("");
-  test_strcpy ("abc");
-	test_strpcpy ("");
-  test_strncpy ("abcdefgh");
+  test_strcpy (self_strcpy);
+  test_strcpy (strcpy);
+	test_stpcpy (self_stpcpy);
+	test_stpcpy (stpcpy);
+  test_strncpy (self_strncpy);
   return 0;
 }
 
@@ -34,14 +35,14 @@ self_strcpy (char *dst, char const *src)
 }
 
 char *
-self_strpcpy (char *dst, char const *src)
+self_stpcpy (char *dst, char const *src)
 {
   char *d = dst;
   while ((*d++ = *src++) != '\0')
     {
       continue;
     }
-  return d;
+  return d - 1;
 }
 
 char *
@@ -67,62 +68,41 @@ self_strncpy (char *dst, char const *src, size_t len)
 }
 
 void
-test_strcpy (char const *src)
+test_strcpy (strcpy_fn fn)
 {
-  strcpy_fn c_fn = strcpy, self_fn = self_strcpy;
-  char *dst1, *dst2;
-  size_t len = strlen (src);
-  dst1 = malloc (len + 1);
-  assert (dst1 != NULL);
-  dst2 = malloc (len + 1);
-  assert (dst2 != NULL);
-  dst1 = c_fn (dst1, src);
-  dst2 = self_fn (dst2, src);
-  assert (memcmp (dst1, dst2, len) == 0);
-  printf ("strcpy: %s\n", dst2);
+	char *dst;
+	dst = malloc (16);
+	assert (dst != NULL);
+	dst = fn (dst, "");
+	assert (*dst == '\0');
+	dst = fn (dst, "abc");
+	assert (memcmp (dst, "abc", 3) == 0);
+	free (dst);
 }
 
 void
-test_strpcpy (char const *src)
+test_stpcpy (stpcpy_fn fn)
 {
-  strcpy_fn c_fn = strcpy, self_fn = self_strcpy;
-  char *dst1, *dst2;
-  size_t len = strlen (src);
-  dst1 = malloc (len + 1);
-  assert (dst1 != NULL);
-  dst2 = malloc (len + 1);
-  assert (dst2 != NULL);
-  dst1 = c_fn (dst1, src);
-  dst2 = self_fn (dst2, src);
-	assert (*dst1 == '\0' && *dst1 == *dst2);
-  assert (memcmp (dst1, dst2, len) == 0);
-  printf ("strpcpy: %s\n", dst2);
+	char *dst, *p;
+	dst = malloc (16);
+	assert (dst != NULL);
+	p = fn (dst, "");
+	assert (*p == '\0' && *dst == '\0');
+	free (dst);
 }
 
 void
-test_strncpy (char const *src)
+test_strncpy (strncpy_fn fn)
 {
-  strncpy_fn c_fn = strncpy, self_fn = self_strncpy;
-  char *dst1, *dst2;
-  size_t len = strlen (src);
-
-  dst1 = malloc (len * 2);
-  assert (dst1 != NULL);
-  dst2 = malloc (len * 2);
-  assert (dst2 != NULL);
-
-	dst1 = c_fn (dst1, src, len);
-  dst2 = self_fn (dst2, src, len);
-  assert (memcmp (dst1, dst2, len) == 0);
-
-  dst1 = c_fn (dst1, src, len + 1);
-  dst2 = self_fn (dst2, src, len + 1);
-  assert (memcmp (dst1, dst2, len + 1) == 0);
-  printf ("strncpy: %s\n", dst2);
-
-  dst1 = c_fn (dst1, src, len + 2);
-  dst2 = self_fn (dst2, src, len + 2);
-  assert (memcmp (dst1, dst2, len + 2) == 0);
-  assert (*(dst1 + len + 1) == '\0' && *(dst1 + len + 1) == *(dst2 + len + 1));
-  printf ("strncpy: %s\n", dst2);
+	char *dst;
+	size_t n = 16;
+	dst = malloc (n);
+	assert (dst != NULL);
+	dst = fn (dst, "", 1);
+	assert (*dst == '\0');
+	dst = fn (dst, "abc", 8);
+	assert (memcmp (dst, "abc", 4) == 0);
+	dst = fn (dst, "abc", 2);
+	assert (memcmp (dst, "abc", 3) == 0);
+	free (dst);
 }
