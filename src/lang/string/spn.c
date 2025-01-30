6e53fd8ea426652a1ac5b59e374b6de922ebaa7d
findidiot <junjie.mars@icloud.com>
2025-01-30 22:08:41 +0800

    lang: string, strspn

--- src/lang/string/spn.c
+++ src/lang/string/spn.c
@@ -1,13 +1,19 @@
 #include "../_lang_.h"
-#include <stdio.h>
-#include <string.h>
-
-typedef size_t (*strspn_fn) (const char *s, const char *d);
 
 size_t self_strspn (const char *s, const char *d);
 size_t self_strcspn (const char *s, const char *d);
 
-static void test_strspn (strspn_fn fn, const char *s, const char *d);
+typedef size_t (*strspn_fn) (const char *, const char *);
+
+static void test_strspn (strspn_fn);
+
+int
+main (void)
+{
+	test_strspn (self_strspn);
+	test_strspn (strspn);
+  return 0;
+}
 
 size_t
 self_strspn (const char *s, const char *d)
@@ -50,28 +56,14 @@ self_strcspn (const char *s, const char *d)
 }
 
 void
-test_strspn (strspn_fn fn, const char *s, const char *d)
+test_strspn (strspn_fn fn)
 {
-  size_t s1 = fn (s, d);
-  fprintf (stdout, "%zu\n", s1);
-}
-
-int
-main (int argc, char **argv)
-{
-  if (argc < 3)
-    {
-      printf ("where the source string and spn?\n");
-      return 0;
-    }
-
-  const char *ss = argv[1];
-  const char *d = argv[2];
-  test_strspn (strspn, ss, d);
-  test_strspn (strcspn, ss, d);
-
-  test_strspn (self_strspn, ss, d);
-  test_strspn (self_strcspn, ss, d);
-
-  return 0;
+	size_t p;
+	char *cs = "the quick brown fox jumps over the lazy dog";
+	p = fn (",,,", cs);
+	assert (p == 0);
+	p = fn ("fox", cs);
+	assert (p == 3);
+	p = fn ("quick", cs);
+	assert (p == 5);
 }
