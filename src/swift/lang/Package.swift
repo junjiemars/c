53f85e294c4cf58a2de6052dd8157607ec65e9a0
junjie <junjie.mars@icloud.com>
2025-06-03 16:34:04 +0800

    clean

--- src/swift/lang/Package.swift
+++ src/swift/lang/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version:5.8
 //
 // The swift-tools-version declares the minimum version of Swift
 // required to build this package.
@@ -6,14 +6,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "lang",
+    name: "Lang",
     targets: [
       // Targets are the basic building blocks of a package,
       // defining a module or a test suite.  Targets can depend on
       // other targets in this package and products from
       // dependencies.
         .executableTarget(
-            name: "lang",
+            name: "Lang",
             path: "Sources"),
     ]
 )
